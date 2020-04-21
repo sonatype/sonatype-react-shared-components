c08c0a4918d189c0923cfb90225605343a7b0475
jlopez <jlopez@sonatype.com>
2020-04-21 09:34:33 -0500

    Better import strategy - CLM-14538

--- lib/src/components/NxModal/types.ts
+++ lib/src/components/NxModal/types.ts
@@ -4,12 +4,12 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, {WeakValidationMap} from 'react';
+import {HTMLAttributes, WeakValidationMap} from 'react';
 import * as PropTypes from 'prop-types';
 
 export type CloseHandler = () => void;
 
-export type Props = React.HTMLAttributes<HTMLDivElement> & {
+export type Props = HTMLAttributes<HTMLDivElement> & {
   onClose: CloseHandler;
 };
 
