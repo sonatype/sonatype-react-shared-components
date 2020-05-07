9180803b56f98b96abf94b78a0020fa0d4dd7231
Dan Miller <danmiller@sonatype.com>
2020-05-07 15:31:51 -0400

    CLM-15583 - Change terminology

--- gallery/src/styles/NxList/NxListPage.tsx
+++ gallery/src/styles/NxList/NxListPage.tsx
@@ -76,9 +76,11 @@ const NxListPage = () =>
             <td className="nx-cell">Use this when you want to have a button on the far right.</td>
           </tr>
           <tr className="nx-table-row">
-            <td className="nx-cell"><code className="nx-code">.nx-list__item .selected</code></td>
-            <td className="nx-cell">Modifier of <code className="nx-code">.nx-list__item</code></td>
-            <td className="nx-cell">Use the selected class when a clickable list item is selected</td>
+            <td className="nx-cell"><code className="nx-code">.selected</code></td>
+            <td className="nx-cell">Utility class that goes along
+              with <code className="nx-code">.nx-list__item</code></td>
+            <td className="nx-cell">Use the <code className="nx-code">selected</code> class
+              alongside <code className="nx-code">.nx-list__item</code> when a clickable list item is selected</td>
           </tr>
           <tr className="nx-table-row">
             <td className="nx-cell"><code className="nx-code">.nx-list__subtext</code></td>
