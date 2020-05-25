a34bee86d8d8af3bf2293e8027189e2829f22699
Glen Hunter <ghunter@sonatype.com>
2020-05-25 13:16:26 -0400

    Classed unclassed h3 and p tags - RSC-97

--- gallery/src/pages/Home.tsx
+++ gallery/src/pages/Home.tsx
@@ -28,7 +28,7 @@ const pageConfig: PageConfig = {
 const Home = () =>
   <>
     <GalleryTile title="How to add a new component to the Gallery" >
-      <h3>Create a page describing the component</h3>
+      <h3 className="nx-h3">Create a page describing the component</h3>
       <ul className="nx-list nx-list--bulleted">
         <li className="nx-list__item">
           For each component or style that you want to add to the Gallery, create a subdirectory
@@ -59,8 +59,8 @@ const Home = () =>
           component example.
         </li>
       </ul>
-      <h3>Add the new page to Gallery navigation</h3>
-      <p>
+      <h3 className="nx-h3">Add the new page to Gallery navigation</h3>
+      <p className="nx-p">
         After you've created the description page, in order to add the component to the Gallery Navigation, you
         will need to add it to the <code className="nx-code">pageConfig</code> object inside of
         the <code className="nx-code">pageConfig.ts</code> file
