412d83898cc60683d407a80923765ff2ca072412
Glen Hunter <ghunter@sonatype.com>
2020-06-04 16:20:53 -0400

    Restored example RSC-126

--- gallery/src/styles/NxTile/NxTileWithActionsExample.tsx
+++ gallery/src/styles/NxTile/NxTileWithActionsExample.tsx
@@ -7,32 +7,6 @@
 import React from 'react';
 
 const NxTileWithActionsExample = () =>
-  <>
-  <div className="nx-tile">
-    <div className="nx-tile-header">
-      <div className="nx-tile-header__title">
-        <h2 className="nx-h2">NX Tile with Actions - and this line is meant to wrap so that we can test truncation</h2>
-      </div>
-      <div className="nx-tile-header__subtitle">The Subtitle</div>
-    </div>
-    <div className="nx-tile-content">
-      Vehicle motion artisanal corrupted warehouse marketing jeans physical drone neon
-    </div>
-  </div>
-  <div className="nx-tile">
-    <div className="nx-tile-header">
-      <div className="nx-tile-header__title">
-        <h2 className="nx-h2">NX Tile with Actions - and this line is meant to wrap so that we can test truncation</h2>
-      </div>
-      <div className="nx-tile-header__subtitle">The Subtitle</div>
-      <div className="nx-tile__actions">
-        <button className="nx-btn nx-btn--tertiary">Action 1</button>
-      </div>
-    </div>
-    <div className="nx-tile-content">
-      Vehicle motion artisanal corrupted warehouse marketing jeans physical drone neon
-    </div>
-  </div>
   <div className="nx-tile">
     <div className="nx-tile-header">
       <div className="nx-tile-header__title">
@@ -48,39 +22,6 @@ const NxTileWithActionsExample = () =>
       Vehicle motion artisanal corrupted warehouse marketing jeans physical drone neon
     </div>
   </div>
-  <div className="nx-tile">
-    <div className="nx-tile-header">
-      <div className="nx-tile-header__title">
-        <h2 className="nx-h2">NX Tile with Actions - and this line is meant to wrap so that we can test truncation</h2>
-      </div>
-      <div className="nx-tile-header__subtitle">The Subtitle</div>
-      <div className="nx-tile__actions">
-        <button className="nx-btn nx-btn--tertiary">Action 1</button>
-        <button className="nx-btn nx-btn--tertiary">Action 2</button>
-        <button className="nx-btn nx-btn--tertiary">Action 3</button>
-      </div>
-    </div>
-    <div className="nx-tile-content">
-      Vehicle motion artisanal corrupted warehouse marketing jeans physical drone neon
-    </div>
-  </div>
-  <div className="nx-tile">
-    <div className="nx-tile-header">
-      <div className="nx-tile-header__title">
-        <h2 className="nx-h2">NX Tile with Actions - and this line is meant to wrap so that we can test truncation</h2>
-      </div>
-      <div className="nx-tile-header__subtitle">The Subtitle</div>
-      <div className="nx-tile__actions">
-        <button className="nx-btn nx-btn--tertiary">Action 1</button>
-        <button className="nx-btn nx-btn--tertiary">Action 2</button>
-        <button className="nx-btn nx-btn--tertiary">Action 3</button>
-        <button className="nx-btn nx-btn--tertiary">Action 4</button>
-      </div>
-    </div>
-    <div className="nx-tile-content">
-      Vehicle motion artisanal corrupted warehouse marketing jeans physical drone neon
-    </div>
-  </div>
-</>;
+;
 
 export default NxTileWithActionsExample;
