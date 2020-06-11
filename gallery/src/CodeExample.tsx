7865b104c3fea7dea07e217127d6cb012f6a283d
Ross Pokorny <rpokorny@sonatype.com>
2020-06-11 14:20:05 -0400

    Use section instead of fragment - RSC-125

--- gallery/src/CodeExample.tsx
+++ gallery/src/CodeExample.tsx
@@ -32,7 +32,7 @@ const CodeExample: FunctionComponent<Props> =
         headerLanguagePart = language ? ` (${language.toUpperCase()})` : '';
 
     return (
-      <>
+      <section>
         <h3 className="nx-h3 nx-tile__section-header">
           Code Snippet
           {headerLanguagePart}
@@ -41,7 +41,7 @@ const CodeExample: FunctionComponent<Props> =
         <SyntaxHighlighter language={language || 'tsx'} style={atomDark}>
           {licenseStrippedContent}
         </SyntaxHighlighter>
-      </>
+      </section>
     );
   };
 
