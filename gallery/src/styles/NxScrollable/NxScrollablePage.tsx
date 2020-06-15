d4a2953fb659d5a6010fb9fc1ae232bc261e73a5
Glen Hunter <glenhunter@users.noreply.github.com>
2020-06-15 10:47:55 -0400

    Update gallery/src/styles/NxScrollable/NxScrollablePage.tsx
    Co-authored-by: Michael Martz <mmartz@sonatype.com>

--- gallery/src/styles/NxScrollable/NxScrollablePage.tsx
+++ gallery/src/styles/NxScrollable/NxScrollablePage.tsx
@@ -24,7 +24,7 @@ const NxScrollablePage = () =>
         Generally <code className="nx-code">.nx-scrollable</code> is applied to blocks like
         <code className="nx-code">.nx-tile-content</code> or <code className="nx-code">.nx-modal-content</code>.
         <code className="nx-code">.nx-scrollable</code> should <strong>not</strong> be used to replicate page or
-        browser scrolling, instead refer to the <a href="#Pages/Page Layout/">page layout templates</a>.
+        browser scrolling, but instead it should refer to the <a href="#Pages/Page Layout/">page layout templates</a>.
       </p>
     </GalleryDescriptionTile>
     <GalleryExampleTile title="General Example"
