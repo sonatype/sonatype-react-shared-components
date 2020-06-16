aaa9d9a1dc0bf96e5adde864af98e4a896aea4ba
Ross Pokorny <rpokorny@sonatype.com>
2020-06-16 14:25:06 -0400

    Add license headers - RSC-158

--- gallery/wdio.conf.js
+++ gallery/wdio.conf.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfigFn = require('./webpack.config.js');
