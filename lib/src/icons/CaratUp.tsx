01f6164604ec672486d935eb13c9eb4c3e460364
Glen Hunter <ghunter@sonatype.com>
2020-06-16 15:49:14 -0400

    Remove fill-rule RSC-123

--- lib/src/icons/CaratUp.tsx
+++ lib/src/icons/CaratUp.tsx
@@ -11,7 +11,6 @@ import './Carat_Up.scss';
 const CaratUp = () =>
   <svg viewBox="0 0 12 8" className="nx-icon nx-icon--carat-up">
     <path fill="currentColor"
-          fill-rule="nonzero"
           transform="translate(6.000000, 4.000000) scale(1, -1) translate(-6.000000, -4.000000)"
           d="M1.00928531,0 L10.9907147,0 C11.8882046,0 12.3369495,1.2403472 11.7036739,1.96348399
               L6.71295924,7.66237653 C6.31873472,8.11254116 5.68126528,8.11254116 5.29123464,7.66237653
