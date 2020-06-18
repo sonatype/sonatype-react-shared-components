b788ce60a07972b8cde78831883a9b95b948f263
Glen Hunter <ghunter@sonatype.com>
2020-06-18 09:50:13 -0400

    Mac Hack Correctly renamed files RSC-109

---
+++ lib/src/icons/ValidationCheck.tsx
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import React from 'react';
+
+import './ValidationCheck.scss';
+
+const ValidationCheck = () =>
+  <svg viewBox="0 0 16 16" className="nx-icon nx-icon--validation-check">
+    <path fill="currentColor"
+          d="M13.7142784,4 L16,6.25 L6.09520643,16 L3.80948484,13.75 L3.81,13.749 L-5.32907052e-14,9.99999135
+             L2.28572159,7.74999135 L6.095,11.5 L13.7142784,4 Z">
+    </path>
+  </svg>;
+
+export default ValidationCheck;
