5a22d4b63375517fe864e81b8990b6276e7b6eda
Ross Pokorny <rpokorny@sonatype.com>
2020-06-23 10:44:23 -0400

    Initial applitools integration - RSC-167

--- gallery/visualtests/homePage.js
+++ gallery/visualtests/homePage.js
@@ -4,9 +4,36 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+const { By, ClassicRunner, Configuration, Eyes, Target } = require('@applitools/eyes-webdriverio');
+
 describe('Home Page', function() {
-  it('looks right', function() {
-    browser.url('#/pages/NxAlert');
-    browser.saveScreenshot('/tmp/screenshot.png');
+  let eyes;
+
+  beforeEach(async function() {
+    eyes = new Eyes(new ClassicRunner());
+
+    const eyesConf = new Configuration();
+    eyesConf.setAppName('React Shared Components');
+    eyesConf.setTestName('Home Page');
+
+    eyes.setConfiguration(eyesConf);
+
+    await eyes.open(browser);
+  });
+
+  afterEach(async function() {
+    await eyes.closeAsync();
+    await eyes.abortIfNotClosed();
+
+    const results = await eyes.getRunner().getAllTestResults(false);
+
+    console.log('Applitools Results', results, results.getAllResults());
+  });
+
+  it('looks right', async function() {
+
+    await browser.url('#/pages/NxAlert');
+    await eyes.check('Home Page', Target.window());
+    await eyes.check('Page Header', Target.region(By.className('nx-page-header')));
   });
 });
