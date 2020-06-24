4431ea107cd4bbd18f401ffe508e136ceeeb3357
Glen Hunter <ghunter@sonatype.com>
2020-06-24 17:19:15 -0400

    Semantic tiles w subsections RSC-155

--- gallery/src/styles/NxTile/NxTileSubsectionExample.tsx
+++ gallery/src/styles/NxTile/NxTileSubsectionExample.tsx
@@ -7,38 +7,46 @@
 import React from 'react';
 
 const NxTileSubsectionExample = () =>
-  <div className="nx-tile">
-    <div className="nx-tile-header">
+  <section className="nx-tile">
+    <header className="nx-tile-header">
       <div className="nx-tile-header__title"><h2 className="nx-h2">NX Simple Tile</h2></div>
-    </div>
+    </header>
     <div className="nx-tile-content">
       <p className="nx-p">
         Numinous vinyl table meta- augmented reality voodoo god meta- order-flow nano- assassin table order-flow.
         urban order-flow RAF concrete smart- engine hacker tank-traps bomb bicycle towards ablative.
         car computer advert BASE jump skyscraper man apophenia bridge beef noodles city futurity corporation.
       </p>
-      <h3 className="nx-h3 nx-tile__section-header">Subsection</h3>
-      <p className="nx-p">
-        semiotics numinous long-chain hydrocarbons. Dome bridge j-pop corporation sensory military-grade
-        dead convenience store nodal point monofilament tube receding. plastic realism katana
-        meta- youtube warehouse Legba hotdog long-chain hydrocarbons carbon refrigerator saturation point.
-      </p>
-      <h3 className="nx-h3 nx-tile__section-header">Subsection</h3>
-      <p className="nx-p">
-        semiotics numinous long-chain hydrocarbons. Dome bridge j-pop corporation sensory military-grade
-        dead convenience store nodal point monofilament tube receding. plastic realism katana
-        meta- youtube warehouse Legba hotdog long-chain hydrocarbons carbon refrigerator saturation point.
-      </p>
-      <h3 className="nx-h3 nx-tile__section-header">Subsection</h3>
-      <p className="nx-p">
-        semiotics numinous long-chain hydrocarbons. Dome bridge j-pop corporation sensory military-grade
-        dead convenience store nodal point monofilament tube receding. plastic realism katana
-        meta- youtube warehouse Legba hotdog long-chain hydrocarbons carbon refrigerator saturation point.
-      </p>
+      <section className="nx-tile-subsection">
+        <header>
+          <h3 className="nx-h3 nx-h3--tile-subsection">Subsection</h3>
+        </header>
+        <p className="nx-p">
+          semiotics numinous long-chain hydrocarbons. Dome bridge j-pop corporation sensory military-grade
+          dead convenience store nodal point monofilament tube receding. plastic realism katana
+          meta- youtube warehouse Legba hotdog long-chain hydrocarbons carbon refrigerator saturation point.
+        </p>
+        <header>
+          <h3 className="nx-h3 nx-h3--tile-subsection">Subsection</h3>
+        </header>
+        <p className="nx-p">
+          semiotics numinous long-chain hydrocarbons. Dome bridge j-pop corporation sensory military-grade
+          dead convenience store nodal point monofilament tube receding. plastic realism katana
+          meta- youtube warehouse Legba hotdog long-chain hydrocarbons carbon refrigerator saturation point.
+        </p>
+        <header>
+          <h3 className="nx-h3 nx-h3--tile-subsection">Subsection</h3>
+        </header>
+        <p className="nx-p">
+          semiotics numinous long-chain hydrocarbons. Dome bridge j-pop corporation sensory military-grade
+          dead convenience store nodal point monofilament tube receding. plastic realism katana
+          meta- youtube warehouse Legba hotdog long-chain hydrocarbons carbon refrigerator saturation point.
+        </p>
+      </section>
     </div>
-    <div className="nx-tile-footer">
+    <footer className="nx-tile-footer">
       <button className="nx-btn nx-btn--primary">Footer Button</button>
-    </div>
-  </div>;
+    </footer>
+  </section>;
 
 export default NxTileSubsectionExample;
