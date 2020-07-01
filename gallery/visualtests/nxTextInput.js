789c0cf0295312378dead361c6ed6085ab89f3e6
Ross Pokorny <rpokorny@sonatype.com>
2020-07-01 12:41:16 -0400

    Remaining NxTextInput tests - RSC-167

--- gallery/visualtests/nxTextInput.js
+++ gallery/visualtests/nxTextInput.js
@@ -9,14 +9,16 @@ const { focusTest, hoverTest, simpleTest } = require('./testUtils');
 
 describe('NxTextInput', function() {
   const simpleComponentSelector = '#nx-text-input-simple-example .nx-text-input',
-      validatableComponentSelector = '#nx-text-input-simple-example .nx-text-input',
-      passwordComponentSelector = '#nx-text-input-simple-example .nx-text-input',
-      textareaComponentSelector = '#nx-text-input-simple-example .nx-text-input',
-      validatableTextareaComponentSelector = '#nx-text-input-simple-example .nx-text-input',
-      disabledComponentSelector = '#nx-text-input-simple-example .nx-text-input';
+      validatableComponentSelector = '#nx-text-input-validation-example .nx-text-input',
+      passwordComponentSelector = '#nx-text-input-password-example .nx-text-input',
+      textareaComponentSelector = '#nx-text-input-textarea-validation-example .nx-text-input',
+      validatableTextareaComponentSelector = '#nx-text-input-textarea-validation-example .nx-text-input',
+      disabledComponentSelector = '#nx-text-input-disabled-example .nx-text-input.pristine',
+      disabledValidComponentSelector = '#nx-text-input-disabled-example .nx-text-input.valid',
+      disabledInvalidComponentSelector = '#nx-text-input-disabled-example .nx-text-input.invalid';
 
-  function getInputElementSelector(componentSelector) {
-    return componentSelector + ' input';
+  function getInputElementSelector(componentSelector, inputType = 'input') {
+    return `${componentSelector} ${inputType}`;
   }
 
   beforeEach(async function() {
@@ -25,6 +27,7 @@ describe('NxTextInput', function() {
 
   describe('Simple NxTextInput', function() {
     it('has a light border when pristine', simpleTest(simpleComponentSelector));
+
     it('has a dark border when hovered',
         hoverTest(simpleComponentSelector, getInputElementSelector(simpleComponentSelector)));
 
@@ -52,5 +55,85 @@ describe('NxTextInput', function() {
 
       await simpleTest(simpleComponentSelector)();
     });
+  });
+
+  describe('Validatable NxTextInput', function() {
+    it('has a light border when pristine', simpleTest(validatableComponentSelector));
+
+    it('has validation styles when valid', async function() {
+      const inputSelector = getInputElementSelector(validatableComponentSelector),
+          [targetElement, inputElement] = await Promise.all([
+            browser.$(validatableComponentSelector),
+            browser.$(inputSelector)
+          ]);
+
+      await targetElement.scrollIntoView({ block: 'center' });
+      await inputElement.setValue('foo');
+      await browser.eyesRegionSnapshot(null, Target.region(targetElement));
+    });
+
+    it('has invalid validation styles when invalid and dirty', async function() {
+      const inputSelector = getInputElementSelector(validatableComponentSelector),
+          [targetElement, inputElement] = await Promise.all([
+            browser.$(validatableComponentSelector),
+            browser.$(inputSelector)
+          ]);
+
+      await targetElement.scrollIntoView({ block: 'center' });
+      await inputElement.setValue('foo');
+      await browser.keys(['Backspace', 'Backspace', 'Backspace']);
+      await browser.eyesRegionSnapshot(null, Target.region(targetElement));
+    });
+  });
+
+  describe('Password NxTextInput', function() {
+    it('displays password dots', async function() {
+      const inputSelector = getInputElementSelector(passwordComponentSelector),
+          [targetElement, inputElement] = await Promise.all([
+            browser.$(passwordComponentSelector),
+            browser.$(inputSelector)
+          ]);
+
+      await targetElement.scrollIntoView({ block: 'center' });
+      await inputElement.setValue('foo');
+      await browser.eyesRegionSnapshot(null, Target.region(targetElement));
+    });
+  });
+
+  describe('Textarea NxTextInput', function() {
+    it('looks right', simpleTest(textareaComponentSelector));
+  });
+
+  describe('Validatable Textarea NxTextInput', function() {
+    it('has validation styles when valid', async function() {
+      const inputSelector = getInputElementSelector(validatableTextareaComponentSelector, 'textarea'),
+          [targetElement, inputElement] = await Promise.all([
+            browser.$(validatableTextareaComponentSelector),
+            browser.$(inputSelector)
+          ]);
+
+      await targetElement.scrollIntoView({ block: 'center' });
+      await inputElement.setValue('foo');
+      await browser.eyesRegionSnapshot(null, Target.region(targetElement));
+    });
+
+    it('has invalid validation styles when invalid and dirty', async function() {
+      const inputSelector = getInputElementSelector(validatableTextareaComponentSelector, 'textarea'),
+          [targetElement, inputElement] = await Promise.all([
+            browser.$(validatableTextareaComponentSelector),
+            browser.$(inputSelector)
+          ]);
+
+      await targetElement.scrollIntoView({ block: 'center' });
+      await inputElement.setValue('foo');
+      await browser.keys(['Backspace', 'Backspace', 'Backspace']);
+      await browser.eyesRegionSnapshot(null, Target.region(targetElement));
+    });
+  });
+
+  describe('Disabled NxTextInput', function() {
+    it('looks disabled when pristine', simpleTest(disabledComponentSelector));
+    it('looks disabled when valid', simpleTest(disabledValidComponentSelector));
+    it('looks disabled when invalid', simpleTest(disabledInvalidComponentSelector));
   });
 });
