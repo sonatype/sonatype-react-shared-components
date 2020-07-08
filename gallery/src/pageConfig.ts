cc2a047ecabe8b082a5b4ed59cc419b89293c549
Glen Hunter <ghunter@sonatype.com>
2020-07-08 16:41:58 -0400

    Restored page link RSC-162

--- gallery/src/pageConfig.ts
+++ gallery/src/pageConfig.ts
@@ -32,7 +32,7 @@ import NxIconPage from './styles/NxIcon/NxIconPage';
 import NxFontAwesomeIconPage from './components/NxFontAwesomeIcon/NxFontAwesomeIconPage';
 import NxCounterPage from './styles/NxCounter/NxCounterPage';
 import NxThreatNumberPage from './styles/NxThreatNumber/NxThreatNumberPage';
-//import NxFormLayoutPage from './styles/NxFormLayout/NxFormLayoutPage';
+import NxFormLayoutPage from './styles/NxFormLayout/NxFormLayoutPage';
 import NxSubmitMaskPage from './components/NxSubmitMask/NxSubmitMaskPage';
 import NxStatefulSubmitMaskPage from './components/NxStatefulSubmitMask/NxStatefulSubmitMaskPage';
 import NxTablePage from './components/NxTable/NxTablePage';
@@ -127,7 +127,7 @@ const pageConfig: PageConfig = {
     'nx-truncate-ellipsis': NxTruncatePage
   },
   'Layout Examples': {
-    //'Form Layout Styles': NxFormLayoutPage,
+    'Form Layout Styles': NxFormLayoutPage,
     'Page Layout': PageLayoutPage
   },
   'JavaScript & TypeScript Utilities': {
