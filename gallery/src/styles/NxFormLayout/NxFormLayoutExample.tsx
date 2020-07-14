9be06e02d0e400705789ade006e3286627ae2101
Glen Hunter <ghunter@sonatype.com>
2020-07-14 14:19:11 -0400

    Fixed vert examples RSC-162

--- gallery/src/styles/NxFormLayout/NxFormLayoutExample.tsx
+++ gallery/src/styles/NxFormLayout/NxFormLayoutExample.tsx
@@ -4,24 +4,16 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { useState } from 'react';
+import React from 'react';
 
 import { NxCheckbox } from '@sonatype/react-shared-components';
 import { NxRadio } from '@sonatype/react-shared-components';
-import { NxTextInput } from '@sonatype/react-shared-components';
+import { NxStatefulTextInput } from '@sonatype/react-shared-components';
 import { NxButton } from '@sonatype/react-shared-components';
 
-import { initialState, userInput } from '@sonatype/react-shared-components/components/NxTextInput/stateHelpers';
-
 export default function NxFormLayoutExample() {
-  const [state, setState] = useState(initialState(''));
-
-  function onChange(val: string) {
-    setState(userInput(null, val));
-  }
-
-  function onKeyPress(key: string) {
-    console.log('Pressed key:', key); // eslint-disable-line
+  function validator(val: string) {
+    return val.length ? null : 'Must be non-empty';
   }
 
   return (
@@ -29,38 +21,38 @@ export default function NxFormLayoutExample() {
       <div className="nx-form-group">
         <label className="nx-label">
           <span className="nx-label__text">Label</span>
-          <NxTextInput  { ...state } onChange={onChange} onKeyPress={onKeyPress}/>
+          <NxStatefulTextInput/>
         </label>
       </div>
       <div className="nx-form-group">
         <label className="nx-label nx-label--optional">
           <span className="nx-label__text">Label</span>
-          <NxTextInput  { ...state } onChange={onChange} onKeyPress={onKeyPress}/>
+          <NxStatefulTextInput validator={validator}/>
         </label>
       </div>
       <div className="nx-form-group">
         <label className="nx-label nx-label--optional">
           <span className="nx-label__text">Long field</span>
           <span className="nx-sub-label">This is a sub-label. The field element below is wider than the default.</span>
-          <NxTextInput { ...state } onChange={onChange} onKeyPress={onKeyPress} className="nx-text-input--long"/>
+          <NxStatefulTextInput className="nx-text-input--long"/>
         </label>
       </div>
       <fieldset className="nx-fieldset">
         <legend className="nx-label">Checkboxes</legend>
         <NxCheckbox isChecked={false}>Checkbox 1</NxCheckbox>
-        <NxCheckbox isChecked={true}>Checkbox 2</NxCheckbox>
+        <NxCheckbox isChecked={false}>Checkbox 2</NxCheckbox>
         <NxCheckbox isChecked={false}>Checkbox 3</NxCheckbox>
       </fieldset>
       <fieldset className="nx-fieldset">
         <legend className="nx-label">Radio buttons</legend>
-        <NxRadio name="demo1" value="demo1" isChecked={true}>Radio Button 1</NxRadio>
+        <NxRadio name="demo1" value="demo1" isChecked={false}>Radio Button 1</NxRadio>
         <NxRadio name="demo2" value="demo2" isChecked={false}>Radio Button 2</NxRadio>
-        <NxRadio name="demo3" value="demo3" isChecked={true}>Radio Button 3</NxRadio>
+        <NxRadio name="demo3" value="demo3" isChecked={false}>Radio Button 3</NxRadio>
       </fieldset>
       <div className="nx-form-group">
         <label className="nx-label">
           <span className="nx-label__text">Textarea</span>
-          <NxTextInput { ...state } onChange={onChange} onKeyPress={onKeyPress} type="textarea" placeholder="placeholder"/>
+          <NxStatefulTextInput type="textarea" placeholder="placeholder"/>
         </label>
       </div>
       <footer className="nx-btn-bar nx-btn-bar--forms">
