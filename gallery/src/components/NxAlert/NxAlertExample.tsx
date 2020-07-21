8c003b32ff11586129b0fb5a3d95274236270266
Ross Pokorny <rpokorny@sonatype.com>
2020-07-21 16:20:05 -0400

    Update examples - RSC-18

--- gallery/src/components/NxAlert/NxAlertExample.tsx
+++ gallery/src/components/NxAlert/NxAlertExample.tsx
@@ -4,18 +4,24 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React from 'react';
+import React, { useState } from 'react';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { NxAlert } from '@sonatype/react-shared-components';
 
 function NxAlertExample() {
-  return (
-    <NxAlert icon={faEye} id="this-id-ends-up-on-the-div" className="nx-alert--modifier">
+  const [isOpen, setIsOpen] = useState(true);
+
+  function dismiss() {
+    setIsOpen(false);
+  }
+
+  return isOpen ? (
+    <NxAlert icon={faEye} id="this-id-ends-up-on-the-div" className="nx-alert--modifier" onClose={dismiss}>
       This is an example alert message.
       It is expected that users create their own modifier classes to alter the styles of this component.
       This alert has long text content in order to demonstrate text wrapping.
     </NxAlert>
-  );
+  ) : null;
 }
 
 export default NxAlertExample;
--- gallery/src/components/NxAlert/NxWarningAlertExample.tsx
+++ gallery/src/components/NxAlert/NxWarningAlertExample.tsx
@@ -4,14 +4,20 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React from 'react';
+import React, { useState } from 'react';
 
 import { NxWarningAlert } from '@sonatype/react-shared-components';
 
 function NxWarningExample() {
-  return (
-    <NxWarningAlert>This is a <strong>warning</strong> message.</NxWarningAlert>
-  );
+  const [isOpen, setIsOpen] = useState(true);
+
+  function dismiss() {
+    setIsOpen(false);
+  }
+
+  return isOpen ? (
+    <NxWarningAlert onClose={dismiss}>This is a <strong>warning</strong> message.</NxWarningAlert>
+  ) : null;
 }
 
 export default NxWarningExample;
