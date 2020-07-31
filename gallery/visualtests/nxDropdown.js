24cd8788455588399c9b97ee3bc7efd0ee68dd1a
Ross Pokorny <rpokorny@sonatype.com>
2020-07-31 10:12:45 -0400

    Initial NxDropdown visual tests - RSC-189

---
+++ gallery/visualtests/nxDropdown.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+const { clickTest, focusTest, focusAndHoverTest, hoverTest, simpleTest } = require('./testUtils');
+
+describe('NxButton', function() {
+  beforeEach(async function() {
+    await browser.url('#/pages/NxDropdown');
+  });
+
+  describe('Default NxDropdown', function() {
+    const selector = '#nx-dropdown-scrolling-example';
+
+    describe('when closed', function() {
+      it('has a light grey border by default', simpleTest(selector));
+      it('Has a dark grey border when hovered', hoverTest(selector));
+      it('Has a light blue border when focused', focusTest(selector));
+      it('Has a dark grey border when focused and hovered', focusAndHoverTest(selector));
+      it('Has a dark grey border and light grey background when clicked', clickTest(selector));
+    });
+
+    describe('when open', function() {
+      beforeEach(async function() {
+        const button = await browser.$(selector);
+
+        await button.click();
+      });
+
+      it('has a dark blue button border with expanded menu', simpleTest(selector));
+    });
+  });
+});
