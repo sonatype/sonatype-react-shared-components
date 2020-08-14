6197f4d18aa3f225960d8da4f2dab07cd60972ed
Glen Hunter <ghunter@sonatype.com>
2020-08-14 12:23:42 -0400

    GalleryExampleTile RSC-162

--- gallery/src/styles/NxFormLayout/NxFormLayoutPage.tsx
+++ gallery/src/styles/NxFormLayout/NxFormLayoutPage.tsx
@@ -6,14 +6,13 @@
  */
 import React from 'react';
 
-import { GalleryDescriptionTile } from '../../gallery-components/GalleryTiles';
-import { GalleryTile } from '../../gallery-components/GalleryTiles';
-import CodeExample from '../../CodeExample';
+import { GalleryDescriptionTile, GalleryExampleTile } from '../../gallery-components/GalleryTiles';
+
 import NxFormLayoutExample from './NxFormLayoutExample';
 import NxFormHorizontalLayoutExample from './NxFormHorizontalLayoutExample';
 
-const NxFormLayoutCode = require('!!raw-loader!./NxFormLayoutExample').default,
-    NxFormHorizontalLayoutCode = require('!!raw-loader!./NxFormHorizontalLayoutExample').default;
+const NxFormLayoutCode = require('!!raw-loader!./NxFormLayoutExample').default;
+const NxFormHorizontalLayoutCode = require('!!raw-loader!./NxFormHorizontalLayoutExample').default;
 
 const NxFormLayoutPage = () =>
   <>
@@ -107,15 +106,19 @@ const NxFormLayoutPage = () =>
       </table>
     </GalleryDescriptionTile>
 
-    <GalleryTile title="NX Form Layout Example">
-      <NxFormLayoutExample />
-      <CodeExample content={NxFormLayoutCode}/>
-    </GalleryTile>
+    <GalleryExampleTile title="General Example"
+                        id="nx-checkbox-example"
+                        codeExamples={NxFormLayoutCode}
+                        liveExample={NxFormLayoutExample}>
+      This example shows a stand vertical form layout with validation on some fields.
+    </GalleryExampleTile>
 
-    <GalleryTile title="NX Form Horizontal Layout Example">
-      <NxFormHorizontalLayoutExample />
-      <CodeExample content={NxFormHorizontalLayoutCode}/>
-    </GalleryTile>
+    <GalleryExampleTile title="Checkbox label should not wrap"
+                        liveExample={NxFormHorizontalLayoutExample}
+                        codeExamples={NxFormHorizontalLayoutCode}>
+      This example demonstrates a form layout with horizontally placed text input fields. Note that the chcekbox and
+      radio fieldsets remain vertically separated, the should not be placed side-by-side.
+    </GalleryExampleTile>
   </>;
 
 export default NxFormLayoutPage;
