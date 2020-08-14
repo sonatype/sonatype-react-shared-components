56e8f97b39f87958855d4bc24f6c66f7547d8ed4
Glen Hunter <ghunter@sonatype.com>
2020-08-14 12:23:29 -0400

    Fixed legends. RSC-162

--- gallery/src/styles/NxFormLayout/NxFormHorizontalLayoutExample.tsx
+++ gallery/src/styles/NxFormLayout/NxFormHorizontalLayoutExample.tsx
@@ -33,16 +33,16 @@ export default function NxFormLayoutExample() {
         </div>
       </div>
       <fieldset className="nx-fieldset">
-        <legend className="nx-label nx-label--optional">Checkboxes</legend>
+        <legend className="nx-label nx-label--optional"><span className="nx-legend__text">Checkboxes</span></legend>
         <NxCheckbox isChecked={false}>Checkbox 1</NxCheckbox>
         <NxCheckbox isChecked={true}>Checkbox 2</NxCheckbox>
         <NxCheckbox isChecked={false}>Checkbox 3</NxCheckbox>
       </fieldset>
       <fieldset className="nx-fieldset">
-        <legend className="nx-label">Radio buttons</legend>
+        <legend className="nx-legend"><span className="nx-legend__text">Radio buttons</span></legend>
         <NxRadio name="demo1" value="demo1" isChecked={true}>Radio Button 1</NxRadio>
         <NxRadio name="demo2" value="demo2" isChecked={false}>Radio Button 2</NxRadio>
-        <NxRadio name="demo3" value="demo3" isChecked={true}>Radio Button 3</NxRadio>
+        <NxRadio name="demo3" value="demo3" isChecked={false}>Radio Button 3</NxRadio>
       </fieldset>
       <footer className="nx-form-footer">
         <NxButton>Cancel</NxButton>
