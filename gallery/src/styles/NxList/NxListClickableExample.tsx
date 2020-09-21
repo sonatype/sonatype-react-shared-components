a662ee27667f2b916ef6a42ec0fa046cd4eb6603
Glen Hunter <ghunter@sonatype.com>
2020-09-21 16:17:52 -0400

    :focus states for clickable list rows RSC-178

--- gallery/src/styles/NxList/NxListClickableExample.tsx
+++ gallery/src/styles/NxList/NxListClickableExample.tsx
@@ -10,29 +10,29 @@ import { NxFontAwesomeIcon } from '@sonatype/react-shared-components';
 
 const NxListClickableExample = () =>
   <ul className="nx-list nx-list--clickable">
-    <li className="nx-list__item">
+    <li className="nx-list__item" tabIndex={0}>
       <span className="nx-list__text">Action 1</span>
       <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
     </li>
-    <li className="nx-list__item">
+    <li className="nx-list__item" tabIndex={0}>
       <span className="nx-list__text nx-truncate-ellipsis">
         This list item should be truncated at the right end edge. youtube weathered network network systemic
         systema claymore mine voodoo god garage monofilament realism order-flow corporation car footage vinyl.
       </span>
       <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
     </li>
-    <li className="nx-list__item selected">
+    <li className="nx-list__item selected" tabIndex={0}>
       <span className="nx-list__text">This list item is selected</span>
       <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
     </li>
-    <li className="nx-list__item">
+    <li className="nx-list__item" tabIndex={0}>
       <span className="nx-list__text">Action 4</span>
       <span className="nx-list__subtext">
         This is a fourth line. It includes text that might relate to the top line or might not.
       </span>
       <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
     </li>
-    <li className="nx-list__item disabled">
+    <li className="nx-list__item disabled" tabIndex={0}>
       <span className="nx-list__text">This list item is disabled</span>
       <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
     </li>
