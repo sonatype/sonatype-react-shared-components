c7f9e80fc6be1e720afdb4dab7e7aa081ef1a42d
Glen Hunter <ghunter@sonatype.com>
2020-09-22 15:48:28 -0400

    Simplify error example RSC-178

--- gallery/src/styles/NxList/NxListErrorStateExample.tsx
+++ gallery/src/styles/NxList/NxListErrorStateExample.tsx
@@ -4,16 +4,14 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, {useState} from 'react';
+import React from 'react';
 import { NxLoadError } from '@sonatype/react-shared-components';
 
 function NxListErrorExample() {
-  const [error] = useState<string | null>('Server Error');
-
   return (
     <ul className="nx-list">
       <li className="nx-list__item nx-list__item--error">
-        <NxLoadError error={error} retryHandler={() => {}} />
+        <NxLoadError error="Server Error" retryHandler={() => {}} />
       </li>
     </ul>
   );
