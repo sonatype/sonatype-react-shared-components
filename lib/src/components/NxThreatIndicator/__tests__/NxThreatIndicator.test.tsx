36688b158f6aeb590c76f086f1eb6d42bc348cfb
Ross Pokorny <rpokorny@sonatype.com>
2020-09-24 16:15:05 -0400

    Update jest tests - RSC-149

--- lib/src/components/NxThreatIndicator/__tests__/NxThreatIndicator.test.tsx
+++ lib/src/components/NxThreatIndicator/__tests__/NxThreatIndicator.test.tsx
@@ -4,40 +4,48 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import { faCircle } from '@fortawesome/free-solid-svg-icons';
+
 import * as enzymeUtils from '../../../__testutils__/enzymeUtils';
 
-import NxThreatBar from '../NxThreatBar';
+import NxThreatIndicator from '../NxThreatIndicator';
+import NxFontAwesomeIcon from '../../NxFontAwesomeIcon/NxFontAwesomeIcon';
 
-describe('NxThreatBar', function() {
-  const getShallowComponent = enzymeUtils.getShallowComponent(NxThreatBar, {});
+describe('NxThreatIndicator', function() {
+  const getShallowComponent = enzymeUtils.getShallowComponent(NxThreatIndicator, {});
 
-  it('renders an svg with the `nx-threat-bar` class', function() {
-    expect(getShallowComponent()).toMatchSelector('.nx-threat-bar');
+  it('renders an faCircle NxFontAwesomeIcon with the `nx-threat-indicator` class', function() {
+    const component = getShallowComponent();
+
+    expect(component).toMatchSelector(NxFontAwesomeIcon);
+    expect(component).toHaveProp('icon', faCircle);
+    expect(component).toHaveClassName('nx-threat-indicator');
   });
 
-  it('sets the nx-threat-bar--unspecified class if no props are passed', function() {
-    expect(getShallowComponent()).toMatchSelector('.nx-threat-bar--unspecified');
+  it('sets the nx-threat-indicator--unspecified class if no props are passed', function() {
+    expect(getShallowComponent()).toMatchSelector('.nx-threat-indicator--unspecified');
   });
 
   it('sets the modifier class using the threatLevelCategory if it is provided', function() {
-    expect(getShallowComponent({ threatLevelCategory: 'low' })).toMatchSelector('.nx-threat-bar--low');
-    expect(getShallowComponent({ threatLevelCategory: 'low' })).not.toMatchSelector('.nx-threat-bar--unspecified');
+    expect(getShallowComponent({ threatLevelCategory: 'low' })).toMatchSelector('.nx-threat-indicator--low');
+    expect(getShallowComponent({ threatLevelCategory: 'low' }))
+        .not.toMatchSelector('.nx-threat-indicator--unspecified');
   });
 
   it('sets the modifier class by converting the policyThreatLevel if it is provided', function() {
-    expect(getShallowComponent({ policyThreatLevel: 0 })).toMatchSelector('.nx-threat-bar--none');
-    expect(getShallowComponent({ policyThreatLevel: 1 })).toMatchSelector('.nx-threat-bar--low');
-    expect(getShallowComponent({ policyThreatLevel: 3 })).toMatchSelector('.nx-threat-bar--moderate');
-    expect(getShallowComponent({ policyThreatLevel: 5 })).toMatchSelector('.nx-threat-bar--severe');
-    expect(getShallowComponent({ policyThreatLevel: 9 })).toMatchSelector('.nx-threat-bar--critical');
-    expect(getShallowComponent({ policyThreatLevel: 9 })).not.toMatchSelector('.nx-threat-bar--unspecified');
+    expect(getShallowComponent({ policyThreatLevel: 0 })).toMatchSelector('.nx-threat-indicator--none');
+    expect(getShallowComponent({ policyThreatLevel: 1 })).toMatchSelector('.nx-threat-indicator--low');
+    expect(getShallowComponent({ policyThreatLevel: 3 })).toMatchSelector('.nx-threat-indicator--moderate');
+    expect(getShallowComponent({ policyThreatLevel: 5 })).toMatchSelector('.nx-threat-indicator--severe');
+    expect(getShallowComponent({ policyThreatLevel: 9 })).toMatchSelector('.nx-threat-indicator--critical');
+    expect(getShallowComponent({ policyThreatLevel: 9 })).not.toMatchSelector('.nx-threat-indicator--unspecified');
   });
 
   it('sets the modifier class using the threatLevelCategory if both props are provided', function() {
     const component = getShallowComponent({ policyThreatLevel: 9, threatLevelCategory: 'low' });
 
-    expect(component).toMatchSelector('.nx-threat-bar--low');
-    expect(component).not.toMatchSelector('.nx-threat-bar--critical');
-    expect(component).not.toMatchSelector('.nx-threat-bar--unspecified');
+    expect(component).toMatchSelector('.nx-threat-indicator--low');
+    expect(component).not.toMatchSelector('.nx-threat-indicator--critical');
+    expect(component).not.toMatchSelector('.nx-threat-indicator--unspecified');
   });
 });
