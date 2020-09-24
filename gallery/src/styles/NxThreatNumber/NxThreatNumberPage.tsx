3ec48d84fd3d264d57ffe37a856fa0700ef73ccb
Ross Pokorny <rpokorny@sonatype.com>
2020-09-24 14:58:37 -0400

    Update nx-threat-number gallery page - RSC-149

--- gallery/src/styles/NxThreatNumber/NxThreatNumberPage.tsx
+++ gallery/src/styles/NxThreatNumber/NxThreatNumberPage.tsx
@@ -8,38 +8,38 @@ import React from 'react';
 
 import { GalleryDescriptionTile, GalleryExampleTile } from '../../gallery-components/GalleryTiles';
 
-const nxThreatNumberTableExampleCode = require('!!raw-loader!./NxThreatNumberTableExample.html').default,
-    nxThreatNumberListExampleCode = require('!!raw-loader!./NxThreatNumberListExample.html').default;
+import NxThreatNumberBasicExample from './NxThreatNumberBasicExample';
+import NxThreatNumberListExample from './NxThreatNumberListExample';
+
+const nxThreatNumberBasicExampleCode = require('!!raw-loader!./NxThreatNumberBasicExample').default,
+    nxThreatNumberListExampleCode = require('!!raw-loader!./NxThreatNumberListExample').default;
 
 const NxThreatNumberPage = () =>
   <>
     <GalleryDescriptionTile>
       <p className="nx-p">
-        When an IQ Policy Threat Number is displayed adjacent to an <code className="nx-code">NxThreatBar</code>,
-        style the number with <code className="nx-code">nx-threat-number</code>
-      </p>
-      <p className="nx-p">
-        Note that if a threat bar is used inside <code className="nx-code">nx-cell</code> the {''}
-        <code className="nx-code">nx-cell--threat-bar</code> modifier must be applied to the table cell. If that cell
-        might have multi-line content, that content would need to be wrapped in a custom class to restore the padding
-        removed by <code className="nx-code">nx-cell--threat-bar</code>.
+        When an IQ Policy Threat Number is displayed adjacent to an <code className="nx-code">NxThreatIndicator</code>,
+        style the number with <code className="nx-code">.nx-threat-number</code>
       </p>
     </GalleryDescriptionTile>
 
-    <GalleryExampleTile title="List Example"
-                        id="nx-threat-number-list-example"
-                        htmlExample={nxThreatNumberListExampleCode}
-                        codeExamples={nxThreatNumberListExampleCode}>
-      An example of <code className="nx-code">nx-threat-number</code> used within
-      an <code className="nx-code">nx-list</code> adjacent to an <code className="nx-code">nx-threat-bar</code>.
+    <GalleryExampleTile title="Basic Example"
+                        id="nx-threat-number-basic-example"
+                        liveExample={NxThreatNumberBasicExample}
+                        codeExamples={nxThreatNumberBasicExampleCode}>
+      An example of <code className="nx-code">.nx-threat-number</code> used
+      adjacent to an <code className="nx-code">NxThreatIndicator</code> in a simple inline layout. This how how the
+      two would be used together within a table cell, for instance.
     </GalleryExampleTile>
 
-    <GalleryExampleTile title="Table Example"
-                        id="nx-threat-number-table-example"
-                        htmlExample={nxThreatNumberTableExampleCode}
-                        codeExamples={nxThreatNumberTableExampleCode}>
-      An example of <code className="nx-code">nx-threat-number</code> used within
-      an <code className="nx-code">nx-table</code> adjacent to an <code className="nx-code">nx-threat-bar</code>.
+    <GalleryExampleTile title="List Example"
+                        id="nx-threat-number-list-example"
+                        liveExample={NxThreatNumberListExample}
+                        codeExamples={nxThreatNumberListExampleCode}>
+      An example of <code className="nx-code">.nx-threat-number</code> used
+      along with <code className="nx-code">NxThreatIndicator</code> in an <code className="nx-code">.nx-list</code>.
+      The layout expectations of <code className="nx-code">.nx-list</code> require a slight customization
+      to the way the elements are arranged.
     </GalleryExampleTile>
   </>;
 
