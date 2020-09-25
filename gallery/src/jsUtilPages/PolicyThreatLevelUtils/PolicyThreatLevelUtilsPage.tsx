65e54b0463e776c7be5915a4f278af8fd7ffe7e1
Glen Hunter <ghunter@sonatype.com>
2020-09-25 13:19:18 -0400

    Converted polict threat level utils def-list to bulleted list RSC-178

--- gallery/src/jsUtilPages/PolicyThreatLevelUtils/PolicyThreatLevelUtilsPage.tsx
+++ gallery/src/jsUtilPages/PolicyThreatLevelUtils/PolicyThreatLevelUtilsPage.tsx
@@ -21,27 +21,25 @@ const PolicyThreatLevelUtilsPage = () =>
         objects and TypeScript types that provide code definitions and mappings between these two concepts.
       </p>
 
-      <dl className="nx-list nx-list--definition-list">
-        <dt className="nx-list__item nx-list__item--label">Policy Threat Level</dt>
-        <dd className="nx-list__item">
-          An integer between 0 and 10, inclusive, that describes the severity of a threat.  Zero represents no
+      <ul className="nx-list nx-list--bulleted">
+        <li className="nx-list__item"><strong>Policy Threat Level:</strong>
+          {' '}An integer between 0 and 10, inclusive, that describes the severity of a threat.  Zero represents no
           threat at all and ten represents the most severe of threats
-        </dd>
-        <dt className="nx-list__item nx-list__item--label">Policy Threat Level Category</dt>
-        <dd className="nx-list__item">
-          In increasing order of severity: <q>none</q>; <q>low</q>; <q>moderate</q>; <q>severe</q>;
+        </li>
+        <li className="nx-list__item"><strong>Policy Threat Level Category:</strong>
+          {' '}In increasing order of severity: <q>none</q>; <q>low</q>; <q>moderate</q>; <q>severe</q>;
           and <q>critical</q>, or <q>unspecified</q>. Each Policy Threat Level is a member of a corresponding
           category:
 
-          <dl className="nx-list nx-list--definition-list">
-            <dt className="nx-list__item nx-list__item--label">none</dt><dd className="nx-list__item">0</dd>
-            <dt className="nx-list__item nx-list__item--label">low</dt><dd className="nx-list__item">1</dd>
-            <dt className="nx-list__item nx-list__item--label">moderate</dt><dd className="nx-list__item">2 - 3</dd>
-            <dt className="nx-list__item nx-list__item--label">severe</dt><dd className="nx-list__item">4 - 7</dd>
-            <dt className="nx-list__item nx-list__item--label">critical</dt><dd className="nx-list__item">8 - 10</dd>
-          </dl>
-        </dd>
-      </dl>
+          <ul className="nx-list nx-list--bulleted">
+            <li className="nx-list__item"><strong>none:</strong> 0</li>
+            <li className="nx-list__item"><strong>low:</strong> 1</li>
+            <li className="nx-list__item"><strong>moderate:</strong> 2 - 3</li>
+            <li className="nx-list__item"><strong>severe:</strong> 4 - 7</li>
+            <li className="nx-list__item"><strong>critical:</strong> 8 - 10</li>
+          </ul>
+        </li>
+      </ul>
     </GalleryDescriptionTile>
 
     <GalleryExampleTile title="ThreatLevelCategory & allThreatLevelCategories"
