528f9c65261f2e3f0698758f64fd752e3b41f25e
Ross Pokorny <rpokorny@sonatype.com>
2020-09-30 12:49:01 -0400

    Fix eslint - RSC-177

--- lib/src/components/NxTableCell/__tests__/NxTableCell.test.tsx
+++ lib/src/components/NxTableCell/__tests__/NxTableCell.test.tsx
@@ -55,7 +55,7 @@ describe('NxTableCell', function () {
       }
 
       expect(component.children()).toMatchElement(<Fixture />);
-    })
+    });
   });
 
   it('shows the sortable icon when the cell isSortable but has no sort direction', function () {
