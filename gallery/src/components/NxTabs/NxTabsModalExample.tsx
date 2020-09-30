cd1e59137e4d99131f452353d993d1a2ff2f5599
Glen Hunter <ghunter@sonatype.com>
2020-09-30 09:20:06 -0400

    Linting RSC-153

--- gallery/src/components/NxTabs/NxTabsModalExample.tsx
+++ gallery/src/components/NxTabs/NxTabsModalExample.tsx
@@ -48,45 +48,45 @@ const NxTabsModalExample = () => {
                 <NxTab>Fourth Tab</NxTab>
               </NxTabList>
               <NxTabPanel>
-              <ul className="nx-list nx-list--bulleted">
-                <li className="nx-list__item">
-                  <span className="nx-list__text">Item 1</span>
-                </li>
-                <li className="nx-list__item">
-                  <span className="nx-list__text">Item 2</span>
-                  <ul className="nx-list nx-list--bulleted">
-                    <li className="nx-list__item">
-                      <span className="nx-list__text">Item 1</span>
-                    </li>
-                    <li className="nx-list__item">
-                      <span className="nx-list__text">Item 2</span>
-                    </li>
-                    <li className="nx-list__item">
-                      <span className="nx-list__text">Item 3</span>
-                      <ul className="nx-list nx-list--bulleted">
-                        <li className="nx-list__item">
-                          <span className="nx-list__text">
-                            Item 1 this list items is also long enough that it wraps, or at least it should if I have typed enough
-                            text, how much wood would a woodchuck chuck
-                          </span>
-                        </li>
-                        <li className="nx-list__item">
-                          <span className="nx-list__text">Item 2</span>
-                        </li>
-                        <li className="nx-list__item">
-                          <span className="nx-list__text">Item 3</span>
-                        </li>
-                      </ul>
-                    </li>
-                    <li className="nx-list__item">
-                      <span className="nx-list__text">Item 4</span>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nx-list__item">
-                  <span className="nx-list__text">Item 3</span>
-                </li>
-              </ul>
+                <ul className="nx-list nx-list--bulleted">
+                  <li className="nx-list__item">
+                    <span className="nx-list__text">Item 1</span>
+                  </li>
+                  <li className="nx-list__item">
+                    <span className="nx-list__text">Item 2</span>
+                    <ul className="nx-list nx-list--bulleted">
+                      <li className="nx-list__item">
+                        <span className="nx-list__text">Item 1</span>
+                      </li>
+                      <li className="nx-list__item">
+                        <span className="nx-list__text">Item 2</span>
+                      </li>
+                      <li className="nx-list__item">
+                        <span className="nx-list__text">Item 3</span>
+                        <ul className="nx-list nx-list--bulleted">
+                          <li className="nx-list__item">
+                            <span className="nx-list__text">
+                              Item 1 this list items is also long enough that it wraps, or at least it should if I have
+                              typed enough text, how much wood would a woodchuck chuck
+                            </span>
+                          </li>
+                          <li className="nx-list__item">
+                            <span className="nx-list__text">Item 2</span>
+                          </li>
+                          <li className="nx-list__item">
+                            <span className="nx-list__text">Item 3</span>
+                          </li>
+                        </ul>
+                      </li>
+                      <li className="nx-list__item">
+                        <span className="nx-list__text">Item 4</span>
+                      </li>
+                    </ul>
+                  </li>
+                  <li className="nx-list__item">
+                    <span className="nx-list__text">Item 3</span>
+                  </li>
+                </ul>
               </NxTabPanel>
               <NxTabPanel>Tab 2</NxTabPanel>
               <NxTabPanel>Tab 3</NxTabPanel>
