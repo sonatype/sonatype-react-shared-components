5b9096de760249a08bf5a025bc1449974550fade
Glen Hunter <ghunter@sonatype.com>
2020-10-07 13:09:26 -0400

    Initial setup for pagination controls RSC-181

---
+++ gallery/src/styles/NxPaginationControls/NxPaginationControls.tsx
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import React from 'react';
+import { NxFontAwesomeIcon, NxCloseButton } from '@sonatype/react-shared-components';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+
+const PaginationControls = () =>
+  <div className="nx-pagination-controls">
+    <NxFontAwesomeIcon icon={faInfoCircle}/>
+    <span>Information! I am an informational alert! Be informed!</span>
+    <NxCloseButton />
+  </div>;
+
+export default NxPaginationControlsExample;
