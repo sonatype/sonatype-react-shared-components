88bde0dd732d9ea4d06a8cc25cd7884e61166f07
Glen Hunter <ghunter@sonatype.com>
2020-10-09 15:13:19 -0400

    Mixed version fixes RSC-222

--- gallery/src/styles/NxCard/NxCardHorizontalExample.tsx
+++ gallery/src/styles/NxCard/NxCardHorizontalExample.tsx
@@ -13,97 +13,137 @@ export default function NxCardVerticalExample() {
   return (
     <div className="nx-card-container nx-card-container--column">
       <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3">Header</h3>
+        </header>
         <div className="nx-card__call-out">
-          <span className="nx-call__call-out-text">XX%</span>
+          <span className="nx-call__call-out-text">42</span>
         </div>
-        <div className="nx-card-content">
-          <header className="nx-card-header">
-            <h3 className="nx-h3 nx-h3--card-header">Card header</h3>
-          </header>
-          <div className="nx-card__text">
-            Descriptive text
-          </div>
-        </div>
-        <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
-      </div>
-      <div className="nx-card nx-card--horizontal">
-        <div className="nx-card__call-out">
-          <NxFontAwesomeIcon icon={faShapes}/>
-        </div>
-        <div className="nx-card-content">
-          <header className="nx-card-header">
-            <h3 className="nx-h3 nx-h3--card-header">Chiba advert hacker hotdog shoes voodoo god 3D-printed</h3>
-          </header>
-          <div className="nx-card__text">
-            Marketing systemic math- hotdog saturation point nodal point footage disposable singularity order-flow
-            systemic
-          </div>
-        </div>
-        <footer className="nx-card-footer"><a href="nx-text-link">Artisanal denim</a></footer>
-      </div>
-      <div className="nx-card nx-card--horizontal">
-        <div className="nx-card__call-out">
-          <span className="nx-call__call-out-text">XXX%</span>
-        </div>
-        <div className="nx-card-content">
-          <header className="nx-card-header">
-            <h3 className="nx-h3 nx-h3--card-header">Render-farm dolphin beef noodles</h3>
-          </header>
-          <div className="nx-card__text">
-            City advert motion apophenia film skyscraper sentient beef noodles industrial grade motion tanto BASE jump
-            girl realism
-          </div>
-        </div>
-        <footer className="nx-card-footer"><a href="nx-text-link">Film sprawl wristwatch math-engine</a></footer>
-      </div>
-      <div className="nx-card nx-card--horizontal">
-        <div className="nx-card__call-out">
-          <span className="nx-call__call-out-text">XXXX%</span>
-        </div>
-        <div className="nx-card-content">
-          <header className="nx-card-header">
-            <h3 className="nx-h3 nx-h3--card-header">Weathered plastic saturation point realism</h3>
-          </header>
-          <div className="nx-card__text">
-            Construct otaku monofilament skyscraper
-          </div>
-        </div>
-        <footer className="nx-card-footer"><a href="nx-text-link">Jabberwocky</a></footer>
-      </div>
-      <div className="nx-card nx-card--horizontal">
-        <div className="nx-card__call-out">
-          <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
-        </div>
-        <div className="nx-card-content">
-          <header className="nx-card-header">
-            <h3 className="nx-h3 nx-h3--card-header">Augmented reality grenade</h3>
-          </header>
-          <div className="nx-card__text">
-            Neural sign shrine corrupted decay fluidity paranoid nodality long-chain hydrocarbons physical shoes 8-bit
-            singularity sensory smart-boat Legba smart-sunglasses spook city
-          </div>
+        <div className="nx-card__text">
+          Text
         </div>
         <footer className="nx-card-footer">
-          <a href="nx-text-link">uplink numinous pen</a>
+          <a href="#" className="nx-text-link">Link</a>
         </footer>
       </div>
       <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3">Card header</h3>
+        </header>
+        <div className="nx-card__call-out">
+          <NxFontAwesomeIcon icon={faShapes} />
+        </div>
+        <div className="nx-card__text">
+          Data point details
+        </div>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">Link</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3">Card header</h3>
+        </header>
+        <div className="nx-card__call-out">
+        <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
+        </div>
+        <div className="nx-card__text">
+          Large icon
+        </div>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">Link</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3 nx-h3--card-header">Card header</h3>
+        </header>
+        <div className="nx-card__call-out">
+          <span className="nx-call__call-out-text">XX%</span>
+        </div>
+        <div className="nx-card__text">
+          Descriptive text
+        </div>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">Link</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3 nx-h3--card-header">Chiba advert hacker hotdog shoes voodoo god 3D-printed</h3>
+        </header>
+        <div className="nx-card__call-out">
+          <NxFontAwesomeIcon icon={faShapes}/>
+        </div>
+        <div className="nx-card__text">
+          Marketing systemic math- hotdog saturation
+          systemic
+        </div>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">Artisanal denim</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3 nx-h3--card-header">Render-farm dolphin beef noodles</h3>
+        </header>
+        <div className="nx-card__call-out">
+          <span className="nx-call__call-out-text">XXX%</span>
+        </div>
+        <div className="nx-card__text">
+          City advert motion apophenia film skyscraper sentient beef
+        </div>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">Film sprawl wristwatch math-engine</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3 nx-h3--card-header">Weathered plastic saturation point realism</h3>
+        </header>
+        <div className="nx-card__call-out">
+          <span className="nx-call__call-out-text">XXXX%</span>
+        </div>
+        <div className="nx-card__text">
+          Construct otaku monofilament skyscraper
+        </div>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">Jabberwocky</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3 nx-h3--card-header">Augmented reality grenade</h3>
+        </header>
+        <div className="nx-card__call-out">
+          <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
+        </div>
+        <div className="nx-card__text">
+          Neural sign shrine corrupted decay fluidity paranoid nodality long-chain hydrocarbons physical shoes 8-bit
+          singularity sensory smart-boat Legba smart-sunglasses spook city
+        </div>
+        <footer className="nx-card-footer">
+
+            <a href="#" className="nx-text-link">uplink numino</a>
+        </footer>
+      </div>
+      <div className="nx-card nx-card--horizontal">
+        <header className="nx-card-header">
+          <h3 className="nx-h3 nx-h3--card-header">
+            Fluidity futurity order-flow city neon pre-DIY wristwatch-space bicycle digital geodesic realism sensory
+            dome otaku rain-ware lights
+          </h3>
+        </header>
         <div className="nx-card__call-out">
           <NxFontAwesomeIcon icon={faChartLine} className="nx-icon--large" />
         </div>
-        <div className="nx-card-content">
-          <header className="nx-card-header">
-            <h3 className="nx-h3 nx-h3--card-header">
-              Fluidity futurity order-flow city neon pre-DIY wristwatch-space bicycle digital geodesic realism sensory
-              dome otaku rain-ware lights
-            </h3>
-          </header>
-          <div className="nx-card__text">
-            BASE jump boy corrupted nano-skyscraper disposable sub-orbital marketing apophenia sign military-grade
-            saturation point drugs
-          </div>
+        <div className="nx-card__text">
+          BASE jump boy corrupted nano-skyscraper disposable sub-orbital marketing apophenia sign military-grade
+          saturation point drugs
         </div>
-        <footer className="nx-card-footer"><a href="nx-text-link">digital military-grade j-pop-ware</a></footer>
+        <footer className="nx-card-footer">
+          <a href="#" className="nx-text-link">digital military-grade j-pop-ware</a>
+        </footer>
       </div>
     </div>
   );
--- gallery/src/styles/NxCard/NxCardVerticalExample.tsx
+++ gallery/src/styles/NxCard/NxCardVerticalExample.tsx
@@ -17,87 +17,138 @@ export default function NxCardVerticalExample() {
           <header className="nx-card-header">
             <h3 className="nx-h3">Header</h3>
           </header>
-          <div className="nx-card-content">
-            <div className="nx-card__call-out">
-              <span className="nx-call__call-out-text">42</span>
-            </div>
-            <div className="nx-card__text">
-              Text
-            </div>
+          <div className="nx-card__call-out">
+            <span className="nx-call__call-out-text">42</span>
           </div>
-          <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
+          <div className="nx-card__text">
+            Text
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Link</a>
+          </footer>
         </div>
         <div className="nx-card nx-card--vertical">
           <header className="nx-card-header">
             <h3 className="nx-h3">Card header</h3>
           </header>
-          <div className="nx-card-content">
-            <div className="nx-card__call-out">
-              <NxFontAwesomeIcon icon={faShapes} />
-            </div>
-            <div className="nx-card__text">
-              Data point details
-            </div>
+          <div className="nx-card__call-out">
+            <NxFontAwesomeIcon icon={faShapes} />
           </div>
-          <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
+          <div className="nx-card__text">
+            Data point details
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Link</a>
+          </footer>
         </div>
         <div className="nx-card nx-card--vertical">
           <header className="nx-card-header">
             <h3 className="nx-h3">Card header</h3>
           </header>
-          <div className="nx-card-content">
-            <div className="nx-card__call-out">
-            <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
-            </div>
-            <div className="nx-card__text">
-              Large icon
-            </div>
+          <div className="nx-card__call-out">
+          <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
           </div>
-          <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
+          <div className="nx-card__text">
+            Large icon
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Link</a>
+          </footer>
         </div>
       </div>
       <div className="nx-card-container nx-card-container--row">
         <div className="nx-card nx-card--vertical">
           <header className="nx-card-header">
-            <h3 className="nx-h3">Military-grade refrigerator</h3>
+            <h3 className="nx-h3 nx-h3--card-header">Card header</h3>
           </header>
-          <div className="nx-card-content">
-            <div className="nx-card__call-out">
-              <NxFontAwesomeIcon icon={faChartLine} className="nx-icon--large" />
-            </div>
-            <div className="nx-card__text">
-              Katana urban systema narrative
-            </div>
+          <div className="nx-card__call-out">
+            <span className="nx-call__call-out-text">XX%</span>
           </div>
-          <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
+          <div className="nx-card__text">
+            Descriptive text
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Link</a>
+          </footer>
         </div>
         <div className="nx-card nx-card--vertical">
           <header className="nx-card-header">
-            <h3 className="nx-h3">Chiba advert hacker hotdog shoes voodoo god 3D-printed</h3>
+            <h3 className="nx-h3 nx-h3--card-header">Chiba advert hacker hotdog shoes voodoo god 3D-printed</h3>
           </header>
-          <div className="nx-card-content">
-            <div className="nx-card__call-out">
-              <span className="nx-call__call-out-text">XXXX%</span>
-            </div>
-            <div className="nx-card__text">
-              Marketing math-hotdog saturation point nodal point footage disposable singularity order-flow
-            </div>
+          <div className="nx-card__call-out">
+            <NxFontAwesomeIcon icon={faShapes}/>
           </div>
-          <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
+          <div className="nx-card__text">
+            Marketing systemic math- hotdog saturation point nodal point footage disposable singularity order-flow
+            systemic
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Artisanal denim</a>
+          </footer>
         </div>
         <div className="nx-card nx-card--vertical">
           <header className="nx-card-header">
-            <h3 className="nx-h3">Card header</h3>
+            <h3 className="nx-h3 nx-h3--card-header">Render-farm dolphin beef noodles</h3>
           </header>
-          <div className="nx-card-content">
-            <div className="nx-card__call-out">
-              <span className="nx-call__call-out-text">XXXXX%</span>
-            </div>
-            <div className="nx-card__text">
-              Descriptive text
-            </div>
+          <div className="nx-card__call-out">
+            <span className="nx-call__call-out-text">XXX%</span>
           </div>
-          <footer className="nx-card-footer"><a href="nx-text-link">Link</a></footer>
+          <div className="nx-card__text">
+            City advert motion apophenia film skyscraper sentient beef noodles industrial grade motion tanto BASE jump
+            girl realism
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Film sprawl wristwatch math-engine</a>
+          </footer>
+        </div>
+      </div>
+      <div className="nx-card-container nx-card-container--row">
+        <div className="nx-card nx-card--vertical">
+          <header className="nx-card-header">
+            <h3 className="nx-h3 nx-h3--card-header">Weathered plastic saturation point realism</h3>
+          </header>
+          <div className="nx-card__call-out">
+            <span className="nx-call__call-out-text">XXXX%</span>
+          </div>
+          <div className="nx-card__text">
+            Construct otaku monofilament skyscraper
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">Jabberwocky</a>
+          </footer>
+        </div>
+        <div className="nx-card nx-card--vertical">
+          <header className="nx-card-header">
+            <h3 className="nx-h3 nx-h3--card-header">Augmented reality grenade</h3>
+          </header>
+          <div className="nx-card__call-out">
+            <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
+          </div>
+          <div className="nx-card__text">
+            Neural sign shrine corrupted decay fluidity paranoid nodality long-chain hydrocarbons physical shoes 8-bit
+            singularity sensory smart-boat Legba smart-sunglasses spook city
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">uplink numinous pen</a>
+          </footer>
+        </div>
+        <div className="nx-card nx-card--vertical">
+          <header className="nx-card-header">
+            <h3 className="nx-h3 nx-h3--card-header">
+              Fluidity futurity order-flow city neon pre-DIY wristwatch-space bicycle digital geodesic realism sensory
+              dome otaku rain-ware lights
+            </h3>
+          </header>
+          <div className="nx-card__call-out">
+            <NxFontAwesomeIcon icon={faChartLine} className="nx-icon--large" />
+          </div>
+          <div className="nx-card__text">
+            BASE jump boy corrupted nano-skyscraper disposable sub-orbital marketing apophenia sign military-grade
+            saturation point drugs
+          </div>
+          <footer className="nx-card-footer">
+            <a href="#" className="nx-text-link">digital military-grade j-pop-ware</a>
+          </footer>
         </div>
       </div>
     </>
