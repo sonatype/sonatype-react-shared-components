f691bf1fa5d8590b2a0336cbca00eca16e23f909
Glen Hunter <ghunter@sonatype.com>
2020-10-15 13:47:51 -0400

    btn in footer example, remove wrapping from alternate example RSC-222

--- gallery/src/styles/NxCard/NxCardHorizontalExample.tsx
+++ gallery/src/styles/NxCard/NxCardHorizontalExample.tsx
@@ -5,9 +5,9 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { faShapes, faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faShapes, faChartLine, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
-import { NxFontAwesomeIcon } from '@sonatype/react-shared-components';
+import { NxFontAwesomeIcon, NxButton } from '@sonatype/react-shared-components';
 
 export default function NxCardHorizontalExample() {
   return (
@@ -51,7 +51,8 @@ export default function NxCardHorizontalExample() {
           Large icon
         </div>
         <footer className="nx-card__footer">
-          <a href="#" className="nx-text-link">Link</a>
+          <NxButton variant="icon-only"><NxFontAwesomeIcon icon={faEdit} className="nx-icon--large" /></NxButton>
+          <NxButton variant="icon-only"><NxFontAwesomeIcon icon={faTrash} className="nx-icon--large" /></NxButton>
         </footer>
       </div>
       <div className="nx-card">
