f8bff817feb04ec001547c690a2e3a010971b05d
Glen Hunter <ghunter@sonatype.com>
2020-10-16 11:46:31 -0400

    Remove unnecessary fragment RSC-222

--- gallery/src/styles/NxCard/NxCardVerticalAlternateExample.tsx
+++ gallery/src/styles/NxCard/NxCardVerticalAlternateExample.tsx
@@ -11,51 +11,49 @@ import { NxFontAwesomeIcon } from '@sonatype/react-shared-components';
 
 export default function NxCardVerticalAltExample() {
   return (
-    <>
-      <div className="nx-card-container nx-card-container--row">
-        <section className="nx-card nx-card--vertical-alt">
-          <header className="nx-card__header">
-            <h3 className="nx-h3">Header</h3>
-          </header>
-          <div className="nx-card__content-container">
-            <div className="nx-card__call-out">
-              <span className="nx-card__call-out-text">42</span>
-            </div>
-            <div className="nx-card__content">
-              Text
-            </div>
+    <div className="nx-card-container nx-card-container--row">
+      <section className="nx-card nx-card--vertical-alt">
+        <header className="nx-card__header">
+          <h3 className="nx-h3">Header</h3>
+        </header>
+        <div className="nx-card__content-container">
+          <div className="nx-card__call-out">
+            <span className="nx-card__call-out-text">42</span>
           </div>
-          <footer className="nx-card__footer"><a href="nx-text-link">Link</a></footer>
-        </section>
-        <section className="nx-card nx-card--vertical-alt">
-          <header className="nx-card__header">
-            <h3 className="nx-h3">Card header</h3>
-          </header>
-          <div className="nx-card__content-container">
-            <div className="nx-card__call-out">
-              <NxFontAwesomeIcon icon={faChartLine} />
-            </div>
-            <div className="nx-card__content">
-              Data point details
-            </div>
+          <div className="nx-card__content">
+            Text
           </div>
-          <footer className="nx-card__footer"><a href="nx-text-link">Link</a></footer>
-        </section>
-        <section className="nx-card nx-card--vertical-alt">
-          <header className="nx-card__header">
-            <h3 className="nx-h3">Card header</h3>
-          </header>
-          <div className="nx-card__content-container">
-            <div className="nx-card__call-out">
-              <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
-            </div>
-            <div className="nx-card__content">
-              Large icon
-            </div>
+        </div>
+        <footer className="nx-card__footer"><a href="nx-text-link">Link</a></footer>
+      </section>
+      <section className="nx-card nx-card--vertical-alt">
+        <header className="nx-card__header">
+          <h3 className="nx-h3">Card header</h3>
+        </header>
+        <div className="nx-card__content-container">
+          <div className="nx-card__call-out">
+            <NxFontAwesomeIcon icon={faChartLine} />
           </div>
-          <footer className="nx-card__footer"><a href="nx-text-link">Link</a></footer>
-        </section>
-      </div>
-    </>
+          <div className="nx-card__content">
+            Data point details
+          </div>
+        </div>
+        <footer className="nx-card__footer"><a href="nx-text-link">Link</a></footer>
+      </section>
+      <section className="nx-card nx-card--vertical-alt">
+        <header className="nx-card__header">
+          <h3 className="nx-h3">Card header</h3>
+        </header>
+        <div className="nx-card__content-container">
+          <div className="nx-card__call-out">
+            <NxFontAwesomeIcon icon={faShapes} className="nx-icon--large" />
+          </div>
+          <div className="nx-card__content">
+            Large icon
+          </div>
+        </div>
+        <footer className="nx-card__footer"><a href="nx-text-link">Link</a></footer>
+      </section>
+    </div>
   );
 }
