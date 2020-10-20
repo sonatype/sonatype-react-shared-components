e1ceae30abd6ef95e50f059a33db8f7662124fbf
aalzate <aalzate@sonatype.com>
2020-10-20 13:58:47 -0500

    Adding more examples to the gallery

--- gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartPage.tsx
+++ gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartPage.tsx
@@ -7,13 +7,19 @@
 import React from 'react';
 
 import { GalleryDescriptionTile, GalleryExampleTile } from '../../gallery-components/GalleryTiles';
+import NxBinaryDonutChartFullExample from './NxBinaryDonutChartFullExample';
+import NxBinaryDonutChartHalfExample from './NxBinaryDonutChartHalfExample';
 import NxBinaryDonutChartMinimalExample from './NxBinaryDonutChartMinimalExample';
 import NxBinaryDonutChartNoHoleExample from './NxBinaryDonutChartNoHoleExample';
 import NxBinaryDonutChartLargeHoleExample from './NxBinaryDonutChartLargeHoleExample';
+import NxBinaryDonutChartQuarterExample from './NxBinaryDonutChartQuarterExample';
 
+const nxBinaryDonutChartFullExample = require('!!raw-loader!./NxBinaryDonutChartFullExample').default;
+const nxBinaryDonutChartHalfExample = require('!!raw-loader!./NxBinaryDonutChartHalfExample').default;
 const nxBinaryDonutChartMinimalExampleCode = require('!!raw-loader!./NxBinaryDonutChartMinimalExample').default;
 const nxBinaryDonutChartNoHoleExample = require('!!raw-loader!./NxBinaryDonutChartNoHoleExample').default;
 const nxBinaryDonutChartLargeHoleExample = require('!!raw-loader!./NxBinaryDonutChartLargeHoleExample').default;
+const nxBinaryDonutChartQuarterExample = require('!!raw-loader!./NxBinaryDonutChartQuarterExample').default;
 
 const NxBinaryDonutChartPage = () =>
   <>
@@ -28,6 +34,27 @@ const NxBinaryDonutChartPage = () =>
                         codeExamples={nxBinaryDonutChartMinimalExampleCode}
                         liveExample={NxBinaryDonutChartMinimalExample}>
         A minimal example of a <code className="nx-code">NxBinaryDonutChart</code>.
+    </GalleryExampleTile>
+
+    <GalleryExampleTile title="Example with half Donut"
+                        className="nx-binary-donut-chart-examples"
+                        codeExamples={nxBinaryDonutChartHalfExample}
+                        liveExample={NxBinaryDonutChartHalfExample}>
+        An example of a half <code className="nx-code">NxBinaryDonutChart</code>.
+    </GalleryExampleTile>
+
+    <GalleryExampleTile title="Example with quarter Donut"
+                        className="nx-binary-donut-chart-examples"
+                        codeExamples={nxBinaryDonutChartQuarterExample}
+                        liveExample={NxBinaryDonutChartQuarterExample}>
+        An example of a quarter <code className="nx-code">NxBinaryDonutChart</code>.
+    </GalleryExampleTile>
+
+    <GalleryExampleTile title="Example with full Donut"
+                        className="nx-binary-donut-chart-examples"
+                        codeExamples={nxBinaryDonutChartFullExample}
+                        liveExample={NxBinaryDonutChartFullExample}>
+        An example of a full <code className="nx-code">NxBinaryDonutChart</code>.
     </GalleryExampleTile>
 
     <GalleryExampleTile title="Example with no Hole"
--- gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartQuarterExample.tsx
+++ gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartQuarterExample.tsx
@@ -1,20 +1,12 @@
-/**
+/*
  * Copyright (c) 2019-present Sonatype, Inc.
  * This program and the accompanying materials are made available under
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-@import '../../scss-shared/nx-colors';
+import React from 'react';
+import { NxBinaryDonutChart } from '@sonatype/react-shared-components';
 
-.nx-binary-donut-chart {
-  height: 30px;
-  fill: none;
-}
-
-.nx-binary-donut-chart__background {
-  stroke: $nx-light-blue;
-}
-
-.nx-binary-donut-chart__arc {
-  stroke: $nx-dark-blue;
+export default function NxBinaryDonutChartQuarterExample() {
+  return <NxBinaryDonutChart percent={25} />;
 }
