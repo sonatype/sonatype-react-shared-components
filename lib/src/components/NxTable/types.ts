8bdd740a4a3ac334f43bca15410c066e22b0cbdf
Ross Pokorny <rpokorny@sonatype.com>
2020-11-06 14:56:54 -0500

    Consolidate types - RSC-294

--- lib/src/components/NxTable/types.ts
+++ lib/src/components/NxTable/types.ts
@@ -4,11 +4,70 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import {TableHTMLAttributes} from 'react';
+import { TableHTMLAttributes, HTMLAttributes, TdHTMLAttributes, ThHTMLAttributes } from 'react';
 import PropTypes from 'prop-types';
 
-export type Props = TableHTMLAttributes<HTMLTableElement>;
+export type NxTableProps = TableHTMLAttributes<HTMLTableElement>;
 
-export const propTypes: PropTypes.ValidationMap<Props> = {
+export const nxTablePropTypes: PropTypes.ValidationMap<NxTableProps> = {
+  children: PropTypes.node
+};
+
+export type NxTableBodyProps = HTMLAttributes<HTMLTableSectionElement> & {
+  isLoading?: boolean | null;
+  error?: string | null;
+  emptyMessage?: string | null;
+  columns?: number | null;
+  retryHandler?: (() => void) | null;
+};
+
+export const nxTableBodyPropTypes: PropTypes.ValidationMap<NxTableBodyProps> = {
+  isLoading: PropTypes.bool,
+  error: PropTypes.string,
+  columns: PropTypes.number,
+  retryHandler: PropTypes.func,
+  children: PropTypes.node
+};
+
+export type NxTableCellProps =
+    (TdHTMLAttributes<HTMLTableCellElement> | ThHTMLAttributes<HTMLTableHeaderCellElement>) & {
+      isHeader?: boolean | null;
+      metaInfo?: boolean | null;
+      isNumeric?: boolean | null;
+      isSortable?: boolean | null;
+      isFilterHeader?: boolean | null;
+      hasIcon?: boolean | null;
+      chevron?: boolean | null;
+      sortDir?: 'asc' | 'desc' | null;
+    };
+
+export const nxTableCellPropTypes: PropTypes.ValidationMap<NxTableCellProps> = {
+  isHeader: PropTypes.bool,
+  metaInfo: PropTypes.bool,
+  isNumeric: PropTypes.bool,
+  isSortable: PropTypes.bool,
+  isFilterHeader: PropTypes.bool,
+  hasIcon: PropTypes.bool,
+  chevron: PropTypes.bool,
+  sortDir: PropTypes.oneOf(['asc', 'desc', null]),
+  children: PropTypes.node
+};
+
+export type NxTableHeadProps = HTMLAttributes<HTMLTableSectionElement>;
+
+export const nxTableHeadePropTypes: PropTypes.ValidationMap<NxTableHeadProps> = {
+  children: PropTypes.node
+};
+
+export type NxTableRowProps = HTMLAttributes<HTMLTableRowElement> & {
+  isHeader?: boolean | null;
+  isFilterHeader?: boolean | null;
+  isClickable?: boolean | null;
+};
+
+export const nxTableRowPropTypes: PropTypes.ValidationMap<NxTableRowProps> = {
+  isHeader: PropTypes.bool,
+  isFilterHeader: PropTypes.bool,
+  isClickable: PropTypes.bool,
   children: PropTypes.node
 };
