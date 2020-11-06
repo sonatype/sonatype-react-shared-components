2c62ecc7d5adfdaa098a4c59f266b8343bc6e815
Ross Pokorny <rpokorny@sonatype.com>
2020-11-06 13:48:36 -0500

    Consolidate types - RSC-293

--- lib/src/components/NxTabs/types.ts
+++ lib/src/components/NxTabs/types.ts
@@ -4,17 +4,38 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { HTMLAttributes, ReactNode } from 'react';
+import { ReactNode, HTMLAttributes, LiHTMLAttributes } from 'react';
 import PropTypes from 'prop-types';
 
-export type Props = HTMLAttributes<HTMLDivElement> & {
+export type NxTabsProps = HTMLAttributes<HTMLDivElement> & {
   activeTab?: number | null ;
   onTabSelect: ((index: number) => void);
   children?: ReactNode | null;
 };
 
-export const propTypes: PropTypes.ValidationMap<Props> = {
+export const nxTabsPropTypes: PropTypes.ValidationMap<NxTabsProps> = {
   activeTab: PropTypes.number,
   onTabSelect: PropTypes.func.isRequired,
   children: PropTypes.node
 };
+
+export type NxTabProps = LiHTMLAttributes<HTMLLIElement> & {
+  index?: number | null;
+};
+
+export const nxTabPropTypes: PropTypes.ValidationMap<NxTabProps> = {
+  index: PropTypes.number,
+  children: PropTypes.node
+};
+
+export type NxTabListProps = HTMLAttributes<HTMLUListElement>;
+
+export const nxTabListPropTypes: PropTypes.ValidationMap<NxTabListProps> = {
+  children: PropTypes.node
+};
+
+export type NxTabPanelProps = HTMLAttributes<HTMLDivElement>;
+
+export const nxTabPanelPropTypes = {
+  className: PropTypes.string
+} as PropTypes.ValidationMap<NxTabPanelProps>;
