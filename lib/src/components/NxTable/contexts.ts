20a90948f04a0b1903b25939db13d97e19fc29d3
Ross Pokorny <rpokorny@sonatype.com>
2020-11-07 10:49:19 -0500

    Better default context value - RSC-294

--- lib/src/components/NxTable/contexts.ts
+++ lib/src/components/NxTable/contexts.ts
@@ -6,5 +6,5 @@
  */
 import React from 'react';
 
-export const ColumnCountContext = React.createContext(0);
+export const ColumnCountContext = React.createContext(1);
 export const HeaderContext = React.createContext(false);
