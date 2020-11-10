b68c8610929a6c4ef75edceeae089ab4a4bda329
Ross Pokorny <rpokorny@sonatype.com>
2020-11-10 12:49:23 -0500

    Implement buttons in header - RSC-229

--- gallery/src/components/NxAccordion/NxAccordionExample.tsx
+++ gallery/src/components/NxAccordion/NxAccordionExample.tsx
@@ -5,14 +5,28 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React, { useState } from 'react';
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
-import { NxAccordion } from '@sonatype/react-shared-components';
+import { NxAccordion, NxFontAwesomeIcon, NxButton } from '@sonatype/react-shared-components';
 
 export default function NxAccordionExample() {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false),
+      header = (
+        <>
+          <span>Foo</span>
+          <div className="nx-btn-bar">
+            <NxButton variant="icon-only">
+              <NxFontAwesomeIcon icon={faCog} />
+            </NxButton>
+            <NxButton variant="icon-only">
+              <NxFontAwesomeIcon icon={faCog} />
+            </NxButton>
+          </div>
+        </>
+      );
 
   return (
-    <NxAccordion open={open} onToggle={setOpen} headerContent="Foo">
+    <NxAccordion open={open} onToggle={setOpen} headerContent={header}>
       <p>Foo Bar Baz</p>
     </NxAccordion>
   );
