82948f719bc8b91b0fca0dfa53db9011801f4444
Ross Pokorny <rpokorny@sonatype.com>
2020-11-19 16:55:48 -0500

    Update examples and docs

--- gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartMinimalExample.tsx
+++ gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartMinimalExample.tsx
@@ -10,12 +10,12 @@ import { NxBinaryDonutChart } from '@sonatype/react-shared-components';
 export default function NxBinaryDonutChartMinimalExample() {
   return (
     <>
-      <NxBinaryDonutChart percent={0} />
+      <NxBinaryDonutChart percent={0} aria-label="An empty donut chart" />
       <NxBinaryDonutChart percent={15} />
       <NxBinaryDonutChart percent={25} />
-      <NxBinaryDonutChart percent={50} />
+      <NxBinaryDonutChart percent={50} aria-label="A half-full donut chart" />
       <NxBinaryDonutChart percent={90} />
-      <NxBinaryDonutChart percent={100} />
+      <NxBinaryDonutChart percent={100} aria-label="A full donut chart" />
     </>
   );
 }
