43934acb596552b1c1a37612cd86f19fd505af9b
Ross Pokorny <rpokorny@sonatype.com>
2020-11-19 11:12:28 -0500

    Fix eslint

--- lib/src/util/idUtil.ts
+++ lib/src/util/idUtil.ts
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { join, map } from "ramda";
+import { join, map } from 'ramda';
 
 /**
  * Returns a string with the given prefix followed by 64 bits worth of randomness. Suitable for use
