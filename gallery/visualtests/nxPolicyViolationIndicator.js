c8192cf02881162f4760922c2aac66d76e5d8597
Glen Hunter <ghunter@sonatype.com>
2020-11-27 14:29:28 -0500

    Visual test RSC-339

--- gallery/visualtests/nxPolicyViolationIndicator.js
+++ gallery/visualtests/nxPolicyViolationIndicator.js
@@ -5,18 +5,18 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 const { Target } = require('@applitools/eyes-webdriverio');
-const { clickTest, focusTest, focusAndHoverTest, hoverTest, simpleTest } = require('./testUtils');
+const { simpleTest } = require('./testUtils');
 
 describe('NxPolicyViolationIndicator', function() {
   beforeEach(async function() {
     await browser.url('#/pages/NxPolicyViolationIndicator');
   });
 
-  const threatIndicatorsListExampleSelector = '#nx-threat-indicator-list-example .nx-list',
-      threatIndicatorsTableExampleSelector = '#nx-threat-indicator-table-example .nx-table',
-      threatIndicatorsSimpleExampleSelector = '#nx-threat-indicator-simple-example .gallery-example-live';
+  const policyViolationIndicatorCategoryExampleSelector =
+      '#nx-policy-violation-indicator-category-example .gallery-example-live',
+      policyViolationIndicatorNumberExampleSelector =
+        '#nx-policy-violation-indicator-number-example .gallery-example-live';
 
-  it('looks right', simpleTest(threatIndicatorsSimpleExampleSelector));
-  it('looks right in a list', simpleTest(threatIndicatorsListExampleSelector));
-  it('looks right in a table', simpleTest(threatIndicatorsTableExampleSelector));
+  it('looks right', simpleTest(policyViolationIndicatorCategoryExampleSelector));
+  it('looks right', simpleTest(policyViolationIndicatorNumberExampleSelector));
 });
