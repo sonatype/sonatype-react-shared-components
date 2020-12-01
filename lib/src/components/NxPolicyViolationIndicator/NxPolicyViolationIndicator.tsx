cc14aa0f2408a3148e653be8c45c748163c82150
Glen Hunter <ghunter@sonatype.com>
2020-12-01 11:18:14 -0500

    Custom classes, DIV attributes RSC-339

--- lib/src/components/NxPolicyViolationIndicator/NxPolicyViolationIndicator.tsx
+++ lib/src/components/NxPolicyViolationIndicator/NxPolicyViolationIndicator.tsx
@@ -5,6 +5,7 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React, { forwardRef } from 'react';
+import classnames from 'classnames';
 import { faExclamationCircle } from '@fortawesome/free-solid-svg-icons';
 
 import { ThreatLevelCategory, categoryByPolicyThreatLevel } from '../../util/threatLevels';
@@ -16,17 +17,17 @@ export { Props } from './types';
 import './NxPolicyViolationIndicator.scss';
 
 const NxPolicyViolationIndicator = forwardRef<HTMLDivElement, Props>(
-  function NxPolicyViolationIndicator({ threatLevelCategory, policyThreatLevel, children }, ref) {
+  function NxPolicyViolationIndicator({ threatLevelCategory, policyThreatLevel, className, children, ...attrs }, ref) {
     const category: ThreatLevelCategory =
           threatLevelCategory ? threatLevelCategory :
           policyThreatLevel != null ? categoryByPolicyThreatLevel[policyThreatLevel] : 
           'unspecified',
 
-        className = `nx-policy-violation-indicator nx-policy-violation-indicator--${category}`,
+        classNames = classnames('nx-policy-violation-indicator nx-policy-violation-indicator--${category}', className),
         label = `threat level ${category}`;
 
     return (
-      <div className={className} ref={ref}>
+      <div className={classNames} ref={ref} {...attrs}>
         <span className="nx-policy-violation-indicator__badge">
           <NxFontAwesomeIcon aria-hidden={false}
                              aria-label={label}
