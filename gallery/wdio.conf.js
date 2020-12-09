1a06d542cbfc71c460a5176a59448154d54b2195
Ross Pokorny <rpokorny@sonatype.com>
2020-12-09 14:44:56 -0500

    Attempt to fix tooltip tests that only fail on jenkins

--- gallery/wdio.conf.js
+++ gallery/wdio.conf.js
@@ -45,7 +45,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './visualtests/*.js'
+        './visualtests/nxOverflowTooltip.js'
     ],
     // Patterns to exclude.
     exclude: [
