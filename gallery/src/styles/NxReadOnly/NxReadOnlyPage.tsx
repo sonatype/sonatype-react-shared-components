431ea3f1f452d542d0ef1e4fed80e38f648ba79d
Glen Hunter <ghunter@sonatype.com>
2020-12-09 14:58:14 -0500

    DL styling RSC-396

--- gallery/src/styles/NxReadOnly/NxReadOnlyPage.tsx
+++ gallery/src/styles/NxReadOnly/NxReadOnlyPage.tsx
@@ -21,11 +21,45 @@ const NxReadOnlyPage = () => {
           <code className="nx-code">.nx-read-only</code> is layed out using a description list
           (<code className="nx-code">&lt;dl&gt;</code>) as the container.
         </p>
+        <table className="nx-table nx-table--gallery-props">
+          <thead>
+            <tr className="nx-table-row">
+              <th className="nx-cell nx-cell--header">Class</th>
+              <th className="nx-cell nx-cell--header">Location</th>
+              <th className="nx-cell nx-cell--header">Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="nx-table-row">
+              <td className="nx-cell"><code className="nx-code">.nx-read-only</code></td>
+              <td className="nx-cell">Top level</td>
+              <td className="nx-cell">
+                This is the basic wrapper class. It is applied to the <code className="nx-code">&lt;dl&gt;</code>.
+              </td>
+            </tr>
+            <tr className="nx-table-row">
+              <td className="nx-cell"><code className="nx-code">.nx-read-only__label</code></td>
+              <td className="nx-cell">Element</td>
+              <td className="nx-cell">
+                This class is applied to the <code className="nx-code">&lt;dt&gt;</code>, it represents the label for
+                the data below.
+              </td>
+            </tr>
+            <tr className="nx-table-row">
+              <td className="nx-cell"><code className="nx-code">.nx-read-only__data</code></td>
+              <td className="nx-cell">Element</td>
+              <td className="nx-cell">
+                This class is applied to any <code className="nx-code">&lt;dd&gt;</code>'s that appear. The data
+                displayed can be any string or HTML markup.
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </GalleryDescriptionTile>
       <GalleryExampleTile title="Styling read-only lists of data"
                           codeExamples={nxReadOnlyDlExampleCode}
                           htmlExample={nxReadOnlyDlExampleCode}>
-        <code className="nx-code">&lt;dl&gt;</code> layout.
+        Standard <code className="nx-code">nx-read-only</code> layout, by default all text wraps.
       </GalleryExampleTile>
     </>
   );
