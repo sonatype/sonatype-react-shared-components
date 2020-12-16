ebc6e803a5c3c2bfa6280f36063c2c258eaadcf0
Glen Hunter <ghunter@sonatype.com>
2020-12-16 14:08:07 -0500

    Improve stateless example RSC-406

--- gallery/src/components/NxToggle/NxToggleExample.tsx
+++ gallery/src/components/NxToggle/NxToggleExample.tsx
@@ -11,41 +11,46 @@ import { NxToggle } from '@sonatype/react-shared-components';
 function NxToggleExample() {
   // this example uses the `useState` hook for succinctness, but you could also manage the state manually
   // in a class component
-  const [isRed, setIsRed] = useState(false),
-      [isBlue, setIsBlue] = useState(false),
-      [isGreen, setIsGreen] = useState(false),
+  const [isWarpOn, setIsWarpOn] = useState(false),
+      [isKrakenOut, setIsKrakenOut] = useState(false),
+      [isShapes, setIsShapes] = useState(false),
       [isDisabled, setIsDisabled] = useState(false),
-      toggleRed = () => setIsRed(!isRed),
-      toggleBlue = () => setIsBlue(!isBlue),
-      toggleGreen = () => setIsGreen(!isGreen),
+      toggleWarp = () => setIsWarpOn(!isWarpOn),
+      toggleKraken = () => setIsKrakenOut(!isKrakenOut),
+      toggleShapes = () => setIsShapes(!isShapes),
       toggleDisabled = () => setIsDisabled(!isDisabled);
 
   return (
-    <fieldset className="nx-fieldset">
-      <legend className="nx-legend">
-        <span className="nx-legend__text">
-          Selected colours: {isRed && 'Red'} {isBlue && 'Blue'} {isGreen && 'Green'}
-        </span>
-      </legend>
-      <NxToggle toggleId="subscribe-check" onChange={toggleRed} isChecked={isRed}>
-        Red
-      </NxToggle>
-      <NxToggle toggleId="no-label-check" onChange={toggleBlue} isChecked={isBlue}>Blue</NxToggle>
-      <NxToggle toggleId="children-check" onChange={toggleGreen} isChecked={isGreen}>
-        <svg width="12px" height="12px" viewBox="-1 -1 2 2">
-          <circle r="1"/>
-        </svg>
-        {' '}
-        Green - A circle, a perfectly round SVG circle, pleasing to the eye, not too big and not too small, just right
-        to appear beside a checkbox and demonstrate that the label wraps
-      </NxToggle>
-      <NxToggle toggleId="disabled-check"
-                disabled={true}
-                onChange={toggleDisabled}
-                isChecked={isDisabled}>
-        This toggle is disabled
-      </NxToggle>
-    </fieldset>
+    <>
+      <h3 className="nx-h3">
+        Here's what you can do:
+      </h3>
+      <div className="nx-form-group">
+        <NxToggle toggleId="subscribe-check" onChange={toggleWarp} isChecked={isWarpOn}>
+          Enable Warp Drive
+        </NxToggle>
+        <NxToggle toggleId="no-label-check" onChange={toggleKraken} isChecked={isKrakenOut}>
+          Release the Kraken!
+        </NxToggle>
+        <NxToggle toggleId="children-check" onChange={toggleShapes} isChecked={isShapes}>
+          <svg width="12px" height="12px" viewBox="-1 -1 2 2">
+            <circle r="1"/>
+          </svg>
+          {' '}
+          Allow shapes - like a circle, a perfectly round SVG circle, pleasing to the eye, not too big and not too
+          small, just right to appear beside a checkbox and demonstrate that the label wraps
+        </NxToggle>
+        <NxToggle toggleId="disabled-check"
+                  disabled={true}
+                  onChange={toggleDisabled}
+                  isChecked={isDisabled}>
+          This toggle is disabled
+        </NxToggle>
+      </div>
+      <p>
+        {isWarpOn && 'Warp drive started'} {isKrakenOut && ' The Kraken is out!'} {isShapes && ' Shapes are allowed'}
+      </p>
+    </>
   );
 }
 
