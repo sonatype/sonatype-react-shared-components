c5aaa128a11565cfd50fb9fcb16777f3d0868fde
Glen Hunter <ghunter@sonatype.com>
2020-12-16 14:17:33 -0500

    Remove prop comments from component tsx files RSC-406

--- lib/src/components/NxToggle/NxToggle.tsx
+++ lib/src/components/NxToggle/NxToggle.tsx
@@ -11,16 +11,6 @@ import './NxToggle.scss';
 import { Props, propTypes } from './types';
 export { Props } from './types';
 
-/**
- * A stateless component for rendering a toggle control with a label
- * @param props.toggleId An id to identify the toggle
- * @param props.onChange A callback for when the toggle control is toggled
- * @param props.isChecked Whether the toggle should be rendered as on/checked or off/unchecked
- * @param props.disabled Whether the toggle should be rendered as disabled or not. When disabled, the onChange
- * callback will not fire
- * @param props.children VDOM rendered as label. Should be
- * [phrasing content](https://www.w3.org/TR/2011/WD-html-markup-20110525/terminology.html#phrasing-content).
- */
 const NxToggle = forwardRef<HTMLLabelElement, Props>(
     function NxToggle({ className, onChange, isChecked, disabled, toggleId, children, ...otherProps }, ref) {
       const labelClasses = classnames('nx-toggle', className, {
--- lib/src/components/NxToggle/stateful/NxStatefulToggle.tsx
+++ lib/src/components/NxToggle/stateful/NxStatefulToggle.tsx
@@ -10,15 +10,6 @@ import NxToggle from '../NxToggle';
 import { Props, propTypes } from './types';
 export { Props } from './types';
 
-/**
- * A stateful component for rendering a toggle control with a label
- * @param props.onChange A callback for when the toggle control is toggled, receiving the new value as an argument
- * @param props.defaultChecked Whether the toggle should start off on/checked or off/unchecked
- * @param props.disabled Whether the toggle control should be rendered as disabled or not.  When disabled, the onChange
- * callback will not fire
- * @param props.children VDOM rendered as label. Should be
- * [phrasing content](https://www.w3.org/TR/2011/WD-html-markup-20110525/terminology.html#phrasing-content).
- */
 const NxStatefulToggle = forwardRef<HTMLLabelElement, Props>(
     function NxStatefulToggle({ defaultChecked, onChange, ...otherProps }, ref) {
       const [isChecked, setIsChecked] = useState(defaultChecked);
