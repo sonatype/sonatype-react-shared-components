526ce860e3098d8d9bfd100967a296a7694b1120
Ross Pokorny <rpokorny@sonatype.com>
2020-12-22 16:36:38 -0500

    Undo extraneous change - RSC-410

--- lib/src/components/NxCheckbox/NxCheckbox.tsx
+++ lib/src/components/NxCheckbox/NxCheckbox.tsx
@@ -45,7 +45,7 @@ const NxCheckbox = forwardRef<HTMLLabelElement, Props>(
             {/* Put a space in the box if not checked,
               * in order to provide a consistent vertical-align baseline
               */}
-            { isChecked ? <FontAwesomeIcon icon={faCheck} /> : ' ' }
+            { isChecked ? <FontAwesomeIcon icon={faCheck} /> : '\u00A0' }
           </span>
           { children &&
             <NxOverflowTooltip>
