06bf1e7e5d3b46293310de399b477fd61dad9f45
Ross Pokorny <rpokorny@sonatype.com>
2021-01-08 12:23:19 -0500

    Fix test eslint issues

--- lib/src/__testutils__/jest-enzyme-overrides.d.ts
+++ lib/src/__testutils__/jest-enzyme-overrides.d.ts
@@ -4,13 +4,15 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import React from 'react';
 /// <reference types="react" />
+/// <reference types="jest-enzyme" />
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 type Selector = string | React.ComponentType<any>;
 
 declare namespace jest {
-  interface Matchers<R> {
+  interface Matchers<R, T> {
     // the type defs that ship with jest-enzyme say that these methods only take strings, but they can actually take
     // React Components as well
     toMatchSelector(s: Selector): void;
