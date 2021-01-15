e10c7a979e424502e484188bcbafc823c8d19fc9
Glen Hunter <ghunter@sonatype.com>
2021-01-15 07:59:42 -0500

    Page and example RSC-361

--- gallery/src/components/NxTag/NxTagExample.tsx
+++ gallery/src/components/NxTag/NxTagExample.tsx
@@ -4,28 +4,11 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { useState } from 'react';
-import { faEye } from '@fortawesome/free-solid-svg-icons';
-import { NxAlert } from '@sonatype/react-shared-components';
+import React from 'react';
+import { NxTag } from '@sonatype/react-shared-components';
 
-function NxAlertExample() {
-  const [isOpen, setIsOpen] = useState(true);
-
-  function dismiss() {
-    setIsOpen(false);
-  }
-
-  return isOpen ? (
-    <NxAlert icon={faEye}
-             iconLabel="Observe"
-             id="this-id-ends-up-on-the-div"
-             className="nx-alert--modifier"
-             onClose={dismiss}>
-      This is an example alert message.
-      It is expected that users create their own modifier classes to alter the styles of this component.
-      This alert has long text content in order to demonstrate text wrapping.
-    </NxAlert>
-  ) : null;
+function NxTagExample() {
+  <NxTag>Leafspring</NxTag>;
 }
 
-export default NxAlertExample;
+export default NxTagExample;
