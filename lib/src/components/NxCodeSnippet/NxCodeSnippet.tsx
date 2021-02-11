dc7fb5a0193a5a90b046fa7c0bdeff64e1ba1e2b
Ross Pokorny <rpokorny@sonatype.com>
2021-02-11 15:36:22 -0500

    Switch to document.execCommand for non-secure page support - RSC-402

--- lib/src/components/NxCodeSnippet/NxCodeSnippet.tsx
+++ lib/src/components/NxCodeSnippet/NxCodeSnippet.tsx
@@ -21,22 +21,18 @@ export default function NxCodeSnippet({ content, label, sublabel, className, onC
       textInputRef = useRef<HTMLDivElement>(null);
 
   function copyToClipboard() {
-    window.navigator.clipboard.writeText(content).then(function() {
-      // select all text in the input to help the user understand what happened
-      if (textInputRef.current) {
-        const textarea = textInputRef.current.querySelector('textarea');
+    if (textInputRef.current) {
+      const textarea = textInputRef.current.querySelector('textarea');
 
-        if (textarea) {
-          textarea.select();
+      if (textarea) {
+        textarea.select();
+        document.execCommand('copy');
+
+        if (onCopyUsingBtn) {
+          onCopyUsingBtn();
         }
       }
-
-      if (onCopyUsingBtn) {
-        onCopyUsingBtn();
-      }
-    }, function(e) {
-      console.error('Error copying to clipboard', e);
-    });
+    }
   }
 
   return (
