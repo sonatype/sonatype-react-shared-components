39d0d7ce2c7378a72ae51acc5a25b4022011bc30
Ross Pokorny <rpokorny@sonatype.com>
2021-02-12 11:02:51 -0500

    Revert "Update tests - RSC-402"
    This reverts commit 1919c37e2c0937af07f8878a6595476ce78e2e1f.

--- lib/src/components/NxCodeSnippet/__tests__/NxCodeSnippet.test.tsx
+++ lib/src/components/NxCodeSnippet/__tests__/NxCodeSnippet.test.tsx
@@ -68,16 +68,25 @@ describe('NxCodeSnippet', function() {
   });
 
   describe('when the button is clicked', function() {
-    let container: HTMLElement | null = null;
-
-    function getElementSelection(element: HTMLTextAreaElement | null) {
-      return element && element.value.slice(element.selectionStart, element.selectionEnd);
-    }
+    let container: HTMLElement | null = null,
+        resolveClipboardPromise: Function | null,
+        rejectClipboardPromise: Function | null;
 
     beforeEach(function() {
       container = document.createElement('div');
       document.body.appendChild(container);
 
+      Object.defineProperty(window.navigator, 'clipboard', {
+        value: {
+          writeText: jest.fn(() => {
+            return new Promise((resolve, reject) => {
+              resolveClipboardPromise = resolve;
+              rejectClipboardPromise = reject;
+            });
+          })
+        },
+        configurable: true
+      });
     });
 
     afterEach(function() {
@@ -86,39 +95,73 @@ describe('NxCodeSnippet', function() {
       }
     });
 
+    function getElementSelection(element: HTMLTextAreaElement) {
+      return element.value.slice(element.selectionStart, element.selectionEnd);
+    }
+
     it('copies the text to the clipboard', function() {
-      Object.defineProperty(document, 'execCommand', {
-        value: jest.fn().mockImplementation(function() {
-          expect(getElementSelection(document.querySelector('textarea'))).toBe('Lorem Ipsum');
-        }),
-        configurable: true
-      });
+      jest.spyOn(window.navigator.clipboard, 'writeText');
 
       const component = getMounted({}, { attachTo: container });
 
-      expect(document.execCommand).not.toHaveBeenCalled();
+      expect(window.navigator.clipboard.writeText).not.toHaveBeenCalled();
 
       component.find(NxButton).simulate('click');
 
-      expect(document.execCommand).toHaveBeenCalledWith('copy');
+      expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith('Lorem Ipsum');
     });
 
-    it('calls onCopyUsingBtn after writing the text to the clipboard', function() {
-      const onCopyUsingBtn = jest.fn();
-
-      Object.defineProperty(document, 'execCommand', {
-        value: jest.fn().mockImplementation(function() {
-          // shouldn't be called until after this
-          expect(onCopyUsingBtn).not.toHaveBeenCalled();
-        }),
-        configurable: true
-      });
-
-      const component = getMounted({ onCopyUsingBtn }, { attachTo: container });
+    it('calls onCopyUsingBtn after writing the text to the clipboard', function(done) {
+      const onCopyUsingBtn = jest.fn(),
+          component = getMounted({ onCopyUsingBtn }, { attachTo: container });
 
       component.find(NxButton).simulate('click');
 
-      expect(onCopyUsingBtn).toHaveBeenCalled();
+      expect(onCopyUsingBtn).not.toHaveBeenCalled();
+
+      resolveClipboardPromise!();
+
+      // the promise then() is called asynchronously so we must do our expectation of its result asynchronously
+      // as well
+      setTimeout(function() {
+        expect(onCopyUsingBtn).toHaveBeenCalled();
+        done();
+      }, 0);
+    });
+
+    it('sets the text selection to the textarea\'s contents after writing the text to the clipboard', function(done) {
+      const component = getMounted({}, { attachTo: container }),
+          textarea = component.find('textarea').getDOMNode() as HTMLTextAreaElement;
+
+      expect(getElementSelection(textarea)).toBe('');
+
+      component.find(NxButton).simulate('click');
+
+      expect(getElementSelection(textarea)).toBe('');
+
+      resolveClipboardPromise!();
+
+      setTimeout(function() {
+        expect(getElementSelection(textarea)).toBe('Lorem Ipsum');
+        done();
+      }, 0);
+    });
+
+    it('does not call onCopyUsingBtn or set the text selection if the copy fails', function(done) {
+      const onCopyUsingBtn = jest.fn(),
+          component = getMounted({ onCopyUsingBtn }, { attachTo: container }),
+          textarea = component.find('textarea').getDOMNode() as HTMLTextAreaElement;
+
+      component.find(NxButton).simulate('click');
+
+      rejectClipboardPromise!();
+
+      setTimeout(function() {
+        expect(getElementSelection(textarea)).toBe('');
+
+        expect(onCopyUsingBtn).not.toHaveBeenCalled();
+        done();
+      }, 0);
     });
   });
 });
