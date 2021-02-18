48c07afb175d11e257eb251e1f102a39839f631b
Andres Ossa <aossa@sonatype.com>
2021-02-18 16:19:44 -0500

    Properly define propTypes - RSC-485

--- lib/src/components/NxSidebarNavigation/types.ts
+++ lib/src/components/NxSidebarNavigation/types.ts
@@ -5,17 +5,47 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import * as PropTypes from 'prop-types';
-import { HTMLAttributes, WeakValidationMap } from 'react';
+import { WeakValidationMap, ReactNode } from 'react';
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
 
-export type Props = HTMLAttributes<HTMLDivElement> & {
-  isOpen?: boolean;
-  helpLink?: string;
-  onToggleOpen: (() => void);
+export interface SidebarNavLinkProps {
+  name: string;
+  href: string;
+  icon: IconProp;
+  current?: boolean | null;
+}
+
+export interface Props {
+  isOpen: boolean;
+  className?: string | null;
+  onToggleClick: (() => void);
+  logo: ReactNode;
+  logoText: string;
+  logoLink: string;
+  links: SidebarNavLinkProps[];
+  helpLink?: string | null;
+  helpText?: string | null;
+  collapsedReleaseText?: string | null;
+  expandedReleaseText?: string | null;
+  attributions?: string[] | null;
 };
 
 export const propTypes: WeakValidationMap<Props> = {
-  isOpen: PropTypes.bool,
+  isOpen: PropTypes.bool.isRequired,
   className: PropTypes.string,
+  onToggleClick: PropTypes.func.isRequired,
+  logo: PropTypes.node.isRequired,
+  logoText: PropTypes.string.isRequired,
+  logoLink: PropTypes.string,
+  links: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    href: PropTypes.string.isRequired,
+    icon: PropTypes.object.isRequired,
+    current: PropTypes.bool
+  })),
   helpLink: PropTypes.string,
-  onToggleOpen: PropTypes.func.isRequired
+  helpText: PropTypes.string,
+  collapsedReleaseText: PropTypes.string,
+  expandedReleaseText: PropTypes.string,
+  attributions: PropTypes.arrayOf(PropTypes.string)
 };
