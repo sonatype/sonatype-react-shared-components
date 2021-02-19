a1b9a33eba156b0832023802316f7179dae14dbe
Andres Ossa <aossa@sonatype.com>
2021-02-19 11:48:02 -0500

    Define props for stateful component - RSC-485

--- lib/src/components/NxSidebarNavigation/stateful/types.ts
+++ lib/src/components/NxSidebarNavigation/stateful/types.ts
@@ -5,15 +5,36 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import * as PropTypes from 'prop-types';
-import { HTMLAttributes, WeakValidationMap } from 'react';
+import { WeakValidationMap, ReactNode } from 'react';
 
-export type Props = HTMLAttributes<HTMLDivElement> & {
-  isDefaultOpen?: boolean;
-  helpLink?: string;
-};
+import { SidebarNavLinkProps, sidebarNavLinkPropTypes } from '../types';
+
+export interface Props {
+  isDefaultOpen: boolean;
+  className?: string | null;
+  onToggleClick: ((newToggleState: boolean) => void);
+  logo: ReactNode;
+  logoText: string;
+  logoLink: string;
+  links: SidebarNavLinkProps[];
+  helpLink?: string | null;
+  helpText?: string | null;
+  collapsedReleaseText?: string | null;
+  expandedReleaseText?: string | null;
+  attributions?: string[] | null;
+}
 
 export const propTypes: WeakValidationMap<Props> = {
+  isDefaultOpen: PropTypes.bool.isRequired,
   className: PropTypes.string,
-  isDefaultOpen: PropTypes.bool,
-  helpLink: PropTypes.string
+  onToggleClick: PropTypes.func.isRequired,
+  logo: PropTypes.node.isRequired,
+  logoText: PropTypes.string.isRequired,
+  logoLink: PropTypes.string,
+  links: PropTypes.arrayOf(sidebarNavLinkPropTypes),
+  helpLink: PropTypes.string,
+  helpText: PropTypes.string,
+  collapsedReleaseText: PropTypes.string,
+  expandedReleaseText: PropTypes.string,
+  attributions: PropTypes.arrayOf(PropTypes.string)
 };
