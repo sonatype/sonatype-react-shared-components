acb5d534b062a74f74cd10fee65f2005650a09c8
Andres Ossa <aossa@sonatype.com>
2021-03-02 09:51:38 -0500

    Lint - RSC-485

--- lib/src/components/NxNavigationSidebar/NxNavigationSidebar.tsx
+++ lib/src/components/NxNavigationSidebar/NxNavigationSidebar.tsx
@@ -5,7 +5,7 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React, { FunctionComponent } from 'react';
-lib/src/components/NxNavigationSidebar/NxNavigationSidebar.tsx
+import classnames from 'classnames';
 
 import NxButton from '../NxButton/NxButton';
 import NxFontAwesomeIcon from '../NxFontAwesomeIcon/NxFontAwesomeIcon';
