5812b377c330f9377bd19a0c04752d6b9bacaecf
Andres Ossa <aossa@sonatype.com>
2021-03-16 14:02:29 -0500

    Remove unused max-width - RSC-485

---
+++ lib/src/components/NxNavigationSidebar/stateful/__tests__/NxStatefulNavigationSidebar.test.tsx
@@ -0,0 +1,27 @@
+// /*
+//  * Copyright (c) 2019-present Sonatype, Inc.
+//  * This program and the accompanying materials are made available under
+//  * the terms of the Eclipse Public License 2.0 which accompanies this
+//  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+//  */
+
+// describe('NxStatefulDropdown', () => {
+//   let container: HTMLDivElement | null;
+
+//   beforeEach(function() {
+//     // Avoid rendering directly on the body.
+//     container = document.createElement('div');
+//     document.body.appendChild(container);
+//   });
+
+//   afterEach(function() {
+//     if (container) {
+//       document.body.removeChild(container);
+//       container = null;
+//     }
+//   });
+
+//   it('renders an NxDropdown component', function() {
+
+//   });
+// });
