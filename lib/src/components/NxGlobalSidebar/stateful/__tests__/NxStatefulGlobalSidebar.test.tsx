cebeb4e5ceb869b4bedd5516b81eeb97e167271e
Andres Ossa <aossa@sonatype.com>
2021-03-16 17:16:15 -0500

    Stateful test - RSC-485

--- lib/src/components/NxGlobalSidebar/stateful/__tests__/NxStatefulGlobalSidebar.test.tsx
+++ lib/src/components/NxGlobalSidebar/stateful/__tests__/NxStatefulGlobalSidebar.test.tsx
@@ -1,27 +1,46 @@
-// /*
-//  * Copyright (c) 2019-present Sonatype, Inc.
-//  * This program and the accompanying materials are made available under
-//  * the terms of the Eclipse Public License 2.0 which accompanies this
-//  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
-//  */
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import React from 'react';
+import { faCrow, faBiohazard } from '@fortawesome/free-solid-svg-icons';
 
-// describe('NxStatefulDropdown', () => {
-//   let container: HTMLDivElement | null;
+import * as enzymeUtils from '../../../../__testutils__/enzymeUtils';
+import NxStatefulGlobalSidebar, { Props } from '../NxStatefulGlobalSidebar';
+import NxGlobalSidebar from '../../NxGlobalSidebar';
 
-//   beforeEach(function() {
-//     // Avoid rendering directly on the body.
-//     container = document.createElement('div');
-//     document.body.appendChild(container);
-//   });
+describe('NxStatefulGlobalSidebar', () => {
+  const minimalProps = {
+        isDefaultOpen: false,
+        children: <p className="child-1">I am children</p>,
+        toggleCloseIcon: faBiohazard,
+        toggleOpenIcon: faCrow,
+        logoImg: 'logoImg',
+        logoAltText: 'alt text',
+        logoLink: '/some-link'
+      },
+      getShallowComponent = enzymeUtils.getShallowComponent<Props>(NxStatefulGlobalSidebar, minimalProps);
 
-//   afterEach(function() {
-//     if (container) {
-//       document.body.removeChild(container);
-//       container = null;
-//     }
-//   });
+  it('renders an NxGlobalSidebar component with passed in props', function() {
+    const component = getShallowComponent();
+    expect(component).toMatchSelector(NxGlobalSidebar);
+    expect(component).toHaveProp('isOpen', false);
+    expect(component).toHaveProp('onToggleClick');
+    expect(component).toHaveProp('toggleOpenIcon', faCrow);
+    expect(component).toHaveProp('toggleCloseIcon', faBiohazard);
+    expect(component).toHaveProp('logoImg', 'logoImg');
+    expect(component).toHaveProp('logoAltText', 'alt text');
+    expect(component).toHaveProp('logoLink', '/some-link');
+    expect(component).toHaveProp('children', minimalProps.children);
+  });
 
-//   it('renders an NxDropdown component', function() {
+  it('changes isOpen when the component is toggled', function() {
+    const component = getShallowComponent();
+    expect(component).toHaveProp('isOpen', false);
 
-//   });
-// });
+    component.simulate('toggleClick');
+    expect(component).toHaveProp('isOpen', true);
+  });
+});
