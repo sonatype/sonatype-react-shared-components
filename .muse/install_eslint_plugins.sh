872a71a4c9e1ee0f520bcb800a8217e50690038e
Troy Neeriemer <tneeriemer@sonatype.com>
2021-03-17 11:12:57 -0500

    Back to top-level check, install more stuff.

--- .muse/install_eslint_plugins.sh
+++ .muse/install_eslint_plugins.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-npm install @typescript-eslint/eslint-plugin@4.18.0 --save-dev
+npm install eslint@7.17.0 --save-dev
+npm install @typescript-eslint/eslint-parser@4.12.0 --save-dev
+npm install @typescript-eslint/eslint-plugin@4.12.0 --save-dev
+npm install eslint-plugin-react@7.22.0 --save-dev
+
