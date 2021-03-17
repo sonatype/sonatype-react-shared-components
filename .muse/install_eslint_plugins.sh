e2ef8c3dbe85b74fd9a610eaf4799559b111e4b5
Troy Neeriemer <tneeriemer@sonatype.com>
2021-03-17 11:16:31 -0500

    Fix package name.

--- .muse/install_eslint_plugins.sh
+++ .muse/install_eslint_plugins.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 npm install eslint@7.17.0 --save-dev
-npm install @typescript-eslint/eslint-parser@4.12.0 --save-dev
+npm install @typescript-eslint/parser@4.12.0 --save-dev
 npm install @typescript-eslint/eslint-plugin@4.12.0 --save-dev
 npm install eslint-plugin-react@7.22.0 --save-dev
 
