5c27f34a436a829d982bc53418e260e86e03c8b7
Andres Ossa <aossa@sonatype.com>
2021-03-17 16:11:22 -0500

    Restore original gallery page - RSC-485

--- gallery/src/Application.tsx
+++ gallery/src/Application.tsx
@@ -4,91 +4,92 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React from 'react';
-import {
-  faArrowLeft,
-  faBiohazard,
-  faCrow
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  NxStatefulGlobalSidebar,
-  NxGlobalSidebarNavigation,
-  NxGlobalSidebarNavigationLink,
-  NxTreeView,
-  NxTreeViewChild,
-  useToggle
-} from '@sonatype/react-shared-components';
+import React, { useEffect } from 'react';
+import { RouteChildrenProps } from 'react-router';
+import { HashRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { mergeAll, values } from 'ramda';
 
-const logo = require('../src/resources/logos/lifecycle.svg');
+// polyfill Array.prototype.includes which is used in query-string
+import 'core-js/features/array/includes';
+import queryString from 'query-string';
+
+import { PageMapping } from './pageConfigTypes';
+import pageConfig from './pageConfig';
+import PageHeader from './PageHeader';
+import GalleryNav from './GalleryNav';
+import Home from './pages/Home';
+import handleQueryParams from './handleQueryParams';
+
+import NxLoadWrapperPageLevelExample from './components/NxLoadWrapper/NxLoadWrapperPageLevelExample';
+import NxViewportSizedExample from './styles/NxViewportSized/NxViewportSizedExample';
+import NxViewportSizedExpandingExample
+  from './styles/NxViewportSized/NxViewportSizedExpandingExample';
+import SectionScrollingWrapper from './styles/NxViewportSized/SectionScrollingWrapper';
+
+const pageMappings: PageMapping = mergeAll(values(pageConfig));
+
+function Page({ match, location }: RouteChildrenProps<{ pageName: string }>) {
+  const pageName = match ? match.params.pageName : null,
+      pageHeader = pageName || 'Home',
+      Content = pageName ? pageMappings[pageName] : Home;
+
+  useEffect(function() {
+    handleQueryParams(queryString.parse(location.search));
+  }, [location.search]);
+
+  if (Content) {
+    // Put a key on <main> so that it re-renders entirely on route change, resetting scroll position
+    return (
+      <main key={pageName || 'home'} className="nx-page-main">
+        <div className="nx-page-title">
+          <h1 className="nx-h1">
+            {pageHeader}
+          </h1>
+        </div>
+        <Content/>
+      </main>
+    );
+  }
+  else {
+    // unknown page
+    return <Redirect to="/" />;
+  }
+}
 
 function Application() {
-  const [toggleCheck, onToggleCollapse] = useToggle(false);
-  const [toggleCheck2, onToggleCollapse2] = useToggle(false);
-
-  const longText = 'Est sint sunt ea laborum minim officia excepteur nostrud deserunt proident officia.';
-
   return (
-    <div id="temporary-navigation-sidebar-example" className="nx-page-content nx-page-content--full-width">
-      <NxStatefulGlobalSidebar isDefaultOpen={true}
-                               toggleOpenIcon={faArrowLeft}
-                               toggleCloseIcon={faCrow}
-                               logoAltText="product name"
-                               logoLink="/home"
-                               logoImg={logo}>
-        <NxGlobalSidebarNavigation>
-          <NxGlobalSidebarNavigationLink isSelected={true}
-                                         href="#alpha"
-                                         icon={faCrow}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#beta"
-                                         icon={faBiohazard}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#gamma"
-                                         icon={faCrow}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#delta"
-                                         icon={faBiohazard}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#epsilon"
-                                         icon={faCrow}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#zeta"
-                                         icon={faBiohazard}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#etha"
-                                         icon={faCrow}
-                                         text="Nav Item" />
-          <NxGlobalSidebarNavigationLink href="#theta"
-                                         icon={faBiohazard}
-                                         text={longText} />
-        </NxGlobalSidebarNavigation>
-        <section className="custom-tree-content nx-global-sidebar__expanded-content">
-          <NxTreeView onToggleCollapse={onToggleCollapse}
-                      isOpen={toggleCheck}
-                      triggerContent="TreeView Content">
-            <NxTreeViewChild>
-              <NxTreeView onToggleCollapse={onToggleCollapse2}
-                          isOpen={toggleCheck2}
-                          triggerContent="Tree View Child">
-                <NxTreeViewChild>Tree View Item Nested</NxTreeViewChild>
-                <NxTreeViewChild>Tree View Item Nested</NxTreeViewChild>
-              </NxTreeView>
-            </NxTreeViewChild>
-            <NxTreeViewChild>Tree View Item</NxTreeViewChild>
-            <NxTreeViewChild>Tree View Item</NxTreeViewChild>
-            <NxTreeViewChild>Tree View Item</NxTreeViewChild>
-          </NxTreeView>
-        </section>
-      </NxStatefulGlobalSidebar>
-      <main className="nx-page-main">
-        <p>
-          Cupidatat pariatur mollit sit pariatur mollit qui.{' '}
-          Est sint sunt ea laborum minim officia excepteur nostrud deserunt proident officia.{' '}
-          Ipsum nostrud do ipsum nisi occaecat reprehenderit aliquip in occaecat culpa consequat pariatur et.{' '}
-          Non adipisicing qui officia nisi do aliquip.
-        </p>
-      </main>
-    </div>
+    <Router>
+      <PageHeader />
+      <div className="nx-page-content">
+        <Switch>
+          <Route exact path="/PageLevelAlertExample">
+            <NxLoadWrapperPageLevelExample/>
+          </Route>
+          <Route>
+            <aside className="nx-page-sidebar" id="gallery-sidebar">
+              <GalleryNav />
+            </aside>
+            <Switch>
+              <Route path="/pages/:pageName" component={Page} />
+              <Route exact path="/" component={Page} />
+
+              {/* Special cases, these examples need their own page separate from their documentation */}
+              <Route exact path="/NxViewportSizedExample">
+                <SectionScrollingWrapper>
+                  <NxViewportSizedExample />
+                </SectionScrollingWrapper>
+              </Route>
+              <Route exact path="/NxViewportSizedExpandingExample">
+                <SectionScrollingWrapper>
+                  <NxViewportSizedExpandingExample />
+                </SectionScrollingWrapper>
+              </Route>
+              <Redirect to="/" />
+            </Switch>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
