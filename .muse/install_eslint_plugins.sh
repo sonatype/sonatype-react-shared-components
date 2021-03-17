48648ab1a984f34bffa30232bd963f6aaf8fd15b
Troy Neeriemer <tneeriemer@sonatype.com>
2021-03-17 11:26:59 -0500

    Install stuff per sub-project...

--- .muse/install_eslint_plugins.sh
+++ .muse/install_eslint_plugins.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
-npm install eslint@7.17.0 --save-dev
+cd gallery
+npm install eslint@7.22.0 --save-dev
 npm install @typescript-eslint/parser@4.12.0 --save-dev
 npm install @typescript-eslint/eslint-plugin@4.12.0 --save-dev
 npm install eslint-plugin-react@7.22.0 --save-dev
-
+cd ../lib
+npm install eslint@7.22.0 --save-dev
+npm install @typescript-eslint/parser@4.12.0 --save-dev
+npm install @typescript-eslint/eslint-plugin@4.12.0 --save-dev
+npm install eslint-plugin-react@7.22.0 --save-dev
