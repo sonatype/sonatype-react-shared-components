d8fff3cb85546ff5cbb18b752a6a169d1d1509df
Ross Pokorny <rpokorny@sonatype.com>
2021-03-19 09:11:01 -0400

    Comment improvements and removing debugging - RSC-494

--- lib/src/util/useDropdownEvents.ts
+++ lib/src/util/useDropdownEvents.ts
@@ -10,8 +10,8 @@ import { KeyboardEventHandler, useEffect, useRef } from 'react';
  * Dropdown global event handling, abstracted into a hook for use in various dropdown-like
  * components (e.g. NxDropdown and NxSegmentedButton)
  * @param closeDropdown - A function that, when called, sets whatever state is necessary to close the dropdown
- * @param onToggleCollapse - The onToggleCollapse function that was provided from outside of the component
- * @param onKeyDown - Any onKeyDown function that might have been provided from outside of the component
+ * @param externalOnToggleCollapse - The onToggleCollapse function that was provided from outside of the component
+ * @param externalOnKeyDown - Any onKeyDown function that might have been provided from outside of the component
  * @return Object containing the following:
  * onKeyDown function to set on the dropdown to handle ESC keypresses within it
  * onToggleCollapse function to set as the click handler on the dropdown toggle
@@ -22,14 +22,6 @@ export default function useDropdownEvents(
   externalOnKeyDown?: KeyboardEventHandler
 ) {
   const isToggling = useRef(false);
-
-  useEffect(function() {
-    console.log('mounting');
-
-    return function() {
-      console.log('unmounting');
-    };
-  }, []);
 
   const onKeyDown: KeyboardEventHandler = event => {
     if (event.key === 'Escape') {
