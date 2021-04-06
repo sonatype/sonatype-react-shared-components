ee516df5ef2ccaff199b3c2cb9b519c3fa563fc8
Glen Hunter <ghunter@sonatype.com>
2021-04-06 16:16:33 -0400

    Type fixes RSC-482

--- lib/src/components/NxThreatCounter/types.ts
+++ lib/src/components/NxThreatCounter/types.ts
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { HTMLAttributes, WeakValidationMap } from 'react';
+import { HTMLAttributes, ValidationMap } from 'react';
 import * as PropTypes from 'prop-types';
 
 export interface Props extends Omit<HTMLAttributes<HTMLDivElement>, 'className' | 'children'> {
@@ -12,11 +12,11 @@ export interface Props extends Omit<HTMLAttributes<HTMLDivElement>, 'className' 
   severeCount: number;
   moderateCount: number;
   lowCount: number;
-  layout?: string;
+  layout?: string | null;
   className?: string | null;
 }
 
-export const propTypes: WeakValidationMap<Props> = {
+export const propTypes: ValidationMap<Props> = {
   criticalCount: PropTypes.number.isRequired,
   severeCount: PropTypes.number.isRequired,
   moderateCount: PropTypes.number.isRequired,
