927d0d67a8bfa8422b8512df059b501b102dc8f0
Ross Pokorny <rpokorny@sonatype.com>
2021-04-09 15:39:04 -0400

    Improve types in withClass - RSC-505

--- lib/src/util/withClass.tsx
+++ lib/src/util/withClass.tsx
@@ -4,24 +4,16 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { ComponentProps, ReactElement } from 'react';
+import React, { FunctionComponent } from 'react';
 import classnames from 'classnames';
-
-type ElementType<E extends keyof JSX.IntrinsicElements> = ReactElement<ComponentProps<E>, E>;
-type ComponentConstructorType<E extends keyof JSX.IntrinsicElements> = (p: ComponentProps<E>) => ElementType<E>;
 
 export default function withClass<E extends keyof JSX.IntrinsicElements>(
   El: E,
   withClassName: string
-): ComponentConstructorType<E> {
-  return function WithClassWrapper({ className, ...otherProps }: ComponentProps<E>): ElementType<E> {
-    // There should be a safer way but I can't figure it out; TS keeps thinking that
-    // the props need to satisfy all possible types of E at the same time if I construct the JSX
-    // using El
-    /* eslint-disable @typescript-eslint/no-explicit-any */
-    const CastEl = El as any,
-        classes = classnames(withClassName, className);
+): FunctionComponent<JSX.IntrinsicElements[E]> {
+  return function WithClassWrapper({ className, ...otherProps }: JSX.IntrinsicElements[E]) {
+    const classes = classnames(withClassName, className);
 
-    return <CastEl className={classes} { ...otherProps } />;
+    return React.createElement(El, { className: classes, ...otherProps});
   };
 }
