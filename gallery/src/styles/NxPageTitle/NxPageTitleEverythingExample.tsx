ca4bf01831b5f464c26d1b3104e0f0b53f43172a
Glen Hunter <ghunter@sonatype.com>
2021-04-21 15:11:09 -0400

    Add dropdown to everything example RSC-468

--- gallery/src/styles/NxPageTitle/NxPageTitleEverythingExample.tsx
+++ gallery/src/styles/NxPageTitle/NxPageTitleEverythingExample.tsx
@@ -5,38 +5,55 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { NxTag } from '@sonatype/react-shared-components';
+import { NxTag, NxDropdown, useToggle } from '@sonatype/react-shared-components';
 
-const NxPageTitleEverythingExample = () =>
-  <div className="nx-page-title">
-    <hgroup className="nx-page-title__headings">
-      <h1 className="nx-h1">
-        Longer Page Title Foo Bar Foo!
-      </h1>
-      <h2 className="nx-h2 nx-page-title__sub-title">
-        This is a page sub-title that will truncate with an ellipsis
-      </h2>
-    </hgroup>
-    <div className="nx-btn-bar">
-      <button className="nx-btn nx-btn--tertiary">First</button>
-      <button className="nx-btn nx-btn--tertiary">Second</button>
+function NxPageTitleEverythingExample() {
+  const [isOpen, onToggleCollapse] = useToggle(false);
+
+  return (
+    <div className="nx-page-title">
+      <hgroup className="nx-page-title__headings">
+        <h1 className="nx-h1">
+          Longer Page Title Foo Bar Foo!
+        </h1>
+        <h2 className="nx-h2 nx-page-title__sub-title">
+          This is a page sub-title that will truncate with an ellipsis
+        </h2>
+      </hgroup>
+      <div className="nx-btn-bar">
+        <NxDropdown label="Example Dropdown" isOpen={isOpen} onToggleCollapse={onToggleCollapse}>
+          <a href="#" className="nx-dropdown-button">
+            <span className="nx-dropdown-button-content">Text Link 1</span>
+          </a>
+          <a href="#" className="nx-dropdown-button">
+            <span className="nx-dropdown-button-content">Text Link 2</span>
+          </a>
+          <a href="#" className="nx-dropdown-button">
+            <span className="nx-dropdown-button-content">Text Link 3</span>
+          </a>
+          <a href="#" className="nx-dropdown-button">
+            <span className="nx-dropdown-button-content">Text Link 4</span>
+          </a>
+        </NxDropdown>
+      </div>
+      <div className="nx-page-title__description">
+        <p className="nx-p">This is a page description.</p>
+        <p className="nx-p">
+          jeans sign papier-mache assassin San Francisco rifle physical 3D-printed denim tanto courier concrete dolphin
+          rebar free-market. tank-traps papier-mache dead free-market tanto drone concrete dolphin sunglasses weathered
+          dead jeans office vehicle nodal point. motion film meta- monofilament knife vinyl post- bridge jeans city
+          Tokyo alcohol marketing girl vehicle.
+        </p>
+      </div>
+      <div className="nx-page-title__tags">
+        <NxTag>Default</NxTag>
+        <NxTag color="purple">Purple</NxTag>
+        <NxTag color="light-blue">Light Blue</NxTag>
+        <NxTag color="pink">Pink</NxTag>
+        <NxTag color="blue">Blue</NxTag>
+      </div>
     </div>
-    <div className="nx-page-title__description">
-      <p className="nx-p">This is a page description.</p>
-      <p className="nx-p">
-        jeans sign papier-mache assassin San Francisco rifle physical 3D-printed denim tanto courier concrete dolphin
-        rebar free-market. tank-traps papier-mache dead free-market tanto drone concrete dolphin sunglasses weathered
-        dead jeans office vehicle nodal point. motion film meta- monofilament knife vinyl post- bridge jeans city
-        Tokyo alcohol marketing girl vehicle.
-      </p>
-    </div>
-    <div className="nx-page-title__tags">
-      <NxTag>Default</NxTag>
-      <NxTag color="purple">Purple</NxTag>
-      <NxTag color="light-blue">Light Blue</NxTag>
-      <NxTag color="pink">Pink</NxTag>
-      <NxTag color="blue">Blue</NxTag>
-    </div>
-  </div>;
+  );
+}
 
 export default NxPageTitleEverythingExample;
