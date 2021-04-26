d750a2589b28d814d2a200c328e9d07da720c898
Glen Hunter <ghunter@sonatype.com>
2021-04-26 09:28:46 -0400

    Add code to .Headings  RSC-468

--- gallery/src/styles/NxPageTitle/NxPageTitlePage.tsx
+++ gallery/src/styles/NxPageTitle/NxPageTitlePage.tsx
@@ -54,7 +54,7 @@ const NxPageTitlePage = () =>
           </tr>
           <tr className="nx-table-row">
             <td className="nx-cell"><code className="nx-code">.nx-page-title__headings</code></td>
-            <td className="nx-cell">NxPageTitle.Headings</td>
+            <td className="nx-cell"><code className="nx-code">NxPageTitle.Headings</code></td>
             <td className="nx-cell">Element</td>
             <td className="nx-cell">
               If there is a sub-title then the <NxCode>&lt;h1&gt;</NxCode> &amp; <NxCode>&lt;h2&gt;</NxCode> should
