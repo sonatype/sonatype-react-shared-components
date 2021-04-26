57752e3be33dd68ed9d362b41c62b2416b67e906
Glen Hunter <glenhunter@users.noreply.github.com>
2021-04-26 09:09:01 -0400

    Update gallery/src/styles/NxPageTitle/NxPageTitlePage.tsx
    Co-authored-by: Ross Pokorny <rpokorny@sonatype.com>

--- gallery/src/styles/NxPageTitle/NxPageTitlePage.tsx
+++ gallery/src/styles/NxPageTitle/NxPageTitlePage.tsx
@@ -57,7 +57,7 @@ const NxPageTitlePage = () =>
             <td className="nx-cell"></td>
             <td className="nx-cell">Element</td>
             <td className="nx-cell">
-              If there is a sub-title then the <NxCode>&lt;h1&gt; &amp; &lt;h2&gt;</NxCode> should both be wrapped
+              If there is a sub-title then the <NxCode>&lt;h1&gt;</NxCode> &amp; <NxCode>&lt;h2&gt;</NxCode> should both be wrapped
               in a containing <code className="nx-code">&lt;hgroup&gt;</code> with this class. If there is only an
               <NxCode>&lt;h1&gt;</NxCode> then this element and its class are optional.
             </td>
