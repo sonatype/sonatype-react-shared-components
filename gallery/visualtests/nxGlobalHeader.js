570b848440766a5ef9c2720aba3f78babbae9a0f
Ross Pokorny <rpokorny@sonatype.com>
2021-04-28 15:42:55 -0400

    Update visual tests - RSC-487

--- gallery/visualtests/nxGlobalHeader.js
+++ gallery/visualtests/nxGlobalHeader.js
@@ -4,24 +4,24 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-describe('NxPageLocalHeader', function() {
+describe('NxGlobalHeader', function() {
   it('looks right with full content', async function() {
-    await browser.url('#/NxPageLocalHeaderFullExample');
+    await browser.url('#/NxGlobalHeaderFullExample');
     await browser.eyesSnapshot(null);
   });
 
   it('looks right without action bar', async function() {
-    await browser.url('#/NxPageLocalHeaderNoActionsExample');
+    await browser.url('#/NxGlobalHeaderNoActionsExample');
     await browser.eyesSnapshot(null);
   });
 
   it('looks right without back button', async function() {
-    await browser.url('#/NxPageLocalHeaderNoBackButtonExample');
+    await browser.url('#/NxGlobalHeaderNoBackButtonExample');
     await browser.eyesSnapshot(null);
   });
 
   it('is completely hidden when empty', async function() {
-    await browser.url('#/NxPageLocalHeaderEmptyExample');
+    await browser.url('#/NxGlobalHeaderEmptyExample');
     await browser.eyesSnapshot(null);
   });
 });
