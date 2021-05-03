e83a4f6b999b26ef7cbdb50b1725c156eae70915
Ross Pokorny <rpokorny@sonatype.com>
2021-05-03 11:16:44 -0400

    Use CSS variable approach for selectable colors - RSC-554

--- lib/src/util/selectableColors.ts
+++ lib/src/util/selectableColors.ts
@@ -4,7 +4,13 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import { map } from 'ramda';
+
+import './selectableColors.scss';
+
 export const selectableColors =
     ['light-blue', 'purple', 'pink', 'blue', 'red', 'green', 'orange', 'yellow', 'lime'] as const;
+
+export const seletableColorClasses = map(color => `nx-selectable-color--${color}`, selectableColors);
 
 export type SelectableColor = (typeof selectableColors)[number];
