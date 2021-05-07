d775fccb69fabde45d590f91987138c8ca190720
Ross Pokorny <rpokorny@sonatype.com>
2021-05-07 10:33:32 -0400

    Update ColorPaletteExample - RSC-549

--- gallery/src/guidelines/ColorPalette/ColorPaletteExample.tsx
+++ gallery/src/guidelines/ColorPalette/ColorPaletteExample.tsx
@@ -4,50 +4,73 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { groupBy, prop, toPairs } from 'ramda';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { toPairs } from 'ramda';
 
-const swatchSCSS =
-    require('@sonatype/react-shared-components/scss-shared/_nx-color-swatches.scss?raw');
 import './Swatcher.scss';
-interface ColorInfo {
-  colorVariable: string;
-  colorName: string;
-  colorHex: string;
+
+const paletteCssPropertyRegex = /^--nx-swatch-(\w+)-\d+$/;
+
+/**
+ * Retrieve all --nx-swatch-... CSS custom properties from currently loaded stylesheets
+ */
+function fetchSwatches(): Record<string, string[]> {
+  // note: the built-in iterables involved here aren't array-like enough to satisfy ramda's TS typings, so it's
+  // simpler to just use imperative code.
+
+  const retval: Record<string, string[]> = {};
+  for (const styleSheet of document.styleSheets) {
+    if (styleSheet instanceof CSSStyleSheet && styleSheet.href && styleSheet.href.endsWith('gallery.css')) {
+
+      for (const rule of styleSheet.cssRules) {
+        if (rule instanceof CSSStyleRule) {
+
+          for (const propName of rule.style) {
+            const matchResult = paletteCssPropertyRegex.exec(propName);
+            if (matchResult) {
+
+              const [colorVar, colorName] = matchResult,
+                  arrToUpdate = retval[colorName] || [];
+
+              arrToUpdate.push(colorVar);
+              retval[colorName] = arrToUpdate;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return retval;
 }
 
-const swatchFile: string[] = swatchSCSS.split('\n');
-
-const swatchLines = swatchFile.map(line => {
-  const matchResults = /^\s*(\--nx-(\w+)-\d+):\s*(hsla\(([\d%]+,\s*){3}[\d%]+\));$/.exec(line);
-
-  if (matchResults) {
-    return {colorVariable: matchResults[1], colorName: matchResults[2], colorHex: matchResults[3]};
-  }
-  else {
-    return null;
-  }
-}).filter(data => data !== null) as ColorInfo[];
-
-const varsByColor = groupBy(prop('colorName'), swatchLines);
+const bodyStyles = getComputedStyle(document.body);
 
 export default function Swatcher() {
+  const [swatches, setSwatches] = useState<Record<string, string[]>>({});
+
+  useEffect(() => {
+    const listener = () => setSwatches(fetchSwatches());
+    window.addEventListener('load', listener);
+    return () => window.removeEventListener('load', listener);
+  }, []);
+
   return (
     <>
-      { toPairs(varsByColor).map(([colorName, colors]) =>
+      { toPairs(swatches).map(([colorName, colorVars]) =>
         <div className="nx-card gallery-card-swatch" key={colorName}>
           <div className="nx-card__header">
             <h3 className="nx-h3">{colorName}</h3>
           </div>
           <div className="nx-card__content">
-            {colors.map(({colorVariable, colorHex}) =>
-              <div className="gallery-swatch" key={colorHex}>
-                <div className="gallery-swatch__thumb" style={{backgroundColor: `${colorHex}`}}></div>
+            {colorVars.map(colorVar =>
+              <div className="gallery-swatch" key={colorVar}>
+                <div className="gallery-swatch__thumb" style={{backgroundColor: `var(${colorVar})`}}></div>
                 <div className="gallery-swatch__hex">
-                  {colorHex}
+                  {bodyStyles.getPropertyValue(colorVar)}
                 </div>
                 <div className="gallery-swatch__variable">
-                  {colorVariable}
+                  {colorVar}
                 </div>
               </div>
             )}
