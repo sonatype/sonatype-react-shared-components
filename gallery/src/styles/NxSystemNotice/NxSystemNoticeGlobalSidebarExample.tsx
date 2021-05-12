5004b60e56d42d1f93d0abda8372e1a226ac257d
Ross Pokorny <rpokorny@sonatype.com>
2021-05-12 11:49:32 -0400

    Improve examples - RSC-570

--- gallery/src/styles/NxSystemNotice/NxSystemNoticeGlobalSidebarExample.tsx
+++ gallery/src/styles/NxSystemNotice/NxSystemNoticeGlobalSidebarExample.tsx
@@ -12,7 +12,9 @@ import {
   NxFontAwesomeIcon,
   NxBackButton,
   NxStatefulGlobalSidebar,
-  NxPageMain
+  NxPageMain,
+  NxTile,
+  NxH2
 } from '@sonatype/react-shared-components';
 import { faArrowLeft, faArrowRight, faLink, faEdit } from '@fortawesome/free-solid-svg-icons';
 
@@ -41,17 +43,26 @@ export default function NxSystemNoticeGlobalSidebarExample() {
         </NxGlobalSidebarNavigation>
       </NxStatefulGlobalSidebar>
       <NxPageMain>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Tortor consequat id porta nibh venenatis cras. Proin libero nunc consequat interdum
-        varius sit amet mattis. Enim praesent elementum facilisis leo vel. A arcu cursus vitae congue mauris
-        rhoncus aenean vel elit. Morbi tincidunt ornare massa eget egestas. Semper auctor neque vitae tempus quam
-        pellentesque nec nam aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Sit amet aliquam id diam
-        maecenas. Porta nibh venenatis cras sed felis eget velit aliquet. Semper feugiat nibh sed pulvinar proin
-        gravida hendrerit lectus a. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Purus non enim
-        praesent elementum facilisis leo. Dui sapien eget mi proin. Ultrices eros in cursus turpis massa tincidunt
-        dui ut ornare. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Turpis egestas maecenas
-        pharetra convallis posuere morbi leo urna. Tortor posuere ac ut consequat semper viverra nam libero.
-        Volutpat ac tincidunt vitae semper. Pretium fusce id velit ut tortor pretium viverra.
+        <NxTile>
+          <NxTile.Header>
+            <NxTile.HeaderTitle>
+              <NxH2>Lorem Ipsum</NxH2>
+            </NxTile.HeaderTitle>
+          </NxTile.Header>
+          <NxTile.Content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Tortor consequat id porta nibh venenatis cras. Proin libero nunc consequat interdum
+            varius sit amet mattis. Enim praesent elementum facilisis leo vel. A arcu cursus vitae congue mauris
+            rhoncus aenean vel elit. Morbi tincidunt ornare massa eget egestas. Semper auctor neque vitae tempus quam
+            pellentesque nec nam aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Sit amet aliquam id diam
+            maecenas. Porta nibh venenatis cras sed felis eget velit aliquet. Semper feugiat nibh sed pulvinar proin
+            gravida hendrerit lectus a. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Purus non enim
+            praesent elementum facilisis leo. Dui sapien eget mi proin. Ultrices eros in cursus turpis massa tincidunt
+            dui ut ornare. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Turpis egestas maecenas
+            pharetra convallis posuere morbi leo urna. Tortor posuere ac ut consequat semper viverra nam libero.
+            Volutpat ac tincidunt vitae semper. Pretium fusce id velit ut tortor pretium viverra.
+          </NxTile.Content>
+        </NxTile>
       </NxPageMain>
     </>
   );
--- gallery/src/styles/NxSystemNotice/NxSystemNoticeMultipleExample.tsx
+++ gallery/src/styles/NxSystemNotice/NxSystemNoticeMultipleExample.tsx
@@ -12,7 +12,9 @@ import {
   NxFontAwesomeIcon,
   NxBackButton,
   NxStatefulGlobalSidebar,
-  NxPageMain
+  NxPageMain,
+  NxTile,
+  NxH2
 } from '@sonatype/react-shared-components';
 import { faArrowLeft, faArrowRight, faLink, faEdit } from '@fortawesome/free-solid-svg-icons';
 
@@ -24,7 +26,10 @@ export default function NxSystemNoticeGlobalSidebarExample() {
       <div className="nx-system-notice-container">
         <div className="nx-system-notice">
           This is a test; this is only a test; only a test; a test; a test; a test; a test; a test; a test;
-          a test; a test; a test; a test; a test;
+          a test; a test; a test; a test; a test; a test; a test; a test; a test; a test;
+          a test; a test; a test; a test; a test; a test; a test; a test; a test; a test;
+          a test; a test; a test; a test; a test; a test; a test; a test; a test; a test;
+          a test; a test; a test; a test; a test; a test; a test; a test; a test; a test;
         </div>
         <div className="nx-system-notice nx-system-notice--alert">
           You should really change your password
@@ -47,17 +52,26 @@ export default function NxSystemNoticeGlobalSidebarExample() {
         </NxGlobalSidebarNavigation>
       </NxStatefulGlobalSidebar>
       <NxPageMain>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Tortor consequat id porta nibh venenatis cras. Proin libero nunc consequat interdum
-        varius sit amet mattis. Enim praesent elementum facilisis leo vel. A arcu cursus vitae congue mauris
-        rhoncus aenean vel elit. Morbi tincidunt ornare massa eget egestas. Semper auctor neque vitae tempus quam
-        pellentesque nec nam aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Sit amet aliquam id diam
-        maecenas. Porta nibh venenatis cras sed felis eget velit aliquet. Semper feugiat nibh sed pulvinar proin
-        gravida hendrerit lectus a. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Purus non enim
-        praesent elementum facilisis leo. Dui sapien eget mi proin. Ultrices eros in cursus turpis massa tincidunt
-        dui ut ornare. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Turpis egestas maecenas
-        pharetra convallis posuere morbi leo urna. Tortor posuere ac ut consequat semper viverra nam libero.
-        Volutpat ac tincidunt vitae semper. Pretium fusce id velit ut tortor pretium viverra.
+        <NxTile>
+          <NxTile.Header>
+            <NxTile.HeaderTitle>
+              <NxH2>Lorem Ipsum</NxH2>
+            </NxTile.HeaderTitle>
+          </NxTile.Header>
+          <NxTile.Content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Tortor consequat id porta nibh venenatis cras. Proin libero nunc consequat interdum
+            varius sit amet mattis. Enim praesent elementum facilisis leo vel. A arcu cursus vitae congue mauris
+            rhoncus aenean vel elit. Morbi tincidunt ornare massa eget egestas. Semper auctor neque vitae tempus quam
+            pellentesque nec nam aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Sit amet aliquam id diam
+            maecenas. Porta nibh venenatis cras sed felis eget velit aliquet. Semper feugiat nibh sed pulvinar proin
+            gravida hendrerit lectus a. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Purus non enim
+            praesent elementum facilisis leo. Dui sapien eget mi proin. Ultrices eros in cursus turpis massa tincidunt
+            dui ut ornare. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Turpis egestas maecenas
+            pharetra convallis posuere morbi leo urna. Tortor posuere ac ut consequat semper viverra nam libero.
+            Volutpat ac tincidunt vitae semper. Pretium fusce id velit ut tortor pretium viverra.
+          </NxTile.Content>
+        </NxTile>
       </NxPageMain>
     </>
   );
