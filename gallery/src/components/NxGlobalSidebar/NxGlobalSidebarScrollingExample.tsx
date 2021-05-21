2ab493efe109ca7513c19bd0826e13bf89a55865
Glen Hunter <glenhunter@users.noreply.github.com>
2021-05-21 16:06:22 -0400

    Update gallery/src/components/NxGlobalSidebar/NxGlobalSidebarScrollingExample.tsx
    Co-authored-by: Ross Pokorny <rpokorny@sonatype.com>

--- gallery/src/components/NxGlobalSidebar/NxGlobalSidebarScrollingExample.tsx
+++ gallery/src/components/NxGlobalSidebar/NxGlobalSidebarScrollingExample.tsx
@@ -104,8 +104,7 @@ export default function NxGlobalSidebarScrollingExample() {
         <NxGlobalSidebarFooter supportText="Support for RSC"
                                supportLink="https://github.com/sonatype/sonatype-react-shared-components"
                                releaseText="3.1.4"
-                               productTagLine="Powered by PLAID VILLAIN">
-        </NxGlobalSidebarFooter>
+                               productTagLine="Powered by PLAID VILLAIN" />
       </NxGlobalSidebar>
       <NxPageMain>Page content</NxPageMain>
     </>
