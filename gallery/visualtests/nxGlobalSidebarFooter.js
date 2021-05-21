ee5616fb8941bf8c224729d9d6d01aecd02152ce
Glen Hunter <glenhunter@users.noreply.github.com>
2021-05-21 16:05:28 -0400

    Update gallery/visualtests/nxGlobalSidebarFooter.js
    Co-authored-by: Ross Pokorny <rpokorny@sonatype.com>

--- gallery/visualtests/nxGlobalSidebarFooter.js
+++ gallery/visualtests/nxGlobalSidebarFooter.js
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-describe('NxGlobalFooter', function() {
+describe('NxGlobalSidebarFooter', function() {
   // Full examples of this component are captured by NxGlobalSidebar
 
   it('looks right in its minimal layout', async function() {
