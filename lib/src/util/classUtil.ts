6c460cb77e9a04de8b7fc50a7b8041f54fc2d587
Ross Pokorny <rpokorny@sonatype.com>
2021-05-25 18:00:09 -0400

    Fix eslint errors - RSC-584

--- lib/src/util/classUtil.ts
+++ lib/src/util/classUtil.ts
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { curryN } from "ramda";
+import { curryN } from 'ramda';
 
 // given a class name, create a regex that will match HTML class attrs that have that class name
 const classMatchRegex = (cls: string) => new RegExp(String.raw`(\s|^)${cls}(\s|$)`);
@@ -13,7 +13,7 @@ const classMatchRegex = (cls: string) => new RegExp(String.raw`(\s|^)${cls}(\s|$
  * @return whether the space-separated className string (fullClassName) includes the specified class
  */
 export const includesClass = curryN(2,
-  (cls: string, fullClassName?: string) => !!(fullClassName && fullClassName.match(classMatchRegex(cls)))
+    (cls: string, fullClassName?: string) => !!(fullClassName && fullClassName.match(classMatchRegex(cls)))
 ) as ((cls: string) => (className?: string) => boolean) & ((cls: string, className?: string) => boolean);
 
 export const includesDisabledClass = includesClass('disabled');
