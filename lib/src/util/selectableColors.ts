a5b51318f29ab35aa41e97e2bcff62c7a3ec3776
Ross Pokorny <rpokorny@sonatype.com>
2021-06-16 13:01:11 -0400

    Reorder colors - RSC-598

--- lib/src/util/selectableColors.ts
+++ lib/src/util/selectableColors.ts
@@ -9,7 +9,7 @@ import { map } from 'ramda';
 import './selectableColors.scss';
 
 export const selectableColors =
-    ['light-blue', 'purple', 'pink', 'blue', 'red', 'green', 'orange', 'yellow', 'lime', 'indigo'] as const;
+    ['purple', 'pink', 'blue', 'red', 'green', 'orange', 'yellow', 'lime', 'light-blue', 'indigo'] as const;
 
 export const selectableColorClasses: readonly string[] =
     map(color => `nx-selectable-color--${color}`, selectableColors);
