2394a77be9878fd60acbf9040c2acd9b547aad7a
Ross Pokorny <rpokorny@sonatype.com>
2021-06-29 11:24:58 -0400

    Update docs - RSC-510

--- gallery/src/components/NxModal/NxModalPage.tsx
+++ gallery/src/components/NxModal/NxModalPage.tsx
@@ -5,7 +5,7 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { NxInfoAlert, NxCode, NxP, NxTextLink } from '@sonatype/react-shared-components';
+import { NxInfoAlert, NxCode, NxP, NxTextLink, NxWarningAlert } from '@sonatype/react-shared-components';
 
 import { GalleryDescriptionTile, GalleryExampleTile } from '../../gallery-components/GalleryTiles';
 import NxModalSimpleExample from './NxModalSimpleExample';
@@ -56,13 +56,45 @@ export default function NxModalPage() {
               </td>
             </tr>
             <tr className="nx-table-row">
-              <td className="nx-cell">onClose</td>
-              <td className="nx-cell">Function (() =&gt; void)</td>
+              <td className="nx-cell">onCancel</td>
+              <td className="nx-cell">Function ((Event) =&gt; void)</td>
               <td className="nx-cell">Yes</td>
               <td className="nx-cell"></td>
               <td className="nx-cell">
-                The function to be called to close the modal when pressing
-                the <code className="nx-code">Escape</code> key.
+                <NxP>
+                  A callback to be called when the browser's native <NxCode>cancel</NxCode> event for the
+                  modal's <NxCode>HTMLDialogElement</NxCode> is fired. The circumstances which will fire this event
+                  depend on the user agent, but typically include when the user presses ESC within the modal.
+                </NxP>
+                <NxP>
+                  It is expected that the handler passed for this prop will typically remove the NxModal from the
+                  JSX rendered to the page. If for whatever reason the handler wants to keep the modal open,
+                  it <strong>must</strong> call <NxCode>preventDefault</NxCode> on the event argument. This prevents
+                  the browser's native dialog closing logic, such as the removal of the <NxCode>open</NxCode> attribute
+                  from the dialog element.
+                </NxP>
+                <NxP>
+                  Note that at the time of writing, proper support for <NxCode>HTMLDialogElement</NxCode>, including
+                  its <NxCode>cancel</NxCode> event, is limited to Chromium-based browsers. In other
+                  browsers, <NxCode>NxModal</NxCode> itself listens for the ESC keypress and synthesizes the event
+                  passed to this callback.
+                </NxP>
+                <NxP>
+                  Also note that any components within the modal that have their own ESC handling should
+                  call <NxCode>preventDefault</NxCode> on any ESC keydowns that they handle in order to prevent the
+                  modal from also handling them. <NxCode>NxDropdown</NxCode> does this automatically.
+                </NxP>
+              </td>
+            </tr>
+            <tr className="nx-table-row">
+              <td className="nx-cell">onClose</td>
+              <td className="nx-cell">Function (() =&gt; void)</td>
+              <td className="nx-cell">No</td>
+              <td className="nx-cell"></td>
+              <td className="nx-cell">
+                <NxWarningAlert>
+                  Deprecated. Old alias for <NxCode>onCancel</NxCode>. Using both at the same time is not supported.
+                </NxWarningAlert>
               </td>
             </tr>
             <tr className="nx-table-row">
