c283836a78d3d30f9679b4967c0bf2ffc6338611
Glen Hunter <ghunter@sonatype.com>
2021-07-07 16:23:41 -0400

    Fix for visual test RSC-590

--- gallery/visualtests/nxOverflowTooltip.js
+++ gallery/visualtests/nxOverflowTooltip.js
@@ -26,7 +26,7 @@ describe('NxOverflowTooltip', function() {
       dynamicExampleTextInputSelector = `${dynamicExampleSelector} .nx-text-input input`,
       dynamicExampleTooltipTargetSelector = `${dynamicExampleSelector} .gallery-example-live .nx-p`,
       tooltipSelector = '.nx-tooltip';
-      maxBeforeOverflowStr = 'WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW',
+      maxBeforeOverflowStr = 'WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW',
       descendantOverflowItemSelector = `${descendantOverflowListSelector} .nx-list__item:nth-child(2)`;
 
   async function openDropdownAndHoverButton(btnSelector) {
