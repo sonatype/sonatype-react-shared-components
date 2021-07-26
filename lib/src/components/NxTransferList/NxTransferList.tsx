f7ea3f9e6fa4884e2756e0d7895f551272e9579f
Ross Pokorny <rpokorny@sonatype.com>
2021-07-26 13:26:40 -0400

    Revert "Make each half a fieldset - RSC-547"
    This reverts commit 55f538fe0cff989e9b25d6fa30ee2b728fbf7cbd.

--- lib/src/components/NxTransferList/NxTransferList.tsx
+++ lib/src/components/NxTransferList/NxTransferList.tsx
@@ -16,7 +16,6 @@ import { toLower, filter, includes, groupBy, partial, identity, prop, pipe } fro
 export { Props, DataItem } from './types';
 
 import './NxTransferList.scss';
-import NxFieldset from '../NxFieldset/NxFieldset';
 
 function TransferListItem<T extends string | number>(props: TransferListItemProps<T>) {
   const { checked, id, displayName, onChange: onChangeProp } = props;
@@ -106,7 +105,8 @@ export default function NxTransferList<T extends string | number>(props: Props<T
 
   return (
     <div className={classnames('nx-transfer-list', classNameProp)} { ...attrs }>
-      <NxFieldset className="nx-transfer-list__half" label={availableItemsLabel || 'Available Items'}>
+      <div className="nx-transfer-list__half">
+        <h5 className="nx-transfer-list__half-header">{availableItemsLabel || 'Available Items'}</h5>
         <div className="nx-transfer-list__control-box">
           <NxFilterInput className="nx-transfer-list__filter"
                          value={availableItemsFilter}
@@ -126,8 +126,9 @@ export default function NxTransferList<T extends string | number>(props: Props<T
             {availableCount} items available
           </div>
         </div>
-      </NxFieldset>
-      <NxFieldset className="nx-transfer-list__half" label={selectedItemsLabel || 'Transferred Items'}>
+      </div>
+      <div className="nx-transfer-list__half">
+        <h5 className="nx-transfer-list__half-header">{selectedItemsLabel || 'Transferred Items'}</h5>
         <div className="nx-transfer-list__control-box">
           <NxFilterInput className="nx-transfer-list__filter"
                          value={selectedItemsFilter}
@@ -147,7 +148,7 @@ export default function NxTransferList<T extends string | number>(props: Props<T
             {selectedCount} items transferred
           </div>
         </div>
-      </NxFieldset>
+      </div>
     </div>
   );
 }
