681c62596e88586a1f60b9f9048b98be7ab58a55
Ross Pokorny <rpokorny@sonatype.com>
2021-07-27 17:26:37 -0400

    Fix typo - RSC-547

--- gallery/src/components/NxTransferList/NxTransferListPage.tsx
+++ gallery/src/components/NxTransferList/NxTransferListPage.tsx
@@ -246,7 +246,7 @@ const NxTransferListPage = () =>
         </NxTile.SubsectionHeader>
         <NxP>
           <NxCode>NxTransferList</NxCode> would typically be used within a form, and should typically be wrapped
-          in an <NxCode>NxFieldset</NxCode> in order to provide a label an spacing for the form field represented
+          in an <NxCode>NxFieldset</NxCode> in order to provide a label and spacing for the form field represented
           by this component. See the <NxTextLink>Form Layout Page</NxTextLink> for an example.
         </NxP>
       </NxTile.Subsection>
