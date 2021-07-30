9bdf9fda4fdc56129df64fa11b11521ad14448e5
Ross Pokorny <rpokorny@sonatype.com>
2021-07-30 14:51:06 -0400

    Improve nx-btn class docs - RSC-647

--- gallery/src/styles/NxBtn/NxBtnPage.tsx
+++ gallery/src/styles/NxBtn/NxBtnPage.tsx
@@ -7,7 +7,7 @@
 import React from 'react';
 
 import {GalleryDescriptionTile, GalleryExampleTile} from '../../gallery-components/GalleryTiles';
-import { NxCode } from '@sonatype/react-shared-components';
+import { NxCode, NxTable, NxTile, NxH3 } from '@sonatype/react-shared-components';
 
 const nxBtnPrimaryCode = require('./NxBtnPrimaryExample.html'),
     nxBtnDefaultCode = require('./NxBtnDefaultExample.html'),
@@ -20,14 +20,73 @@ const NxBtnPage = () =>
     <GalleryDescriptionTile>
       <p className="nx-p"><code className="nx-code">.nx-btn</code> is the standard class for all buttons.</p>
       <p className="nx-p">
-        When a button is not contained in a <code className="nx-code">footer</code>, then an enclosing
-        <code className="nx-code">.nx-btn-bar</code> is generally required to ensure that the buttons are spaced
-        appropriately from other content.
+        Buttons are typically encapsulated within "button bars" implemented using the <NxCode>nx-btn-bar</NxCode> class.
+        This class mediates the layout of groups of buttons within various containers such as footers, table cells, etc.
       </p>
       <p className="nx-p">
         When working in React, it is recommended to use the <NxCode>NxButton</NxCode> component which provides a more
-        convenient way to use these styles.
+        convenient way to use these styles. For the button bar, there is an <NxCode>NxBtnBar</NxCode> convenience
+        component.
       </p>
+      <NxTile.Subsection>
+        <NxTile.SubsectionHeader>
+          <NxH3>Classes</NxH3>
+        </NxTile.SubsectionHeader>
+        <NxTable>
+          <NxTable.Head>
+            <NxTable.Row>
+              <NxTable.Cell>Class Name</NxTable.Cell>
+              <NxTable.Cell>Location</NxTable.Cell>
+              <NxTable.Cell>Description</NxTable.Cell>
+            </NxTable.Row>
+          </NxTable.Head>
+          <NxTable.Body>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>nx-btn</NxCode></NxTable.Cell>
+              <NxTable.Cell>Button elements</NxTable.Cell>
+              <NxTable.Cell>Base class for RSC-styled buttons, which use "secondary" styling by default</NxTable.Cell>
+            </NxTable.Row>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>nx-btn-bar</NxCode></NxTable.Cell>
+              <NxTable.Cell>Container of buttons</NxTable.Cell>
+              <NxTable.Cell>Wrapper for one or more buttons within used within other RSC elements</NxTable.Cell>
+            </NxTable.Row>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>nx-btn--primary</NxCode></NxTable.Cell>
+              <NxTable.Cell>Modifier on <NxCode>.nx-btn</NxCode></NxTable.Cell>
+              <NxTable.Cell>Makes the button a "primary" button</NxTable.Cell>
+            </NxTable.Row>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>nx-btn--tertiary</NxCode></NxTable.Cell>
+              <NxTable.Cell>Modifier on <NxCode>.nx-btn</NxCode></NxTable.Cell>
+              <NxTable.Cell>Makes the button a "tertiary" button</NxTable.Cell>
+            </NxTable.Row>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>nx-btn--error</NxCode></NxTable.Cell>
+              <NxTable.Cell>Modifier on <NxCode>.nx-btn</NxCode></NxTable.Cell>
+              <NxTable.Cell>
+                Buttons that indicate or which are used in conjunction with errors and which are styled accordingly
+              </NxTable.Cell>
+            </NxTable.Row>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>nx-btn--icon-only</NxCode></NxTable.Cell>
+              <NxTable.Cell>Modifier on <NxCode>.nx-btn</NxCode></NxTable.Cell>
+              <NxTable.Cell>
+                Should be used on buttons which only contain a icon and no text content
+              </NxTable.Cell>
+            </NxTable.Row>
+            <NxTable.Row>
+              <NxTable.Cell><NxCode>disabled</NxCode></NxTable.Cell>
+              <NxTable.Cell>Modifier on <NxCode>.nx-btn</NxCode></NxTable.Cell>
+              <NxTable.Cell>
+                Buttons may be disabled either via attribute or via this class. The attribute should be preferred, but
+                the class may be used when mouse events are still desired on the button – buttons disabled via the
+                attribute do not fire mouse events.
+              </NxTable.Cell>
+            </NxTable.Row>
+          </NxTable.Body>
+        </NxTable>
+      </NxTile.Subsection>
     </GalleryDescriptionTile>
 
     <GalleryExampleTile title="Default"
