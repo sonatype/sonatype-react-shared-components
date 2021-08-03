d385f2d9609f3dd8989d640a94a9560a6647868b
Abhinav Bhatta <abhatta@sonatype.com>
2021-08-03 11:44:29 -0400

    Add tests for description lists RSC-609

---
+++ lib/src/components/NxList/__tests__/NxListDescription.test.tsx
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import NxList from '../NxList';
+import { NxListDescriptionProps } from '../types';
+import { getShallowComponent } from '../../../__testutils__/enzymeUtils';
+
+describe('NxListDescription', function() {
+
+  const minimalProps: NxListDescriptionProps = {};
+  const getShallow = getShallowComponent(NxList.Description, minimalProps);
+
+  it('renders children correctly', function() {
+    const children = ['Test Description'];
+    const contentEl = getShallow({children});
+    expect(contentEl).toExist();
+    expect(contentEl).toContainExactlyOneMatchingElement('dd');
+    expect(contentEl).toMatchSelector('.nx-list__description');
+    expect(contentEl).toHaveText('Test Description');
+  });
+
+  it('adds extra classes correctly', function() {
+    const children = ['Test Description'];
+    const contentEl = getShallow({children, className: 'customClassName'});
+    expect(contentEl).toExist();
+    expect(contentEl).toContainExactlyOneMatchingElement('dd');
+    expect(contentEl).toMatchSelector('.nx-list__description');
+    expect(contentEl).toHaveClassName('customClassName');
+    expect(contentEl).toHaveText('Test Description');
+  });
+});
---
+++ lib/src/components/NxList/__tests__/NxListDescriptionTerm.test.tsx
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import NxList from '../NxList';
+import { NxListDescriptionTermProps } from '../types';
+import { getShallowComponent } from '../../../__testutils__/enzymeUtils';
+
+describe('NxListDescriptionTerm', function() {
+
+  const minimalProps: NxListDescriptionTermProps = {};
+  const getShallow = getShallowComponent(NxList.DescriptionTerm, minimalProps);
+
+  it('renders children correctly', function() {
+    const children = ['Test Description Term'];
+    const contentEl = getShallow({children});
+    expect(contentEl).toExist();
+    expect(contentEl).toContainExactlyOneMatchingElement('dt');
+    expect(contentEl).toMatchSelector('.nx-list__term');
+    expect(contentEl).toHaveText('Test Description Term');
+  });
+
+  it('adds extra classes correctly', function() {
+    const children = ['Test Description Term'];
+    const contentEl = getShallow({children, className: 'customClassName'});
+    expect(contentEl).toExist();
+    expect(contentEl).toContainExactlyOneMatchingElement('dt');
+    expect(contentEl).toMatchSelector('.nx-list__term');
+    expect(contentEl).toHaveClassName('customClassName');
+    expect(contentEl).toHaveText('Test Description Term');
+  });
+});
