255eb871aebffa3f8037af54c431a8864ebc967b
Glen Hunter <ghunter@sonatype.com>
2021-08-06 13:20:08 -0400

    Subsections RSC-432

--- gallery/src/pages/Accessibility.tsx
+++ gallery/src/pages/Accessibility.tsx
@@ -6,7 +6,7 @@
  */
 import React from 'react';
 import {GalleryTile} from '../gallery-components/GalleryTiles';
-import { NxP, NxCode, NxH3, NxList, NxTextLink } from '@sonatype/react-shared-components';
+import { NxP, NxCode, NxH3, NxList, NxTextLink, NxTile } from '@sonatype/react-shared-components';
 
 const Accessibility = () =>
   <GalleryTile title="Accessibility">
@@ -24,63 +24,71 @@ const Accessibility = () =>
       something that works in ChromeVox might not work in VoiceOver.
     </NxP>
 
-    <NxH3 className="nx-tile__section-header">Making your pages accessible</NxH3>
-    <NxP>
-      Ensuring you use the components in an accessible manner is important, here are a few tips to help you make your
-      pages accessible.
-    </NxP>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Making your pages accessible</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
+        Ensuring you use the components in an accessible manner is important, here are a few tips to help you make your
+        pages accessible.
+      </NxP>
 
-    <NxList className="nx-list--bulleted">
-      <NxList.Item>Tab order is important. Make sure that the tab order of the page is logical.</NxList.Item>
-      <NxList.Item>Related to the above, does the keyboard focus order follow the visual layout?</NxList.Item>
-      <NxList.Item>Unique title element for each page or view?</NxList.Item>
-      <NxList.Item>Do all images and visual graphs have alt text?</NxList.Item>
-      <NxList.Item>
-        Make sure that you take advantage of the props and attributes that have been added to the components to improve
-        their accessibility. Many component documentation pages have an Accessibility section with extra information.
-      </NxList.Item>
-      <NxList.Item>
-        <NxTextLink external href="https://developer.mozilla.org/en-US/docs/Glossary/Semantics">
-          Semantic HTML
-        </NxTextLink>
-        {' '}is a critical aspect of accessibility. Headings should always use the correct
-        {' '}<NxCode>&lt;h#&gt;</NxCode> tag, paragraphs should be wrapped in <NxCode>&lt;p&gt;</NxCode> tags, etc.
-      </NxList.Item>
-      <NxList.Item>
-        All form elements should have a label.
-      </NxList.Item>
-      <NxList.Item>Have you checked your page in ChromeVox and/or VoiceOver?</NxList.Item>
-    </NxList>
+      <NxList className="nx-list--bulleted">
+        <NxList.Item>Tab order is important. Make sure that the tab order of the page is logical.</NxList.Item>
+        <NxList.Item>Unique title element for each page or view?</NxList.Item>
+        <NxList.Item>Do all images and visual graphs have alt text?</NxList.Item>
+        <NxList.Item>
+          Make sure that you take advantage of the props and attributes that have been added to the components to
+          improve their accessibility. Many component documentation pages have an Accessibility section with extra
+          information.
+        </NxList.Item>
+        <NxList.Item>
+          <NxTextLink external href="https://developer.mozilla.org/en-US/docs/Glossary/Semantics">
+            Semantic HTML
+          </NxTextLink>
+          {' '}is a critical aspect of accessibility. Headings should always use the correct
+          {' '}<NxCode>&lt;h#&gt;</NxCode> tag, paragraphs should be wrapped in <NxCode>&lt;p&gt;</NxCode> tags, etc.
+        </NxList.Item>
+        <NxList.Item>
+          All form elements should have a label.
+        </NxList.Item>
+        <NxList.Item>Have you checked your page in ChromeVox and/or VoiceOver?</NxList.Item>
+      </NxList>
+    </NxTile.Subsection>
 
-    <NxH3 className="nx-tile__section-header">
-      Accessibility Resources
-    </NxH3>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Accessibility Resources</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxList className="nx-list--bulleted">
+        <NxList.Item>
+          <NxTextLink href="https://docs.sonatype.com/display/ENG/Accessibility+Best+Practices">
+            Sonatype's Accessibility Best Practices
+          </NxTextLink>
+        </NxList.Item>
+        <NxList.Item>
+          <NxTextLink href="https://developer.mozilla.org/en-US/docs/Web/Accessibility">
+            MDN accessibility page
+          </NxTextLink>
+        </NxList.Item>
+        <NxList.Item>
+          <NxTextLink href="https://www.w3.org/standards/webdesign/accessibility">
+            W3C accessibility page
+          </NxTextLink>
+        </NxList.Item>
+      </NxList>
+    </NxTile.Subsection>
 
-    <NxList className="nx-list--bulleted">
-      <NxList.Item>
-        <NxTextLink href="https://docs.sonatype.com/display/ENG/Accessibility+Best+Practices">
-          Sonatype's Accessibility Best Practices
-        </NxTextLink>
-      </NxList.Item>
-      <NxList.Item>
-        <NxTextLink href="https://developer.mozilla.org/en-US/docs/Web/Accessibility">
-          MDN accessibility page
-        </NxTextLink>
-      </NxList.Item>
-      <NxList.Item>
-        <NxTextLink href="https://www.w3.org/standards/webdesign/accessibility">
-          W3C accessibility page
-        </NxTextLink>
-      </NxList.Item>
-    </NxList>
-
-    <NxH3 className="nx-tile__section-header">
-      Discover an accessibility problem? Have a suggestion for an improvement?
-    </NxH3>
-
-    <NxP>
-      Hit us up in #accessibility or #react-components.
-    </NxP>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>
+          Discover an accessibility problem? Have a suggestion for an improvement?
+        </NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
+        Hit us up in #accessibility or #react-components.
+      </NxP>
+    </NxTile.Subsection>
 
   </GalleryTile>;
 
