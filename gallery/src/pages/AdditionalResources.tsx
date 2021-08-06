0bb9ebe0a56b38d1a36bb6bce12f6dc31bd9be5f
Glen Hunter <ghunter@sonatype.com>
2021-08-06 11:57:24 -0400

    Added home, contributing, styling and additional RSC-653

--- gallery/src/pages/AdditionalResources.tsx
+++ gallery/src/pages/AdditionalResources.tsx
@@ -6,104 +6,103 @@
  */
 import React from 'react';
 import {GalleryTile} from '../gallery-components/GalleryTiles';
+import { NxP, NxTile, NxH3, NxTextLink } from '@sonatype/react-shared-components';
 
 const AdditionalResources = () =>
   <GalleryTile title="Additional Resources">
-    <p className="nx-p">
+    <NxP>
       We have curated a list of additional resources you can use to increase your understanding of react and redux.
-    </p>
+    </NxP>
 
-    <h3 className="nx-h3 nx-tile__section-header">Classes / Tutorials</h3>
-    <ul className="nx-list nx-list--bulleted">
-      <li className="nx-list__item">
-        <a rel="noreferrer" href="https://reactjs.org/tutorial/tutorial.html" target="_blank">
-          Official ReactJS Tutorial
-        </a>
-        {' '}(Beginner Level)
-      </li>
-      <li className="nx-list__item">
-        <a rel="noreferrer"
-           href="https://redux.js.org/basics/basic-tutorial"
-           target="_blank">Official Redux Getting Started
-        </a>
-        {' '}(Beginner Level)
-      </li>
-      <li className="nx-list__item">
-        <a rel="noreferrer"
-           href="https://egghead.io/courses/getting-started-with-redux"
-           target="_blank">
-          Getting started with Redux
-        </a>
-        {' '}(egghead.io beginner course)
-      </li>
-      <li className="nx-list__item">
-        <a rel="noreferrer"
-           href="https://www.pluralsight.com/paths/react"
-           target="_blank">
-          Pluralsight React Path
-        </a>
-        {' '}(beginner to advanced)
-      </li>
-      <li className="nx-list__item">
-        <a rel="noreferrer"
-           href="https://reactforbeginners.com/"
-           target="_blank">
-          React for beginners
-        </a>
-      </li>
-    </ul>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Classes / Tutorials</NxH3>
+      </NxTile.SubsectionHeader>
+      <ul className="nx-list nx-list--bulleted">
+        <li className="nx-list__item">
+          <NxTextLink external href="https://reactjs.org/tutorial/tutorial.html">
+            Official ReactJS Tutorial
+          </NxTextLink>
+          {' '}(Beginner Level)
+        </li>
+        <li className="nx-list__item">
+          <NxTextLink external href="https://redux.js.org/basics/basic-tutorial">
+            Official Redux Getting Started
+          </NxTextLink>
+          {' '}(Beginner Level)
+        </li>
+        <li className="nx-list__item">
+          <NxTextLink external href="https://egghead.io/courses/getting-started-with-redux">
+            Getting started with Redux
+          </NxTextLink>
+          {' '}(egghead.io beginner course)
+        </li>
+        <li className="nx-list__item">
+          <NxTextLink external href="https://www.pluralsight.com/paths/react">
+            Pluralsight React Path
+          </NxTextLink>
+          {' '}(beginner to advanced)
+        </li>
+        <li className="nx-list__item">
+          <NxTextLink external href="https://reactforbeginners.com/">
+            React for beginners
+          </NxTextLink>
+        </li>
+      </ul>
+    </NxTile.Subsection>
 
-    <h3 className="nx-h3 nx-tile__section-header">Good Reads</h3>
-    <ul className="nx-list nx-list--bulleted">
-      <li className="nx-list__item">
-        <a rel="noreferrer" href="https://reactjs.org/docs/hello-world.html" target="_blank">
-          Main Concepts in React
-        </a>
-      </li>
-      <li className="nx-list__item">
-        <a rel="noreferrer" href="https://reactjs.org/docs/thinking-in-react.html" target="_blank">Thinking in React</a>
-      </li>
-      <li className="nx-list__item">
-        Many apps utilizing React and Redux together utilize the official{' '}
-        <a rel="noreferrer" href="https://react-redux.js.org/using-react-redux/connect-mapstate" target="_blank">
-          react-redux
-        </a>
-        {' '}connectors, as well as the{' '}
-        <a rel="noreferrer" href="https://redux-toolkit.js.org/introduction/quick-start" target="_blank">
-          Redux toolkit
-        </a>
-        , which is a set of patterns to reduce boilerplate in “vanilla Redux”.
-      </li>
-      <li className="nx-list__item">
-        <a rel="noreferrer"
-           href="https://www.youtube.com/watch?v=nYkdrAPrdcw&time_continue=781"
-           target="_blank">
-          Video about Flux
-        </a>
-        , which is a precursor to redux. Uses some real-world problems to motivate the concept of one-way dataflow for
-        state management, which is what Redux is all about.
-      </li>
-    </ul>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Good Reads</NxH3>
+      </NxTile.SubsectionHeader>
+      <ul className="nx-list nx-list--bulleted">
+        <li className="nx-list__item">
+          <NxTextLink external href="https://reactjs.org/docs/hello-world.html">
+            Main Concepts in React
+          </NxTextLink>
+        </li>
+        <li className="nx-list__item">
+          <NxTextLink external href="https://reactjs.org/docs/thinking-in-react.html">Thinking in React</NxTextLink>
+        </li>
+        <li className="nx-list__item">
+          Many apps utilizing React and Redux together utilize the official{' '}
+          <NxTextLink external href="https://react-redux.js.org/using-react-redux/connect-mapstate">
+            react-redux
+          </NxTextLink>
+          {' '}connectors, as well as the{' '}
+          <NxTextLink external href="https://redux-toolkit.js.org/introduction/quick-start">
+            Redux toolkit
+          </NxTextLink>
+          , which is a set of patterns to reduce boilerplate in “vanilla Redux”.
+        </li>
+        <li className="nx-list__item">
+          <NxTextLink external href="https://www.youtube.com/watch?v=nYkdrAPrdcw&time_continue=781">
+            Video about Flux
+          </NxTextLink>
+          , which is a precursor to redux. Uses some real-world problems to motivate the concept of one-way dataflow for
+          state management, which is what Redux is all about.
+        </li>
+      </ul>
+    </NxTile.Subsection>
 
-    <h3 className="nx-h3 nx-tile__section-header">Getting your hands dirty</h3>
-    <ul className="nx-list nx-list--bulleted">
-      <li className="nx-list__item">
-        <a rel="noreferrer"
-           href="https://github.com/sonatype/sonatype-application-builder"
-           target="_blank">
-          Internal Sonatype Application Builder (SAB) Template
-        </a>
-        {' - '}A great place to start for new projects and a fun place to poke around.
-        It is also setup as a GitHub Template, just click the "use this template" button (or click{' '}
-        <a rel="noreferrer"
-           href="https://github.com/sonatype/sonatype-application-builder/generate"
-           target="_blank">
-          here
-        </a>
-        ) in GitHub to get started. Note that, at this time, this is an internal to Sonatype template.
-      </li>
-    </ul>
-
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Getting your hands dirty</NxH3>
+      </NxTile.SubsectionHeader>
+      <ul className="nx-list nx-list--bulleted">
+        <li className="nx-list__item">
+          <NxTextLink external href="https://github.com/sonatype/sonatype-application-builder">
+            Internal Sonatype Application Builder (SAB) Template
+          </NxTextLink>
+          {' - '}A great place to start for new projects and a fun place to poke around.
+          It is also setup as a GitHub Template, just click the "use this template" button (or click{' '}
+          <NxTextLink external href="https://github.com/sonatype/sonatype-application-builder/generate">
+            here
+          </NxTextLink>
+          ) in GitHub to get started. Note that, at this time, this is an internal to Sonatype template.
+        </li>
+      </ul>
+    </NxTile.Subsection>
   </GalleryTile>;
 
 export default AdditionalResources;
--- gallery/src/pages/StylingComponents.tsx
+++ gallery/src/pages/StylingComponents.tsx
@@ -6,135 +6,130 @@
  */
 import React from 'react';
 import {GalleryTile} from '../gallery-components/GalleryTiles';
+import { NxP, NxH3, NxH4, NxCode, NxTile, NxTextLink } from '@sonatype/react-shared-components';
 
 const StylingRSCPage = () =>
   <GalleryTile title="How to style your app when using RSC">
-    <p className="nx-p">
+    <NxP>
       If you have styling needs that aren't covered by the styles included in React Shared Components (RSC),
       then you're going to have to create your own styles.
-    </p>
-    <section className="nx-tile-subsection">
-      <header className="nx-tile-subsection__header">
-        <h3 className="nx-h3">Page &amp; component specific stylesheets</h3>
-      </header>
-      <p className="nx-p">
+    </NxP>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Page &amp; component specific stylesheets</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
         Why have page and component specific stylesheets? By grouping our styles together by use and (ideally) locating
         the SCSS next to the HTML that it modifies we decrease clutter/noise in our main stylesheet, keep our styles
         focused on what they modify, and also help maintain a clear separation between code, layout, and styling.
-      </p>
-      <h4 className="nx-h4">Modifying component styles</h4>
-      <p className="nx-p">
+      </NxP>
+      <NxH4>Modifying component styles</NxH4>
+      <NxP>
         If you need to modify any of the stock RSC styles for use within your project you should create either a single
         file to contain your modifications or keep them within a page specific stylesheet if the changes are localised
-        to a page. For example in IQ has an SCSS file named <code className="nx-code">_nx-overrides.scss</code> in
+        to a page. For example in IQ has an SCSS file named <NxCode>_nx-overrides.scss</NxCode> in
         which all IQ specific modifications to RSC styles are stored. If you are starting a brand new project you
         probably won't need to do this but for existing projects where RSC styles will be mixing with legacy styles it
         can be invaluable.
-      </p>
-    </section>
-    <section className="nx-tile-subsection">
-      <header className="nx-tile-subsection__header">
-        <h3 className="nx-h3">B.E.M.</h3>
-      </header>
-      <p className="nx-p">
+      </NxP>
+    </NxTile.Subsection>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>B.E.M.</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
         You might have noticed lots of dashes and underscores in our class names. That's because we use BEM naming
         by default. BEM stands for "Block", "Element", and "Modifier".{' '}
-        <a href="https://www.toptal.com/css/introduction-to-bem-methodology" target="_blank">Learn more about BEM</a>.
+        <NxTextLink external href="https://www.toptal.com/css/introduction-to-bem-methodology">
+          Learn more about BEM
+        </NxTextLink>.
         We use a slightly modified version of BEM in the RSC; we have added namespaces and utility classes.
-      </p>
-    </section>
-    <section className="nx-tile-subsection">
-      <header className="nx-tile-subsection__header">
-        <h3 className="nx-h3">Namespaces</h3>
-      </header>
-      <p className="nx-p">
+      </NxP>
+    </NxTile.Subsection>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Namespaces</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
         Because the RSC styles are used by multiple apps we wanted to clearly distinguish between RSC styles and
         custom app styles. To that end we use a namespace prefix in our class names. In RSC that prefix
-        is <code className="nx-code">nx-</code>. In IQ they use <code className="nx-code">iq-</code> as
+        is <NxCode>nx-</NxCode>. In IQ they use <NxCode>iq-</NxCode> as
         a prefix in order to differentiate between classes in IQ and classes from RSC. When you
         create custom CSS in your app you should create a simple unique prefix for your app.
-      </p>
-    </section>
-    <section className="nx-tile-subsection">
-      <header className="nx-tile-subsection__header">
-        <h3 className="nx-h3">Utility classes</h3>
-      </header>
-      <p className="nx-p">
+      </NxP>
+    </NxTile.Subsection>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>Utility classes</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
         You may notice as you work with the various RSC that some components have CSS classes that don't follow
         normal BEM naming conventions. Classes like:
-      </p>
+      </NxP>
       <div className="nx-list nx-list--bulleted">
         <ul>
           <li className="nx-list__item">
-            <code className="nx-code">open</code> &amp; <code className="nx-code">closed</code>
+            <NxCode>open</NxCode> &amp; <NxCode>closed</NxCode>
           </li>
           <li className="nx-list__item">
-            <code className="nx-code">disabled</code>
+            <NxCode>disabled</NxCode>
           </li>
           <li className="nx-list__item">
-            <code className="nx-code">pristine</code>, <code className="nx-code">valid</code>,
-            {' '}<code className="nx-code">invalid</code>
+            <NxCode>pristine</NxCode>, <NxCode>valid</NxCode>,
+            {' '}<NxCode>invalid</NxCode>
           </li>
           <li className="nx-list__item">
-            <code className="nx-code">selected</code> &amp; <code className="nx-code">unselected</code>
+            <NxCode>selected</NxCode> &amp; <NxCode>unselected</NxCode>
           </li>
         </ul>
       </div>
-      <p className="nx-p">
+      <NxP>
         These are commonly refered to as "utility classes". Utility classes usually describe a change in a components'
         visual state. These classes are common across all components that might need them, especially in the case of the
         validation utility classes.
-      </p>
-    </section>
-    <section className="nx-tile-subsection">
-      <header className="nx-tile-subsection__header">
-        <h3 className="nx-h3">CSS Resources</h3>
-      </header>
-      <p className="nx-p">
+      </NxP>
+    </NxTile.Subsection>
+    <NxTile.Subsection>
+      <NxTile.SubsectionHeader>
+        <NxH3>CSS Resources</NxH3>
+      </NxTile.SubsectionHeader>
+      <NxP>
         Many components in the RSC are laid out using CSS Flexbox or CSS Grid. Flexbox is well
         established, but Grid is quite new. The syntax for both can be a little confusing for those who aren't used to
         them. There are many resources available on the web to help new users, the ones linked below are used by the RSC
         team for reference.
-      </p>
-      <h4 className="nx-h4">Flexbox</h4>
+      </NxP>
+      <NxH4>Flexbox</NxH4>
       <ul className="nx-list nx-list--bulleted">
         <li className="nx-list__item">
-          <a rel="noreferrer"
-             href="https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Flexbox"
-             target="_blank">
+          <NxTextLink external href="https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Flexbox">
             MDN Layout Guide: Flexbox
-          </a>
+          </NxTextLink>
         </li>
         <li className="nx-list__item">
-          <a rel="noreferrer"
-             href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/"
-             target="_blank">
+          <NxTextLink external href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/">
             CSS Tricks Guide to CSS Flexbox
-          </a>
+          </NxTextLink>
         </li>
       </ul>
       <h4 className="nx-h4">CSS Grid</h4>
       <ul className="nx-list nx-list--bulleted">
         <li className="nx-list__item">
-          <a rel="noreferrer"
-             href="https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Grids"
-             target="_blank">
+          <NxTextLink external href="https://developer.mozilla.org/en-US/docs/Learn/CSS/CSS_layout/Grids">
             MDN Layout Guide: CSS Grid
-          </a>
+          </NxTextLink>
         </li>
         <li className="nx-list__item">
-          <a rel="noreferrer"
-             href="https://css-tricks.com/snippets/css/complete-guide-grid/"
-             target="_blank">
+          <NxTextLink external href="https://css-tricks.com/snippets/css/complete-guide-grid/">
             CSS Tricks Guide to CSS Grid
-          </a>
+          </NxTextLink>
         </li>
       </ul>
-      <p className="nx-p">
+      <NxP>
         If you have questions about how to use RSC's styles in your app, or how to create customs style for your app
         #react-components on Slack is a good place to ask.
-      </p>
-    </section>
+      </NxP>
+    </NxTile.Subsection>
   </GalleryTile>;
 
 export default StylingRSCPage;
