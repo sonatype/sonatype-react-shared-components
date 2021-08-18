232e2041b24fc492f1c5d7556a7275017e0029db
Ross Pokorny <rpokorny@sonatype.com>
2021-08-18 12:03:47 -0400

    Revert "Revert "Add visual test - RSC-651""
    This reverts commit d6e39e2bd7dca9997a648ce8a92f7455478d19fb.

--- gallery/src/components/NxModal/NxModalFormExample.tsx
+++ gallery/src/components/NxModal/NxModalFormExample.tsx
@@ -37,7 +37,7 @@ export default function NxModalFormExample() {
 
     setTimeout(function() {
       setLoading(false);
-    }, 1000);
+    }, 2000);
   }
 
   function onUsernameChange(val: string) {
