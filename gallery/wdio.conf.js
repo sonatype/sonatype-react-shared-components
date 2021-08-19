a4fe852f6fcd0624bcb100b105c2bb3b03a15c5b
Ross Pokorny <rpokorny@sonatype.com>
2021-08-19 09:44:02 -0400

    Remove accidental visual test file filter - RSC-651

--- gallery/wdio.conf.js
+++ gallery/wdio.conf.js
@@ -46,7 +46,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './visualtests/nxModal.js'
+        './visualtests/*.js'
     ],
     // Patterns to exclude.
     exclude: [
