2dbf977bf7499fe8627b4c827efd28d49073ed3a
Ross Pokorny <rpokorny@sonatype.com>
2021-08-19 09:36:02 -0400

    Version bump - RSC-651

--- gallery/wdio.conf.js
+++ gallery/wdio.conf.js
@@ -46,7 +46,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './visualtests/*.js'
+        './visualtests/nxModal.js'
     ],
     // Patterns to exclude.
     exclude: [
