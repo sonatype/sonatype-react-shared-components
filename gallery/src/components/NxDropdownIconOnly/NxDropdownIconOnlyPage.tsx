1f69f847ab5f9427fe89cbfe8c1ee5b4c4924ff5
Glen Hunter <ghunter@sonatype.com>
2021-08-23 12:35:50 -0400

    Documentation tweaks RSC-660

--- gallery/src/components/NxDropdownIconOnly/NxDropdownIconOnlyPage.tsx
+++ gallery/src/components/NxDropdownIconOnly/NxDropdownIconOnlyPage.tsx
@@ -5,9 +5,9 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { NxTable, NxTextLink, NxCode, NxH3, NxP, NxTile } from '@sonatype/react-shared-components';
-
+import { NxTable, NxTextLink, NxCode, NxH3, NxP, NxTile, NxFontAwesomeIcon } from '@sonatype/react-shared-components';
 import {GalleryDescriptionTile, GalleryExampleTile} from '../../gallery-components/GalleryTiles';
+import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 
 import NxDropdownIconOnlyNavigationExample from './NxDropdownIconOnlyNavigationExample';
 import NxDropdownIconOnlyLinksExample from './NxDropdownIconOnlyLinksExample';
@@ -48,17 +48,20 @@ const NxDropdownPage = () =>
               <NxTable.Cell>icon</NxTable.Cell>
               <NxTable.Cell>string | VDOM</NxTable.Cell>
               <NxTable.Cell>Yes</NxTable.Cell>
-              <NxTable.Cell>The name of the FontAwesome icon to use in the button.</NxTable.Cell>
+              <NxTable.Cell>
+                The name of the FontAwesome icon to use in the button. The default is <NxCode>faEllipsisV</NxCode>
+                {' '} <NxFontAwesomeIcon icon={faEllipsisV}></NxFontAwesomeIcon>.
+              </NxTable.Cell>
             </NxTable.Row>
             <NxTable.Row>
-              <NxTable.Cell>toggleTooltip</NxTable.Cell>
+              <NxTable.Cell>title</NxTable.Cell>
               <NxTable.Cell>string | NxTooltip Props</NxTable.Cell>
               <NxTable.Cell>Yes</NxTable.Cell>
               <NxTable.Cell>
-                Describes a tooltip to be placed on the dropdowns' toggle element. For accessibility reasons this prop
-                is required on <NxCode>NxDropdownIconOnly</NxCode>. There are two ways to specify the tooltip: the
-                simpler way is to simply specify the tooltip text as a string. If control of more complex tooltip
-                options is desired, an object can be passed which will serve as the props for
+                Describes the text for a tooltip to be placed on the dropdowns' toggle element. For accessibility
+                reasons this prop is required on <NxCode>NxDropdownIconOnly</NxCode>. There are two ways to specify the
+                tooltip: the simpler way is to simply specify the tooltip text as a string. If control of more complex
+                tooltip options is desired, an object can be passed which will serve as the props for
                 {' '}<NxCode>NxTooltip</NxCode>.
               </NxTable.Cell>
             </NxTable.Row>
