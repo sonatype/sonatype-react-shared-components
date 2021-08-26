7a71879de601e80541895211818fe1c7ff932311
Glen Hunter <ghunter@sonatype.com>
2021-08-26 11:45:00 -0400

    Stateful docs RSC-660

--- gallery/src/components/NxStatefulIconDropdown/NxStatefulIconDropdownPage.tsx
+++ gallery/src/components/NxStatefulIconDropdown/NxStatefulIconDropdownPage.tsx
@@ -5,8 +5,7 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { NxTable, NxCode, NxP, NxTextLink, NxFontAwesomeIcon } from '@sonatype/react-shared-components';
-import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
+import { NxCode, NxP, NxTextLink } from '@sonatype/react-shared-components';
 
 import {GalleryDescriptionTile, GalleryExampleTile} from '../../gallery-components/GalleryTiles';
 
@@ -18,41 +17,10 @@ const NxStatefulIconDropdownPage = () =>
   <>
     <GalleryDescriptionTile>
       <NxP>
-        A variation of the icon only dropdown menu component which uses a single icon as its toggle.{' '}
-        <NxCode>NxStatefulIconDropdown</NxCode> shares all of the same props as{' '}
-        <NxTextLink href="#/pages/NxIconDropdown">NxIconDropdown</NxTextLink>{' '}
-        as well as the ones listed below.
+        A dropdown menu component which uses a single icon as its toggle and therefore resembles an icon-only
+        button. <NxCode>NxStatefulIconDropdown</NxCode> shares all of the same props as{' '}
+        <NxTextLink href="#/pages/NxIconDropdown">NxIconDropdown</NxTextLink>.
       </NxP>
-      <NxTable>
-        <NxTable.Head>
-          <NxTable.Row>
-            <NxTable.Cell>Prop</NxTable.Cell>
-            <NxTable.Cell>Type</NxTable.Cell>
-            <NxTable.Cell>Required</NxTable.Cell>
-            <NxTable.Cell>Details</NxTable.Cell>
-          </NxTable.Row>
-        </NxTable.Head>
-        <NxTable.Body>
-          <NxTable.Row>
-            <NxTable.Cell>icon</NxTable.Cell>
-            <NxTable.Cell>FontAwesome Icon</NxTable.Cell>
-            <NxTable.Cell>Yes</NxTable.Cell>
-            <NxTable.Cell>
-              The FontAwesome icon to use in the button. The default is <NxCode>faEllipsisV</NxCode>
-              {' '} <NxFontAwesomeIcon icon={faEllipsisV}></NxFontAwesomeIcon>.
-            </NxTable.Cell>
-          </NxTable.Row>
-          <NxTable.Row>
-            <NxTable.Cell>title</NxTable.Cell>
-            <NxTable.Cell>string | NxTooltip Props</NxTable.Cell>
-            <NxTable.Cell>Yes</NxTable.Cell>
-            <NxTable.Cell>
-              Describes the text for a tooltip to be placed on the dropdowns' toggle element. For accessibility
-              reasons this prop is required on <NxCode>NxStatefulIconDropdown</NxCode>.
-            </NxTable.Cell>
-          </NxTable.Row>
-        </NxTable.Body>
-      </NxTable>
     </GalleryDescriptionTile>
 
     <GalleryExampleTile title="General Example"
