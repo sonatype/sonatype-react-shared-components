d8432d36c8c4f5e3fe5532155e0a81f8eb259ce4
Andrew Prasetya <aprasetya@sonatype.com>
2021-10-18 10:24:25 -0400

    Progress on header. - RSC-581

---
+++ gallery/src/PageHeader/PageHeader.tsx
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import React from 'react';
+
+import packageJson from '../../package.json';
+
+const plaidVillain = require('./plaid-villain.svg');
+const tartanLogotype = require('./tartan-logotype.svg');
+
+import './PageHeader.scss';
+
+const PageHeader = () => (
+  <header className="nx-gallery-page-header">
+    <h1 className="nx-gallery-page-header__logo">
+      <a className="nx-gallery-page-header__home-link"
+         href="#"
+         aria-label="home">
+        <img className="nx-gallery-page-header__logo-image"
+             src={plaidVillain}
+             alt="A cute but villainous Tartan logo" />
+        <img className="nx-gallery-page-header__logotype-image"
+             src={tartanLogotype}
+             alt="Sonatype Tartan" />
+        <span className="nx-gallery-page-header__name">
+          React Shared Component Library
+        </span>
+      </a>
+    </h1>
+    <div className="nx-gallery-page-header__version">
+      Version {packageJson.version}
+    </div>
+  </header>
+);
+
+export default PageHeader;
