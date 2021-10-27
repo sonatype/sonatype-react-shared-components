ae555d6efd09db6cfad8eb8611d5d1378ddee238
Andrew Prasetya <aprasetya@sonatype.com>
2021-10-27 09:47:56 -0400

    Fixed eslint issue. - RSC-709

--- gallery/src/components/NxTooltip/NxTooltipPage.tsx
+++ gallery/src/components/NxTooltip/NxTooltipPage.tsx
@@ -54,7 +54,8 @@ export default function NxTooltipPage() {
               <NxTable.Cell>No</NxTable.Cell>
               <NxTable.Cell>
                 Which side of the element the tooltip should render on. Defaults to top. "top" and "bottom" position
-                the tooltip flush to the left edge of the element, "top-middle" and "bottom-middle" center the tooltip, while "top-end" and "bottom-end" position it flush to the right edge.
+                the tooltip flush to the left edge of the element, "top-middle" and "bottom-middle" center the tooltip,
+                while "top-end" and "bottom-end" position it flush to the right edge.
               </NxTable.Cell>
             </NxTable.Row>
             <NxTable.Row>
