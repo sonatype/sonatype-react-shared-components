ce1d71c0bc3119765562d2cd088ffab9585fc1c3
Ross Pokorny <rpokorny@sonatype.com>
2021-10-28 09:45:47 -0400

    Update nxTree

--- gallery/visualtests/nxTree.js
+++ gallery/visualtests/nxTree.js
@@ -4,13 +4,10 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-const { clickTest, focusTest, simpleTest, simpleTestLongElement } = require('./testUtils');
+const { setupBrowser } = require('./testUtils');
 
 describe('NxTree', function() {
-  beforeEach(async function() {
-    await browser.url('#/pages/NxTree');
-    await browser.refresh();
-  });
+  const { clickTest, focusTest, simpleTest, blurElement } = setupBrowser('#/pages/NxTree');
 
   const nonCollapsibleExampleSelector = '#nx-tree-non-collapsible-example .nx-tree',
       collapsibleExampleSelector = '#nx-tree-collapsible-example .gallery-example-live > .nx-tree',
@@ -21,7 +18,7 @@ describe('NxTree', function() {
           .nx-tree > .nx-tree__item > .nx-tree > .nx-tree__item > .nx-tree > .nx-tree__item:nth-child(2) >
           .nx-tree__collapse-label`;
 
-  it('looks right with a single top entry and no collapsing', simpleTestLongElement(nonCollapsibleExampleSelector));
+  it('looks right with a single top entry and no collapsing', (nonCollapsibleExampleSelector));
   it('looks right with a single top entry and collapsing', simpleTest(collapsibleExampleSelector));
   it('looks right with a multiple top entries and no collapsing', simpleTest(nonCollapsibleMultiTopExampleSelector));
   it('looks right with a multiple top entries and collapsing', simpleTest(collapsibleMultiTopExampleSelector));
@@ -35,19 +32,17 @@ describe('NxTree', function() {
             .nx-tree__item:last-child .nx-tree__item .nx-tree__item:last-child .nx-tree__item:last-child
             .nx-tree__collapse-label`,
         input2Selector = `${collapseControlSelector2} input`,
-        [tree, collapseControl1, collapseControl2, collapseInput2] = await Promise.all([
-          browser.$(collapsibleMultiTopExampleSelector),
-          browser.$(collapseControlSelector1),
-          browser.$(collapseControlSelector2),
-          browser.$(input2Selector)
-        ]);
+      [tree, collapseControl1, collapseControl2, collapseInput2] = await waitAndGetElements(
+          collapsibleMultiTopExampleSelector,
+          collapseControlSelector1,
+          collapseControlSelector2,
+          input2Selector
+      );
 
     await collapseControl1.scrollIntoView({ block: 'center' });
     await collapseControl1.click();
     await collapseControl2.click();
-    await browser.execute(function(el) {
-      el.blur();
-    }, collapseInput2);
+    await blurElement(collapseInput2);
 
     await simpleTest(collapsibleExampleSelector)();
   });
