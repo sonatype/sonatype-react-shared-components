52b176d42882a60d1724d770baac1a8937c3b70c
Andrew Prasetya <aprasetya@sonatype.com>
2021-11-05 16:29:12 -0400

    Added test. - RSC-622

--- lib/src/util/__tests__/withClass.test.tsx
+++ lib/src/util/__tests__/withClass.test.tsx
@@ -4,12 +4,25 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import React from 'react';
+import { mount } from 'enzyme';
 import withClass from '../withClass';
 import { getShallowComponent } from '../../__testutils__/enzymeUtils';
 
 describe('withClass', function() {
   const ExampleComponent = withClass('hgroup', 'foo-bar'),
       getShallow = getShallowComponent(ExampleComponent, {});
+
+  it('forwards a ref to the specified label element', function() {
+    const ExampleLabelComponent = withClass('label', 'foo-bar');
+    const ref = React.createRef<HTMLLabelElement>();
+
+    // note: the fragment is necessary to get around an enzyme issue:
+    // https://github.com/enzymejs/enzyme/issues/1852#issuecomment-433145879
+    const label = mount(<><ExampleLabelComponent ref={ref}>foo</ExampleLabelComponent></>);
+
+    expect(ref.current).toBe(label.getDOMNode());
+  });
 
   it('creates a component constructor that makes an element with the specified tag and class', function() {
     expect(getShallow()).toMatchSelector('hgroup.foo-bar');
