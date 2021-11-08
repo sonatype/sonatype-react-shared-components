9a1b019c160b279547337fc6a3b1b1f6feae24f0
Andrew Prasetya <aprasetya@sonatype.com>
2021-11-08 10:23:59 -0500

    Removed extra whitespaces. - RSC-622

--- lib/src/util/withClass.tsx
+++ lib/src/util/withClass.tsx
@@ -9,8 +9,8 @@ import classnames from 'classnames';
 
 type NativeElType<E extends keyof JSX.IntrinsicElements> =
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  JSX.IntrinsicElements[E] extends DetailedHTMLProps<any, infer A> ? A : 
-  JSX.IntrinsicElements[E] extends SVGProps<infer B> ? B : 
+  JSX.IntrinsicElements[E] extends DetailedHTMLProps<any, infer A> ? A :
+  JSX.IntrinsicElements[E] extends SVGProps<infer B> ? B :
   never;
 
 export default function withClass<E extends keyof JSX.IntrinsicElements>(
