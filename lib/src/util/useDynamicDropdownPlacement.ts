7ffe0d9b9aa4bce3796d4823c2bc61775ba985d5
Abhinav Bhatta <abhatta@sonatype.com>
2021-12-14 08:39:48 -0500

    Update comments

--- lib/src/util/useDynamicDropdownPlacement.ts
+++ lib/src/util/useDynamicDropdownPlacement.ts
@@ -7,10 +7,10 @@
 import { useState, useEffect, RefObject } from 'react';
 
 /**
- * Convenience hook that determins whether a dropdown menu should open on the top or on the right
+ * Convenience hook that determines whether a dropdown menu should open on the top or on the right
  * instead of opening the menu on the bottom or on the left respectively.
  * This custom hook basically checks the position of the dropdown based on where it is in the current page,
- * and determines if the dropdown menu should be placed on the top or on the right.
+ * and determines if the dropdown menu should be placed on the top or on the right based on scroll or resize.
  * @param dropdownDivRef the ref to the div that holds the dropdown menu.
  * @param childrenCount the number of items the dropdown menu contains to dynamically allocate spacing.
  * @return a [boolean, boolean] if the dropdown menu should be placed on top and on the right respectively.
@@ -26,6 +26,10 @@ const useDynamicDropdownPlacement = (
   const [openTop, setOpenTop] = useState<boolean>(false);
   const [openRight, setOpenRight] = useState<boolean>(false);
 
+  // The event listeners look for changes to be made to the dropdown position if the user
+  // changes the window size by either scrolling the page or resizing the page.
+  // childrenCount is added as a dependency because in components like NxSearchDropdown, the height of the
+  // menu depends on the number of results/menu items shown in the component.
   useEffect(() => {
     window.addEventListener('scroll', determinePlacement);
     window.addEventListener('resize', determinePlacement);
@@ -42,7 +46,14 @@ const useDynamicDropdownPlacement = (
       const rect = dropdownDivRef.current.getBoundingClientRect();
       const offsetHeight = rect.bottom + menuHeight;
 
+      // The menu is rendered on the bottom by default.
+      // If the bottom of the dropdown goes beyond the window height, open the menu on top,
+      // i.e. setOpenTop(true), which adds the class 'nx-dropdown-menu__open-top' to 'nx-dropdown-menu'
       offsetHeight >= windowHeight ? setOpenTop(true) : setOpenTop(false);
+
+      // The menu is rendered on the left by default.
+      // If the x position goes beyond the viewport, render the menu on the right,
+      // i.e. setOpenRight(true), which adds the class 'nx-dropdown-menu__open-right' to 'nx-dropdown-menu'
       rect.x - ICON_ONLY_DROPDOWN_WIDTH < 0 ? setOpenRight(true) : setOpenRight(false);
     }
   };
