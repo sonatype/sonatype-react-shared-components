556b7ad89d27ab1ec20826e8763bf468cc5a0e6c
Andrew Prasetya <aprasetya@sonatype.com>
2022-01-05 15:02:18 -0500

    Added custom styling for NxList when it is nested inside NxAccordion. - RSC-824

--- gallery/src/components/NxAccordion/NxAccordionPage.tsx
+++ gallery/src/components/NxAccordion/NxAccordionPage.tsx
@@ -12,10 +12,12 @@ import { GalleryDescriptionTile, GalleryExampleTile } from '../../gallery-compon
 import NxAccordionSimpleExample from './NxAccordionExample';
 import NxAccordionComplexExample from './NxAccordionComplexExample';
 import NxAccordionTertiaryButtonExample from './NxAccordionTertiaryButtonExample';
+import NxAccordionWithNxListExample from './NxAccordionWithNxListExample';
 
 const NxAccordionSimpleCode = require('./NxAccordionExample?raw'),
     NxAccordionComplexCode = require('./NxAccordionComplexExample?raw'),
-    NxAccordionTertiaryButtonCode = require('./NxAccordionTertiaryButtonExample?raw');
+    NxAccordionTertiaryButtonCode = require('./NxAccordionTertiaryButtonExample?raw'),
+    NxAccordionWithNxListCode = require('./NxAccordionWithNxListExample?raw');
 
 const NxAccordionPage = () =>
   <>
@@ -158,6 +160,14 @@ const NxAccordionPage = () =>
                         liveExample={NxAccordionSimpleExample}
                         codeExamples={NxAccordionSimpleCode}>
       A simple example of an <NxCode>NxAccordion</NxCode>.
+    </GalleryExampleTile>
+
+    <GalleryExampleTile title="Example with nested NxList"
+                        id="nx-accordion-nested-nx-list-example"
+                        defaultCheckeredBackground={true}
+                        liveExample={NxAccordionWithNxListExample}
+                        codeExamples={NxAccordionWithNxListCode}>
+      An example of an <NxCode>NxAccordion</NxCode> with nested <NxCode>NxList</NxCode>.
     </GalleryExampleTile>
 
     <GalleryExampleTile title="Example with optional elements"
