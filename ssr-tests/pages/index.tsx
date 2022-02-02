6a55aa2a12dbf5d5ad08243cff4f280e43c72cfa
Ross Pokorny <rpokorny@sonatype.com>
2022-02-02 17:49:51 -0500

    Bring in nextjs server setup - RSC-859

---
+++ ssr-tests/pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { NxStableUniqueIdContext, NxButton, NxModal, NxP, NxPageHeader, NxPageMain, useUniqueId }
+  from '@sonatype/react-shared-components';
+import { useState } from 'react';
+
+const Home: NextPage = () => {
+  const [showModal, setShowModal] = useState(false);
+  const modalCloseHandler = () => setShowModal(false);
+  const id = useUniqueId('foo');
+
+  return (
+    <div className="nx-page" id={id}>
+      <NxPageHeader />
+      <NxPageMain>
+        <NxButton onClick={() => setShowModal(true)}>
+          Advanced Search' Options
+        </NxButton>
+
+        {showModal && (
+          <NxModal onCancel={() => setShowModal(false)}>foo</NxModal>
+        )}
+      </NxPageMain>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <NxStableUniqueIdContext>
+      <Home />
+    </NxStableUniqueIdContext>
+  );
+}
