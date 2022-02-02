e735465edc292e4bec2b4ca8efca67aee8a6fe1a
Ross Pokorny <rpokorny@sonatype.com>
2022-02-02 13:58:31 -0500

    Implement stable id context - RSC-872

---
+++ lib/src/components/NxStableUniqueIdContext/NxStableUniqueIdContext.tsx
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import React, { ProviderProps } from 'react';
+import { StableUniqueIdContext } from '../../util/idUtil';
+
+export default function NxStableUniqueIdContext(props: Omit<ProviderProps<never>, 'value'>) {
+  return <StableUniqueIdContext.Provider value={{ value: 0 }} { ...props } />;
+}
