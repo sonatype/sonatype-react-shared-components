fcd4cf4747b407e2570394b03c318b45edb22c6c
Ross Pokorny <rpokorny@sonatype.com>
2022-02-02 09:29:40 -0500

    Only load node crypto on server - RSC-867

--- server/pages/crypto-polyfill.js
+++ server/pages/crypto-polyfill.js
@@ -1,9 +1,13 @@
 // Polyfill Webcrypto APIs expected by @sonatype/react-shared-components
 
-const nodeCrypto = require('crypto');
+// Without this conditional, webpack will polyfill the nodejs crypto library on the client using crypto-browserify.
+// This pulls in a huge amount of dependency code that is 100% unneeded
+if (!process.browser) {
+  const nodeCrypto = require('crypto');
 
-global.crypto = {
-  getRandomValues(typedArray) {
-    nodeCrypto.randomFillSync(typedArray);
-  }
-};
+  global.crypto = {
+    getRandomValues(typedArray) {
+      nodeCrypto.randomFillSync(typedArray);
+    }
+  };
+}
