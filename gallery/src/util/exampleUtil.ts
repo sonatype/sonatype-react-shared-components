78d0064b85aec5a12d11497eeafcfa1b5ea14474
Abhinav Bhatta <abhatta@sonatype.com>
2022-02-07 06:18:41 -0700

    Create new exampleUtil.js file

---
+++ gallery/src/util/exampleUtil.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+
+// Quick and dirty removal of any comment appearing at the beginning of the content, as it is assumed to be
+// a license which is not helpful to the example display. Both HTML-syntax and C-syntax comments are removed
+export const removeLicense = (content: string) =>
+  content
+      .replace(/^<!--(.|\n)*?-->\s*\n?/, '')
+      .replace(/^\/\*(.|\n)*?\*\/\s*\n?/, '');
+
+// Copies a string to the clipboard.
+export const copyTextToClipboard = (textToCopy: string) => {
+  // navigator clipboard api needs a secure context (https)
+  if (navigator.clipboard && window.isSecureContext) {
+    // navigator clipboard api method'
+    navigator.clipboard.writeText(textToCopy);
+  }
+  else {
+    // text area method
+    const textArea = document.createElement('textarea');
+    textArea.value = textToCopy;
+    // make the textarea not visible
+    textArea.style.position = 'fixed';
+    textArea.style.opacity = '0';
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+  }
+};
