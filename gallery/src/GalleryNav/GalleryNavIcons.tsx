c438b43bdd09ecd4b3202e00e519494b7874d9d2
Glen Hunter <ghunter@sonatype.com>
2022-02-07 16:38:33 -0500

    move icon size to css, adjust size and position RSC-811

--- gallery/src/GalleryNav/GalleryNavIcons.tsx
+++ gallery/src/GalleryNav/GalleryNavIcons.tsx
@@ -8,8 +8,6 @@ import React from 'react';
 
 export const GalleryNavIconCSS3 = () => (
   <svg className="nx-icon gallery-nav-icon gallery-nav-icon--css3"
-       width="20"
-       height="20"
        viewBox="0 0 20 20"
        fill="none"
        xmlns="http://www.w3.org/2000/svg">
