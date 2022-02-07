492e2a6f81dd3ad4c95833298a63274b7eeb52fd
Glen Hunter <ghunter@sonatype.com>
2022-02-07 16:41:57 -0500

    SVG fixes RSC-811

--- gallery/src/GalleryNav/GalleryNavIcons.tsx
+++ gallery/src/GalleryNav/GalleryNavIcons.tsx
@@ -7,15 +7,9 @@
 import React from 'react';
 
 export const GalleryNavIconCSS3 = () => (
-  <svg className="nx-icon gallery-nav-icon gallery-nav-icon--css3"
-       viewBox="0 0 20 20"
-       fill="none"
-       xmlns="http://www.w3.org/2000/svg">
-    <path d="M1.5 7.5H6.5V9.16667H3.16667V11.6667H6.5V13.3333H1.5V7.5Z"
-          fill="currentcolor" />
-    <path d="M13.1667 7.5H18.1667V8.95833H14.8334V9.79167H18.1667V13.3333H13.1667V11.875H16.5001V11.0417H13.1667V7.5Z"
-          fill="currentcolor"/>
-    <path d="M7.33325 7.5H12.3333V8.95833H8.99992V9.79167H12.3333V13.3333H7.33325V11.875H10.6666V11.0417H7.33325V7.5Z"
-          fill="currentcolor"/>
+  <svg className="nx-icon gallery-nav-icon gallery-nav-icon--css3" viewBox="0 0 20 20">
+    <path d="M1.5 7.5H6.5V9.16667H3.16667V11.6667H6.5V13.3333H1.5V7.5Z" />
+    <path d="M13.1667 7.5H18.1667V8.95833H14.8334V9.79167H18.1667V13.3333H13.1667V11.875H16.5001V11.0417H13.1667V7.5Z"/>
+    <path d="M7.33325 7.5H12.3333V8.95833H8.99992V9.79167H12.3333V13.3333H7.33325V11.875H10.6666V11.0417H7.33325V7.5Z"/>
   </svg>
 );
