c317082dbd30979bda4d250e32a51adf56df77e8
Ross Pokorny <rpokorny@sonatype.com>
2022-02-08 15:15:55 -0500

    Run SSR tests twice - RSC-859

--- ssr-tests/__tests__/index.js
+++ ssr-tests/__tests__/index.js
@@ -1,10 +1,11 @@
 describe('SSR Test Page', function() {
-  it('renders without error', async function() {
+  async function renderTest() {
+    await browser.refresh();
     await browser.url('/');
 
     const mainEl = await browser.$('.nx-page-main'),
-        logs = await browser.getLogs('browser'),
-        errorLogs = logs.filter(({ level }) => level === 'SEVERE');
+      logs = await browser.getLogs('browser'),
+      errorLogs = logs.filter(({ level }) => level === 'SEVERE');
 
     if (logs.length) {
       console.log('logs', logs);
@@ -14,5 +15,10 @@ describe('SSR Test Page', function() {
 
     // hydration errors (such as id mismatches) will show up as console errors but the page will still render
     expect(errorLogs.length).toBe(0);
-  });
+  }
+
+  it('renders without error', renderTest);
+
+  // some hydration errors might only show up during successive page loads from a single server run
+  it('renders without error again', renderTest);
 });
