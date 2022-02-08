b27213f4648f7de94bcbe6848371f7438b27754f
Ross Pokorny <rpokorny@sonatype.com>
2022-02-08 15:38:08 -0500

    Attempt to fix license checker problems - RSC-859

--- ssr-tests/pages/_document.tsx
+++ ssr-tests/pages/_document.tsx
@@ -1,4 +1,11 @@
 /*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+
+/*
  * Derived from https://nextjs.org/docs/advanced-features/custom-document .
  * Necessary to add nx-html and nx-body classes
  */
