ae1fafab2e2b63ff879cb527e211ad4a7eab938f
Ross Pokorny <rpokorny@sonatype.com>
2022-02-08 15:08:24 -0500

    Assert no console errors - RSC-859

--- ssr-tests/__tests__/index.js
+++ ssr-tests/__tests__/index.js
@@ -2,8 +2,17 @@ describe('SSR Test Page', function() {
   it('renders without error', async function() {
     await browser.url('/');
 
-    const mainEl = await browser.$('.nx-page-main');
+    const mainEl = await browser.$('.nx-page-main'),
+        logs = await browser.getLogs('browser'),
+        errorLogs = logs.filter(({ level }) => level === 'SEVERE');
+
+    if (logs.length) {
+      console.log('logs', logs);
+    }
 
     expect(mainEl).toBeDefined();
+
+    // hydration errors (such as id mismatches) will show up as console errors but the page will still render
+    expect(errorLogs.length).toBe(0);
   });
 });
