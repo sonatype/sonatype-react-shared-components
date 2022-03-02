bba637a2afec08f975fe2ba0f7cfaaf7b1f00995
Ross Pokorny <rpokorny@sonatype.com>
2022-03-02 08:46:36 -0500

    Add h1s to global header examples - RSC-911

--- gallery/src/styles/NxGlobalHeader/NxGlobalHeaderNoBackButtonExample.tsx
+++ gallery/src/styles/NxGlobalHeader/NxGlobalHeaderNoBackButtonExample.tsx
@@ -11,7 +11,10 @@ import {
   NxButton,
   NxFontAwesomeIcon,
   NxStatefulGlobalSidebar,
-  NxPageMain
+  NxPageMain,
+  NxP,
+  NxPageTitle,
+  NxH1
 } from '@sonatype/react-shared-components';
 import { faArrowLeft, faArrowRight, faLink, faEdit } from '@fortawesome/free-solid-svg-icons';
 
@@ -37,16 +40,21 @@ export default function NxGlobalHeaderNoBackButtonExample() {
         </NxGlobalSidebarNavigation>
       </NxStatefulGlobalSidebar>
       <NxPageMain>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-        dolore magna aliqua. Tortor consequat id porta nibh venenatis cras. Proin libero nunc consequat interdum
-        varius sit amet mattis. Enim praesent elementum facilisis leo vel. A arcu cursus vitae congue mauris
-        rhoncus aenean vel elit. Morbi tincidunt ornare massa eget egestas. Semper auctor neque vitae tempus quam
-        pellentesque nec nam aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Sit amet aliquam id diam
-        maecenas. Porta nibh venenatis cras sed felis eget velit aliquet. Semper feugiat nibh sed pulvinar proin
-        gravida hendrerit lectus a. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Purus non enim
-        praesent elementum facilisis leo. Dui sapien eget mi proin. Ultrices eros in cursus turpis massa tincidunt
-        dui ut ornare. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Turpis egestas maecenas
-        pharetra convallis posuere morbi leo urna.
+        <NxPageTitle>
+          <NxH1>Lorem Ipsum</NxH1>
+        </NxPageTitle>
+        <NxP>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+          dolore magna aliqua. Tortor consequat id porta nibh venenatis cras. Proin libero nunc consequat interdum
+          varius sit amet mattis. Enim praesent elementum facilisis leo vel. A arcu cursus vitae congue mauris
+          rhoncus aenean vel elit. Morbi tincidunt ornare massa eget egestas. Semper auctor neque vitae tempus quam
+          pellentesque nec nam aliquam. Faucibus nisl tincidunt eget nullam non nisi est sit. Sit amet aliquam id diam
+          maecenas. Porta nibh venenatis cras sed felis eget velit aliquet. Semper feugiat nibh sed pulvinar proin
+          gravida hendrerit lectus a. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Purus non enim
+          praesent elementum facilisis leo. Dui sapien eget mi proin. Ultrices eros in cursus turpis massa tincidunt
+          dui ut ornare. Et ligula ullamcorper malesuada proin libero nunc consequat interdum. Turpis egestas maecenas
+          pharetra convallis posuere morbi leo urna.
+        </NxP>
       </NxPageMain>
     </>
   );
