8444162e69a225aaa662d393103dc03c8b6e3fcb
Ross Pokorny <rpokorny@sonatype.com>
2022-03-04 11:01:59 -0500

    Improve example - RSC-895

--- gallery/src/components/NxTextLink/NxTextLinkTruncationExample.tsx
+++ gallery/src/components/NxTextLink/NxTextLinkTruncationExample.tsx
@@ -5,19 +5,24 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { NxCode, NxTextLink } from '@sonatype/react-shared-components';
+import { NxCode, NxP, NxTextLink } from '@sonatype/react-shared-components';
 
 export default function NxTextLinkTruncationExample() {
   return (
-    <div style={{ width: '220px', border: '1px solid red' }}>
-      <NxTextLink href="#/">This link uses the default NxTextLink styling and wraps within its container</NxTextLink>
-      <NxTextLink href="#/" truncate>
-        This link uses the <NxCode>truncate</NxCode> prop and thus truncates with an ellipsis
-      </NxTextLink>
-      <NxTextLink href="https://www.google.com/" external truncate>
-        This external link uses the <NxCode>truncate</NxCode> prop and thus truncates with an ellipsis.
-        Note that the text before the external link icon truncates, so that the icon itself is always visible.
-      </NxTextLink>
+    <div style={{ width: '475px', border: '1px solid red' }}>
+      <NxP>
+        <NxTextLink href="#/">This link uses the default NxTextLink styling and wraps within its container</NxTextLink>
+      </NxP>
+      <NxP>
+        <NxTextLink href="#/" truncate>
+          This link uses the <NxCode>truncate</NxCode> prop and thus truncates with an ellipsis
+        </NxTextLink>
+      </NxP>
+      <NxP>
+        <NxTextLink href="https://www.google.com/" external truncate>
+          This external link shows the icon even when the text is truncated.
+        </NxTextLink>
+      </NxP>
     </div>
   );
 }
