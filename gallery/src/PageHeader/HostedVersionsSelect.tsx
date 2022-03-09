70d3ea431bda0bbef6fffe4ce21fb2b018d7c0fc
Andrew Prasetya <aprasetya@sonatype.com>
2022-03-09 14:36:50 -0500

    Updated build and versions select behaviour. - RSC-810

--- gallery/src/PageHeader/HostedVersionsSelect.tsx
+++ gallery/src/PageHeader/HostedVersionsSelect.tsx
@@ -4,31 +4,68 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { ChangeEventHandler } from 'react';
+import React, { useEffect, ChangeEventHandler, useState } from 'react';
 import { NxFormSelect } from '@sonatype/react-shared-components';
 
-import hostedVersionsJson from '../../hosted-versions.json';
+import packageJson from '../../package.json';
 
 import './HostedVersionsSelect.scss';
 
+const CURRENT_VERSION = packageJson.version;
+const HOSTED_VERSIONS_JSON_URL = 'https://gallery.sonatype.dev/versions/latest/hosted-versions.json';
+
+const getHostedVersion = () => fetch(HOSTED_VERSIONS_JSON_URL, {
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+}).then((response) => {
+  if (!response.ok) {
+    throw new Error('HTTP error.');
+  }
+  return response.json();
+});
+
 const HostedVersionsSelect = () => {
+  const [isLoadingHostedVersions, setIsLoadingHostedVersions] = useState(false);
+  const [hostedVersions, setHostedVersions] = useState([CURRENT_VERSION]);
+
   const onChange: ChangeEventHandler<HTMLSelectElement> = event =>
     window.location.href = `/versions/${event.target.value}`;
+
+  useEffect(() => {
+    setIsLoadingHostedVersions(true);
+    getHostedVersion()
+        .then((hostedVersions) => {
+          setHostedVersions(hostedVersions);
+          setIsLoadingHostedVersions(false);
+        })
+        .catch(() => {
+          setHostedVersions([CURRENT_VERSION]);
+          setIsLoadingHostedVersions(false);
+        });
+  }, []);
 
   return (
     <div className="hosted-versions-select">
       <label className="hosted-versions-select__label" htmlFor="hosted-versions-select">
         Version
       </label>
-      <NxFormSelect className="hosted-versions-select__select nx-form-select--short"
-                    id="hosted-versions-select"
-                    onChange={onChange}>
-        {
-          hostedVersionsJson.map(
-              version => <option key={version} value={version}>{version}</option>
-          )
-        }
-      </NxFormSelect>
+      {
+        !isLoadingHostedVersions && (
+          <NxFormSelect className="hosted-versions-select__select nx-form-select--short"
+                        id="hosted-versions-select"
+                        defaultValue={CURRENT_VERSION}
+                        onChange={onChange}>
+            {
+              hostedVersions.map(
+                  version =>
+                    <option key={version} value={version}>{version}</option>
+              )
+            }
+          </NxFormSelect>
+        )
+      }
     </div>
   );
 };
