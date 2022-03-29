c1a5f25436288978bfd3233e28316d900eeecc4d
Abhinav Bhatta <abhatta@sonatype.com>
2022-03-29 08:31:38 -0400

    Update unit test

--- lib/src/components/NxTable/__tests__/NxTableCell.test.tsx
+++ lib/src/components/NxTable/__tests__/NxTableCell.test.tsx
@@ -93,6 +93,7 @@ describe('NxTableCell', function () {
         expect(component.children().length).toBe(1);
         expect(component.find('span')).not.toHaveText('foo');
         expect(component.find('span')).toHaveText('Select Row');
+        expect(component.find('span')).toHaveClassName('visually-hidden');
       });
     });
   });
@@ -149,6 +150,7 @@ describe('NxTableCell', function () {
         expect(component.children().length).toBe(1);
         expect(component.find('span')).not.toHaveText('foo');
         expect(component.find('span')).toHaveText('Select Row');
+        expect(component.find('span')).toHaveClassName('visually-hidden');
       });
     });
   });
