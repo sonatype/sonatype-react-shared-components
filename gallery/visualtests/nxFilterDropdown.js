7fb0b5a07c34fdb68e898635e0a5f2e1bdeb0e83
Ross Pokorny <rpokorny@sonatype.com>
2022-04-04 13:20:29 -0400

    Port NxFilterDropdown visual tests - RSC-893

--- gallery/visualtests/nxFilterDropdown.js
+++ gallery/visualtests/nxFilterDropdown.js
@@ -4,14 +4,26 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-const { Region, Target } = require('@applitools/eyes-webdriverio');
-const { clickTest, focusTest, focusAndHoverTest, hoverTest, simpleTest } = require('./testUtils');
+const { setupBrowser } = require('./testUtils');
 
 describe('NxFilterDropdown', function() {
-  beforeEach(async function() {
-    await browser.url('#/pages/Filter Dropdown');
-    await browser.refresh();
-  });
+  const {
+    waitAndGetElements,
+    moveMouseAway,
+    scrollIntoView,
+    blurElement,
+    getPage,
+    clickTest,
+    focusTest,
+    focusAndHoverTest,
+    hoverTest,
+    simpleTest,
+    checkScreenshot
+  } = setupBrowser('#/pages/Filter Dropdown');
+
+  async function isInDocument(el) {
+    return el.evaluate(e => e.isConnected);
+  }
 
   const tableExampleSelector = '#nx-filter-dropdown-table-example .gallery-example-live',
       dropdownSelector = `${tableExampleSelector} .nx-filter-dropdown`,
@@ -31,46 +43,32 @@ describe('NxFilterDropdown', function() {
 
   describe('NxFilterDropdown when open', function() {
     beforeEach(async function() {
-      const button = await browser.$(toggleSelector);
+      const [button] = await waitAndGetElements(toggleSelector);
 
-      await button.scrollIntoView({ block: 'center' });
       await button.click();
     });
 
     it('has a dark button border with expanded menu', async function() {
-      const [targetElement, toggleElement] = await Promise.all([
-        browser.$(dropdownSelector),
-        browser.$(toggleSelector)
-      ]);
+      const [targetElement, toggleElement] = await waitAndGetElements(dropdownSelector, toggleSelector);
 
-      await targetElement.scrollIntoView({ block: 'center' });
-      await targetElement.moveTo({ xOffset: -10, yOffset: -10 });
-      await browser.execute(function(el) {
-        el.blur();
-      }, toggleElement);
+      await moveMouseAway();
+      await blurElement(toggleElement);
 
-      const { x, y } = await targetElement.getLocation();
-      const region = new Region(parseInt(x, 10), parseInt(y, 10), 251, 376);
-
-      await browser.eyesRegionSnapshot(null, Target.region(region));
+      await checkScreenshot(targetElement, 251, 376);
     });
 
     it('has focus, hover, and selection styles for checkboxes', async function() {
-      const [dropdownEl, cb1, cb2] = await Promise.all([
-        browser.$(dropdownSelector),
-        browser.$(`${dropdownSelector} .nx-checkbox:first-child`),
-        browser.$(`${dropdownSelector} .nx-checkbox:nth-child(2)`)
-      ]);
+      const [dropdownEl, cb1, cb2] = await waitAndGetElements(
+        dropdownSelector,
+        `${dropdownSelector} .nx-checkbox:first-child`,
+        `${dropdownSelector} .nx-checkbox:nth-child(2)`
+      );
 
       // after this cb1 should be selected and focused, cb2 should be hovered
-      await cb1.scrollIntoView({ block: 'center' });
       await cb1.click();
-      await cb2.moveTo();
+      await cb2.hover();
 
-      const { x, y } = await dropdownEl.getLocation();
-      const region = new Region(parseInt(x, 10), parseInt(y, 10), 251, 376);
-
-      await browser.eyesRegionSnapshot(null, Target.region(region));
+      await checkScreenshot(dropdownEl, 251, 376);
     });
 
     describe('NxFilterDropdown reset button', function() {
@@ -78,17 +76,17 @@ describe('NxFilterDropdown', function() {
           checkboxSelector = `${dropdownSelector} .nx-checkbox:last-of-type`;
 
       beforeEach(async function() {
-        const resetBtn = await browser.$(resetBtnSelector);
-        await resetBtn.scrollIntoView();
+        const [resetBtn] = await waitAndGetElements(resetBtnSelector);
+        await scrollIntoView(resetBtn);
       });
 
       it('looks disabled when no items are selected', simpleTest(tableExampleSelector));
 
       describe('NxFilterDropdown reset button when items are selected', function() {
         beforeEach(async function() {
-          const checkbox = await browser.$(checkboxSelector);
+          const [checkbox] = await waitAndGetElements(checkboxSelector);
 
-          checkbox.click();
+          await checkbox.click();
         });
 
         it('looks like a link', simpleTest(tableExampleSelector));
@@ -102,85 +100,72 @@ describe('NxFilterDropdown', function() {
 
   describe('Short NxFilterDropdown', function() {
     it('looks right when open', async function() {
-      const [dropdown, button] = await Promise.all([
-        browser.$(shortDropdownSelector),
-        browser.$(shortDropdownSelector + ' .nx-dropdown__toggle')
-      ]);
+      const [dropdown, button] = await waitAndGetElements(
+        shortDropdownSelector,
+        shortDropdownSelector + ' .nx-dropdown__toggle'
+      );
 
-      await button.scrollIntoView({ block: 'center' });
       await button.click();
 
-      const { x, y } = await dropdown.getLocation();
-      const region = new Region(parseInt(x, 10), parseInt(y, 10), 150, 244);
-
-      await browser.eyesRegionSnapshot(null, Target.region(region));
+      await checkScreenshot(dropdown, 150, 244);
     });
   });
 
   describe('NxFilterDropdown with custom placeholder and no Reset', function() {
     it('looks right when open', async function() {
-      const [dropdown, button] = await Promise.all([
-        browser.$(nonDefaultDropdownSelector),
-        browser.$(nonDefaultDropdownSelector + ' .nx-dropdown__toggle')
-      ]);
+      const [dropdown, button] = await waitAndGetElements(
+        nonDefaultDropdownSelector,
+        nonDefaultDropdownSelector + ' .nx-dropdown__toggle'
+      );
 
-      await button.scrollIntoView({ block: 'center' });
       await button.click();
 
-      const { x, y } = await dropdown.getLocation();
-      const region = new Region(parseInt(x, 10), parseInt(y, 10), 251, 212);
-
-      await browser.eyesRegionSnapshot(null, Target.region(region));
+      await checkScreenshot(dropdown, 251, 212);
     });
   });
 
   describe('NxFilterDropdown with label', function() {
     it('looks right when open', async function() {
-      const [formGroup, button] = await Promise.all([
-        browser.$(labelledDropdownSelector),
-        browser.$(labelledDropdownSelector + ' .nx-dropdown__toggle')
-      ]);
+      const [formGroup, button] = await waitAndGetElements(
+        labelledDropdownSelector,
+        labelledDropdownSelector + ' .nx-dropdown__toggle'
+      );
 
-      await button.scrollIntoView({ block: 'center' });
       await button.click();
 
-      const { x, y } = await formGroup.getLocation();
-      const region = new Region(parseInt(x, 10), parseInt(y, 10), 251, 244);
-
-      await browser.eyesRegionSnapshot(null, Target.region(region));
+      await checkScreenshot(formGroup, 251, 244);
     });
   });
 
   it('stays open when a checkbox is clicked', async function() {
-    const button = await browser.$(dropdownSelector + ' .nx-dropdown__toggle');
+    const [button] = await waitAndGetElements(dropdownSelector + ' .nx-dropdown__toggle');
 
-    await button.scrollIntoView({ block: 'center' });
     await button.click();
 
-    const [dropdownMenu, checkbox] = await Promise.all([
-      browser.$(`${dropdownSelector} .nx-dropdown-menu`),
-      browser.$(`${dropdownSelector} .nx-checkbox:first-child`)
-    ]);
+    const [dropdownMenu, checkbox] = await waitAndGetElements(
+      `${dropdownSelector} .nx-dropdown-menu`,
+      `${dropdownSelector} .nx-checkbox:first-child`
+    );
 
-    await checkbox.scrollIntoView({ block: 'center' });
     await checkbox.click();
 
-    expect(await dropdownMenu.isDisplayed()).toBe(true);
+    expect(await isInDocument(dropdownMenu)).toBe(true);
   });
 
   it('closes when a click outside of the dropdown occurs', async function() {
-    const [table, button] = await Promise.all([
-      browser.$(tableExampleSelector),
-      browser.$(dropdownSelector + ' .nx-dropdown__toggle')
-    ]);
+    const [table, button] = await waitAndGetElements(
+      tableExampleSelector,
+      dropdownSelector + ' .nx-dropdown__toggle'
+    );
 
-    await table.scrollIntoView({ block: 'center' });
+    await scrollIntoView(table);
     await button.click();
 
-    // click outside of menu
-    await table.click({ y: 50 });
+    const [dropdownMenu] = await waitAndGetElements(`${dropdownSelector} .nx-dropdown-menu`);
 
-    const dropdownMenu = await browser.$(`${dropdownSelector} .nx-dropdown-menu`);
-    expect(await dropdownMenu.isDisplayed()).toBe(false);
+    // click outside of menu
+    await getPage().mouse.click(10, 10);
+
+    expect(await isInDocument(dropdownMenu)).toBe(false);
   });
 });
