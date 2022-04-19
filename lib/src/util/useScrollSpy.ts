9cdb9f674587de6be4e27228fce4309d55c91131
Ross Pokorny <rpokorny@sonatype.com>
2022-04-19 14:44:48 -0400

    Adjust API - RSC-995

--- lib/src/util/useScrollSpy.ts
+++ lib/src/util/useScrollSpy.ts
@@ -9,25 +9,19 @@ import _useScrollSpy from 'react-use-scrollspy';
 
 type RefsParentType = Record<string, RefObject<HTMLElement>>;
 
-interface useScrollSpyParams<T extends RefsParentType> {
-  offsetPx?: number;
-  refs: T;
-}
-
 /**
  * Sets up scroll spy logic. The actual tracking of the current scroll position is handled by the
  * third-party react-use-scrollspy library, but we add the ability to initiate a programmatic scroll
  * to a given spied element, and the ability to refer to scrollable section by a name rather than by array index.
  */
-export default function useScrollSpy<T extends RefsParentType>({ offsetPx, refs }: useScrollSpyParams<T>) {
+export default function useScrollSpy<T extends RefsParentType>(refs: T/*, offsetPx?: number*/) {
   const refsList = Object.values(refs),
-      activeIdx = _useScrollSpy({ offsetPx, sectionElementRefs: refsList }),
-      activeRefName = Object.keys(refs)[activeIdx],
-      activeRef = refs[activeRefName];
+      activeIdx = _useScrollSpy({ /*offsetPx,*/ sectionElementRefs: refsList }),
+      activeRef = Object.keys(refs)[activeIdx];
 
   function scrollTo(refName: keyof T) {
     refs[refName].current?.scrollIntoView();
   }
 
-  return { scrollTo, activeRefName, activeRef };
+  return { scrollTo, activeRef };
 }
