bbd02bb7ab54aaa5d1d0e195f6f7246de0dac95d
Ross Pokorny <rpokorny@sonatype.com>
2022-04-27 13:59:10 -0400

    Update unit tests - RSC-995

--- lib/src/util/__tests__/useScrollSpy.test.tsx
+++ lib/src/util/__tests__/useScrollSpy.test.tsx
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { useRef } from 'react';
+import React, { forwardRef, useRef } from 'react';
 import { mount } from 'enzyme';
 
 import useScrollSpy from '../useScrollSpy';
@@ -23,28 +23,16 @@ describe('useScrollSpy', function() {
     container.remove();
   });
 
-  it('returns an object with an onScroll function, and a scrollTo function', function() {
+  it('returns an object with a scrollTo function', function() {
     const Fixture = () => {
-      const { onScroll, scrollTo, activeSection } = useScrollSpy({ foo: useRef(null) });
+      const { scrollTo } = useScrollSpy({ foo: useRef(null) });
 
-      return <div onScroll={onScroll} onClick={scrollTo as any}>{activeSection}</div>;
+      return <div onClick={scrollTo as any}></div>;
     };
 
     const component = mount(<Fixture />).find('div');
 
-    expect(component.prop('onScroll')).toBeInstanceOf(Function);
     expect(component.prop('onClick')).toBeInstanceOf(Function);
-  });
-
-  it('initially returns the first object key in its param as the activeSection', function() {
-    const Fixture = () => {
-      const { activeSection } = useScrollSpy({ foo: useRef(null), bar: useRef(null) });
-
-      return <div>{activeSection}</div>;
-    };
-
-    const component = mount(<Fixture />);
-    expect(component.text()).toBe('foo');
   });
 
   it('throws an error if passed an empty object', function() {
@@ -55,6 +43,48 @@ describe('useScrollSpy', function() {
     };
 
     expect(() => mount(<Fixture />)).toThrowError();
+  });
+
+  describe('withScrollSpy', function() {
+    it('can wrap an element without errors', function() {
+      const Fixture = () => {
+        const { withScrollSpy } = useScrollSpy({ foo: useRef(null) });
+
+        return withScrollSpy(<div id="foo" />);
+      };
+
+      expect(mount(<Fixture />)).toContainMatchingElement('#foo');
+    });
+
+    it('does not override the ref on the wrapped element', function() {
+      const Fixture = forwardRef<HTMLDivElement>((_, ref) => {
+        const { withScrollSpy } = useScrollSpy({ foo: useRef(null) });
+
+        return withScrollSpy(<div ref={ref} id="foo" />);
+      });
+
+      const ref = React.createRef<HTMLDivElement>(),
+          component = mount(<Fixture ref={ref} />);
+
+      expect(ref.current).toBe(component.getDOMNode());
+    });
+
+    it('does not override the onScroll on the wrapped element', function() {
+      const Fixture = (({ onScroll }: { onScroll: any }) => {
+        const { withScrollSpy } = useScrollSpy({ foo: useRef(null) });
+
+        return withScrollSpy(<div onScroll={onScroll} id="foo" />);
+      });
+
+      const onScroll = jest.fn(),
+          component = mount(<Fixture onScroll={onScroll} />);
+
+      expect(onScroll).not.toHaveBeenCalled();
+
+      component.find('#foo').simulate('scroll', { currentTarget: component.getDOMNode() });
+
+      expect(onScroll).toHaveBeenCalled();
+    });
   });
 
   // jsdom does not really implement getBoundingClientRect - it just sets all of the fields to zero.
