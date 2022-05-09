27ebe3cba3615552a683364868a03fa0fb842d05
Abhinav Bhatta <abhatta@sonatype.com>
2022-05-09 15:13:02 -0400

    Make absolute - fixed positoning work

--- lib/src/components/NxToast/NxToastContainer.tsx
+++ lib/src/components/NxToast/NxToastContainer.tsx
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import NxToast from './NxToast';
 import { NxToastContainerProps, nxToastContainerPropTypes } from './types';
@@ -12,20 +12,34 @@ import classNames from 'classnames';
 
 const NxToastContainer = (props: NxToastContainerProps) => {
   const { toasts } = props,
-      className = classNames('nx-toast__container'),
+      className = classNames('nx-toast__container--wrapper'),
       toastContainerRef = useRef<HTMLDivElement | null>(null);
+
+  const [domReady, setDomReady] = React.useState(false);
+
+  useEffect(() => {
+    setDomReady(true);
+  }, []);
+
+  const nxPageMainDOM = document.querySelector('.nx-page-main') as HTMLElement;
+
+  if (!domReady) {
+    return null;
+  }
 
   return createPortal(
     <div className={className} ref={toastContainerRef}>
-      {toasts.sort((a, b) => (a.toastId > b.toastId) ? -1 : 1).map(toast => (
-        <NxToast key={toast.toastId}
-                 toastId={toast.toastId}
-                 type={toast.type}
-                 message={toast.message}
-                 toastContainerRef={toastContainerRef} />
-      ))}
+      <div className="nx-toast__container--content">
+        {toasts.sort((a, b) => (a.toastId > b.toastId) ? -1 : 1).map(toast => (
+          <NxToast key={toast.toastId}
+                   toastId={toast.toastId}
+                   type={toast.type}
+                   message={toast.message}
+                   toastContainerRef={toastContainerRef} />
+        ))}
+      </div>
     </div>,
-    document.body
+    nxPageMainDOM
   );
 };
 
