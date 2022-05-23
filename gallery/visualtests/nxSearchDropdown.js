c0eab925e6857525f49ed4981f68ff455e87936e
Ross Pokorny <rpokorny@sonatype.com>
2022-05-23 13:01:16 -0400

    Add draft of keynav functional tests - RSC-983

--- gallery/visualtests/nxSearchDropdown.js
+++ gallery/visualtests/nxSearchDropdown.js
@@ -178,4 +178,146 @@ describe('NxSearchDropdown', function() {
 
     await checkScreenshot(component, 800, 125);
   });
+
+  describe('keynav', function() {
+    it('has one tab stop for the input and one for the result list when visible', async function() {
+      const [component, input] = await waitAndGetElements(
+            basicExampleSelector,
+            `${basicExampleSelector} .nx-filter-input input`
+          ),
+          page = getPage();
+
+      await input.focus();
+
+      // tab-navigate away and back to confirm that the input is a tab stop
+      await page.keyboard.down('Shift');
+      await page.keyboard.press('Tab');
+      await page.keyboard.up('Shift');
+
+      await page.keyboard.press('Tab');
+
+      expect(await isFocused(input)).toBe(true);
+
+      await page.keyboard.press('Tab');
+
+      // component is only one tab stop currently
+      expect(await page.evaluate((c) => c.contains(document.activeElement), component)).toBe(false);
+
+      await input.focus();
+      await page.keyboard.type('10');
+      const [loadingSpinner] = await waitAndGetElements(`${basicExampleSelector} .nx-loading-spinner`);
+      await page.keyboard.press('Tab');
+
+      // component is still only one tab stop, the loading spinner is not tabbable
+      expect(await page.evaluate((c) => c.contains(document.activeElement), component)).toBe(false);
+
+      await input.focus();
+      const [firstBtn] = await waitAndGetElements(`${basicExampleSelector} .nx-dropdown-button:first-child`);
+      await page.keyboard.press('Tab');
+
+      expect(await isFocused(firstBtn)).toBe(true);
+
+      await page.keyboard.press('Tab');
+
+      // result list is a second tab stop, but only one (not one per button)
+      expect(await page.evaluate((c) => c.contains(document.activeElement), component)).toBe(false);
+    });
+
+    it('moves between result buttons using Up, Down, Home, and End', async function() {
+      const [input] = await waitAndGetElements(`${basicExampleSelector} .nx-filter-input input`),
+          page = getPage();
+
+      await input.type('1');
+      const [firstBtn, secondBtn, secondToLastBtn, lastBtn] = await waitAndGetElements(
+        `${basicExampleSelector} .nx-dropdown-button:first-child`,
+        `${basicExampleSelector} .nx-dropdown-button:nth-child(2)`,
+        `${basicExampleSelector} .nx-dropdown-button:nth-last-child(2)`,
+        `${basicExampleSelector} .nx-dropdown-button:last-child`
+      );
+
+      await firstBtn.focus();
+
+      await page.keyboard.press('ArrowDown');
+      expect(await isFocused(secondBtn)).toBe(true);
+
+      await page.keyboard.press('ArrowUp');
+      expect(await isFocused(firstBtn)).toBe(true);
+
+      await page.keyboard.press('End');
+      expect(await isFocused(lastBtn)).toBe(true);
+
+      await page.keyboard.press('ArrowUp');
+      expect(await isFocused(secondToLastBtn)).toBe(true);
+
+      await page.keyboard.press('Home');
+      expect(await isFocused(firstBtn)).toBe(true);
+    });
+
+    it('moves back to the text input on Shift+Tab from any dropdown button', async function() {
+      const [input] = await waitAndGetElements(`${basicExampleSelector} .nx-filter-input input`),
+          page = getPage();
+
+      await input.type('1');
+      const [firstBtn, secondBtn, secondToLastBtn, lastBtn] = await waitAndGetElements(
+        `${basicExampleSelector} .nx-dropdown-button:first-child`,
+        `${basicExampleSelector} .nx-dropdown-button:nth-child(2)`,
+        `${basicExampleSelector} .nx-dropdown-button:nth-last-child(2)`,
+        `${basicExampleSelector} .nx-dropdown-button:last-child`
+      );
+
+      await secondBtn.focus();
+
+      await page.keyboard.down('Shift');
+      await page.keyboard.press('Tab');
+      await page.keyboard.up('Shift');
+      expect(await isFocused(input)).toBe(true);
+    });
+
+    it('clears the text and focuses the input when Escape is pressed', async function() {
+      const [input] = await waitAndGetElements(`${basicExampleSelector} .nx-filter-input input`),
+          page = getPage();
+
+      await input.type('1');
+      const [firstBtn] = await waitAndGetElements(`${basicExampleSelector} .nx-dropdown-button:first-child`);
+
+      // check pressing esc on the text input
+      page.keyboard.press('Escape');
+      expect(await isFocused(input)).toBe(true);
+      expect(await input.evaluate(el => el.value)).toBe('');
+
+      await input.type('1');
+      const [firstBtnAgain] = await waitAndGetElements(`${basicExampleSelector} .nx-dropdown-button:first-child`);
+      await firstBtnAgain.focus();
+
+      // check pressing esc on a dropdown button
+      page.keyboard.press('Escape');
+      expect(await isFocused(input)).toBe(true);
+      expect(await input.evaluate(el => el.value)).toBe('');
+
+      const [errorInput] = await waitAndGetElements(`${errorExampleSelector} .nx-filter-input input`);
+
+      await errorInput.type('1');
+      const [errorRetryBtn] = await waitAndGetElements(`${errorExampleSelector} .nx-dropdown-menu .nx-btn--error`);
+      await errorRetryBtn.focus();
+
+      // check pressing esc on error Retry button
+      page.keyboard.press('Escape');
+      expect(await isFocused(input)).toBe(true);
+      expect(await input.evaluate(el => el.value)).toBe('');
+    });
+
+    it('resets focus to the text input when the dropdown closes due to selection', async function() {
+      const [input] = await waitAndGetElements(`${basicExampleSelector} .nx-filter-input input`),
+          page = getPage();
+
+      await input.type('1');
+      const [firstBtn] = await waitAndGetElements(`${basicExampleSelector} .nx-dropdown-button:first-child`);
+
+      await firstBtn.focus();
+
+      dismissResultingDialog(async () => await firstBtn.click());
+
+      expect(await isFocused(input)).toBe(true);
+    });
+  });
 });
