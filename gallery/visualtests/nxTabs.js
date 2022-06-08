ab813602ad3da258cc5d97ed92b62f2c247029a7
Andrew Prasetya <aprasetya@sonatype.com>
2022-06-08 11:44:27 -0400

    Fixed tests. - RSC-992

--- gallery/visualtests/nxTabs.js
+++ gallery/visualtests/nxTabs.js
@@ -20,12 +20,17 @@ describe('NxTabs', function() {
     a11yTest
   } = setupBrowser('#/pages/Tabs?noCheckeredBackground');
 
+  function hasClass(element, cls) {
+    return element.evaluate((el, cls) => el.classList.contains(cls), cls);
+  }
+
   const tabTileExampleSelector = '#nx-tab-tile-example .nx-tile',
       tabTileNoHeaderExampleSelector = '#nx-tab-tile-no-header-example .nx-tile',
       tabOutsideTileExampleSelector = '#nx-tab-tile-no-header-example .nx-tile',
       tabModalExampleSelector = '#nx-tab-modal-example',
       tabModalNoHeaderExampleSelector = '#nx-tab-modal-no-header-example',
-      tabManualActivationModeExampleSelector = '#nx-tab-manual-activation-mode-example .nx-tabs';
+      tabManualActivationModeExampleSelector = '#nx-tab-manual-activation-mode-example',
+      tabAutomaticActivationModeExampleSelector = '#nx-tab-tile-no-header-example';
 
   it('looks right in a Tile', simpleTest(tabTileExampleSelector));
   it('looks right in a Tile with no header', simpleTest(tabTileNoHeaderExampleSelector));
@@ -37,13 +42,28 @@ describe('NxTabs', function() {
 
     it('has regular text and no border by default', simpleTest(selector));
     it('has semi-bold text when hovered',
-        hoverTest(tabTileExampleSelector, `${tabTileExampleSelector} .nx-tab:nth-child(2)`));
+        hoverTest(
+            tabManualActivationModeExampleSelector,
+            `${tabManualActivationModeExampleSelector} .nx-tab:nth-child(2)`));
     it('has a light blue border and semi-bold text when clicked',
-        clickTest(tabTileExampleSelector, `${tabTileExampleSelector} .nx-tab:nth-child(2)`));
+        clickTest(
+            tabManualActivationModeExampleSelector,
+            `${tabManualActivationModeExampleSelector} .nx-tab:nth-child(2)`));
     it('has a light blue border when focused',
-        focusTest(tabTileExampleSelector, `${tabTileExampleSelector} .nx-tab:nth-child(2)`));
+        focusTest(
+            tabManualActivationModeExampleSelector,
+            `${tabManualActivationModeExampleSelector} .nx-tab:nth-child(2)`));
     it('has a light blue border and semi-bold text when focused and hovered',
-        focusAndHoverTest(tabTileExampleSelector, `${tabTileExampleSelector} .nx-tab:nth-child(2)`));
+        focusAndHoverTest(
+            tabManualActivationModeExampleSelector,
+            `${tabManualActivationModeExampleSelector} .nx-tab:nth-child(2)`));
+  });
+
+  describe('Check tab panel', function() {
+    it('has a light blue border when focused',
+        focusTest(
+            tabManualActivationModeExampleSelector,
+            `${tabManualActivationModeExampleSelector} .nx-tab-panel`));
   });
 
   function simpleModalTest(exampleSelector) {
@@ -71,9 +91,9 @@ describe('NxTabs', function() {
 
   describe('keyboard navigation', function() {
     it('focuses on the tab when it is clicked', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
 
-      const secondTab = await tabs.$('.nx-tab:nth-child(2)');
+      const secondTab = await example.$('.nx-tab:nth-child(2)');
 
       expect(await isFocused(secondTab)).toBe(false);
 
@@ -82,12 +102,12 @@ describe('NxTabs', function() {
       expect(await isFocused(secondTab)).toBe(true);
     });
 
-    it('focuses on the nx-tab-panel after tabbing from nx-tab', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+    it('focuses on the nx-tab-panel and link inside the panel after tabbing from nx-tab', async function() {
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
       const page = getPage();
 
-      const activeTab = await tabs.$('.nx-tab:nth-child(0)');
-      const tabPanel = await tabs.$('.nx-tab-panel');
+      const activeTab = await example.$('.nx-tab:nth-child(1)');
+      const tabPanel = await example.$('.nx-tab-panel');
 
       await activeTab.focus();
 
@@ -96,14 +116,20 @@ describe('NxTabs', function() {
       await page.keyboard.press('Tab');
 
       expect(await isFocused(tabPanel)).toBe(true);
+
+      const tabPanelAnchor = await example.$('.nx-tab-panel a');
+
+      await page.keyboard.press('Tab');
+
+      expect(await isFocused(tabPanelAnchor)).toBe(true);
     });
 
     it('focuses back on active nx-tab from nx-tab-panel when shift+tab is activated', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
       const page = getPage();
 
-      const activeTab = await tabs.$('.nx-tab:nth-child(0)');
-      const tabPanel = await tabs.$('.nx-tab-panel');
+      const activeTab = await example.$('.nx-tab:nth-child(1)');
+      const tabPanel = await example.$('.nx-tab-panel');
 
       await activeTab.focus();
 
@@ -120,14 +146,14 @@ describe('NxTabs', function() {
       expect(await isFocused(activeTab)).toBe(true);
     });
 
-    it('focuses on the correct tab when arrow right is pressed but'
+    it('focuses on the correct tab when arrow right is pressed but '
       + 'does not activate it when in manual activation mode', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
       const page = getPage();
 
-      const firstTab = await tabs.$('.nx-tab:nth-child(1)');
-      const thirdTab = await tabs.$('.nx-tab:nth-child(3)');
-      const fourthTab = await tabs.$('.nx-tab:nth-child(4)');
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const thirdTab = await example.$('.nx-tab:nth-child(3)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
 
       await thirdTab.click();
 
@@ -145,14 +171,39 @@ describe('NxTabs', function() {
       expect(await hasClass(firstTab, 'active')).toBe(false);
     });
 
-    it('focuses on the correct tab when arrow left is pressed but'
-    + 'does not activate it when in manual activation mode', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+    it('focuses and activates the correct tab when arrow right is pressed '
+    + 'and when in automatic activation mode', async function() {
+      const [example] = await waitAndGetElements(tabAutomaticActivationModeExampleSelector);
       const page = getPage();
 
-      const firstTab = await tabs.$('.nx-tab:nth-child(1)');
-      const secondTab = await tabs.$('.nx-tab:nth-child(2)');
-      const fourthTab = await tabs.$('.nx-tab:nth-child(4)');
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const thirdTab = await example.$('.nx-tab:nth-child(3)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
+
+      await thirdTab.click();
+
+      expect(await isFocused(thirdTab)).toBe(true);
+      expect(await hasClass(thirdTab, 'active')).toBe(true);
+
+      await page.keyboard.press('ArrowRight');
+
+      expect(await isFocused(fourthTab)).toBe(true);
+      expect(await hasClass(fourthTab, 'active')).toBe(true);
+
+      await page.keyboard.press('ArrowRight');
+
+      expect(await isFocused(firstTab)).toBe(true);
+      expect(await hasClass(firstTab, 'active')).toBe(true);
+    });
+
+    it('focuses on the correct tab when arrow left is pressed but '
+    + 'does not activate it when in manual activation mode', async function() {
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+      const page = getPage();
+
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const secondTab = await example.$('.nx-tab:nth-child(2)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
 
       await secondTab.click();
 
@@ -168,18 +219,40 @@ describe('NxTabs', function() {
 
       expect(await isFocused(fourthTab)).toBe(true);
       expect(await hasClass(fourthTab, 'active')).toBe(false);
-
-      await page.keyboard.press('Home');
-      await page.keyboard.press('End');
     });
 
-    it('focuses on the first tab when home key is pressed'
-    + 'but does not activate it when activation mode is manual', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+    it('focuses and activates the correct tab when arrow left is pressed '
+    + 'and when in automatic activation mode', async function() {
+      const [example] = await waitAndGetElements(tabAutomaticActivationModeExampleSelector);
       const page = getPage();
 
-      const firstTab = await tabs.$('.nx-tab:nth-child(1)');
-      const fourthTab = await tabs.$('.nx-tab:nth-child(4)');
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const secondTab = await example.$('.nx-tab:nth-child(2)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
+
+      await secondTab.click();
+
+      expect(await isFocused(secondTab)).toBe(true);
+      expect(await hasClass(secondTab, 'active')).toBe(true);
+
+      await page.keyboard.press('ArrowLeft');
+
+      expect(await isFocused(firstTab)).toBe(true);
+      expect(await hasClass(firstTab, 'active')).toBe(true);
+
+      await page.keyboard.press('ArrowLeft');
+
+      expect(await isFocused(fourthTab)).toBe(true);
+      expect(await hasClass(fourthTab, 'active')).toBe(true);
+    });
+
+    it('focuses on the first tab when home key is pressed '
+    + 'but does not activate it when activation mode is manual', async function() {
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+      const page = getPage();
+
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
 
       await fourthTab.click();
 
@@ -192,13 +265,32 @@ describe('NxTabs', function() {
       expect(await hasClass(firstTab, 'active')).toBe(false);
     });
 
-    it('focuses on the last tab when end key is pressed'
-    + 'but does not activate it when activation mode is manual', async function() {
-      const [tabs] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+    it('focuses and activates the first tab when home key is pressed '
+    + 'and when activation mode is automatic', async function() {
+      const [example] = await waitAndGetElements(tabAutomaticActivationModeExampleSelector);
       const page = getPage();
 
-      const firstTab = await tabs.$('.nx-tab:nth-child(1)');
-      const fourthTab = await tabs.$('.nx-tab:nth-child(4)');
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
+
+      await fourthTab.click();
+
+      expect(await isFocused(fourthTab)).toBe(true);
+      expect(await hasClass(fourthTab, 'active')).toBe(true);
+
+      await page.keyboard.press('Home');
+
+      expect(await isFocused(firstTab)).toBe(true);
+      expect(await hasClass(firstTab, 'active')).toBe(true);
+    });
+
+    it('focuses on the last tab when End key is pressed '
+    + 'but does not activate it when activation mode is manual', async function() {
+      const [example] = await waitAndGetElements(tabManualActivationModeExampleSelector);
+      const page = getPage();
+
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
 
       await firstTab.focus();
 
@@ -211,7 +303,24 @@ describe('NxTabs', function() {
       expect(await hasClass(fourthTab, 'active')).toBe(false);
     });
 
-    // nx-tab-tile-no-header-example
+    it('focuses and activates the last tab when End key is pressed '
+    + 'and when activation mode is automatic', async function() {
+      const [example] = await waitAndGetElements(tabAutomaticActivationModeExampleSelector);
+      const page = getPage();
+
+      const firstTab = await example.$('.nx-tab:nth-child(1)');
+      const fourthTab = await example.$('.nx-tab:nth-child(4)');
+
+      await firstTab.focus();
+
+      expect(await isFocused(firstTab)).toBe(true);
+      expect(await hasClass(firstTab, 'active')).toBe(true);
+
+      await page.keyboard.press('End');
+
+      expect(await isFocused(fourthTab)).toBe(true);
+      expect(await hasClass(fourthTab, 'active')).toBe(true);
+    });
   });
 
   it('passes a11y checks', a11yTest());
