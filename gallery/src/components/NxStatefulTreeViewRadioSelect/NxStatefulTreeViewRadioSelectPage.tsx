90d2bd97b13c8392683c6edf6dd10aca74452dab
Alex Chen <alexanderfschen@gmail.com>
2022-06-10 13:12:05 -0700

    Refactored link for cleanliness

--- gallery/src/components/NxStatefulTreeViewRadioSelect/NxStatefulTreeViewRadioSelectPage.tsx
+++ gallery/src/components/NxStatefulTreeViewRadioSelect/NxStatefulTreeViewRadioSelectPage.tsx
@@ -22,7 +22,9 @@ const NxStatefulTreeViewRadioSelectPage = () =>
         to <NxCode>NxStatefulCollapsibleRadioSelect</NxCode> and
         is now deprecated and will be removed in the next major version.
         Please use
-        {' '}<NxTextLink href="#/pages/Stateful%20Collapsible%20Radio-Select">NxStatefulCollapsibleRadioSelect
+        {' '}
+        <NxTextLink href="#/pages/Stateful%20Collapsible%20Radio-Select">
+          NxStatefulCollapsibleRadioSelect
         </NxTextLink>
         {' '}instead.
       </NxWarningAlert>
