958c02390008cd29adcbb67558ae1955a7c3b549
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2022-06-17 17:19:58 -0400

    Add role meter and aria attributes

--- gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartMinimalExample.tsx
+++ gallery/src/components/NxBinaryDonutChart/NxBinaryDonutChartMinimalExample.tsx
@@ -11,10 +11,10 @@ export default function NxBinaryDonutChartMinimalExample() {
   return (
     <>
       <NxBinaryDonutChart percent={0} aria-label="0 out of 100 components identified" />
-      <NxBinaryDonutChart percent={15} role="presentation" />
-      <NxBinaryDonutChart percent={25} role="presentation" />
+      <NxBinaryDonutChart percent={15} aria-label="15 out of 100 components identified" />
+      <NxBinaryDonutChart percent={25} aria-label="25 out of 100 components identified" />
       <NxBinaryDonutChart percent={50} aria-label="50 out of 100 components identified" />
-      <NxBinaryDonutChart percent={90} role="presentation" />
+      <NxBinaryDonutChart percent={90} aria-label="90 out of 100 components identified" />
       <NxBinaryDonutChart percent={100} aria-label="100 out of 100 components identified" />
     </>
   );
