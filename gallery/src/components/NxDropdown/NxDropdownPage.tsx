037a234d3caed4ff71df373e54bc9e720d1c7aaa
Andrew Prasetya <aprasetya@sonatype.com>
2022-06-22 11:15:44 -0400

    Fixed eslint problem. - RSC-973

--- gallery/src/components/NxDropdown/NxDropdownPage.tsx
+++ gallery/src/components/NxDropdown/NxDropdownPage.tsx
@@ -262,7 +262,8 @@ const NxDropdownPage = () =>
               <NxTable.Cell><NxCode>nx-dropdown-icon</NxCode></NxTable.Cell>
               <NxTable.Cell>
                 <NxCode>FontAwesomeIcon</NxCode>, within the label and inside the dropdown content.{' '}
-                Except for in the case where it is inside <NxCode>NxButton</NxCode> and <NxCode>nx-dropdown-link</NxCode>
+                Except for in the case where it is inside{' '}
+                <NxCode>NxButton</NxCode> and <NxCode>nx-dropdown-link</NxCode>
               </NxTable.Cell>
               <NxTable.Cell>
                 This applies the correct color to the icon.
