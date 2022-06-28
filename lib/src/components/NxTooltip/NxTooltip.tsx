18cb3aec427204ba7a0d678f033a3ed470e17f41
Ross Pokorny <rpokorny@sonatype.com>
2022-06-28 14:57:04 -0400

    Remove IntersectionObserver usage - RSC-1110

--- lib/src/components/NxTooltip/NxTooltip.tsx
+++ lib/src/components/NxTooltip/NxTooltip.tsx
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { createContext, FunctionComponent, Ref, useCallback, useContext, useEffect, useRef, useState } from 'react';
+import React, { createContext, FunctionComponent, useContext, useEffect, useState } from 'react';
 import classnames from 'classnames';
 import Tooltip, { TooltipProps } from '@material-ui/core/Tooltip';
 
@@ -13,9 +13,6 @@ import { Props, propTypes, TooltipPlacement } from './types';
 export { Props, propTypes, TooltipPlacement } from './types';
 
 import './NxToolTip.scss';
-import useMergedRef from '../../util/useMergedRef';
-import useIntersectionObserverRef from '@rooks/use-intersection-observer-ref';
-import { any, prop } from 'ramda';
 import batch from './updateBatcher';
 
 function convertPlacement(placement: TooltipPlacement | null | undefined): TooltipProps['placement'] {
@@ -37,7 +34,7 @@ function convertPlacement(placement: TooltipPlacement | null | undefined): Toolt
 
 // thanks to PropTypes, we have to accept null in addition to undefined for all optional properties. MUI doesn't
 // seem to follow that convention, so this function converts all null-valued props to undefined-valued props
-function fixOptional(props: Omit<Props, 'title' | 'children'>): Omit<TooltipProps, 'title' | 'children'> {
+function fixOptional(props: Omit<Props, 'title'>): Omit<TooltipProps, 'title'> {
   return {
     ...props,
     className: props.className || undefined,
@@ -58,55 +55,22 @@ function fixOptional(props: Omit<Props, 'title' | 'children'>): Omit<TooltipProp
 export const TooltipContext = createContext<boolean>(false);
 
 const NxTooltip: FunctionComponent<Props> =
-    function NxTooltip({ className, title, children, scrollContainerRef, ...otherProps }) {
+    function NxTooltip({ className, title, ...otherProps }) {
 
-      /*
-       * For performance reasons, in some cases it can be worthwhile to only activate tooltips on items
-       * that are scrolled into view: For instance in a large NxTransferList where only about 10 items are
-       * visible at a time, but there could be thousands, all with tooltips. When scrollContainerRef is defined,
-       * we use an IntersectionObserver to track whether this item is scrolled into view within that container,
-       * and enable the tooltip only if so.
-       */
-      const handleIntersection = useCallback(function handleIntersection(entries: IntersectionObserverEntry[]) {
-        if (scrollContainerRef) {
-          // use batching. If there are many tooltips checking their scroll position, batching improves throughput
-          // substantially
-          batch(() => {
-            if (!isUnmounted.current) {
-              setIsVisisble(any(prop('isIntersecting'), entries));
-            }
-          });
-        }
-      }, [scrollContainerRef]);
-
-      const [scrollContainer, setScrollContainer] = useState<HTMLElement | null>(null),
-          [isVisible, setIsVisisble] = useState(!scrollContainerRef),
-
+      const [initialized, setInitialized] = useState(false),
           tooltipClassName = classnames('nx-tooltip', className),
-          parentModal = useContext(NxModalContext),
-          [intersectionRef] = useIntersectionObserverRef(handleIntersection, { root: scrollContainer }),
-
-          isUnmounted = useRef(false),
-          ref: Ref<HTMLElement> = useMergedRef(children.ref, intersectionRef),
-          childrenWithRef = React.cloneElement(children, { ref });
+          parentModal = useContext(NxModalContext);
 
       useEffect(function() {
-        // The useIntersectionObserverRef API requires an actual DOM node for the root (rather than a Ref containing
-        // one). On first render the ref isn't typically populated yet, so we have to use useEffect to wait until it
-        // is populated, and then set it as a state value to trigger a re-render
-        setScrollContainer(scrollContainerRef?.current ?? null);
-      }, [scrollContainerRef?.current]);
-
-      useEffect(() => () => { isUnmounted.current = true; }, []);
+        batch(() => { setInitialized(true); });
+      }, []);
 
       return (
         <TooltipContext.Provider value={true}>
           <Tooltip { ...fixOptional(otherProps) }
-                   title={isVisible && title || ''}
+                   title={initialized && title || ''}
                    classes={{ tooltip: tooltipClassName }}
-                   PopperProps={{ container: parentModal }}>
-            {childrenWithRef}
-          </Tooltip>
+                   PopperProps={{ container: parentModal }} />
         </TooltipContext.Provider>
       );
     };
