0f600bbe04b3942cf2e6068a3a33126859bfc389
EmilyBermudez88 <ebermudez@sonatype.com>
2022-06-28 12:16:19 -0400

    add class if rows prop exists

--- lib/src/components/NxTextInput/NxTextInput.tsx
+++ lib/src/components/NxTextInput/NxTextInput.tsx
@@ -34,6 +34,7 @@ export const PrivateNxTextInput = forwardRef<HTMLDivElement, Props>(
         onKeyPress,
         disabled,
         prefixContent,
+        rows,
         ...attrs
       } = props;
 
@@ -48,6 +49,7 @@ export const PrivateNxTextInput = forwardRef<HTMLDivElement, Props>(
       const isTextArea = type === 'textarea',
           element = isTextArea ? 'textarea' : 'input',
           typeAttr = isTextArea ? undefined : (type || 'text'),
+          classes = rows && isTextArea? 'nx-text-input__input textarea-custom-sizing' : 'nx-text-input__input',
           isInvalid = validatable && hasValidationErrors(validationErrors),
           firstValidationError = validatable && getFirstValidationError(validationErrors),
           internalClassName = classnames('nx-text-input', className, {
@@ -81,21 +83,26 @@ export const PrivateNxTextInput = forwardRef<HTMLDivElement, Props>(
         }
       }
 
+      // let classes
+      // rows? console.log('hey') : console.log('nope');
+      // rows?  classes= 'nx-text-input__input custom';
+
       return (
         <div ref={forwardedRef} className={internalClassName}>
           {/* This click handler doesn't need to be accessible, it is only to mitigate a mouse-specific
             * presentational concern
             */}
           {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions*/}
-          <div className="nx-text-input__box" onClick={setFocusToInput}>
+          <div className="nx-text-input__box custom" onClick={setFocusToInput}>
             {prefixContent}
             {React.createElement(element, {
               ...newProps,
               disabled,
+              rows,
               ref: inputRef,
               type: typeAttr,
               onChange: inputOnChange,
-              className: 'nx-text-input__input',
+              className: classes,
               onKeyPress: inputOnKeyPress,
               'aria-invalid': isInvalid,
               'aria-errormessage': disabled ? undefined : invalidMessageId
