b5e79fea843d8db0df263695f7a3ad6ed61c50ed
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2022-07-08 17:37:51 -0700

    Update props in NxBinaryDonutChart.tsx file RSC-990

--- lib/src/components/NxBinaryDonutChart/NxBinaryDonutChart.tsx
+++ lib/src/components/NxBinaryDonutChart/NxBinaryDonutChart.tsx
@@ -13,13 +13,13 @@ import classnames from 'classnames';
 
 const NxBinaryDonutChart = forwardRef<SVGSVGElement, Props>(
     function NxBinaryDonutChart(props, ref) {
-      const { innerRadiusPercent, percent, value, className, maxVal, ...svgAttrs } = props;
-      
+      const { innerRadiusPercent, value, percent, className, maxVal, ...svgAttrs } = props;
+
       const donutClasses = classnames('nx-binary-donut-chart', className);
 
       const max = maxVal ?? 100;
-      const val = value ?? 100;
-      const perc = percent ?? Math.round(val/ max * 100);
+      const calculatedPercent = value ? Math.round(value / max * 100) : percent as number;
+      const calculatedValue = value ?? calculatedPercent * max / 100;
 
       // _Not counting the borders_, the inner radius is defined as a percentage
       // of the outer radius. Of the 30x30 viewbox, 1px on each side is border - the area that
@@ -31,22 +31,21 @@ const NxBinaryDonutChart = forwardRef<SVGSVGElement, Props>(
       const donutWidth = 14 - innerRadius;
       const arcR = innerRadius + (donutWidth / 2);
 
-      
       // Add 0.5π because we want the angle from the top of the circle, not the right
-      const arcEndAngle = (Math.PI / 2) + (2 * Math.PI * (perc / 100));
+      const arcEndAngle = (Math.PI / 2) + (2 * Math.PI * (calculatedPercent / 100));
       // Multiply by -1 to fill clockwise not counter-clockwise
       const arcEndX = -1 * arcR * Math.cos(arcEndAngle);
       // Multiply by -1 because the y value increases going downwards on the screen, not upwards
       const arcEndY = -1 * arcR * Math.sin(arcEndAngle);
-      const largeArc = perc > 50 ? 1 : 0;
-      
+      const largeArc = calculatedPercent > 50 ? 1 : 0;
+
       return (
         <svg ref={ref}
              viewBox="-15 -15 30 30"
              role="meter"
              aria-valuemin={0}
              aria-valuemax={max}
-             aria-valuenow={val}
+             aria-valuenow={calculatedValue}
              className={donutClasses}
              {...svgAttrs}>
           {/*
@@ -62,12 +61,12 @@ const NxBinaryDonutChart = forwardRef<SVGSVGElement, Props>(
                       `M 0 ${-innerBorderRadius}
                         a ${innerBorderRadius} ${innerBorderRadius} 0 1 1 0 ${innerBorderRadius * 2}
                         a ${innerBorderRadius} ${innerBorderRadius} 0 1 1 0 ${-innerBorderRadius * 2}`}`} />
-          { perc > 0 && perc < 100 &&
+          { calculatedPercent > 0 && calculatedPercent < 100 &&
             <path className="nx-binary-donut-chart__arc"
                   d={`M 0 ${-arcR} A ${arcR} ${arcR} 0 ${largeArc} 1 ${arcEndX} ${arcEndY}`}
                   strokeWidth={donutWidth}/>
           }
-          { perc >= 100 &&
+          { calculatedPercent >= 100 &&
             <circle className="nx-binary-donut-chart__arc" strokeWidth={donutWidth} r={arcR}/>
           }
         </svg>
