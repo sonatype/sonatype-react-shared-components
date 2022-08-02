acbb446d951641b1c6456a3ef3876286ccdf6c65
Ross Pokorny <rpokorny@sonatype.com>
2022-08-02 16:02:49 -0400

    Expose in index.ts - RSC-1061

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -325,3 +325,5 @@ export { default as NxSystemNotice } from './components/NxSystemNotice/NxSystemN
 export { default as NxProgressBar } from './components/NxProgressBar/NxProgressBar';
 
 export * from './components/NxStatusIndicator/NxStatusIndicator';
+
+export { default as NxFileUpload, Props as NxFileUploadProps } from './components/NxFileUpload/NxFileUpload';
