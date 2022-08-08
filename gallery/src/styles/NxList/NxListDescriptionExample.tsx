62cc76a55789b27fac38cf4b4186b50cb3d24783
Alex Chen <alexanderfschen@gmail.com>
2022-08-08 13:23:20 -0700

    Created NxDescriptionList button and link HTML examples

---
+++ gallery/src/styles/NxList/NxListDescriptionExample.tsx
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+import React from 'react';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { NxFontAwesomeIcon} from '@sonatype/react-shared-components';
+
+const NxListDescriptionExample = () =>
+  <div>
+    <h3 className="nx-h3">
+      List heading
+    </h3>
+    <dl className="nx-list nx-list--description-list">
+      <div className="nx-list__item">
+        <dt className="nx-list__term">Item 1</dt>
+        <dd className="nx-list__description">
+          A very interesting item. The first item. You might say it's the original. The item to begin
+          all items. But not to end them; definitely not. Rest assured there will be more items after this one
+          and they will definitely also have descriptions.
+        </dd>
+      </div>
+      <div className="nx-list__item">
+        <dt className="nx-list__term">Item 2</dt>
+        <dd className="nx-list__description">
+          The second item.
+        </dd>
+      </div>
+      <div className="nx-list__item">
+        <dt className="nx-list__term">Item 3</dt>
+        <dd className="nx-list__description">
+          The third item. Not interesting at all, unlike the first item.
+        </dd>
+      </div>
+      <div className="nx-list__item">
+        <dt className="nx-list__term">Item 9000000000000000000000000000000001</dt>
+        <dd className="nx-list__description">It's over 9000!</dd>
+      </div>
+      <div className="nx-list__item nx-list__item--clickable">
+        <dt className="nx-list__term">
+          <button className="nx-list__btn" onClick={() => { alert('You clicked a clickable row!'); }}>
+            This is a clickable row
+          </button>
+        </dt>
+        <dd className="nx-list__description">
+          <button className="nx-list__btn" onClick={() => { alert('You clicked a clickable row!'); }}>
+            Clicking this row will create an alert
+            <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
+          </button>
+        </dd>
+      </div>
+      <div className="nx-list__item nx-list__item--clickable">
+        <dt className="nx-list__term">
+          <a href="https://developer.mozilla.org/en-US/" className="nx-list__link">
+            This clickable row is a link.
+          </a>
+        </dt>
+        <dd className="nx-list__description">
+          <a href="https://developer.mozilla.org/en-US/" className="nx-list__link">
+            This clicking this row will take you to MDN.
+            <NxFontAwesomeIcon icon={faAngleRight} className="nx-chevron" />
+          </a>
+        </dd>
+      </div>
+    </dl>
+  </div>;
+
+export default NxListDescriptionExample;
--- gallery/src/styles/NxList/NxListExamples.tsx
+++ gallery/src/styles/NxList/NxListExamples.tsx
@@ -13,6 +13,7 @@ import NxListClickableLinksExample from './NxListClickableLinksExample';
 import NxListWithActionsExample from './NxListWithActionsExample';
 import NxListErrorExample from './NxListErrorStateExample';
 import NxListLoadingExample from './NxListLoadingExample';
+import NxListDescriptionCodeExample from './NxListDescriptionExample';
 import NxListDeprecatedClickableExample from './NxListDeprecatedClickableExample';
 
 const NxListSimpleCode = require('./NxListDefaultExample.html'),
@@ -25,7 +26,7 @@ const NxListSimpleCode = require('./NxListDefaultExample.html'),
     NxListEmptyCode = require('./NxListEmptyExample.html'),
     NxListErrorStateCode = require('./NxListErrorStateExample.tsx?raw'),
     NxListLoadingCode = require('./NxListLoadingExample.tsx?raw'),
-    NxListDescriptionCode = require('./NxListDescriptionExample.html'),
+    NxListDescriptionCode = require('./NxListDescriptionExample?raw'),
     NxListDeprecatedClickableCode = require('./NxListDeprecatedClickableExample?raw');
 
 const NxListsExamples = () =>
@@ -108,7 +109,7 @@ const NxListsExamples = () =>
 
     <GalleryExampleTile title="NxList description list example"
                         id="nx-list-description-example"
-                        htmlExample={NxListDescriptionCode}
+                        liveExample={NxListDescriptionCodeExample}
                         codeExamples={NxListDescriptionCode}>
       Basic <NxCode>nx-list--description</NxCode> with a heading. Wrapping is demonstrated in both
       the terms and the descriptions.
