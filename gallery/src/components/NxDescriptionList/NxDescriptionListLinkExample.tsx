143bff39b459a28450a785b381f48b9c4461094e
Ross Pokorny <rpokorny@sonatype.com>
2022-08-12 17:20:53 -0400

    Fix import formatting - RSC-1077

--- gallery/src/components/NxDescriptionList/NxDescriptionListLinkExample.tsx
+++ gallery/src/components/NxDescriptionList/NxDescriptionListLinkExample.tsx
@@ -5,7 +5,7 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { NxDescriptionList} from '@sonatype/react-shared-components';
+import { NxDescriptionList } from '@sonatype/react-shared-components';
 
 const NxDescriptionListLinkExample = () =>
   <NxDescriptionList>
