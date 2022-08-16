70cacb1170cb91803ce12579a61b1ccfd4298bc6
Andrew Prasetya <aprasetya@sonatype.com>
2022-08-16 11:59:24 -0400

    Removed unused import. - RSC-902

--- gallery/src/components/NxToast/NxToastFullPageVariationsExample.tsx
+++ gallery/src/components/NxToast/NxToastFullPageVariationsExample.tsx
@@ -4,7 +4,7 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { useState} from 'react';
 import { reject, propEq } from 'ramda';
 
