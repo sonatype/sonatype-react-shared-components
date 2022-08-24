888047f1ced59fc260a2a3880c0a945a1e85636b
Ross Pokorny <rpokorny@sonatype.com>
2022-08-24 14:13:38 -0400

    Initial impl of form select validation - RSC-1091

--- lib/src/components/NxFormSelect/NxFormSelect.tsx
+++ lib/src/components/NxFormSelect/NxFormSelect.tsx
@@ -4,22 +4,45 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useContext } from 'react';
 import classnames from 'classnames';
 
 import { Props } from './types';
-import { omit } from 'ramda';
+import { FormAriaContext } from '../NxForm/context';
+import { getFirstValidationError, hasValidationErrors } from '../../util/validationUtil';
+import { useUniqueId } from '../../util/idUtil';
+
+import './NxFormSelect.scss';
+
 export { Props };
 
-const NxFormSelect = forwardRef<HTMLSelectElement, Props>(
-    function NxFormSelect(props: Props, forwardedRef) {
-      const { className: classNameProp, ...otherProps } = props,
-          className = classnames('nx-form-select', classNameProp);
-      const attrs = omit(['isPristine'], otherProps);
-      return (
-        <select ref={forwardedRef} { ...{ className, ...attrs } } />
-      );
-    }
-);
+const NxFormSelect = forwardRef<HTMLSelectElement, Props>(function NxFormSelect(props: Props, forwardedRef) {
+  const { className: classNameProp, validatable, isPristine, validationErrors, ...attrs } = props,
+      { showValidationErrors: formShowValidationErrors } = useContext(FormAriaContext),
+      showValidationErrors = formShowValidationErrors || !isPristine,
+      isInvalid = !!(validatable && showValidationErrors && hasValidationErrors(validationErrors)),
+      firstValidationError = validatable && getFirstValidationError(validationErrors),
+      invalidMessageId = useUniqueId('nx-form-select-invalid-message'),
+      className = classnames('nx-form-select', classNameProp, {
+        pristine: isPristine,
+        invalid: isInvalid,
+        valid: !isPristine && validatable && !isInvalid
+      });
+
+  return (
+    <div className={className}>
+      <select className="nx-form-select__select"
+              ref={forwardedRef}
+              { ...attrs }
+              aria-invalid={isInvalid}
+              aria-errormessage={invalidMessageId} />
+      { isInvalid &&
+        <div id={invalidMessageId} role="alert" className="nx-field-validation-message">
+          {firstValidationError}
+        </div>
+      }
+    </div>
+  );
+});
 
 export default NxFormSelect;
