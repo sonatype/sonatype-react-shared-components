555cc17452bdecc016552508eb65cf9ebd91b2b0
Ross Pokorny <rpokorny@sonatype.com>
2022-08-26 14:10:08 -0400

    Add validation unit tests - RSC-1090

--- lib/src/components/NxFormSelect/__tests__/NxFormSelect.test.tsx
+++ lib/src/components/NxFormSelect/__tests__/NxFormSelect.test.tsx
@@ -9,7 +9,9 @@ import userEvent from '@testing-library/user-event';
 
 import { rtlRender, rtlRenderElement } from '../../../__testutils__/rtlUtils';
 
-import NxFormSelect from '../NxFormSelect';
+import NxFormSelect, { Props } from '../NxFormSelect';
+import { render, within } from '@testing-library/react';
+import NxForm from '../../NxForm/NxForm';
 
 describe('NxFormSelect', function() {
   const minimalProps = { value: '', onChange: () => {} },
@@ -83,5 +85,402 @@ describe('NxFormSelect', function() {
 
     expect(onChange).toHaveBeenCalledTimes(1);
     expect(capturedValue!).toBe('Bar');
+  });
+
+  describe('when not validatable', function() {
+    const nonValidatableMinimalProps = { ...minimalProps, validatable: true };
+
+    describe('when pristine', function() {
+      const pristineMinimalProps = { ...nonValidatableMinimalProps, isPristine: true };
+
+      describe('when there are no validation errors', function() {
+        const noValidationErrorsMinimalProps = pristineMinimalProps,
+            quickRender = rtlRender(NxFormSelect, noValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const component = quickRender();
+
+          expect(component.queryByRole('alert')).not.toBeInTheDocument();
+          expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...nonValidatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          it('has no validation alert and no select a11y error message', function() {
+            const component = quickRender();
+
+            expect(component.queryByRole('alert')).not.toBeInTheDocument();
+            expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+          });
+
+          it('does not set aria-invalid on the select', function() {
+            expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+
+      describe('when there are validation errors', function() {
+        const singleValidationErrorsMinimalProps = { ...pristineMinimalProps, validationErrors: 'foo' },
+            multiValidationErrorsMinimalProps = { ...pristineMinimalProps, validationErrors: ['bar', 'foo'] },
+            singleRender = rtlRender(NxFormSelect, singleValidationErrorsMinimalProps),
+            multiRender = rtlRender(NxFormSelect, multiValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const singleError = singleRender(),
+              multiError = multiRender();
+
+          expect(singleError.queryByRole('alert')).not.toBeInTheDocument();
+          expect(singleError.getByRole('combobox')).not.toHaveErrorMessage();
+
+          expect(multiError.queryByRole('alert')).not.toBeInTheDocument();
+          expect(multiError.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(singleRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          expect(multiRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...nonValidatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          it('has no validation alert and no select a11y error message', function() {
+            const component = quickRender();
+
+            expect(component.queryByRole('alert')).not.toBeInTheDocument();
+            expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+          });
+
+          it('does not set aria-invalid on the select', function() {
+            expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+    });
+
+    describe('when not pristine', function() {
+      const pristineMinimalProps = { ...nonValidatableMinimalProps, isPristine: true };
+
+      describe('when there are no validation errors', function() {
+        const noValidationErrorsMinimalProps = pristineMinimalProps,
+            quickRender = rtlRender(NxFormSelect, noValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const component = quickRender();
+
+          expect(component.queryByRole('alert')).not.toBeInTheDocument();
+          expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...nonValidatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          it('has no validation alert and no select a11y error message', function() {
+            const component = quickRender();
+
+            expect(component.queryByRole('alert')).not.toBeInTheDocument();
+            expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+          });
+
+          it('does not set aria-invalid on the select', function() {
+            expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+
+      describe('when there are validation errors', function() {
+        const singleValidationErrorsMinimalProps = { ...pristineMinimalProps, validationErrors: 'foo' },
+            multiValidationErrorsMinimalProps = { ...pristineMinimalProps, validationErrors: ['bar', 'foo'] },
+            singleRender = rtlRender(NxFormSelect, singleValidationErrorsMinimalProps),
+            multiRender = rtlRender(NxFormSelect, multiValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const singleError = singleRender(),
+              multiError = multiRender();
+
+          expect(singleError.queryByRole('alert')).not.toBeInTheDocument();
+          expect(singleError.getByRole('combobox')).not.toHaveErrorMessage();
+
+          expect(multiError.queryByRole('alert')).not.toBeInTheDocument();
+          expect(multiError.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(singleRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          expect(multiRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...nonValidatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          it('has no validation alert and no select a11y error message', function() {
+            const component = quickRender();
+
+            expect(component.queryByRole('alert')).not.toBeInTheDocument();
+            expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+          });
+
+          it('does not set aria-invalid on the select', function() {
+            expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+    });
+  });
+
+  describe('when validatable', function() {
+    const validatableMinimalProps = { ...minimalProps, validatable: true };
+
+    describe('when pristine', function() {
+      const pristineMinimalProps = { ...validatableMinimalProps, isPristine: true };
+
+      describe('when there are no validation errors', function() {
+        const noValidationErrorsMinimalProps = pristineMinimalProps,
+            quickRender = rtlRender(NxFormSelect, noValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const component = quickRender();
+
+          expect(component.queryByRole('alert')).not.toBeInTheDocument();
+          expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...validatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          it('has no validation alert and no select a11y error message', function() {
+            const component = quickRender();
+
+            expect(component.queryByRole('alert')).not.toBeInTheDocument();
+            expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+          });
+
+          it('does not set aria-invalid on the select', function() {
+            expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+
+      describe('when there are validation errors', function() {
+        const singleValidationErrorsMinimalProps = { ...pristineMinimalProps, validationErrors: 'foo' },
+            multiValidationErrorsMinimalProps = { ...pristineMinimalProps, validationErrors: ['bar', 'foo'] },
+            singleRender = rtlRender(NxFormSelect, singleValidationErrorsMinimalProps),
+            multiRender = rtlRender(NxFormSelect, multiValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const singleError = singleRender(),
+              multiError = multiRender();
+
+          expect(singleError.queryByRole('alert')).not.toBeInTheDocument();
+          expect(singleError.getByRole('combobox')).not.toHaveErrorMessage();
+
+          expect(multiError.queryByRole('alert')).not.toBeInTheDocument();
+          expect(multiError.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(singleRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          expect(multiRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...validatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          const singleRender = () => quickRender(singleValidationErrorsMinimalProps),
+              multiRender = () => quickRender(multiValidationErrorsMinimalProps);
+
+          it('has a validation alert and select a11y error message based on the first error', function() {
+            const singleError = singleRender(),
+                multiError = multiRender();
+
+            expect(singleError.getByRole('alert')).toHaveTextContent('foo');
+            expect(singleError.getByRole('combobox')).toHaveErrorMessage('foo');
+
+            expect(multiError.getByRole('alert')).toHaveTextContent('bar');
+            expect(multiError.getByRole('combobox')).toHaveErrorMessage('bar');
+          });
+
+          it('sets aria-invalid on the select', function() {
+            const singleError = singleRender(),
+                multiError = multiRender();
+
+            expect(singleError.getByRole('combobox')).toHaveAttribute('aria-invalid', 'true');
+            expect(multiError.getByRole('combobox')).toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+    });
+
+    describe('when not pristine', function() {
+      const nonPristineMinimalProps = { ...validatableMinimalProps, isPristine: false };
+
+      describe('when there are no validation errors', function() {
+        const noValidationErrorsMinimalProps = nonPristineMinimalProps,
+            quickRender = rtlRender(NxFormSelect, noValidationErrorsMinimalProps);
+
+        it('has no validation alert and no select a11y error message', function() {
+          const component = quickRender();
+
+          expect(component.queryByRole('alert')).not.toBeInTheDocument();
+          expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+        });
+
+        it('does not set aria-invalid on the select', function() {
+          expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...validatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          it('has no validation alert and no select a11y error message', function() {
+            const component = quickRender();
+
+            expect(component.queryByRole('alert')).not.toBeInTheDocument();
+            expect(component.getByRole('combobox')).not.toHaveErrorMessage();
+          });
+
+          it('does not set aria-invalid on the select', function() {
+            expect(quickRender().getByRole('combobox')).not.toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+
+      describe('when there are validation errors', function() {
+        const singleValidationErrorsMinimalProps = { ...nonPristineMinimalProps, validationErrors: 'foo' },
+            multiValidationErrorsMinimalProps = { ...nonPristineMinimalProps, validationErrors: ['bar', 'foo'] },
+            singleRender = rtlRender(NxFormSelect, singleValidationErrorsMinimalProps),
+            multiRender = rtlRender(NxFormSelect, multiValidationErrorsMinimalProps);
+
+        it('has a validation alert and select a11y error message based on the first error', function() {
+          const singleError = singleRender(),
+              multiError = multiRender();
+
+          expect(singleError.getByRole('alert')).toHaveTextContent('foo');
+          expect(singleError.getByRole('combobox')).toHaveErrorMessage('foo');
+
+          expect(multiError.getByRole('alert')).toHaveTextContent('bar');
+          expect(multiError.getByRole('combobox')).toHaveErrorMessage('bar');
+        });
+
+        it('sets aria-invalid on the select', function() {
+          const singleError = singleRender(),
+              multiError = multiRender();
+
+          expect(singleError.getByRole('combobox')).toHaveAttribute('aria-invalid', 'true');
+          expect(multiError.getByRole('combobox')).toHaveAttribute('aria-invalid', 'true');
+        });
+
+        describe('when in a form when showValidationErrors', function() {
+          function quickRender(extraProps?: Partial<Props>) {
+            const renderResult = render(
+                  <NxForm showValidationErrors onSubmit={() => {}}>
+                    <NxFormSelect data-testid="select" { ...validatableMinimalProps } { ...extraProps } />
+                  </NxForm>
+                ),
+                boundQueries = within(renderResult.container);
+
+            return { ...renderResult, ...boundQueries };
+          }
+
+          const singleRender = () => quickRender(singleValidationErrorsMinimalProps),
+              multiRender = () => quickRender(multiValidationErrorsMinimalProps);
+
+          it('has a validation alert and select a11y error message based on the first error', function() {
+            const singleError = singleRender(),
+                multiError = multiRender();
+
+            expect(singleError.getByRole('alert')).toHaveTextContent('foo');
+            expect(singleError.getByRole('combobox')).toHaveErrorMessage('foo');
+
+            expect(multiError.getByRole('alert')).toHaveTextContent('bar');
+            expect(multiError.getByRole('combobox')).toHaveErrorMessage('bar');
+          });
+
+          it('sets aria-invalid on the select', function() {
+            const singleError = singleRender(),
+                multiError = multiRender();
+
+            expect(singleError.getByRole('combobox')).toHaveAttribute('aria-invalid', 'true');
+            expect(multiError.getByRole('combobox')).toHaveAttribute('aria-invalid', 'true');
+          });
+        });
+      });
+    });
   });
 });
