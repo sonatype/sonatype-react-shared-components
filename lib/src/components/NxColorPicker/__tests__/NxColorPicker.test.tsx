2bb0d679bae6c10bccb6c1ea191adfa45dbeb77a
Ross Pokorny <rpokorny@sonatype.com>
2022-08-31 12:21:12 -0400

    Validation unit tests - RSC-1091

--- lib/src/components/NxColorPicker/__tests__/NxColorPicker.test.tsx
+++ lib/src/components/NxColorPicker/__tests__/NxColorPicker.test.tsx
@@ -4,11 +4,14 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import React from 'react';
 import userEvent from '@testing-library/user-event';
+import { render, within } from '@testing-library/react';
 
 import { rtlRender, rtlRenderElement } from '../../../__testutils__/rtlUtils';
 import NxColorPicker, { Props } from '../NxColorPicker';
 import { selectableColors } from '../../../util/selectableColors';
+import NxForm from '../../NxForm/NxForm';
 
 describe('NxColorPicker', function() {
   const minimalProps = { label: 'My Color Picker' },
@@ -79,5 +82,52 @@ describe('NxColorPicker', function() {
         radio = view.getByRole('radio', { name: /turquoise/i })
 
     await user.click(radio);
+  });
+
+  describe('validation', function() {
+    it('adds an alert with the first specified validation message when isPristine is false', function() {
+      expect(quickRender().queryByRole('alert')).not.toBeInTheDocument();
+      expect(quickRender({ isPristine: true, validationErrors: 'asdf' }).queryByRole('alert')).not.toBeInTheDocument();
+      expect(quickRender({ isPristine: true }).queryByRole('alert')).not.toBeInTheDocument();
+
+      const withValidationErrors = quickRender({ isPristine: false, validationErrors: ['asdf', 'zxcv'] }),
+          validationErrorEl = withValidationErrors.getByRole('alert');
+
+      expect(validationErrorEl).toHaveTextContent('asdf');
+    });
+
+    it('adds an alert with the first specified validation message when within an NxForm with showValidationErrors',
+        function() {
+          function renderWithForm(showValidationErrors: boolean, props?: Partial<Props>) {
+            return render(
+              <NxForm onSubmit={() => {}} showValidationErrors={showValidationErrors}>
+                <NxColorPicker { ...minimalProps } { ...props } />
+              </NxForm>
+            );
+          }
+
+          expect(renderWithForm(true).queryByRole('alert')).not.toBeInTheDocument();
+
+          const withValidationErrors = renderWithForm(true, { validationErrors: ['asdf', 'zxcv'] }),
+              validationErrorEl = withValidationErrors.getByRole('alert');
+
+          expect(validationErrorEl).toHaveTextContent('asdf');
+        }
+    );
+
+    it('sets the validation error as the accessible description of the fieldset', function() {
+      expect(renderEl()).not.toHaveAccessibleDescription();
+      expect(renderEl({ isPristine: true, validationErrors: 'asdf' })).not.toHaveAccessibleDescription();
+      expect(renderEl({ isPristine: true })).not.toHaveAccessibleDescription();
+      expect(renderEl({ isPristine: false, validationErrors: ['asdf', 'zxcv'] })).toHaveAccessibleDescription('asdf');
+
+      const formView = render(
+        <NxForm onSubmit={() => {}} showValidationErrors={true}>
+          <NxColorPicker { ...minimalProps } validationErrors="asdf" />
+        </NxForm>
+      );
+
+      expect(within(formView.container).getByRole('group')).toHaveAccessibleDescription('asdf');
+    });
   });
 });
