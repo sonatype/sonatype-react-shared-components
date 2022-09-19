f4bba1f3398c67447bd496b46583ce8e9cbc09da
Ross Pokorny <rpokorny@sonatype.com>
2022-09-19 16:56:34 -0400

    Add children to props - RSC-991

--- lib/src/components/NxMeter/types.ts
+++ lib/src/components/NxMeter/types.ts
@@ -5,14 +5,16 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import * as PropTypes from 'prop-types';
-import { MeterHTMLAttributes } from 'react';
+import { MeterHTMLAttributes, ReactNode } from 'react';
 
 export interface Props extends Omit<MeterHTMLAttributes<HTMLMeterElement>, 'max' | 'min' | 'low' | 'high' | 'optimum'> {
   max?: number | null;
   value: number;
+  children: Exclude<ReactNode, null | undefined>;
 }
 
 export const propTypes: PropTypes.ValidationMap<Props> = {
   max: PropTypes.number,
-  value: PropTypes.number.isRequired
+  value: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired
 };
