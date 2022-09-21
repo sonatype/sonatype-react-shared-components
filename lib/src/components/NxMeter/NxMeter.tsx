18e847cde1be29df86f5e5ffa36d2030b1c9c1c3
Ross Pokorny <rpokorny@sonatype.com>
2022-09-21 15:06:28 -0400

    NxMeter code cleanup - RSC-991

--- lib/src/components/NxMeter/NxMeter.tsx
+++ lib/src/components/NxMeter/NxMeter.tsx
@@ -15,14 +15,13 @@ import NxTooltip from '../NxTooltip/NxTooltip';
 export { Props };
 
 const NxMeter = forwardRef<HTMLMeterElement, Props>(function NxMeter(props, ref) {
-  const { className: classNameProp, max: maxProp, children, value, ...otherAttributes } = props,
+  const { className: classNameProp, max: maxProp, children, ...otherAttributes } = props,
       className = classnames('nx-meter', classNameProp),
       max: number = maxProp ?? 100;
 
   return (
     <NxTooltip title={children}>
-      <meter ref={ref} className={className} max={max} value={value} { ...otherAttributes }>
-        {/* <span className="nx-meter__bar" style={{ width: `${valuePercent}%` }} /> */}
+      <meter ref={ref} className={className} max={max} { ...otherAttributes }>
         <span className="nx-meter__content">{children}</span>
       </meter>
     </NxTooltip>
