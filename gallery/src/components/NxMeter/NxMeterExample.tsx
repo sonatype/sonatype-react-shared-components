ec30b49109126c6df636e8605388062c64124a12
Ross Pokorny <rpokorny@sonatype.com>
2022-09-21 15:46:08 -0400

    Fix meter spacing in example - RSC-991

--- gallery/src/components/NxMeter/NxMeterExample.tsx
+++ gallery/src/components/NxMeter/NxMeterExample.tsx
@@ -11,22 +11,22 @@ export default function NxMeterExample() {
   return (
     <>
       <div>
-        Fuel Level: <NxMeter value={0}>Empty!</NxMeter>
+        Fuel Level:<NxMeter value={0}>Empty!</NxMeter>
       </div>
       <div>
-        Fuel Level: <NxMeter value={50}>50%</NxMeter>
+        Fuel Level:<NxMeter value={50}>50%</NxMeter>
       </div>
       <div>
-        Fuel Level: <NxMeter value={100}>Full</NxMeter>
+        Fuel Level:<NxMeter value={100}>Full</NxMeter>
       </div>
       <div>
-        Operable Engines: <NxMeter value={0} max={4}>0 out of 4</NxMeter>
+        Operable Engines:<NxMeter value={0} max={4}>0 out of 4</NxMeter>
       </div>
       <div>
-        Operable Engines: <NxMeter value={3} max={4}>3 out of 4</NxMeter>
+        Operable Engines:<NxMeter value={3} max={4}>3 out of 4</NxMeter>
       </div>
       <div>
-        Operable Engines: <NxMeter value={4} max={4}>4 out of 4</NxMeter>
+        Operable Engines:<NxMeter value={4} max={4}>4 out of 4</NxMeter>
       </div>
     </>
   );
