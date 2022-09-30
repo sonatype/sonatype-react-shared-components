d0c061cb0eb09b33eb91d5d58b94c9b390c9eea3
EBermudez88 <ebermudez@sonatype.com>
2022-09-30 13:06:15 -0400

    add visual testing for themeSettingsModal - RSC-1170

---
+++ gallery/visualtests/themeSettingsModal.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2019-present Sonatype, Inc.
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
+ */
+
+const { setupBrowser } = require('./testUtils');
+
+describe('ThemeSettingsModal', function() {
+  const { waitAndGetElements, wait, getPage, checkFullPageScreenshot } = setupBrowser('#/');
+
+  const openModalBtn = '.gallery-page-header__theme-settings-button',
+      themeSettingsModal = '.gallery-theme-settings-modal',
+      enableChangesToggle = `${themeSettingsModal} .nx-form-group label:nth-of-type(2)`,
+      darkModeRadio = `${themeSettingsModal} .nx-radio:nth-of-type(2)`,
+      lightModeRadio = `${themeSettingsModal} .nx-radio:nth-of-type(3)`;
+
+  async function openThemeSettingsModal() {
+    const [targetElement] = await waitAndGetElements(openModalBtn);
+    await targetElement.click();
+    await waitAndGetElements(themeSettingsModal);
+  }
+
+  async function selectModeChoice(radioElement) {
+    await openThemeSettingsModal();
+
+    const [targetElement] = await waitAndGetElements(radioElement);
+    targetElement.click();
+    await wait(400);
+
+    await checkFullPageScreenshot();
+  }
+
+  beforeEach(async function() {
+    await getPage().setViewport({ width: 1366, height: 1000 });
+  });
+
+  it('disallows any theme changes by default', async function() {
+    await openThemeSettingsModal();
+    await checkFullPageScreenshot();
+  });
+
+  it('enables radios when opted in to theme changes',
+      async function() {
+        await openThemeSettingsModal();
+
+        const [toggle] = await waitAndGetElements(enableChangesToggle);
+        toggle.click();
+        await wait(400);
+
+        await checkFullPageScreenshot();
+      }
+  );
+
+  it('changes display theme to dark when dark mode radio is selected', async function() {
+    await selectModeChoice(darkModeRadio);
+  });
+
+  it('changes display theme to light when light mode radio is selected', async function() {
+    await selectModeChoice(lightModeRadio);
+  });
+
+});
