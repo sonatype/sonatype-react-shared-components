5fb9866a023f3854984afd914c8d34323d11a6e1
Andrew Prasetya <aprasetya@sonatype.com>
2022-11-02 16:25:02 -0400

    Progress on basic keynav. - RSC-989

--- lib/src/components/NxDropdownMenu/NxDropdownMenu.tsx
+++ lib/src/components/NxDropdownMenu/NxDropdownMenu.tsx
@@ -4,8 +4,9 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, { useLayoutEffect, forwardRef } from 'react';
+import React, { useLayoutEffect, forwardRef, useState, useEffect, useRef, KeyboardEventHandler } from 'react';
 import classnames from 'classnames';
+import useMergedRef from '@react-hook/merged-ref';
 
 import { Props, propTypes } from './types';
 
@@ -19,13 +20,66 @@ export { Props };
  */
 /* eslint-disable-next-line react/prop-types */
 const NxDropdownMenu = forwardRef<HTMLDivElement, Props>(function NxDropdownMenu(props, ref) {
-  const { onClosing, className: classNameProp, ...attrs } = props,
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [focusedIndex, setFocusedIndex] = useState<number>(0);
+  const mergedRef = useMergedRef(menuRef, ref);
+
+  const { onClosing, className: classNameProp, children, ...attrs } = props,
       className = classnames('nx-dropdown-menu', classNameProp);
 
   // onClosing must execute when this element is being removed but BEFORE it actually gets removed from the DOM
   useLayoutEffect(() => onClosing, []);
 
-  return <div ref={ref} { ...{ className, ...attrs } } />;
+  useEffect(() => {
+    // <a> or <button>
+    if (menuRef.current) {
+      menuRef.current.focus();
+      // const elements = menuRef.current.querySelectorAll<HTMLAnchorElement | HTMLButtonElement>('a, button');
+    }
+  }, [menuRef]);
+
+  const handleKeyDown: KeyboardEventHandler<HTMLDivElement> = (event) => {
+    if (menuRef.current) {
+      const elements = menuRef.current.querySelectorAll<HTMLAnchorElement | HTMLButtonElement>('a, button');
+
+      switch (event.key) {
+        case 'ArrowUp':
+          event.preventDefault();
+          if (document.activeElement === menuRef.current) {
+            setFocusedIndex(0);
+            elements[0].focus();
+          }
+          else {
+            const updatedFocusedIndex = (focusedIndex - 1 >= 0) ? focusedIndex - 1 : elements.length - 1;
+            setFocusedIndex(updatedFocusedIndex);
+            elements[updatedFocusedIndex].focus();
+          }
+          break;
+        case 'ArrowDown':
+          event.preventDefault();
+          if (document.activeElement === menuRef.current) {
+            setFocusedIndex(0);
+            elements[0].focus();
+          }
+          else {
+            const updatedFocusedIndex = (focusedIndex + 1 >= elements.length) ? 0 : focusedIndex + 1;
+            setFocusedIndex(updatedFocusedIndex);
+            elements[updatedFocusedIndex].focus();
+          }
+          break;
+      }
+    }
+  };
+
+  return (
+    <div ref={mergedRef}
+         onKeyDown={handleKeyDown}
+         tabIndex={0}
+         role="menu"
+         { ...{ className, ...attrs } }>
+      { children }
+    </div>
+  );
 });
 
 NxDropdownMenu.propTypes = propTypes;
