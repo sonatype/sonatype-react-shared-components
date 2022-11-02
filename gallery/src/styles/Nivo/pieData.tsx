001fe492aa0930e6eaf3daee018bf1bd91417b1d
Rani Patel <ranipatel@Interns-Rani-P.local>
2022-11-02 10:12:27 -0500

    fixed spacing - RSC-765

--- gallery/src/styles/Nivo/pieData.tsx
+++ gallery/src/styles/Nivo/pieData.tsx
@@ -6,44 +6,12 @@
    */
 
 export const pieData = [
-  {
-    'id': 'donuts',
-    'label': 'donuts',
-    'value': 357
-  },
-  {
-    'id': 'ice cream',
-    'label': 'ice cream',
-    'value': 411
-  },
-  {
-    'id': 'pizza',
-    'label': 'pizza',
-    'value': 528
-  },
-  {
-    'id': 'fries',
-    'label': 'fries',
-    'value': 232
-  },
-  {
-    'id': 'oreos',
-    'label': 'oreos',
-    'value': 222
-  },
-  {
-    'id': 'quesadilla',
-    'label': 'quesadilla',
-    'value': 411
-  },
-  {
-    'id': 'nachos',
-    'label': 'nachos',
-    'value': 328
-  },
-  {
-    'id': 'salad',
-    'label': 'salad',
-    'value': 86
-  }
+  { 'id': 'donuts', 'label': 'donuts', 'value': 357 },
+  { 'id': 'ice cream', 'label': 'ice cream', 'value': 411 },
+  { 'id': 'pizza', 'label': 'pizza', 'value': 528 },
+  { 'id': 'fries', 'label': 'fries', 'value': 232 },
+  { 'id': 'oreos', 'label': 'oreos', 'value': 222 },
+  { 'id': 'quesadilla', 'label': 'quesadilla', 'value': 411 },
+  { 'id': 'nachos', 'label': 'nachos', 'value': 328 },
+  { 'id': 'salad', 'label': 'salad', 'value': 86 }
 ];
