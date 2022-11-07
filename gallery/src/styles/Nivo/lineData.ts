61bb8430c26adb1265e42f28d13910e316bedb79
Rani Patel <ranipatel@Interns-Rani-P.local>
2022-11-07 13:21:25 -0600

    remove quotes - RSC-765

--- gallery/src/styles/Nivo/lineData.ts
+++ gallery/src/styles/Nivo/lineData.ts
@@ -26,69 +26,69 @@ export const lineData = [
   {
     id: 'france',
     data: [
-      { 'x': 'plane', y: 75 },
-      { 'x': 'helicopter', y: 83 },
-      { 'x': 'boat', y: 151 },
-      { 'x': 'train', y: 3 },
-      { 'x': 'subway', y: 11 },
-      { 'x': 'bus', y: 113 },
-      { 'x': 'car', y: 4 },
-      { 'x': 'moto', y: 141 },
-      { 'x': 'bicycle', y: 126 },
-      { 'x': 'horse', y: 277 },
-      { 'x': 'skateboard', y: 31 },
-      { 'x': 'others', y: 237 }
+      { x: 'plane', y: 75 },
+      { x: 'helicopter', y: 83 },
+      { x: 'boat', y: 151 },
+      { x: 'train', y: 3 },
+      { x: 'subway', y: 11 },
+      { x: 'bus', y: 113 },
+      { x: 'car', y: 4 },
+      { x: 'moto', y: 141 },
+      { x: 'bicycle', y: 126 },
+      { x: 'horse', y: 277 },
+      { x: 'skateboard', y: 31 },
+      { x: 'others', y: 237 }
     ]
   },
   {
     id: 'us',
     data: [
-      { 'x': 'plane', y: 204 },
-      { 'x': 'helicopter', y: 222 },
-      { 'x': 'boat', y: 36 },
-      { 'x': 'train', y: 126 },
-      { 'x': 'subway', y: 184 },
-      { 'x': 'bus', y: 128 },
-      { 'x': 'car', y: 200 },
-      { 'x': 'moto', y: 223 },
-      { 'x': 'bicycle', y: 107 },
-      { 'x': 'horse', y: 104 },
-      { 'x': 'skateboard', y: 124 },
-      { 'x': 'others', y: 186 }
+      { x: 'plane', y: 204 },
+      { x: 'helicopter', y: 222 },
+      { x: 'boat', y: 36 },
+      { x: 'train', y: 126 },
+      { x: 'subway', y: 184 },
+      { x: 'bus', y: 128 },
+      { x: 'car', y: 200 },
+      { x: 'moto', y: 223 },
+      { x: 'bicycle', y: 107 },
+      { x: 'horse', y: 104 },
+      { x: 'skateboard', y: 124 },
+      { x: 'others', y: 186 }
     ]
   },
   {
     id: 'germany',
     data: [
-      { 'x': 'plane', y: 180 },
-      { 'x': 'helicopter', y: 23 },
-      { 'x': 'boat', y: 155 },
-      { 'x': 'train', y: 115 },
-      { 'x': 'subway', y: 115 },
-      { 'x': 'bus', y: 76 },
-      { 'x': 'car', y: 55 },
-      { 'x': 'moto', y: 38 },
-      { 'x': 'bicycle', y: 85 },
-      { 'x': 'horse', y: 207 },
-      { 'x': 'skateboard', y: 276 },
-      { 'x': 'others', y: 2 }
+      { x: 'plane', y: 180 },
+      { x: 'helicopter', y: 23 },
+      { x: 'boat', y: 155 },
+      { x: 'train', y: 115 },
+      { x: 'subway', y: 115 },
+      { x: 'bus', y: 76 },
+      { x: 'car', y: 55 },
+      { x: 'moto', y: 38 },
+      { x: 'bicycle', y: 85 },
+      { x: 'horse', y: 207 },
+      { x: 'skateboard', y: 276 },
+      { x: 'others', y: 2 }
     ]
   },
   {
     id: 'norway',
     data: [
-      { 'x': 'plane', y: 292 },
-      { 'x': 'helicopter', y: 54 },
-      { 'x': 'boat', y: 144 },
-      { 'x': 'train', y: 159 },
-      { 'x': 'subway', y: 32 },
-      { 'x': 'bus', y: 202 },
-      { 'x': 'car', y: 171 },
-      { 'x': 'moto', y: 117 },
-      { 'x': 'bicycle', y: 184 },
-      { 'x': 'horse', y: 54 },
-      { 'x': 'skateboard', y: 2 },
-      { 'x': 'others', y: 288 }
+      { x: 'plane', y: 292 },
+      { x: 'helicopter', y: 54 },
+      { x: 'boat', y: 144 },
+      { x: 'train', y: 159 },
+      { x: 'subway', y: 32 },
+      { x: 'bus', y: 202 },
+      { x: 'car', y: 171 },
+      { x: 'moto', y: 117 },
+      { x: 'bicycle', y: 184 },
+      { x: 'horse', y: 54 },
+      { x: 'skateboard', y: 2 },
+      { x: 'others', y: 288 }
     ]
   }
 ];
