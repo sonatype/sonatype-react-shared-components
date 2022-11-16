2404df74b517db0b783ddec5bd265266bf40df6c
Rani Patel <ranipatel@Interns-Rani-P.attlocal.net>
2022-11-16 13:37:17 -0600

    changed class test - RSC-1229

--- lib/src/components/NxTextLink/__tests__/NxTextLink.test.tsx
+++ lib/src/components/NxTextLink/__tests__/NxTextLink.test.tsx
@@ -13,6 +13,17 @@ describe('NxTextLink', function() {
   const quickRender = rtlRender(NxTextLink, {});
   const renderEl = rtlRenderElement(NxTextLink, {});
 
+  it('adds additional specified classes', function() {
+    const el = renderEl()!
+    const customEl = renderEl({ className: 'foo' })!;
+
+    expect(customEl).toHaveClass('foo');
+
+    for (const cls of Array.from(el.classList)) {
+      expect(customEl).toHaveClass(cls);
+    }
+  });
+
   it('adds additional specified props', function() {
     const el = renderEl({ id: 'foo', lang: 'en_US' });
 
