ee44c5c4832db6fcf01954776add2b2c6f43a252
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2022-11-16 10:50:40 -0800

    Test NxStatefulTextInput - RSC-1232

--- lib/src/components/NxTextInput/stateful/__tests__/NxStatefulTextInput.test.tsx
+++ lib/src/components/NxTextInput/stateful/__tests__/NxStatefulTextInput.test.tsx
@@ -12,7 +12,7 @@ import userEvent from '@testing-library/user-event';
 import NxStatefulTextInput, { PublicProps } from '../NxStatefulTextInput';
 import NxForm from '../../../NxForm/NxForm';
 
-describe('PrivateNxStatefulTextInput', function() {
+describe('NxStatefulTextInput', function() {
   const minimalProps: PublicProps & RefAttributes<HTMLDivElement> = {
         defaultValue: ''
       },
