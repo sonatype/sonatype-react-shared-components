d90926f741e3ce45781229b29afce572b46bf788
Rani Patel <ranipatel@Interns-Rani-P.attlocal.net>
2022-11-21 12:38:08 -0600

    readonly spelling - RSC-1236

--- lib/src/components/NxRadio/__tests__/NxRadio.test.tsx
+++ lib/src/components/NxRadio/__tests__/NxRadio.test.tsx
@@ -114,9 +114,9 @@ describe('NxRadio', function() {
   });
 
   it('sets the input as readonly if there is no onChange handler', function() {
-    expect(renderEl()?.querySelector('input')).not.toHaveAttribute('readOnly');
-    expect(renderEl({ onChange: undefined })?.querySelector('input')).toHaveAttribute('readOnly');
-    expect(renderEl({ onChange: null })?.querySelector('input')).toHaveAttribute('readOnly');
+    expect(renderEl()?.querySelector('input')).not.toHaveAttribute('readonly');
+    expect(renderEl({ onChange: undefined })?.querySelector('input')).toHaveAttribute('readonly');
+    expect(renderEl({ onChange: null })?.querySelector('input')).toHaveAttribute('readonly');
   });
 
   it('adds id attribute to radio input when radioId prop is provided', function () {
