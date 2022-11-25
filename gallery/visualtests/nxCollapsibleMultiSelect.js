f96fe93d9a6acfb9bc0b3faf3c4d3417cc0a3137
Andrew Prasetya <aprasetya@sonatype.com>
2022-11-25 16:49:08 -0500

    Fixed nx-counter not aligned when disabled. - RSC-1262

--- gallery/visualtests/nxCollapsibleMultiSelect.js
+++ gallery/visualtests/nxCollapsibleMultiSelect.js
@@ -10,8 +10,14 @@ describe('NxCollapsibleMultiSelect', function() {
   const { focusTest, simpleTest, a11yTest } = setupBrowser('#/pages/Collapsible Multi-Select');
 
   const selector = '#nx-collapsible-multi-select-example .nx-collapsible-items--select';
+  const disabledWithTooltipSelector
+    = '#nx-collapsible-multi-select-disabled-with-tooltip-example .gallery-example-live';
 
   it('looks right', simpleTest(selector));
+
+  describe('NxCollapsibleMultiSelect Disabled With Tooltip', function() {
+    it('looks right', simpleTest(disabledWithTooltipSelector));
+  });
 
   describe('NxCollapsibleMultiSelect checkbox', function() {
     const checkboxSelector = selector + ' .nx-collapsible-items__child:nth-child(2) .nx-radio-checkbox__input';
