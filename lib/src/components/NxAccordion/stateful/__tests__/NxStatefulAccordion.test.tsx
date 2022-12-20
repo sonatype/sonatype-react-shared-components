17f2d62b34bab91b699923bf44a2dc2aa54ea578
Rani Patel <ranipatel@Interns-Rani-P.attlocal.net>
2022-12-20 14:20:22 -0600

    toggle open - RSC-1285

--- lib/src/components/NxAccordion/stateful/__tests__/NxStatefulAccordion.test.tsx
+++ lib/src/components/NxAccordion/stateful/__tests__/NxStatefulAccordion.test.tsx
@@ -4,16 +4,16 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import React from 'react';
 import userEvent from '@testing-library/user-event';
 
 import NxStatefulAccordion from '../NxStatefulAccordion';
-
-import { rtlRenderElement } from '../../../../__testutils__/rtlUtils';
-//import { fireEvent } from '@testing-library/react';
+import NxAccordion from '../../NxAccordion';
+import { rtlRender, rtlRenderElement } from '../../../../__testutils__/rtlUtils';
 
 describe('NxStatefulAccordion', function() {
 
-  //const quickRender = rtlRender(NxStatefulAccordion, {});
+  const quickRender = rtlRender(NxStatefulAccordion, {});
   const renderEl = rtlRenderElement(NxStatefulAccordion, {});
 
   it('renders an NxAccordion with the specified props', function() {
@@ -42,28 +42,23 @@ describe('NxStatefulAccordion', function() {
   });
 
   it('toggles the NxAccordion open prop when the NxAccordion onToggle callback is called', async function() {
-    const user = userEvent.setup();
-    const mockOnToggle = jest.fn();
-    const el = renderEl({ onToggle: mockOnToggle, defaultOpen: false })!;
+    const user = userEvent.setup(),
+        onToggle = jest.fn(),
+        el = quickRender({ onToggle,
+          children: (
+            <NxAccordion.Header>
+              <NxAccordion.Title>Foo</NxAccordion.Title>
+            </NxAccordion.Header>
+          )});
 
-    expect(el).not.toHaveAttribute('open');
+    const header = el.getByRole('button')!;
 
-    const open = el.querySelector('.nx-accordion')!;
+    expect(onToggle).not.toHaveBeenCalled();
+    expect(el.getByRole('group')).not.toHaveAttribute('open');
 
-    await user.click(open);
+    await user.click(header);
 
-    //await fireEvent.change(mockOnToggle);
-
-    expect(mockOnToggle).toHaveBeenCalledTimes(1);
-
-    // act(function() {
-    //   component.simulate('toggle');
-    // });
-
-    // act(function() {
-    //   component.simulate('toggle');
-    // });
-
-    expect(el).toHaveAttribute('open');
+    expect(onToggle).toHaveBeenCalled();
+    expect(el.getByRole('group')).toHaveAttribute('open');
   });
 });
