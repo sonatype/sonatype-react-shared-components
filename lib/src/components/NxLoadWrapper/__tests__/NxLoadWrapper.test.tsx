cd909078f69d8a97cad9a419bf41c52a20dc4b1f
Serhii Bondarchuk <63853236+Serhii-Bondarchuk@users.noreply.github.com>
2023-01-10 15:08:13 +0200

    inital commit

--- lib/src/components/NxLoadWrapper/__tests__/NxLoadWrapper.test.tsx
+++ lib/src/components/NxLoadWrapper/__tests__/NxLoadWrapper.test.tsx
@@ -4,48 +4,57 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+// import { screen } from '@testing-library/react';
 import React from 'react';
-import * as enzymeUtils from '../../../__testutils__/enzymeUtils';
-import NxLoadWrapper, { Props } from '../NxLoadWrapper';
-import NxLoadError from '../../NxLoadError/NxLoadError';
-import NxLoadingSpinner from '../../NxLoadingSpinner/NxLoadingSpinner';
+// import { rtlRender, rtlRenderElement } from '../../../__testutils__/rtlUtils';
+import { rtlRenderElement } from '../../../__testutils__/rtlUtils';
+
+import NxLoadWrapper from '../NxLoadWrapper';
+
+// import NxLoadError from '../../NxLoadError/NxLoadError';
+// import NxLoadingSpinner from '../../NxLoadingSpinner/NxLoadingSpinner';
 
 describe('NxLoadError', function() {
   const children = <div className="children"/>,
-      retryHandler = jest.fn(),
-      getShallowComponent = enzymeUtils.getShallowComponent<Props>(NxLoadWrapper, { children, retryHandler });
+      retryHandler = jest.fn();
+  const renderEl = rtlRenderElement(NxLoadWrapper, {children, retryHandler});
 
   it('renders a NxLoadError if there is an error', function() {
-    const ErrorFixture = () => <NxLoadError error="foo" />;
-
-    expect(getShallowComponent({ error: 'foo' })).toMatchElement(<ErrorFixture/>);
-    expect(getShallowComponent({ error: 'foo', loading: true })).toMatchElement(<ErrorFixture />);
+    const componentWithoutError = renderEl();
+    const componentWithError = renderEl({error: 'Error!'});
+    expect(componentWithoutError).not.toHaveAttribute('role', 'alert');
+    expect(componentWithoutError?.textContent).toContain('');
+    expect(componentWithError).toBeInTheDocument();
+    expect(componentWithError).toHaveAttribute('role', 'alert');
+    expect(componentWithError?.textContent).toContain('Error!');
   });
 
-  it('passes the retryHandler to NxLoadError', function () {
-    expect(getShallowComponent({ error: 'foo' }).find(NxLoadError)).toHaveProp('retryHandler', retryHandler);
-  });
+  // it('passes the retryHandler to NxLoadError', function () {
+  //   expect(getShallowComponent({ error: 'foo' }).find(NxLoadError)).toHaveProp('retryHandler', retryHandler);
+  // const text = screen.getByText('dfgdgdgd');
+  //   expect(text).toBeInTheDocument();
+  // });
 
-  it('renders a loading spinner if error is not set and loading is true', function() {
-    const SpinnerFixture = () => <NxLoadingSpinner />;
+  // it('renders a loading spinner if error is not set and loading is true', function() {
+  //   const SpinnerFixture = () => <NxLoadingSpinner />;
 
-    expect(getShallowComponent({ loading: true })).toMatchElement(<SpinnerFixture/>);
-    expect(getShallowComponent({ error: 'foo', loading: true })).not.toMatchElement(<SpinnerFixture/>);
-  });
+  //   expect(getShallowComponent({ loading: true })).toMatchElement(<SpinnerFixture/>);
+  //   expect(getShallowComponent({ error: 'foo', loading: true })).not.toMatchElement(<SpinnerFixture/>);
+  // });
 
-  it('renders provided children if loading is false and error is unset', function() {
-    expect(getShallowComponent()).toContainReact(children);
-    expect(getShallowComponent({ loading: false })).toContainReact(children);
-    expect(getShallowComponent({ error: 'foo' })).not.toContainReact(children);
-    expect(getShallowComponent({ loading: true })).not.toContainReact(children);
-  });
+  // it('renders provided children if loading is false and error is unset', function() {
+  //   expect(getShallowComponent()).toContainReact(children);
+  //   expect(getShallowComponent({ loading: false })).toContainReact(children);
+  //   expect(getShallowComponent({ error: 'foo' })).not.toContainReact(children);
+  //   expect(getShallowComponent({ loading: true })).not.toContainReact(children);
+  // });
 
-  it('renders children provided by a function if loading is false and error is unset', function() {
-    const childrenFn = () => children;
+  // it('renders children provided by a function if loading is false and error is unset', function() {
+  //   const childrenFn = () => children;
 
-    expect(getShallowComponent({ children: childrenFn })).toContainReact(children);
-    expect(getShallowComponent({ children: childrenFn, loading: false })).toContainReact(children);
-    expect(getShallowComponent({ children: childrenFn, error: 'foo' })).not.toContainReact(children);
-    expect(getShallowComponent({ children: childrenFn, loading: true })).not.toContainReact(children);
-  });
+  //   expect(getShallowComponent({ children: childrenFn })).toContainReact(children);
+  //   expect(getShallowComponent({ children: childrenFn, loading: false })).toContainReact(children);
+  //   expect(getShallowComponent({ children: childrenFn, error: 'foo' })).not.toContainReact(children);
+  //   expect(getShallowComponent({ children: childrenFn, loading: true })).not.toContainReact(children);
+  // });
 });
