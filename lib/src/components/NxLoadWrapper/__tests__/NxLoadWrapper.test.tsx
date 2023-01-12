78a493683d4a2e4a515b7a25bbac20cebc8344ab
Serhii Bondarchuk <63853236+Serhii-Bondarchuk@users.noreply.github.com>
2023-01-12 12:38:11 +0200

    added describe blocks

--- lib/src/components/NxLoadWrapper/__tests__/NxLoadWrapper.test.tsx
+++ lib/src/components/NxLoadWrapper/__tests__/NxLoadWrapper.test.tsx
@@ -20,62 +20,61 @@ describe('NxLoadError', function() {
   const renderEl = rtlRenderElement(NxLoadWrapper, minimalProps);
   const quickRender = rtlRender(NxLoadWrapper, minimalProps);
 
-  it('renders a component and checked if there was an error', function() {
-    const componentWithError = renderEl({error: 'Error!'});
-    expect(componentWithError).toBeInTheDocument();
-    expect(componentWithError).toHaveAttribute('role', 'alert');
-    expect(componentWithError?.textContent).toContain('Error!');
+  describe('when there is an error', ()=> {
+    it('an alert is rendered', ()=> {
+      const componentWithError = renderEl({error: 'Error!'});
+      expect(componentWithError).toBeInTheDocument();
+      expect(componentWithError).toHaveAttribute('role', 'alert');
+      expect(componentWithError?.textContent).toContain('Error!');
+    });
+
+    it('Retry button is rendered within the alert', ()=> {
+      expect(renderEl()?.textContent).not.toContain('Retry');
+      expect(quickRender({ error: 'Error' }).getByRole('button', { name: 'Retry' }))
+          .toBeInTheDocument();
+    });
+
+    it('retryHandler is called when that Retry button is clicked', async ()=> {
+      const user = userEvent.setup();
+      const retryHandler = jest.fn();
+      expect(renderEl()?.textContent).not.toContain('Retry');
+      expect(quickRender({ error: 'Error', retryHandler }).getByRole('button', { name: 'Retry' }))
+          .toBeInTheDocument();
+      const retryButton = screen.getByRole('button', { name: 'Retry' });
+      expect(retryHandler).not.toHaveBeenCalled();
+      await user.click(retryButton);
+      expect(retryHandler).toHaveBeenCalled();
+    });
+
+    it('children content and loading spinner are not rendered when error is present', ()=> {
+      const toCheck = ['Loading…', 'Foo'];
+      toCheck.forEach(nameToCheck => {
+        expect(renderEl({error: 'Error'})?.textContent).not.toContain(nameToCheck);
+      });
+
+    });
+
   });
 
-  it('passes the retryHandler to component', function () {
-    const elWithoutRetryButton = renderEl();
-    expect(elWithoutRetryButton?.textContent).not.toContain('Retry');
-    expect(quickRender({ error: 'Error' }).getByRole('button', { name: 'Retry' }))
-        .toBeInTheDocument();
-  });
-
-  it('renders a loading spinner if error is not set and loading is true', function() {
-    const elWithoutRetryButton = renderEl();
-    expect(elWithoutRetryButton?.textContent).not.toContain('Loading…');
+  describe('when it is loading', ()=> {
+    expect(renderEl()?.textContent).not.toContain('Loading…');
     expect(quickRender({ loading: true }).container.textContent).toContain('Loading…');
   });
 
-  it('renders provided children if loading is false and error is unset', function() {
-    const { container: withChildren } = quickRender();
-    const { container: withLoading } = quickRender({
-      loading: true,
-      error: ''
+  describe('renders provided children', ()=> {
+    it('renders provided children if loading is false and error is unset', ()=> {
+      const { container: withChildren } = quickRender();
+      expect(withChildren.textContent).toContain('Foo');
     });
-    const { container: withError } = quickRender({
-      loading: false,
-      error: 'Error'
+
+    it('renders children provided by a function if loading is false and error is unset', ()=> {
+      const childrenFn = () => children;
+      const { container: withFuncChild } = quickRender({
+        children: childrenFn,
+        loading: false,
+        error: ''
+      });
+      expect(withFuncChild.textContent).toContain('Foo');
     });
-    expect(withChildren.textContent).toContain('Foo');
-    expect(withLoading.textContent).toContain('Loading…');
-    expect(withError.textContent).toContain('Error');
-
-  });
-
-  it('renders children provided by a function if loading is false and error is unset', function() {
-    const childrenFn = () => children;
-    const { container: withFuncChild } = quickRender({
-      children: childrenFn,
-      loading: false,
-      error: ''
-    });
-    expect(withFuncChild.textContent).toContain('Foo');
-  });
-
-  it('passes the retryHandler to component and check that Retry button was clicked', async function () {
-    const user = userEvent.setup();
-    const retryHandler = jest.fn();
-    const elWithoutRetryButton = renderEl();
-    expect(elWithoutRetryButton?.textContent).not.toContain('Retry');
-    expect(quickRender({ error: 'Error', retryHandler }).getByRole('button', { name: 'Retry' }))
-        .toBeInTheDocument();
-    const retryButton = screen.getByRole('button', { name: 'Retry' });
-    expect(retryHandler).not.toHaveBeenCalled();
-    await user.click(retryButton);
-    expect(retryHandler).toHaveBeenCalled();
   });
 });
