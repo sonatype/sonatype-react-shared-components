b397934dbf3e31c7748aeba782f5567ca144f588
Serhii Bondarchuk <63853236+Serhii-Bondarchuk@users.noreply.github.com>
2023-01-13 13:01:32 +0200

    avoid changing for iconOnly buttons

--- gallery/src/components/NxAccordion/NxAccordionButtonHeaderExample.tsx
+++ gallery/src/components/NxAccordion/NxAccordionButtonHeaderExample.tsx
@@ -19,8 +19,8 @@ export default function NxAccordionButtonHeaderExample() {
           ellipsis truncation.
         </NxAccordion.Title>
         <div className="nx-btn-bar">
-          <NxButton variant="tertiary" onClick={() => { alert('Tertiary Button clicked'); }}>
-            Tertiary Button
+          <NxButton variant="tertiary" onClick={() => { alert('Button clicked'); }}>
+            Edit
           </NxButton>
         </div>
       </NxAccordion.Header>
