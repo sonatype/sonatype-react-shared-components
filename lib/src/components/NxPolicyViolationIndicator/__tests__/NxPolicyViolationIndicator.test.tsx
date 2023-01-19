0b762b3c947ba17e8afa6c8a6c9953acfed8541a
EBermudez88 <ebermudez@sonatype.com>
2023-01-19 11:57:36 -0500

    convert tests to rtl - RSC-1348

--- lib/src/components/NxPolicyViolationIndicator/__tests__/NxPolicyViolationIndicator.test.tsx
+++ lib/src/components/NxPolicyViolationIndicator/__tests__/NxPolicyViolationIndicator.test.tsx
@@ -4,47 +4,89 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import * as enzymeUtils from '../../../__testutils__/enzymeUtils';
-import 'jest-enzyme';
 
-import NxPolicyViolationIndicator, { Props } from '../NxPolicyViolationIndicator';
-import NxFontAwesomeIcon from '../../NxFontAwesomeIcon/NxFontAwesomeIcon';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { rtlRender, rtlRenderElement } from '../../../__testutils__/rtlUtils';
+import NxPolicyViolationIndicator from '../NxPolicyViolationIndicator';
 
 describe('NxPolicyViolationIndicator', function() {
-  const getShallowComponent = enzymeUtils.getShallowComponent<Props>(NxPolicyViolationIndicator, {});
+  const quickRender = rtlRender(NxPolicyViolationIndicator, {}),
+      renderEl = rtlRenderElement(NxPolicyViolationIndicator, {});
 
-  it('sets the nx-policy-violation-indicator--unspecified class if no props are passed', function() {
-    expect(getShallowComponent()).toMatchSelector('.nx-policy-violation-indicator--unspecified');
+  it('adds specified classnames in addition to the defaults', function() {
+    const defaultEl = renderEl()!,
+        el = renderEl({ className: 'foo' })!;
+
+    expect(el).toHaveClass('foo');
+
+    for (const cls of Array.from(defaultEl.classList)) {
+      expect(el).toHaveClass(cls);
+    }
   });
 
-  it('sets the modifier class using the threatLevelCategory if it is provided', function() {
-    expect(getShallowComponent({ threatLevelCategory: 'low' })).toMatchSelector('.nx-policy-violation-indicator--low');
-    expect(getShallowComponent({ threatLevelCategory: 'low' }))
-        .not.toMatchSelector('.nx-policy-violation-indicator--unspecified');
+  it('adds additional attributes to the top-level element', function() {
+    const el = renderEl({ id: 'foo', lang: 'en' });
+
+    expect(el).toHaveAttribute('id', 'foo');
+    expect(el).toHaveAttribute('lang', 'en');
   });
 
-  it('sets the modifier class by converting the policyThreatLevel if it is provided', function() {
-    expect(getShallowComponent({ policyThreatLevel: 0 })).toMatchSelector('.nx-policy-violation-indicator--none');
-    expect(getShallowComponent({ policyThreatLevel: 1 })).toMatchSelector('.nx-policy-violation-indicator--low');
-    expect(getShallowComponent({ policyThreatLevel: 3 })).toMatchSelector('.nx-policy-violation-indicator--moderate');
-    expect(getShallowComponent({ policyThreatLevel: 5 })).toMatchSelector('.nx-policy-violation-indicator--severe');
-    expect(getShallowComponent({ policyThreatLevel: 9 })).toMatchSelector('.nx-policy-violation-indicator--critical');
-    expect(getShallowComponent({ policyThreatLevel: 9 }))
-        .not.toMatchSelector('.nx-policy-violation-indicator--unspecified');
+  it('applies a ref to the top level element', function() {
+    const ref = React.createRef<HTMLDivElement>(),
+        el = renderEl({ref: ref})!;
+
+    expect(ref.current).toBe(el);
   });
 
-  it('sets the modifier class using the threatLevelCategory if both props are provided', function() {
-    const component = getShallowComponent({ policyThreatLevel: 9, threatLevelCategory: 'low' });
+  it('sets the text according to threatLevelCategory if no text is provided', function() {
+    const { container, rerender } = render(
+      <NxPolicyViolationIndicator threatLevelCategory='none'></NxPolicyViolationIndicator>
+    );
+    expect(container).toHaveTextContent('none');
 
-    expect(component).toMatchSelector('.nx-policy-violation-indicator--low');
-    expect(component).not.toMatchSelector('.nx-policy-violation-indicator--critical');
-    expect(component).not.toMatchSelector('.nx-policy-violation-indicator--unspecified');
+    rerender(<NxPolicyViolationIndicator threatLevelCategory='moderate'></NxPolicyViolationIndicator>);
+    expect(container).toHaveTextContent('moderate');
   });
 
-  it('adds aria attrs to help the icon show up for screen readers', function() {
-    const component = getShallowComponent({ policyThreatLevel: 9, threatLevelCategory: 'low' });
+  it('overrides the default threatLevelCategory text if text is provided', function() {
+    const { container } = render(
+      <NxPolicyViolationIndicator threatLevelCategory='low'>Foo</NxPolicyViolationIndicator>
+    );
 
-    expect(component.find(NxFontAwesomeIcon)).toHaveProp('aria-label', 'threat level low');
-    expect(component.find(NxFontAwesomeIcon)).toHaveProp('aria-hidden', false);
+    expect(container).not.toHaveTextContent('low');
+    expect(container).toHaveTextContent('Foo');
+  });
+
+  it('sets threatLevelCategory to unspecified if not defined', function() {
+    const unspecifiedEl = renderEl()!;
+    expect(unspecifiedEl).toHaveTextContent('unspecified');
+  });
+
+  it('sets the threatLevelCategory by converting policyThreatLevel if it is provided',
+      function() {
+        expect(renderEl({ policyThreatLevel: 0})!).toHaveTextContent('none');
+        expect(renderEl({ policyThreatLevel: 1})!).toHaveTextContent('low');
+        expect(renderEl({ policyThreatLevel: 3})!).toHaveTextContent('moderate');
+        expect(renderEl({ policyThreatLevel: 5})!).toHaveTextContent('severe');
+        expect(renderEl({ policyThreatLevel: 9})!).toHaveTextContent('critical');
+        expect(renderEl({ policyThreatLevel: 0})!).toHaveTextContent('none');
+        expect(renderEl()!).toHaveTextContent('unspecified');
+      });
+
+  it('renders an indicator according to threatLevelCategory over policyThreatNumber of both are provided', function() {
+    expect(renderEl({ policyThreatLevel: 9, threatLevelCategory: 'low'})).toHaveTextContent('low');
+  });
+
+  it('adds aria-label to icon', function() {
+    expect(quickRender().getByRole('img')).toHaveAttribute('aria-label', 'threat level unspecified');
+    expect(quickRender({ threatLevelCategory: 'low' }).getByRole('img'))
+        .toHaveAttribute('aria-label', 'threat level low');
+    expect(quickRender({ threatLevelCategory: 'severe'}).getByRole('img'))
+        .toHaveAttribute('aria-label', 'threat level severe');
+  });
+
+  it('sets aria-hidden to false on icon', function() {
+    expect(quickRender().getByRole('img')).toHaveAttribute('aria-hidden', 'false');
   });
 });
