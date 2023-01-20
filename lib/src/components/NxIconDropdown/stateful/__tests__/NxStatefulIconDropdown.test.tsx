b3817170ad78693886e19da04a0ede098ca27a60
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2023-01-20 10:18:44 -0800

    Stateful component RTL tests - RSC-1231

--- lib/src/components/NxIconDropdown/stateful/__tests__/NxStatefulIconDropdown.test.tsx
+++ lib/src/components/NxIconDropdown/stateful/__tests__/NxStatefulIconDropdown.test.tsx
@@ -5,187 +5,123 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import { act } from 'react-dom/test-utils';
-import { mount, ReactWrapper } from 'enzyme';
-import 'jest-enzyme';
-import * as enzymeUtils from '../../../../__testutils__/enzymeUtils';
+import { rtlRender, rtlRenderElement, userEvent } from '../../../../__testutils__/rtlUtils';
+import { waitFor } from '@testing-library/react';
 
-import NxButton from '../../../NxButton/NxButton';
-import NxIconDropdown from '../../NxIconDropdown';
 import NxStatefulIconDropdown, { Props } from '../NxStatefulIconDropdown';
 
 describe('NxStatefulIconDropdown', () => {
-  let container: HTMLDivElement | null;
+  const childClickSpy = jest.fn();
 
-  const minimalProps = {
+  const minimalProps: Props = {
         title: 'stateful dropdown',
-        children: <a>Hello</a>
+        children: <a href='#' onClick={childClickSpy}>Hello</a>
       },
-      getShallowComponent = enzymeUtils.getShallowComponent<Props>(NxStatefulIconDropdown, minimalProps),
-      getMountedComponent = enzymeUtils.getMountedComponent<Props>(NxStatefulIconDropdown, minimalProps);
+      quickRender = rtlRender(NxStatefulIconDropdown, minimalProps),
+      renderEl = rtlRenderElement(NxStatefulIconDropdown, minimalProps);
 
-  beforeEach(function() {
-    // Avoid rendering directly on the body.
-    container = document.createElement('div');
-    document.body.appendChild(container);
+  it('renders a button with type=button', function() {
+    const toggleBtn = quickRender().getByRole('button');
+
+    expect(toggleBtn).toBeInTheDocument();
+    expect(toggleBtn).toHaveAttribute('type', 'button');
   });
 
-  afterEach(function() {
-    if (container) {
-      document.body.removeChild(container);
-      container = null;
+  it('renders the dropdown menu containing the children', async function() {
+    const user = userEvent.setup(),
+        { container, getByRole } = quickRender(),
+        toggleBtn = getByRole('button');
+
+    await user.click(toggleBtn);
+
+    // dropdown menu does't have a role
+    const menu = container.querySelector('div.nx-dropdown-menu'),
+        link = getByRole('link');
+
+    expect(menu).toBeInTheDocument();
+    expect(link).toBeInTheDocument();
+    expect(menu).toContainElement(link);
+    expect(link).toHaveTextContent('Hello');
+  });
+
+  it('adds specified classNames to the element in addition to the defaults', function() {
+    const el = renderEl({ className: 'foo' }),
+        defaultEl = renderEl()!;
+
+    expect(el).toHaveClass('foo');
+
+    for (const cls of Array.from(defaultEl.classList)) {
+      expect(el).toHaveClass(cls);
     }
   });
 
-  it('renders an NxIconDropdown component', function() {
-    const component = getShallowComponent();
-    expect(component).toMatchSelector(NxIconDropdown);
-    expect(component).toHaveProp('title', 'stateful dropdown');
+  it('passes extra attrs', function() {
+    const el = renderEl({ id: 'some-id', lang: 'en' });
+
+    expect(el).toHaveAttribute('id', 'some-id');
+    expect(el).toHaveAttribute('lang', 'en');
   });
 
-  it('renders an NxIconDropdown component passing on the given props', function() {
-    const component = getShallowComponent({
-      className: 'extra-class',
-      disabled: true
-    });
-    const statelessDropdown = component.find(NxIconDropdown);
+  it('renders a button with an accessible name when title prop is supplied', async function() {
+    const toggleBtn = quickRender().getByRole('button');
 
-    expect(component).toMatchSelector(NxIconDropdown);
-    expect(statelessDropdown).toHaveProp('title', 'stateful dropdown');
-    expect(statelessDropdown).toHaveProp('disabled', true);
-    expect(statelessDropdown).toHaveProp('children', (<a>Hello</a>));
+    expect(toggleBtn).toBeInTheDocument();
+    await waitFor(() => expect(toggleBtn).toHaveAccessibleName('stateful dropdown'));
   });
 
-  it('opens the dropdown when the toggle is clicked', function() {
-    const mounted = getMountedComponent(undefined, { attachTo: container });
+  it('toggles the dropdown when the toggle button is clicked', async function() {
+    const user = userEvent.setup(),
+        { container, getByRole } = quickRender(),
+        toggleBtn = getByRole('button');
 
-    expect(mounted.find('.nx-dropdown-menu')).not.toExist();
+    expect(container.querySelector('div.nx-dropdown-menu')).not.toBeInTheDocument();
 
-    mounted.find(NxButton).simulate('click');
-    expect(mounted.find('.nx-dropdown-menu')).toExist();
-    mounted.unmount();
+    await user.click(toggleBtn);
+    expect(container.querySelector('div.nx-dropdown-menu')).toBeInTheDocument();
+
+    await waitFor(() => user.click(toggleBtn));
+    expect(container.querySelector('div.nx-dropdown-menu')).not.toBeInTheDocument();
   });
 
-  it('closes the dropdown if the menu is open and the toggle is clicked', function() {
-    const mounted = getMountedComponent(undefined, { attachTo: container });
+  it('closes the dropdown when the Escape key is pressed on this component', async function() {
+    const user = userEvent.setup(),
+        { container, getByRole } = quickRender(),
+        toggleBtn = getByRole('button');
 
-    act(() => {
-      mounted.find('button.nx-icon-dropdown__toggle').getDOMNode().dispatchEvent(
-          new MouseEvent('click', { bubbles: true }));
-    });
-    mounted.update();
-    expect(mounted.find('.nx-dropdown-menu')).toExist();
+    await user.click(toggleBtn);
+    expect(container.querySelector('div.nx-dropdown-menu')).toBeInTheDocument();
 
-    act(() => {
-      mounted.find('button.nx-icon-dropdown__toggle').getDOMNode().dispatchEvent(
-          new MouseEvent('click', { bubbles: true }));
-    });
-    mounted.update();
-    expect(mounted.find('.nx-dropdown-menu')).not.toExist();
-    mounted.unmount();
+    toggleBtn.focus();
+    await user.keyboard('[Escape]');
+    expect(container.querySelector('div.nx-dropdown-menu')).not.toBeInTheDocument();
   });
 
-  it('closes the dropdown when the Escape key is pressed on this component', function() {
-    const page = (
-      <div>
-        <NxStatefulIconDropdown title="stateful dropdown" />
-      </div>
-    );
-    const element = mount(page, { attachTo: container });
-    element.find(NxButton).simulate('click');
-    expect(element.find('.nx-dropdown-menu')).toExist();
+  it('closes the dropdown when an outside click happens', async function() {
+    const user = userEvent.setup(),
+        { container, getByRole } = quickRender(),
+        toggleBtn = getByRole('button');
 
-    element.find(NxIconDropdown).simulate('keyDown', {key: 'Escape'});
-    expect(element.find('.nx-dropdown-menu')).not.toExist();
+    await user.click(toggleBtn);
+    expect(container.querySelector('div.nx-dropdown-menu')).toBeInTheDocument();
 
-    element.unmount();
+    await waitFor(() => user.click(document.body));
+    expect(container.querySelector('div.nx-dropdown-menu')).not.toBeInTheDocument();
   });
 
-  it('closes the dropdown when an outside click happens', function() {
-    let element: ReactWrapper;
+  it('closes the dropdown when a child is clicked - after calling the child\'s click handler', async function() {
+    const user = userEvent.setup(),
+        { container, getByRole } = quickRender(),
+        toggleBtn = getByRole('button');
 
-    // `act` ensures that updates related to “units” of interaction with a user interface have been processed
-    // and applied to the DOM before you make any assertions.
-    // See https://reactjs.org/blog/2019/02/06/react-v16.8.0.html#testing-hooks
-    // and https://reactjs.org/docs/testing-recipes.html#act
-    act(() => {
-      element = mount(
-        <div>
-          <NxStatefulIconDropdown title="stateful dropdown" />
-          <button id="test-btn">click</button>
-        </div>,
-        { attachTo: container }
-      );
-    });
+    await user.click(toggleBtn);
 
-    act(() => {
-      // Jest/JSDom need actual events to be able to trigger effects properly
-      // See https://stackoverflow.com/questions/27557624/simulating-click-on-document-reactjs-jsdom
-      element.find('button.nx-icon-dropdown__toggle').getDOMNode().dispatchEvent(new MouseEvent('click', {
-        bubbles: true
-      }));
-    });
-    element!.update();
-    expect(element!.find('.nx-dropdown-menu')).toExist();
+    const menu = container.querySelector('div.nx-dropdown-menu'),
+        child = getByRole('link');
 
-    act(() => {
-      element!.find('#test-btn').getDOMNode().dispatchEvent(new MouseEvent('click', {
-        bubbles: true
-      }));
-    });
-    element!.update();
-    expect(element!.find('.nx-dropdown-menu')).not.toExist();
+    expect(menu).toBeInTheDocument();
 
-    act(() => {
-      element!.find('button.nx-icon-dropdown__toggle').getDOMNode().dispatchEvent(new MouseEvent('click', {
-        bubbles: true
-      }));
-    });
-    element!.update();
-    expect(element!.find('.nx-dropdown-menu')).toExist();
-
-    element!.unmount();
-  });
-
-  it('closes the dropdown when a child is clicked - after calling the child\'s click handler', function() {
-    let element: ReactWrapper;
-
-    const childClickSpy = jest.fn();
-    act(() => {
-      element = mount(
-        <NxStatefulIconDropdown title="stateful dropdown">
-          <a id="child" onClick={childClickSpy}>Hello</a>
-        </NxStatefulIconDropdown>,
-        { attachTo: container }
-      );
-    });
-
-    act(() => {
-      element.find('button.nx-icon-dropdown__toggle').getDOMNode().dispatchEvent(new MouseEvent('click', {
-        bubbles: true
-      }));
-    });
-    element!.update();
-    expect(element!.find('.nx-dropdown-menu')).toExist();
-
-    act(() => {
-      element!.find('#child').getDOMNode().dispatchEvent(new MouseEvent('click', {
-        bubbles: true
-      }));
-    });
-    element!.update();
-
+    await waitFor(() => user.click(child));
     expect(childClickSpy).toHaveBeenCalled();
-    expect(element!.find('.nx-dropdown-menu')).not.toExist();
-
-    act(() => {
-      element!.find('button.nx-icon-dropdown__toggle').getDOMNode().dispatchEvent(new MouseEvent('click', {
-        bubbles: true
-      }));
-    });
-    element!.update();
-    expect(element!.find('.nx-dropdown-menu')).toExist();
-
-    element!.unmount();
+    expect(menu).not.toBeInTheDocument();
   });
 });
