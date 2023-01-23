90384b82c1b3e088c4144729e663709de552c392
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2023-01-23 07:57:30 -0800

    Use getByTitle and runTimers - RSC-1362

--- lib/src/components/NxSmallThreatCounter/__tests__/NxSmallThreatCounter.test.tsx
+++ lib/src/components/NxSmallThreatCounter/__tests__/NxSmallThreatCounter.test.tsx
@@ -4,21 +4,13 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { RenderResult } from '@testing-library/react';
-import { waitFor } from '@testing-library/react';
-import { rtlRender, rtlRenderElement } from '../../../__testutils__/rtlUtils';
+import { rtlRender, rtlRenderElement, runTimers } from '../../../__testutils__/rtlUtils';
 
 import NxSmallThreatCounter, { Props } from '../NxSmallThreatCounter';
 
 describe('NxSmallThreatCounter', function() {
   const quickRender = rtlRender<Props>(NxSmallThreatCounter, {}),
       renderEl = rtlRenderElement<Props>(NxSmallThreatCounter, {});
-
-  function getCounter(component: RenderResult, category: string) {
-    return component.getByText((_, e) =>
-      !!(e?.parentElement?.className.includes('nx-small-threat-counter-container')
-      && e?.className?.includes(category)));
-  }
 
   it('renders nothing if all six indicators are undefined', function() {
     expect(renderEl()).not.toBeInTheDocument();
@@ -55,60 +47,78 @@ describe('NxSmallThreatCounter', function() {
   });
 
   it('renders a counter with an accessible name of Critical when criticalCount is specified', async function() {
-    const component = quickRender({criticalCount: 0}),
-        counter = getCounter(component, 'critical');
+    const component = quickRender({criticalCount: 0});
+
+    await runTimers();
+
+    const counter = component.getByTitle('Critical');
 
     expect(counter).toBeInTheDocument();
     expect(counter).toHaveTextContent('0');
-    await waitFor(() => expect(counter).toHaveAccessibleName('Critical'));
+    expect(counter).toHaveAccessibleName('Critical');
   });
 
   it('renders a counter with an accessible name of Severe when severeCount is specified', async function() {
-    const component = quickRender({severeCount: 1}),
-        counter = getCounter(component, 'severe');
+    const component = quickRender({severeCount: 1});
+
+    await runTimers();
+
+    const counter = component.getByTitle('Severe');
 
     expect(counter).toBeInTheDocument();
     expect(counter).toHaveTextContent('1');
-    await waitFor(() => expect(counter).toHaveAccessibleName('Severe'));
+    expect(counter).toHaveAccessibleName('Severe');
   });
 
   it('renders a counter with an accessible name of Moderate when moderateCount is specified', async function() {
-    const component = quickRender({moderateCount: 2}),
-        counter = getCounter(component, 'moderate');
+    const component = quickRender({moderateCount: 2});
+
+    await runTimers();
+
+    const counter = component.getByTitle('Moderate');
 
     expect(counter).toBeInTheDocument();
     expect(counter).toHaveTextContent('2');
-    await waitFor(() => expect(counter).toHaveAccessibleName('Moderate'));
+    expect(counter).toHaveAccessibleName('Moderate');
   });
 
   it('renders a counter with an accessible name of Low when lowCount is specified', async function() {
-    const component = quickRender({lowCount: 3}),
-        counter = getCounter(component, 'low');
+    const component = quickRender({lowCount: 3});
+
+    await runTimers();
+
+    const counter = component.getByTitle('Low');
 
     expect(counter).toBeInTheDocument();
     expect(counter).toHaveTextContent('3');
-    await waitFor(() => expect(counter).toHaveAccessibleName('Low'));
+    expect(counter).toHaveAccessibleName('Low');
   });
 
   it('renders a counter with an accessible name of None when noneCount is specified', async function() {
-    const component = quickRender({noneCount: 4}),
-        counter = getCounter(component, 'none');
+    const component = quickRender({noneCount: 4});
+
+    await runTimers();
+
+    const counter = component.getByTitle('None');
 
     expect(counter).toBeInTheDocument();
     expect(counter).toHaveTextContent('4');
-    await waitFor(() => expect(counter).toHaveAccessibleName('None'));
+    expect(counter).toHaveAccessibleName('None');
   });
 
   it('renders a counter with an accessible name of Unspecified when unspecifiedCount is specified', async function() {
-    const component = quickRender({unspecifiedCount: 5}),
-        counter = getCounter(component, 'unspecified');
+    const component = quickRender({unspecifiedCount: 5});
+
+    await runTimers();
+
+    const counter = component.getByTitle('Unspecified');
 
     expect(counter).toBeInTheDocument();
     expect(counter).toHaveTextContent('5');
-    await waitFor(() => expect(counter).toHaveAccessibleName('Unspecified'));
+    expect(counter).toHaveAccessibleName('Unspecified');
   });
 
-  it('renders all six indicators', function() {
+  it('renders all six indicators', async function() {
     const component = quickRender({
       criticalCount: 66,
       severeCount: 55,
@@ -117,12 +127,15 @@ describe('NxSmallThreatCounter', function() {
       noneCount: 22,
       unspecifiedCount: 11
     });
-    const criticalCounter = getCounter(component, 'critical'),
-        severeCounter = getCounter(component, 'severe'),
-        moderateCounter = getCounter(component, 'moderate'),
-        lowCounter = getCounter(component, 'low'),
-        noneCounter = getCounter(component, 'none'),
-        unspecifiedCounter = getCounter(component, 'unspecified');
+
+    await runTimers();
+
+    const criticalCounter = component.getByTitle('Critical'),
+        severeCounter = component.getByTitle('Severe'),
+        moderateCounter = component.getByTitle('Moderate'),
+        lowCounter = component.getByTitle('Low'),
+        noneCounter = component.getByTitle('None'),
+        unspecifiedCounter = component.getByTitle('Unspecified');
 
     expect(criticalCounter).toHaveTextContent('66');
     expect(severeCounter).toHaveTextContent('55');
@@ -133,24 +146,30 @@ describe('NxSmallThreatCounter', function() {
   });
 
   it('displays maxDigits\' nines and a plus sign for any counts that go over the max when maxDigits is specified' +
-    'default is three digits', function() {
+    'default is three digits', async function() {
     const twoDigitMaxComponent = quickRender({ criticalCount: 100000, maxDigits: 2 }),
-        fiveDigitDefaultComponent = quickRender({ moderateCount: 1000000 }),
-        twoDigitMaxCounter = getCounter(twoDigitMaxComponent, 'critical'),
-        fiveDigitDefaultCounter = getCounter(fiveDigitDefaultComponent, 'moderate');
+        fiveDigitDefaultComponent = quickRender({ moderateCount: 1000000 });
+
+    await runTimers();
+
+    const twoDigitMaxCounter = twoDigitMaxComponent.getByTitle('Critical'),
+        fiveDigitDefaultCounter = fiveDigitDefaultComponent.getByTitle('Moderate');
 
     expect(twoDigitMaxCounter).toHaveTextContent('99+');
     expect(fiveDigitDefaultCounter).toHaveTextContent('999+');
   });
 
-  it('does not cut off the displayed number when maxDigits is Infinity', function() {
+  it('does not cut off the displayed number when maxDigits is Infinity', async function() {
     const component = quickRender({
-          criticalCount: 34592384579138,
-          severeCount: 1,
-          maxDigits: Infinity
-        }),
-        criticalCounter = getCounter(component, 'critical'),
-        severeCounter = getCounter(component, 'severe');
+      criticalCount: 34592384579138,
+      severeCount: 1,
+      maxDigits: Infinity
+    });
+
+    await runTimers();
+
+    const criticalCounter = component.getByTitle('Critical'),
+        severeCounter = component.getByTitle('Severe');
 
     expect(criticalCounter).toBeInTheDocument();
     expect(criticalCounter).toHaveTextContent('34592384579138');
