72ec7b7c37c16f4e70e8ad3746360195b8867c27
Abhinav Bhatta <abhatta@sonatype.com>
2023-01-24 12:37:04 -0500

    Use RTL, update tests

--- lib/src/components/NxThreatIndicator/__tests__/NxThreatIndicator.test.tsx
+++ lib/src/components/NxThreatIndicator/__tests__/NxThreatIndicator.test.tsx
@@ -4,116 +4,84 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import { faCircle } from '@fortawesome/free-solid-svg-icons';
-
-import * as enzymeUtils from '../../../__testutils__/enzymeUtils';
-import 'jest-enzyme';
-
-import NxThreatIndicator from '../NxThreatIndicator';
-import NxFontAwesomeIcon from '../../NxFontAwesomeIcon/NxFontAwesomeIcon';
-import NxToolip from '../../NxTooltip/NxTooltip';
+import NxThreatIndicator, { Props } from '../NxThreatIndicator';
+import { rtlRender, userEvent } from '../../../__testutils__/rtlUtils';
+import { screen } from '@testing-library/react';
+import { allThreatLevelCategories } from '../../../util/threatLevels';
 
 describe('NxThreatIndicator', function() {
-  const getShallowComponent = enzymeUtils.getShallowComponent(NxThreatIndicator, {});
+  const quickRender = rtlRender<Props>(NxThreatIndicator, {});
 
-  it('renders an faCircle NxFontAwesomeIcon with the `nx-threat-indicator` class', function() {
-    const icon = getShallowComponent().find(NxFontAwesomeIcon);
-
-    expect(icon).toHaveClassName('nx-threat-indicator');
-    expect(icon).toExist();
-    expect(icon).toHaveProp('icon', faCircle);
+  it('renders a threat indicator svg', function() {
+    const { container } = quickRender(),
+        threatIndicator = container.querySelector('svg');
+    expect(threatIndicator).toBeInTheDocument();
   });
 
-  it('sets the nx-threat-indicator--unspecified class if no props are passed', function() {
-    const icon = getShallowComponent().find(NxFontAwesomeIcon);
+  it('takes precedence of threatLevelCategory if both props are provided', async function() {
+    const { container } = quickRender({ policyThreatLevel: 9, threatLevelCategory: 'low' }),
+        user = userEvent.setup(),
+        threatIndicator = container.querySelector('svg')!;
 
-    expect(icon).toMatchSelector('.nx-threat-indicator--unspecified');
-  });
-
-  it('sets the modifier class using the threatLevelCategory if it is provided', function() {
-    const icon = getShallowComponent({ threatLevelCategory: 'low' }).find(NxFontAwesomeIcon);
-
-    expect(icon).toMatchSelector('.nx-threat-indicator--low');
-    expect(icon).not.toMatchSelector('.nx-threat-indicator--unspecified');
-  });
-
-  it('sets the modifier class by converting the policyThreatLevel if it is provided', function() {
-    expect(getShallowComponent({ policyThreatLevel: 0 }).find('.nx-threat-indicator'))
-        .toMatchSelector('.nx-threat-indicator--none');
-    expect(getShallowComponent({ policyThreatLevel: 1 }).find('.nx-threat-indicator'))
-        .toMatchSelector('.nx-threat-indicator--low');
-    expect(getShallowComponent({ policyThreatLevel: 3 }).find('.nx-threat-indicator'))
-        .toMatchSelector('.nx-threat-indicator--moderate');
-    expect(getShallowComponent({ policyThreatLevel: 5 }).find('.nx-threat-indicator'))
-        .toMatchSelector('.nx-threat-indicator--severe');
-    expect(getShallowComponent({ policyThreatLevel: 9 }).find('.nx-threat-indicator'))
-        .toMatchSelector('.nx-threat-indicator--critical');
-    expect(getShallowComponent({ policyThreatLevel: 9 }).find('.nx-threat-indicator'))
-        .not.toMatchSelector('.nx-threat-indicator--unspecified');
-  });
-
-  it('sets the modifier class using the threatLevelCategory if both props are provided', function() {
-    const component = getShallowComponent({ policyThreatLevel: 9, threatLevelCategory: 'low' });
-    const icon = component.find(NxFontAwesomeIcon);
-
-    expect(icon).toMatchSelector('.nx-threat-indicator--low');
-    expect(icon).not.toMatchSelector('.nx-threat-indicator--critical');
-    expect(icon).not.toMatchSelector('.nx-threat-indicator--unspecified');
+    await user.hover(threatIndicator);
+    const tooltip = await screen.findByRole('tooltip');
+    expect(tooltip).toHaveTextContent('Low');
   });
 
   it('adds aria attrs to help the icon show up for screen readers', function() {
-    const component = getShallowComponent({ policyThreatLevel: 9, threatLevelCategory: 'low' });
+    const { container } = quickRender({ policyThreatLevel: 9, threatLevelCategory: 'low' }),
+        threatIndicator = container.querySelector('svg')!;
 
-    expect(component.find(NxFontAwesomeIcon)).toHaveProp('aria-label', 'threat level low');
-    expect(component.find(NxFontAwesomeIcon)).not.toHaveProp('aria-hidden');
+    expect(threatIndicator).toHaveAttribute('aria-label', 'threat level low');
+    expect(threatIndicator).not.toHaveAttribute('aria-hidden', true);
   });
 
-  it('should have the correct default tooltip title', function() {
-    const none = getShallowComponent({ threatLevelCategory: 'none' }).find(NxToolip);
-    const low = getShallowComponent({ threatLevelCategory: 'low' }).find(NxToolip);
-    const moderate = getShallowComponent({ threatLevelCategory: 'moderate' }).find(NxToolip);
-    const severe = getShallowComponent({ threatLevelCategory: 'severe' }).find(NxToolip);
-    const critical = getShallowComponent({ threatLevelCategory: 'critical' }).find(NxToolip);
+  describe('should have default tooltips', function() {
+    allThreatLevelCategories.forEach(category => {
+      it(`for ${category} category`, async function() {
+        const { container } = quickRender({ threatLevelCategory: category }),
+            user = userEvent.setup(),
+            threatIndicator = container.querySelector('svg')!,
+            categoryTitleCase = category[0].toUpperCase() + category.slice(1);
 
-    expect(none).toHaveProp('title', 'None');
-    expect(low).toHaveProp('title', 'Low');
-    expect(moderate).toHaveProp('title', 'Moderate');
-    expect(severe).toHaveProp('title', 'Severe');
-    expect(critical).toHaveProp('title', 'Critical');
+        await user.hover(threatIndicator);
+        const tooltip = await screen.findByRole('tooltip');
+        expect(tooltip).toHaveTextContent(categoryTitleCase);
+      });
+    });
   });
 
-  it('should show custom tooltip title', function() {
-    const tooltip = getShallowComponent({ title: 'Extinction Level Threat' }).find(NxToolip);
+  it('should show custom tooltip title', async function() {
+    const { container } = quickRender({ title: 'Extinction Level Threat' }),
+        user = userEvent.setup(),
+        threatIndicator = container.querySelector('svg')!;
 
-    expect(tooltip).toHaveProp('title', 'Extinction Level Threat');
+    await user.hover(threatIndicator);
+    const tooltip = await screen.findByRole('tooltip');
+    expect(tooltip).toHaveTextContent('Extinction Level Threat');
   });
 
-  it('adds specified class names to the element', function() {
-    const icon = getShallowComponent({ className: 'foo', threatLevelCategory: 'low' }).find(NxFontAwesomeIcon);
+  it('should hide tooltip when presentational prop is set true', async function() {
+    const el = quickRender({ presentational: true }),
+        user = userEvent.setup(),
+        threatIndicator = el.getByRole('presentation', { hidden: true });
 
-    expect(icon).toHaveClassName('foo');
-    expect(icon).toHaveClassName('nx-threat-indicator');
-    expect(icon).toHaveClassName('nx-threat-indicator--low');
-  });
-
-  it('should hide tooltip when presentational prop is set true', function() {
-    const component = getShallowComponent({ presentational: true });
-    const tooltip = component.find(NxToolip);
-    const icon = component.find(NxFontAwesomeIcon);
-
-    expect(tooltip).not.toExist();
-    expect(icon).toExist();
+    expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
+    await user.hover(threatIndicator);
+    expect(screen.queryByRole('tooltip')).not.toBeInTheDocument();
   });
 
   it('should set the role to "presentation" when presentational prop is true', function() {
-    const icon = getShallowComponent({ presentational: true }).find(NxFontAwesomeIcon);
+    const el = quickRender({ presentational: true }),
+        threatIndicator = el.getByRole('presentation', { hidden: true });
 
-    expect(icon).toHaveProp('role', 'presentation');
+    expect(threatIndicator).toBeInTheDocument();
   });
 
   it('should set aria-label to undefined when presentational prop is true', function() {
-    const icon = getShallowComponent({ presentational: true }).find(NxFontAwesomeIcon);
+    const el = quickRender({ presentational: true }),
+        threatIndicator = el.getByRole('presentation', { hidden: true });
 
-    expect(icon).toHaveProp('aria-label', undefined);
+    expect(threatIndicator).not.toHaveAttribute('aria-label');
   });
 });
