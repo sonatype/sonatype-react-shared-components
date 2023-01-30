1439819ab70fb5941991a4224b37109065e12f52
Ross Pokorny <rpokorny@sonatype.com>
2023-01-30 12:54:00 -0500

    Port reactUtil tests to RTL - RSC-1409

--- lib/src/util/__tests__/reactUtil.test.tsx
+++ lib/src/util/__tests__/reactUtil.test.tsx
@@ -4,13 +4,16 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
+import { render } from '@testing-library/react';
 import React, { ReactElement, ReactNode } from 'react';
-import { shallow } from 'enzyme';
-import 'jest-enzyme';
 
 import { ensureElement, ensureStartEndElements } from '../reactUtil';
 
 describe('reactUtil', function() {
+  function renderEl(node: ReactElement) {
+    return render(node).container.firstElementChild;
+  }
+
   describe('ensureStartEndElements', function() {
     it('passes through unrendering values as-is', function() {
       expect(ensureStartEndElements(null)).toBe(null);
@@ -20,14 +23,22 @@ describe('reactUtil', function() {
     });
 
     it('passes through elements as-is', function() {
-      expect(shallow(ensureStartEndElements(<span>foo</span>) as ReactElement)).toMatchElement(<span>foo</span>);
-      expect(shallow(ensureStartEndElements(<div>foo</div>) as ReactElement)).toMatchElement(<div>foo</div>);
+      const span = <span>foo</span>,
+          div = <div>foo</div>;
+
+      expect(ensureStartEndElements(span)).toBe(span);
+      expect(ensureStartEndElements(div)).toBe(div);
     });
 
     it('wraps strings and numbers in a span', function() {
-      expect(shallow(ensureStartEndElements('foo') as ReactElement)).toMatchElement(<span>foo</span>);
-      expect(shallow(ensureStartEndElements(5) as ReactElement)).toMatchElement(<span>5</span>);
-      expect(shallow(ensureStartEndElements(NaN) as ReactElement)).toMatchElement(<span>NaN</span>);
+      const wrappedString = renderEl(ensureStartEndElements('foo') as ReactElement)!,
+          wrappedNumber = renderEl(ensureStartEndElements(5) as ReactElement)!;
+
+      expect(wrappedString.tagName).toBe('SPAN');
+      expect(wrappedString).toHaveTextContent('foo');
+
+      expect(wrappedNumber.tagName).toBe('SPAN');
+      expect(wrappedNumber).toHaveTextContent('5');
     });
 
     it('passes through arrays that start and end with elements', function() {
@@ -43,66 +54,98 @@ describe('reactUtil', function() {
     });
 
     it('wraps arrays that start with a string', function() {
-      const arr = ['bar', <span key="baz">baz</span>];
+      const arr = ['bar', <span key="baz">baz</span>],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureStartEndElements(arr) as ReactElement))
-          .toMatchElement(<span>bar<span key="baz">baz</span></span>);
+      expect(renderResult).toContainHTML('<span>bar<span>baz</span></span>')
+
+      // ensure key is preserved
+      expect(result.props.children[1].key).toBe('baz');
     });
 
     it('wraps arrays that start with a number', function() {
-      const arr = [5, <span key="baz">baz</span>];
+      const arr = [5, <span key="baz">baz</span>],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureStartEndElements(arr) as ReactElement))
-          .toMatchElement(<span>5<span key="baz">baz</span></span>);
+      expect(renderResult).toContainHTML('<span>5<span>baz</span></span>')
+
+      // ensure key is preserved
+      expect(result.props.children[1].key).toBe('baz');
     });
 
     it('wraps arrays that end with a string', function() {
-      const arr = [<span key="foo">foo</span>, 'bar'];
+      const arr = [<span key="foo">foo</span>, 'bar'],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureStartEndElements(arr) as ReactElement))
-          .toMatchElement(<span><span key="foo">foo</span>bar</span>);
+      expect(renderResult).toContainHTML('<span><span>foo</span>bar</span>')
+
+      // ensure key is preserved
+      expect(result.props.children[0].key).toBe('foo');
     });
 
     it('wraps arrays that end with a number', function() {
-      const arr = [<span key="foo">foo</span>, 5];
+      const arr = [<span key="foo">foo</span>, 5],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureStartEndElements(arr) as ReactElement))
-          .toMatchElement(<span><span key="foo">foo</span>5</span>);
+      expect(renderResult).toContainHTML('<span><span>foo</span>5</span>')
+
+      // ensure key is preserved
+      expect(result.props.children[0].key).toBe('foo');
     });
 
     it('wraps arrays whose first printable element is a string', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [nonPrintableVal, 'bar', <span key="baz">baz</span>];
+        const arr = [nonPrintableVal, 'bar', <span key="baz">baz</span>],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureStartEndElements(arr) as ReactElement))
-            .toMatchElement(<span>{nonPrintableVal}bar<span key="baz">baz</span></span>);
+        expect(renderResult).toContainHTML('<span>bar<span>baz</span></span>')
+
+        // ensure key is preserved
+        expect(result.props.children[2].key).toBe('baz');
       });
     });
 
     it('wraps arrays whose first printable element is a number', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [nonPrintableVal, 5, <span key="baz">baz</span>];
+        const arr = [nonPrintableVal, 5, <span key="baz">baz</span>],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureStartEndElements(arr) as ReactElement))
-            .toMatchElement(<span>{nonPrintableVal}5<span key="baz">baz</span></span>);
+        expect(renderResult).toContainHTML('<span>5<span>baz</span></span>')
+
+        // ensure key is preserved
+        expect(result.props.children[2].key).toBe('baz');
       });
     });
 
     it('wraps arrays whose last printable element is a string', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [<span key="foo">foo</span>, 'bar', nonPrintableVal];
+        const arr = [<span key="foo">foo</span>, 'bar', nonPrintableVal],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureStartEndElements(arr) as ReactElement))
-            .toMatchElement(<span><span key="bar">foo</span>bar{nonPrintableVal}</span>);
+        expect(renderResult).toContainHTML('<span><span>foo</span>bar</span>')
+
+        // ensure key is preserved
+        expect(result.props.children[0].key).toBe('foo');
       });
     });
 
     it('wraps arrays whose last printable element is a number', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [<span key="foo">foo</span>, 5, nonPrintableVal];
+        const arr = [<span key="foo">foo</span>, 5, nonPrintableVal],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureStartEndElements(arr) as ReactElement))
-            .toMatchElement(<span><span key="bar">foo</span>5{nonPrintableVal}</span>);
+        expect(renderResult).toContainHTML('<span><span>foo</span>5</span>')
+
+        // ensure key is preserved
+        expect(result.props.children[0].key).toBe('foo');
       });
     });
 
@@ -118,22 +161,36 @@ describe('reactUtil', function() {
     });
 
     it('passes through elements as-is', function() {
-      expect(shallow(ensureElement(<span>foo</span>) as ReactElement)).toMatchElement(<span>foo</span>);
-      expect(shallow(ensureElement(<div>foo</div>) as ReactElement)).toMatchElement(<div>foo</div>);
+      const span = <span>foo</span>,
+          div = <div>foo</div>;
+
+      expect(ensureElement(span)).toBe(span);
+      expect(ensureElement(div)).toBe(div);
     });
 
+
     it('wraps strings and numbers in a span', function() {
-      expect(shallow(ensureElement('foo') as ReactElement)).toMatchElement(<span>foo</span>);
-      expect(shallow(ensureElement(5) as ReactElement)).toMatchElement(<span>5</span>);
-      expect(shallow(ensureElement(NaN) as ReactElement)).toMatchElement(<span>NaN</span>);
+      const wrappedString = renderEl(ensureElement('foo') as ReactElement)!,
+          wrappedNumber = renderEl(ensureElement(5) as ReactElement)!;
+
+      expect(wrappedString.tagName).toBe('SPAN');
+      expect(wrappedString).toHaveTextContent('foo');
+
+      expect(wrappedNumber.tagName).toBe('SPAN');
+      expect(wrappedNumber).toHaveTextContent('5');
     });
 
     // The key difference from ensureStartEndElements
     it('wraps arrays that start and end with elements', function() {
-      const arr = [<span key="foo">foo</span>, 'bar', null, false, <span key="baz">baz</span>];
+      const arr = [<span key="foo">foo</span>, 'bar', null, false, <span key="baz">baz</span>],
+            result = ensureElement(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-      expect(shallow(ensureElement(arr) as ReactElement))
-          .toMatchElement(<span><span key="foo">foo</span>bar<span key="baz">baz</span></span>);
+      expect(renderResult)
+          .toContainHTML('<span><span>foo</span>bar<span>baz</span></span>');
+
+      expect(result.props.children[0].key).toBe('foo');
+      expect(result.props.children[4].key).toBe('baz');
     });
 
     it('passes through the empty array', function() {
@@ -143,66 +200,98 @@ describe('reactUtil', function() {
     });
 
     it('wraps arrays that start with a string', function() {
-      const arr = ['bar', <span key="baz">baz</span>];
+      const arr = ['bar', <span key="baz">baz</span>],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureElement(arr) as ReactElement))
-          .toMatchElement(<span>bar<span key="baz">baz</span></span>);
+      expect(renderResult).toContainHTML('<span>bar<span>baz</span></span>')
+
+      // ensure key is preserved
+      expect(result.props.children[1].key).toBe('baz');
     });
 
     it('wraps arrays that start with a number', function() {
-      const arr = [5, <span key="baz">baz</span>];
+      const arr = [5, <span key="baz">baz</span>],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureElement(arr) as ReactElement))
-          .toMatchElement(<span>5<span key="baz">baz</span></span>);
+      expect(renderResult).toContainHTML('<span>5<span>baz</span></span>')
+
+      // ensure key is preserved
+      expect(result.props.children[1].key).toBe('baz');
     });
 
     it('wraps arrays that end with a string', function() {
-      const arr = [<span key="foo">foo</span>, 'bar'];
+      const arr = [<span key="foo">foo</span>, 'bar'],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureElement(arr) as ReactElement))
-          .toMatchElement(<span><span key="foo">foo</span>bar</span>);
+      expect(renderResult).toContainHTML('<span><span>foo</span>bar</span>')
+
+      // ensure key is preserved
+      expect(result.props.children[0].key).toBe('foo');
     });
 
     it('wraps arrays that end with a number', function() {
-      const arr = [<span key="foo">foo</span>, 5];
+      const arr = [<span key="foo">foo</span>, 5],
+          result = ensureStartEndElements(arr) as ReactElement,
+          renderResult = renderEl(result);
 
-      expect(shallow(ensureElement(arr) as ReactElement))
-          .toMatchElement(<span><span key="foo">foo</span>5</span>);
+      expect(renderResult).toContainHTML('<span><span>foo</span>5</span>')
+
+      // ensure key is preserved
+      expect(result.props.children[0].key).toBe('foo');
     });
 
     it('wraps arrays whose first printable element is a string', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [nonPrintableVal, 'bar', <span key="baz">baz</span>];
+        const arr = [nonPrintableVal, 'bar', <span key="baz">baz</span>],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureElement(arr) as ReactElement))
-            .toMatchElement(<span>{nonPrintableVal}bar<span key="baz">baz</span></span>);
+        expect(renderResult).toContainHTML('<span>bar<span>baz</span></span>')
+
+        // ensure key is preserved
+        expect(result.props.children[2].key).toBe('baz');
       });
     });
 
     it('wraps arrays whose first printable element is a number', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [nonPrintableVal, 5, <span key="baz">baz</span>];
+        const arr = [nonPrintableVal, 5, <span key="baz">baz</span>],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureElement(arr) as ReactElement))
-            .toMatchElement(<span>{nonPrintableVal}5<span key="baz">baz</span></span>);
+        expect(renderResult).toContainHTML('<span>5<span>baz</span></span>')
+
+        // ensure key is preserved
+        expect(result.props.children[2].key).toBe('baz');
       });
     });
 
     it('wraps arrays whose last printable element is a string', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [<span key="foo">foo</span>, 'bar', nonPrintableVal];
+        const arr = [<span key="foo">foo</span>, 'bar', nonPrintableVal],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureElement(arr) as ReactElement))
-            .toMatchElement(<span><span key="bar">foo</span>bar{nonPrintableVal}</span>);
+        expect(renderResult).toContainHTML('<span><span>foo</span>bar</span>')
+
+        // ensure key is preserved
+        expect(result.props.children[0].key).toBe('foo');
       });
     });
 
     it('wraps arrays whose last printable element is a number', function() {
       [null, undefined, false, true].forEach(function(nonPrintableVal) {
-        const arr = [<span key="foo">foo</span>, 5, nonPrintableVal];
+        const arr = [<span key="foo">foo</span>, 5, nonPrintableVal],
+            result = ensureStartEndElements(arr) as ReactElement,
+            renderResult = renderEl(result);
 
-        expect(shallow(ensureElement(arr) as ReactElement))
-            .toMatchElement(<span><span key="bar">foo</span>5{nonPrintableVal}</span>);
+        expect(renderResult).toContainHTML('<span><span>foo</span>5</span>')
+
+        // ensure key is preserved
+        expect(result.props.children[0].key).toBe('foo');
       });
     });
 
