6304f6455d3fbd673491a96ae94b0bc146f6eb9f
Andrew Prasetya <aprasetya@sonatype.com>
2023-01-31 12:51:48 -0500

    Updated NxTable test. - RSC-1393

--- lib/src/components/NxTable/__tests__/NxTable.test.tsx
+++ lib/src/components/NxTable/__tests__/NxTable.test.tsx
@@ -7,9 +7,10 @@
 import React, { useContext } from 'react';
 
 import { render, screen, within, waitFor } from '@testing-library/react';
-import { rtlRenderElement } from '../../../__testutils__/rtlUtils';
+import { rtlRenderElement, userEvent, runTimers } from '../../../__testutils__/rtlUtils';
 
-import { RowContext } from '../contexts';
+import { HeaderContext, RowContext } from '../contexts';
+import { NxTableCellProps } from '../types';
 
 import NxTable from '../NxTable';
 import NxTableHead from '../NxTableHead';
@@ -178,18 +179,18 @@ describe('NxTable', function() {
     });
 
     it('does not show the emptyMessage when isLoading', function() {
-      render(<NxTableBody emptyMessage="Empty message" isLoading />);
+      render(<NxTable><NxTableBody emptyMessage="Empty message" isLoading /></NxTable>);
       expect(screen.getByRole('rowgroup')).not.toHaveTextContent('Empty message');
     });
 
     it('does not show the emptyMessage when in error', function() {
-      render(<NxTableBody emptyMessage="Empty message" error="Errr" retryHandler={() => { }} />);
+      render(<NxTable><NxTableBody emptyMessage="Empty message" error="Errr" retryHandler={() => { }} /></NxTable>);
 
       expect(screen.getByRole('rowgroup')).not.toHaveTextContent('Empty message');
     });
 
     it('removes the emptyMessage when children are added', async function() {
-      const { rerender } = render(<NxTableBody emptyMessage="Empty message"></NxTableBody>);
+      const { rerender } = render(<NxTable><NxTableBody emptyMessage="Empty message"></NxTableBody></NxTable>);
 
       expect(screen.getByRole('rowgroup')).toHaveTextContent('Empty message');
 
@@ -265,13 +266,15 @@ describe('NxTable', function() {
 
       const { rerender } = render(
         <NxTable>
-          <NxTableRow isFilterHeader={true}>
-            <NxTableCell>Foo</NxTableCell>
-            <NxTableCell>Bar</NxTableCell>
-            <NxTableCell />
-            <NxTableCell>Baz</NxTableCell>
-            <NxTableCell><ContextDependentChild /></NxTableCell>
-          </NxTableRow>
+          <NxTableBody>
+            <NxTableRow isFilterHeader={true}>
+              <NxTableCell>Foo</NxTableCell>
+              <NxTableCell>Bar</NxTableCell>
+              <NxTableCell />
+              <NxTableCell>Baz</NxTableCell>
+              <NxTableCell><ContextDependentChild /></NxTableCell>
+            </NxTableRow>
+          </NxTableBody>
         </NxTable>
       );
 
@@ -324,6 +327,133 @@ describe('NxTable', function() {
       );
 
       expect(screen.getByTestId('context-dependent')).toHaveTextContent('false');
+    });
+  });
+
+  describe('NxTableCell', function() {
+    const renderHeaderComponent = (extraProps?: NxTableCellProps) => render(
+      <NxTable>
+        <NxTableHead>
+          <NxTableRow>
+            <HeaderContext.Provider value={true}>
+              <NxTableCell { ...extraProps }/>
+            </HeaderContext.Provider>
+          </NxTableRow>
+        </NxTableHead>
+      </NxTable>
+    ).getByRole('columnheader') as HTMLElement;
+
+    describe('when the chevron prop is true', function() {
+      describe('when not isHeader', function() {
+        it('sets the button accessible name from the RowContext', function() {
+          render(
+            <NxTable>
+              <NxTableBody>
+                <NxTableRow>
+                  <RowContext.Provider value={{ label: 'foobar', isFilterHeader: false }}>
+                    <NxTableCell chevron/>
+                  </RowContext.Provider>
+                </NxTableRow>
+              </NxTableBody>
+            </NxTable>
+          );
+
+          expect(screen.getByRole('button')).toHaveAccessibleName('foobar');
+        });
+      });
+
+      describe('when isHeader', function() {
+        it('renders a visually hidden header cell with the text "Select Row"', function() {
+          const component = renderHeaderComponent({
+            chevron: true,
+            sortDir: 'asc',
+            children: <span>foo</span>
+          })!;
+
+          expect(component).toHaveTextContent('Select Row');
+        });
+      });
+    });
+
+    describe('when the rowBtnIcon prop is set', function() {
+      describe('when isHeader', function() {
+        it('sets default aria-sort', function() {
+          expect(renderHeaderComponent({ isSortable: true })).toHaveAttribute('aria-sort', 'none');
+        });
+
+        it('sets ascending aria-sort', function() {
+          expect(renderHeaderComponent({ isSortable: true, sortDir: 'asc' })).toHaveAttribute('aria-sort', 'ascending');
+        });
+
+        it('sets descending aria-sort', function() {
+          expect(
+              renderHeaderComponent({ isSortable: true, sortDir: 'desc' })
+          ).toHaveAttribute('aria-sort', 'descending');
+        });
+
+        it('renders default tooltip and content', async function() {
+          const user = userEvent.setup();
+
+          const sortDefault = within(
+              renderHeaderComponent({ isSortable: true, children: 'foo' })!
+          ).getByRole('button');
+
+          await user.hover(sortDefault);
+          await runTimers();
+          const tooltip = await screen.findByRole('tooltip');
+          expect(tooltip).toBeInTheDocument();
+          expect(tooltip).toHaveTextContent('foo unsorted');
+        });
+
+        it('renders ascending tooltip and content', async function() {
+          const user = userEvent.setup();
+
+          const sortAsc = within(
+              renderHeaderComponent({ isSortable: true, children: 'foo', sortDir: 'asc' })!
+          ).getByRole('button');
+
+          await user.hover(sortAsc);
+          await runTimers();
+          const tooltip = await screen.findByRole('tooltip');
+          expect(tooltip).toBeInTheDocument();
+          expect(tooltip).toHaveTextContent('foo ascending');
+        });
+
+        it('renders ascending tooltip and content', async function() {
+          const user = userEvent.setup();
+
+          const sortDesc = within(
+              renderHeaderComponent({ isSortable: true, children: 'foo', sortDir: 'desc' })!
+          ).getByRole('button');
+
+          await user.hover(sortDesc);
+          await runTimers();
+          const tooltip = await screen.findByRole('tooltip');
+          expect(tooltip).toBeInTheDocument();
+          expect(tooltip).toHaveTextContent('foo descending');
+        });
+
+        it('sets default button accessible name to match the tooltip', function() {
+          expect(
+              within(renderHeaderComponent({ isSortable: true, children: 'foo' })
+              ).getByRole('button'))
+              .toHaveAccessibleName('foo unsorted');
+        });
+
+        it('sets ascending button accessible name to match the tooltip', function() {
+          expect(
+              within(renderHeaderComponent({ isSortable: true, children: 'foo', sortDir: 'asc' })
+              ).getByRole('button'))
+              .toHaveAccessibleName('foo ascending');
+        });
+
+        it('sets descending button accessible name to match the tooltip', function() {
+          expect(
+              within(renderHeaderComponent({ isSortable: true, children: 'foo', sortDir: 'desc' })
+              ).getByRole('button'))
+              .toHaveAccessibleName('foo descending');
+        });
+      });
     });
   });
 });
