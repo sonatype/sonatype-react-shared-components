eac3bbd16c514cb8b8b309e74eb13eff71cdf463
Ross Pokorny <rpokorny@sonatype.com>
2023-02-02 15:41:41 -0500

    Enable HTTP/2 and HTTPS

--- gallery/vite.config.js
+++ gallery/vite.config.js
@@ -1,8 +1,16 @@
+const fs = require('fs');
+
 export default {
   define: {
     PX_API_KEY: JSON.stringify(process.env.PX_API_KEY)
   },
   resolve: {
     preserveSymlinks: true
+  },
+  server: {
+    https: {
+      key: fs.readFileSync('localhost.key'),
+      cert: fs.readFileSync('localhost.crt')
+    }
   }
 };
