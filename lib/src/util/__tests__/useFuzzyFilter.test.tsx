2ff71b6c5621d1b0ff536f7e06a3b697232290d0
EBermudez88 <ebermudez@sonatype.com>
2023-02-07 09:51:25 -0500

    update rtl tests- RSC-1453

--- lib/src/util/__tests__/useFuzzyFilter.test.tsx
+++ lib/src/util/__tests__/useFuzzyFilter.test.tsx
@@ -4,12 +4,12 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-/* eslint react/prop-types: 0 */
-import React, {FunctionComponent} from 'react';
+
+import React from 'react';
 import Fuse from 'fuse.js';
 
-import {getShallowComponent} from '../../__testutils__/enzymeUtils';
-import 'jest-enzyme';
+import { render } from '@testing-library/react';
+import { rtlRender, userEvent} from '../../__testutils__/rtlUtils';
 import useFuzzyFilter from '../useFuzzyFilter';
 
 interface Entry {
@@ -21,12 +21,6 @@ interface Props {
   input: Entry[];
   options: Fuse.IFuseOptions<Entry>;
 }
-
-// since hooks can only be used inside a function component we wrap it inside one
-const HookWrapper: FunctionComponent<Props> = ({input, options}) => {
-  const hookResult = useFuzzyFilter(input, options);
-  return <div data-result={hookResult} />;
-};
 
 describe('useFuzzyFilter', function () {
 
@@ -42,7 +36,7 @@ describe('useFuzzyFilter', function () {
       name: 'Skateboard'
     }, {
       id: 'moped',
-      name: 'Moped'
+      name: 'Moped or Scooter'
     }
   ];
 
@@ -51,50 +45,113 @@ describe('useFuzzyFilter', function () {
     threshold: 0.1
   };
 
-  const getShallow = getShallowComponent<Props>(HookWrapper, {input, options});
+  // since hooks can only be used inside a function component we wrap it inside one
+  function FixtureWithFilter({ input, options }: Props) {
+    const [output, filterTerm, setFilterTerm] = useFuzzyFilter(input, options);
+
+    function onChange(e:any) {
+      setFilterTerm(e.target.value);
+    }
+
+    return (
+      <>
+        <input onChange={onChange} value={filterTerm.toString()}/>
+        { output.map((result) => <span data-testid="result" key={result.id}>{result.name}</span>) }
+      </>
+    );
+  }
+
+  const renderWithFilter = rtlRender<Props>(FixtureWithFilter, {input, options});
 
   it('returns unchanged input if filter was not set', function () {
-    const shallowRender = getShallow();
-    const [output, filter] = shallowRender.prop('data-result');
-    expect(output).toBe(input);
-    expect(filter).toBe('');
+    function FixtureNoFilter() {
+      const [output] = useFuzzyFilter(input, options);
+      return (
+        <>
+          { output.map((result) => <span key={result.id} data-testid="result">{result.name} </span>) }
+        </>
+      );
+    }
+
+    const view = render(<FixtureNoFilter />),
+        outputItems = view.getAllByTestId('result');
+
+    expect(outputItems.length).toBe(4);
+    expect(outputItems[0]).toHaveTextContent('Bicycle');
+    expect(outputItems[1]).toHaveTextContent('Motorcycle');
+    expect(outputItems[2]).toHaveTextContent('Skateboard');
+    expect(outputItems[3]).toHaveTextContent('Moped');
   });
 
-  it('returns unchanged input if filter was set to empty string', function () {
-    const shallowRender = getShallow();
-    const [,, setFilter] = shallowRender.prop('data-result');
-    setFilter('');
+  it('returns unchanged input if filter was set to empty string', async function () {
+    const view = renderWithFilter(),
+        outputItems = view.getAllByTestId('result'),
+        inputEl = view.getByRole('textbox');
 
-    const [output, filter] = shallowRender.prop('data-result');
-    expect(output).toBe(input);
-    expect(filter).toBe('');
+    expect(inputEl).toHaveValue('');
+    expect(outputItems.length).toBe(4);
+    expect(outputItems[0]).toHaveTextContent('Bicycle');
+    expect(outputItems[1]).toHaveTextContent('Motorcycle');
+    expect(outputItems[2]).toHaveTextContent('Skateboard');
+    expect(outputItems[3]).toHaveTextContent('Moped');
   });
 
-  it('returns unchanged input if filter was set to null', function () {
-    const shallowRender = getShallow();
-    const [,, setFilter] = shallowRender.prop('data-result');
-    setFilter(null);
+  it('returns unchanged input if filter contains only spaces', async function() {
+    const user = userEvent.setup(),
+        view = renderWithFilter(),
+        inputEl = view.getByRole('textbox');
 
-    const [output, filter] = shallowRender.prop('data-result');
-    expect(output).toBe(input);
-    expect(filter).toBe(null);
+    await user.type(inputEl, '[Space]');
+
+    const outputItems = view.getAllByTestId('result');
+    expect(outputItems.length).toBe(4);
+    expect(inputEl).toHaveValue(' ');
   });
 
-  it('filters input and returns new filter term', function () {
-    const shallowRender = getShallow();
-    const [,, setFilter] = shallowRender.prop('data-result');
-    setFilter('Mo');
+  it('filters input and returns new filter term', async function() {
+    const user = userEvent.setup(),
+        view = renderWithFilter(),
+        inputEl = view.getByRole('textbox');
+    let outputItems = view.getAllByTestId('result');
 
-    const [output, filter] = shallowRender.prop('data-result');
-    expect(output).toEqual([
-      {
-        id: 'motorcycle',
-        name: 'Motorcycle'
-      }, {
-        id: 'moped',
-        name: 'Moped'
-      }
-    ]);
-    expect(filter).toBe('Mo');
+    expect(outputItems.length).toBe(4);
+    await user.type(inputEl, 'cy');
+
+    outputItems = view.getAllByTestId('result');
+    expect(outputItems.length).toBe(2);
+    expect(outputItems[0]).toHaveTextContent('Bicycle');
+    expect(outputItems[1]).toHaveTextContent('Motorcycle');
+    expect(inputEl).toHaveValue('cy');
+  });
+
+  it('uses trimmed filter for search', async function() {
+    const user = userEvent.setup(),
+        view = renderWithFilter(),
+        inputEl = view.getByRole('textbox');
+    let outputItems = view.getAllByTestId('result');
+
+    expect(outputItems.length).toBe(4);
+    await user.type(inputEl, ' cycle');
+
+    outputItems = view.getAllByTestId('result');
+    expect(outputItems.length).toBe(2);
+    expect(outputItems[0]).toHaveTextContent('Bicycle');
+    expect(outputItems[1]).toHaveTextContent('Motorcycle');
+    expect(inputEl).toHaveValue(' cycle');
+  });
+
+  it('matches even when the term is located at the tail of the string', async function() {
+    const user = userEvent.setup(),
+        view = renderWithFilter(),
+        inputEl = view.getByRole('textbox');
+    let outputItems = view.getAllByTestId('result');
+
+    expect(outputItems.length).toBe(4);
+    await user.type(inputEl, ' scooter');
+
+    outputItems = view.getAllByTestId('result');
+    expect(outputItems.length).toBe(1);
+    expect(outputItems[0]).toHaveTextContent('Moped or Scooter');
+    expect(inputEl).toHaveValue(' scooter');
   });
 });
