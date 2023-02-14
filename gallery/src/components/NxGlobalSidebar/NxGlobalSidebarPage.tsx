06e7472a19d874623f7aa1d566d8773d0ff4c38a
EBermudez88 <ebermudez@sonatype.com>
2023-02-14 12:13:35 -0500

    add documentation for onToggleClick- RSC-1449

--- gallery/src/components/NxGlobalSidebar/NxGlobalSidebarPage.tsx
+++ gallery/src/components/NxGlobalSidebar/NxGlobalSidebarPage.tsx
@@ -72,6 +72,12 @@ export default function NxGlobalSidebarPage() {
                 <NxTable.Cell>This value determines whether the sidebar is open or closed.</NxTable.Cell>
               </NxTable.Row>
               <NxTable.Row>
+                <NxTable.Cell>onToggleClick</NxTable.Cell>
+                <NxTable.Cell>Function (() =&gt; void)</NxTable.Cell>
+                <NxTable.Cell>Yes</NxTable.Cell>
+                <NxTable.Cell> A callback that fires when the sidebar's open/close toggle is clicked.</NxTable.Cell>
+              </NxTable.Row>
+              <NxTable.Row>
                 <NxTable.Cell>toggleOpenIcon</NxTable.Cell>
                 <NxTable.Cell>IconDefinition</NxTable.Cell>
                 <NxTable.Cell>Yes</NxTable.Cell>
