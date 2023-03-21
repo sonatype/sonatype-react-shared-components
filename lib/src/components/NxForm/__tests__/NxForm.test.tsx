5fc6469c0dee477ca029534283a2b9bca3fd7633
Andrew Prasetya <aprasetya@sonatype.com>
2023-03-21 12:26:27 -0400

    Added test. - RSC-1500

--- lib/src/components/NxForm/__tests__/NxForm.test.tsx
+++ lib/src/components/NxForm/__tests__/NxForm.test.tsx
@@ -258,6 +258,11 @@ describe('NxForm', function() {
             .toBeInTheDocument();
       });
 
+      it('renders an alert with an accessible name', function() {
+        const alert = quickRender({ validationErrors: 'foo'}).queryByRole('alert', { name: 'form validation errors' });
+        expect(alert).toBeInTheDocument();
+      });
+
       it('includes the first validation error within the alert content', function() {
         expect(within(renderEl({ validationErrors: 'foo' })!).getByRole('alert')).toHaveTextContent(/foo/);
         expect(within(renderEl({ validationErrors: ['foo', 'bar'] })!).getByRole('alert')).toHaveTextContent(/foo/);
@@ -267,6 +272,11 @@ describe('NxForm', function() {
       it('does not render when there is a submitError', function() {
         expect(quickRender({ submitError: 'bar', validationErrors: 'foo' }).getByRole('alert'))
             .toHaveTextContent(/bar/);
+      });
+
+      it('renders with an accessible name', function() {
+        const status = quickRender({ submitMaskState: true }).queryByRole('status', { name: 'form submit status' });
+        expect(status).toBeInTheDocument();
       });
     });
   });
