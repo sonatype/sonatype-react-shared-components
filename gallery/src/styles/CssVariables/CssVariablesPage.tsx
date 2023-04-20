c9c72b329bb24ded71010a64d404642a7b28faf9
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2023-04-20 14:18:44 +0800

    Update variables doc and css styles - RSC-1027

--- gallery/src/styles/CssVariables/CssVariablesPage.tsx
+++ gallery/src/styles/CssVariables/CssVariablesPage.tsx
@@ -703,6 +703,21 @@ const CssVariablesPage = () => {
             <ColorDocRow colorVar="--nx-color-policy-threat-slider-text">
               The default text color of <NxCode>NxPolicyThreatSlider</NxCode>.
             </ColorDocRow>
+            <ColorDocRow colorVar="--nx-color-list-background-hover">
+              The background color of the list item of <NxCode>NxList</NxCode> on hover.
+            </ColorDocRow>
+            <ColorDocRow colorVar="--nx-color-list-background-selected">
+              The background color of the list item of <NxCode>NxList</NxCode> on selected.
+            </ColorDocRow>
+            <ColorDocRow colorVar="--nx-color-list-text">
+              The text color of the list item of <NxCode>NxList</NxCode>.
+            </ColorDocRow>
+            <ColorDocRow colorVar="--nx-color-list-text-selected">
+              The selected text color of the list item of <NxCode>NxList</NxCode>.
+            </ColorDocRow>
+            <ColorDocRow colorVar="--nx-color-list-icon">
+              The icon color of the list item of <NxCode>NxList</NxCode>.
+            </ColorDocRow>
             <ColorDocRow colorVar="--nx-color-radio-checkbox-selected">
               The default selected color for radios and checkboxes.
             </ColorDocRow>
