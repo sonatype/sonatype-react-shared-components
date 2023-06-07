28bfafb170b4f6f01d73eae632eb5e97afdae83a
Cindy Ke <cindyke@Cindys-MacBook-Pro-ST.local>
2023-06-07 15:35:25 -0700

    Fix eslint - RSC-1721

--- gallery/src/components/NxSmallTag/NxSmallTagLayoutExample.tsx
+++ gallery/src/components/NxSmallTag/NxSmallTagLayoutExample.tsx
@@ -15,7 +15,10 @@ function NxSmallTagLayoutExample() {
         <NxSmallTag>Pink</NxSmallTag>
       </NxH1>
       <NxP>Paragraph Text / 16px / Regular<NxSmallTag>Pink</NxSmallTag></NxP>
-      <NxP style={{ fontSize: 'var(--nx-font-size-s)' }}>Paragraph Text / 14px / Regular<NxSmallTag>Pink</NxSmallTag></NxP>
+      <NxP style={{ fontSize: 'var(--nx-font-size-s)' }}>
+        Paragraph Text / 14px / Regular
+        <NxSmallTag>Pink</NxSmallTag>
+      </NxP>
       <NxP>
         <span>
           Paragraph of wrapping text - Toffee liquorice sugar plum halvah carrot cake. Powder jelly-o marzipan
