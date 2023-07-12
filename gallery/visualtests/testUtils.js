cb332157cab5fc0b7bb50a92f59fc6e64229ea64
Ross Pokorny <rpokorny@sonatype.com>
2023-07-12 09:38:00 -0400

    Tentatively fix tooltip screenshots with longer wait - RSC-1550

--- gallery/visualtests/testUtils.js
+++ gallery/visualtests/testUtils.js
@@ -14,7 +14,7 @@ const pageUrl = `file://${__dirname}/../dist/index.html`;
 
 const { AxePuppeteer } = require('@axe-core/puppeteer');
 
-const TOOLTIP_WAIT = 900;
+const TOOLTIP_WAIT = 5000;
 
 module.exports = {
   TOOLTIP_WAIT,
