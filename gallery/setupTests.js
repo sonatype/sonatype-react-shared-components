6907b6db73d23c3b2394d11a4ab28e23e543f6c0
Ross Pokorny <rpokorny@sonatype.com>
2023-07-14 09:53:01 -0400

    Increase retryTimes to deal with tooltip inconsistencies - RSC-1550

--- gallery/setupTests.js
+++ gallery/setupTests.js
@@ -7,8 +7,14 @@
 const { configureToMatchImageSnapshot } = require('jest-image-snapshot');
 const path = require('path');
 
-// To try to mitigate flaky tests due to blinking text carets and the like
-jest.retryTimes(3);
+// To try to mitigate flaky tests due to blinking text carets and the like, as well what I can only think is a browser
+// bug in the screenshotting of tooltips. Tooltip screenshots are currently prone to incorrectly having a very slight
+// degree of transparency, no matter how much time is given for them to fade in. Experimentation appeared to reveal
+// that this only tends to happen in screenshots that are clipped (not full page) and that when multiple screenshots
+// are taken of the same element right after one another it can happen in any one of them (or multiple). This
+// inconsistency seems to suggest a bug the the screenshotting and/or clipping code, which appears to be implemented
+// within the browser or devtools, rather than puppeteer. All we can really do about it is increase the retryTimes.
+jest.retryTimes(6);
 
 const darkEnv = process.env.RSC_GALLERY_THEME === 'DARK';
 
