b385b07e4f01dd6ea666229a405f885dfcf6f91c
Ross Pokorny <rpokorny@sonatype.com>
2023-08-25 08:44:44 -0400

    Refactor and fix stateful collapsible selects - RSC-1736

--- lib/src/components/NxCollapsibleItemsSelect/NxCollapsibleRadioSelect/stateful/NxStatefulCollapsibleRadioSelect.tsx
+++ lib/src/components/NxCollapsibleItemsSelect/NxCollapsibleRadioSelect/stateful/NxStatefulCollapsibleRadioSelect.tsx
@@ -4,44 +4,18 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React, {FunctionComponent, useState} from 'react';
-import { omit } from 'ramda';
+import React from 'react';
 
 import NxCollapsibleRadioSelect from '../NxCollapsibleRadioSelect';
 import { Props, propTypes } from './types';
-import { Option, OptionWithStringName, stringName } from '../../commonTypes';
+import { Option } from '../../commonTypes';
 export { Props, Option } from './types';
-import useFuzzyFilter from '../../../../util/useFuzzyFilter';
-import {textContent} from '../../../../util/childUtil';
+import useStatefulCollapsibleSelect from './useStatefulCollapsibleSelect';
 
-const NxStatefulCollapsibleRadioSelect: FunctionComponent<Props> =
-function NxStatefulCollapsibleRadioSelect<T extends Option>(props: Props<T>) {
-  const { options, optionTooltipGenerator: optionTooltipGeneratorProp } = props,
-      isOpenInitialState = !!props.isOpen;
+export default function NxStatefulCollapsibleRadioSelect<T extends Option>(props: Props<T>) {
+  const collapsibleSelectState = useStatefulCollapsibleSelect(props);
 
-  const [isOpen, toggleOpen] = useState(isOpenInitialState),
-      onToggleCollapse = () => {
-        toggleOpen(!isOpen);
-      };
-  const optionsWithStringName: OptionWithStringName[] =
-    options.map(option => ({ ...option, [stringName]: textContent(option.name) }));
-
-  const [filteredOptions, filter, setFilter] =
-    useFuzzyFilter(optionsWithStringName, {keys: [stringName], threshold: 0.1});
-
-  const optionTooltipGenerator = optionTooltipGeneratorProp && function optionTooltipGenerator(option: Option) {
-    return optionTooltipGeneratorProp(omit([stringName], option));
-  };
-
-  return <NxCollapsibleRadioSelect {...props}
-                                   optionTooltipGenerator={optionTooltipGenerator}
-                                   isOpen={isOpen}
-                                   onToggleCollapse={onToggleCollapse}
-                                   filteredOptions={filteredOptions}
-                                   onFilterChange={setFilter}
-                                   filter={filter} />;
-};
+  return <NxCollapsibleRadioSelect { ...props } { ...collapsibleSelectState } />;
+}
 
 NxStatefulCollapsibleRadioSelect.propTypes = propTypes;
-
-export default NxStatefulCollapsibleRadioSelect;
