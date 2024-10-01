838c6eb19d00e14179ed5f3d34bdaf6b3c41c002
Ross Pokorny <rpokorny@sonatype.com>
2024-10-01 11:15:24 -0400

    Update tests - RSC-1781

--- lib/src/components/NxGlobalSidebar/__tests__/NxGlobalSidebar.test.tsx
+++ lib/src/components/NxGlobalSidebar/__tests__/NxGlobalSidebar.test.tsx
@@ -5,161 +5,48 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import NxGlobalSidebar, { Props } from '../NxGlobalSidebar';
+import { within } from '@testing-library/react';
 import { faBiohazard, faCrow } from '@fortawesome/free-solid-svg-icons';
+
+import NxGlobalSidebar, { Props } from '../NxGlobalSidebar';
 import { rtlRender, runTimers, userEvent } from '../../../__testutils__/rtlUtils';
-import { screen, within } from '@testing-library/react';
-import NxGlobalSidebarNavigation from '../NxGlobalSidebarNavigation';
-import NxGlobalSidebarNavigationLink from '../NxGlobalSidebarNavigationLink';
-import NxGlobalSidebarFooter from '../NxGlobalSidebarFooter';
 
 describe('NxGlobalSidebar', function() {
   const minimalProps: Props = {
     isOpen: false,
     toggleOpenIcon: faCrow,
     toggleCloseIcon: faBiohazard,
-    onToggleClick: () => {},
-    logoImg: 'logoImg',
-    logoAltText: 'alt text',
-    logoLink: '/ref'
+    onToggleClick: () => {}
   };
 
   const quickRender = rtlRender<Props>(NxGlobalSidebar, minimalProps);
 
-  describe('children', function() {
-    describe('when only the NxGlobalSidebarNavigation is a provided as a child', function () {
-      it('renders an element with role="navigation" within an element with role="complementary"', function() {
-        const children =
-          <NxGlobalSidebarNavigation key="1">
-            <NxGlobalSidebarNavigationLink icon={faCrow} text="testLink" href="testLink"/>
-          </NxGlobalSidebarNavigation>,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            nav = within(complementary).getByRole('navigation');
-
-        expect(complementary).toBeInTheDocument();
-        expect(nav).toBeInTheDocument();
-      });
-    });
-
-    describe('when only the NxGlobalSidebarFooter is a provided as a child', function () {
-      it('renders a footer with role="contentinfo"', function() {
-        const children =
-          <NxGlobalSidebarFooter key="1"
-                                 supportText="Support for RSC"
-                                 supportLink="https://github.com/sonatype/sonatype-react-shared-components"
-                                 releaseText="Release 3.1.4"
-                                 productTagLine="Powered by PLAID VILLAIN" />,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            footer = view.getByRole('contentinfo');
-
-        expect(footer).toBeInTheDocument();
-        expect(complementary).not.toContainElement(footer);
-      });
-    });
-
-    describe('when only extra content is a provided as a child', function () {
-      it('renders the extra content within an element with role="complementary"', function() {
-        const children =
-          <section key="1" className="gallery-custom-sidebar-content nx-global-sidebar__other-content nx-scrollable">
-            <div className="nx-global-sidebar__expanded-content">
-              <span data-testid="testspan">myspan</span>
-            </div>
-          </section>,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            extraContent = within(complementary).getByTestId('testspan');
-
-        expect(complementary).toBeInTheDocument();
-        expect(extraContent).toBeInTheDocument();
-      });
-    });
-
-    describe('when NxGlobalSidebarNavigation, NxGlobalSidebarFooter, and extra content ' +
-    'are provided as children', function () {
-      it('renders an element with role="navigation" within an element with role="complementary" ' +
-       'and a footer with role="contentinfo"', function() {
-        const children =
-          <React.Fragment key="1">
-            <NxGlobalSidebarNavigation>
-              <NxGlobalSidebarNavigationLink icon={faCrow} text="testLink" href="testLink"/>
-            </NxGlobalSidebarNavigation>
-            <section key="2" className="gallery-custom-sidebar-content nx-global-sidebar__other-content nx-scrollable">
-              <div className="nx-global-sidebar__expanded-content">
-                <span data-testid="testspan">myspan</span>
-              </div>
-            </section>
-            <NxGlobalSidebarFooter supportText="Support for RSC"
-                                   supportLink="https://github.com/sonatype/sonatype-react-shared-components"
-                                   releaseText="Release 3.1.4"
-                                   productTagLine="Powered by PLAID VILLAIN" />
-          </React.Fragment>,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            nav = within(complementary).getByRole('navigation'),
-            extraContent = within(complementary).getByTestId('testspan'),
-            footer = view.getByRole('contentinfo');
-
-        expect(complementary).toBeInTheDocument();
-        expect(nav).toBeInTheDocument();
-        expect(extraContent).toBeInTheDocument();
-        expect(footer).toBeInTheDocument();
-      });
-    });
-  });
-
-  it('renders a link to `logoLink` with the `logoImg` and the `altLogoText`', function() {
+  it('renders a "Collapse Menu" button with aria-controls set to the sidebar id', async function() {
     const view = quickRender(),
-        link = view.getByRole('link'),
-        logo = view.getByRole('img');
-
-    expect(link).toHaveAttribute('href', '/ref');
-    expect(logo).toHaveAttribute('src', 'logoImg');
-    expect(logo).toHaveAccessibleName('alt text');
-  });
-
-  it('renders an icon-only button with tooltip "Collapse Sidebar" if isOpen is true', async function() {
-    const view = quickRender({ isOpen: true }),
-        sidebarId = view.container.firstElementChild?.getAttribute('id'),
-        user = userEvent.setup();
+        sidebarId = view.container.firstElementChild?.getAttribute('id');
 
     await runTimers();
 
-    const toggleBtn = view.getByRole('button', { name: /collapse sidebar/i });
+    const toggleBtn = view.getByRole('button', { name: /collapse menu/i });
 
-    expect(toggleBtn).toHaveAccessibleName('Collapse Sidebar');
-    expect(toggleBtn).toHaveAttribute('aria-expanded', 'true');
+    expect(toggleBtn).toHaveAccessibleName('Collapse Menu');
     expect(toggleBtn).toHaveAttribute('aria-controls', sidebarId);
-
-    await user.hover(toggleBtn);
-
-    const tooltip = await screen.findByRole('tooltip');
-    expect(tooltip).toBeInTheDocument();
-    expect(tooltip).toHaveTextContent('Collapse Sidebar');
   });
 
-  it('renders an icon-only button with tooltip "Expand Sidebar" if isOpen is false', async function() {
-    const view = quickRender({ isOpen: false }),
-        sidebarId = view.container.firstElementChild?.getAttribute('id'),
-        user = userEvent.setup();
+  it('sets the Collapse Menu button\'s aria-expanded attribute to the isOpen prop', async function() {
+    const closedView = quickRender({ isOpen: false }),
+        openView = quickRender({ isOpen: true });
 
     await runTimers();
 
-    const toggleBtn = view.getByRole('button', { name: /expand sidebar/i });
+    const closedToggleBtn = closedView.getByRole('button', { name: /Collapse Menu/i }),
+        openToggleBtn = openView.getByRole('button', { name: /Collapse Menu/i });
 
-    expect(toggleBtn).toHaveAccessibleName('Expand Sidebar');
-    expect(toggleBtn).toHaveAttribute('aria-expanded', 'false');
-    expect(toggleBtn).toHaveAttribute('aria-controls', sidebarId);
-
-    await user.hover(toggleBtn);
-
-    const tooltip = await screen.findByRole('tooltip');
-    expect(tooltip).toBeInTheDocument();
-    expect(tooltip).toHaveTextContent('Expand Sidebar');
+    expect(closedToggleBtn).toHaveAttribute('aria-expanded', 'false');
+    expect(openToggleBtn).toHaveAttribute('aria-expanded', 'true');
   });
 
-  it('renders passed in children', function() {
+  it('renders passed in children within a navigation element named "global sidebar"', function() {
     const children = (
       <React.Fragment key="1">
         <div className="child-1" data-testid="div1">
@@ -168,10 +55,12 @@ describe('NxGlobalSidebar', function() {
         <div className="child-2" data-testid="div2"></div>
       </React.Fragment>
     );
-    const view = quickRender({ children });
-    expect(view.getByTestId('div1')).toBeInTheDocument();
-    expect(view.getByTestId('p1')).toBeInTheDocument();
-    expect(view.getByTestId('div2')).toBeInTheDocument();
+    const view = quickRender({ children }),
+        nav = view.getByRole('navigation', { name: 'global sidebar' });
+
+    expect(within(nav).getByTestId('div1')).toBeInTheDocument();
+    expect(within(nav).getByTestId('p1')).toBeInTheDocument();
+    expect(within(nav).getByTestId('div2')).toBeInTheDocument();
   });
 
   it('calls onToggleClick when toggle button is pressed', async function() {
--- lib/src/components/NxGlobalSidebar/stateful/__tests__/NxStatefulGlobalSidebar.test.tsx
+++ lib/src/components/NxGlobalSidebar/stateful/__tests__/NxStatefulGlobalSidebar.test.tsx
@@ -5,188 +5,63 @@
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
 import React from 'react';
-import NxStatefulGlobalSidebar, { Props } from '../NxStatefulGlobalSidebar';
+import { within } from '@testing-library/react';
 import { faBiohazard, faCrow } from '@fortawesome/free-solid-svg-icons';
+
+import NxStatefulGlobalSidebar, { Props } from '../NxStatefulGlobalSidebar';
 import { rtlRender, runTimers, userEvent } from '../../../../__testutils__/rtlUtils';
-import { screen, within } from '@testing-library/react';
-import NxGlobalSidebarNavigation from '../../NxGlobalSidebarNavigation';
-import NxGlobalSidebarNavigationLink from '../../NxGlobalSidebarNavigationLink';
-import NxGlobalSidebarFooter from '../../NxGlobalSidebarFooter';
 
 describe('NxStatefulGlobalSidebar', function() {
   const minimalProps: Props = {
-    isDefaultOpen: true,
+    isDefaultOpen: false,
     toggleOpenIcon: faCrow,
-    toggleCloseIcon: faBiohazard,
-    logoImg: 'logoImg',
-    logoAltText: 'alt text',
-    logoLink: '/ref'
+    toggleCloseIcon: faBiohazard
   };
 
   const quickRender = rtlRender<Props>(NxStatefulGlobalSidebar, minimalProps);
 
-  describe('children', function() {
-    describe('when only the NxGlobalSidebarNavigation is a provided as a child', function () {
-      it('renders an element with role="navigation" within an element with role="complementary"', function() {
-        const children =
-          <NxGlobalSidebarNavigation key="1">
-            <NxGlobalSidebarNavigationLink icon={faCrow} text="testLink" href="testLink"/>
-          </NxGlobalSidebarNavigation>,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            nav = within(complementary).getByRole('navigation');
-
-        expect(complementary).toBeInTheDocument();
-        expect(nav).toBeInTheDocument();
-      });
-    });
-
-    describe('when only the NxGlobalSidebarFooter is a provided as a child', function () {
-      it('renders a footer with role="contentinfo"', function() {
-        const children =
-          <NxGlobalSidebarFooter key="1"
-                                 supportText="Support for RSC"
-                                 supportLink="https://github.com/sonatype/sonatype-react-shared-components"
-                                 releaseText="Release 3.1.4"
-                                 productTagLine="Powered by PLAID VILLAIN" />,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            footer = view.getByRole('contentinfo');
-
-        expect(footer).toBeInTheDocument();
-        expect(complementary).not.toContainElement(footer);
-      });
-    });
-
-    describe('when only extra content is a provided as a child', function () {
-      it('renders the extra content within an element with role="complementary"', function() {
-        const children =
-          <section key="1" className="gallery-custom-sidebar-content nx-global-sidebar__other-content nx-scrollable">
-            <div className="nx-global-sidebar__expanded-content">
-              <span data-testid="testspan">myspan</span>
-            </div>
-          </section>,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            extraContent = within(complementary).getByTestId('testspan');
-
-        expect(complementary).toBeInTheDocument();
-        expect(extraContent).toBeInTheDocument();
-      });
-    });
-
-    describe('when NxGlobalSidebarNavigation, NxGlobalSidebarFooter, and extra content ' +
-    'are provided as children', function () {
-      it('renders an element with role="navigation" within an element with role="complementary" ' +
-       'and a footer with role="contentinfo"', function() {
-        const children =
-          <React.Fragment key="1">
-            <NxGlobalSidebarNavigation>
-              <NxGlobalSidebarNavigationLink icon={faCrow} text="testLink" href="testLink"/>
-            </NxGlobalSidebarNavigation>
-            <section key="2" className="gallery-custom-sidebar-content nx-global-sidebar__other-content nx-scrollable">
-              <div className="nx-global-sidebar__expanded-content">
-                <span data-testid="testspan">myspan</span>
-              </div>
-            </section>
-            <NxGlobalSidebarFooter supportText="Support for RSC"
-                                   supportLink="https://github.com/sonatype/sonatype-react-shared-components"
-                                   releaseText="Release 3.1.4"
-                                   productTagLine="Powered by PLAID VILLAIN" />
-          </React.Fragment>,
-            view = quickRender({ ...minimalProps, children }),
-            complementary = view.getByRole('complementary'),
-            nav = within(complementary).getByRole('navigation'),
-            extraContent = within(complementary).getByTestId('testspan'),
-            footer = view.getByRole('contentinfo');
-
-        expect(complementary).toBeInTheDocument();
-        expect(nav).toBeInTheDocument();
-        expect(extraContent).toBeInTheDocument();
-        expect(footer).toBeInTheDocument();
-      });
-    });
-  });
-
-  it('renders a link to `logoLink` with the `logoImg` and the `altLogoText`', function() {
+  it('renders a "Collapse Menu" button with aria-controls set to the sidebar id', async function() {
     const view = quickRender(),
-        link = view.getByRole('link'),
-        logo = view.getByRole('img');
-
-    expect(link).toHaveAttribute('href', '/ref');
-    expect(logo).toHaveAttribute('src', 'logoImg');
-    expect(logo).toHaveAccessibleName('alt text');
-  });
-
-  it('renders an icon-only button with tooltip "Collapse Sidebar" if isDefaultOpen is true', async function() {
-    const view = quickRender({ isDefaultOpen: true }),
-        sidebarId = view.container.firstElementChild?.getAttribute('id'),
-        user = userEvent.setup();
+        sidebarId = view.container.firstElementChild?.getAttribute('id');
 
     await runTimers();
 
-    const toggleBtn = view.getByRole('button', { name: /collapse sidebar/i });
+    const toggleBtn = view.getByRole('button', { name: /collapse menu/i });
 
-    expect(toggleBtn).toHaveAccessibleName('Collapse Sidebar');
-    expect(toggleBtn).toHaveAttribute('aria-expanded', 'true');
+    expect(toggleBtn).toHaveAccessibleName('Collapse Menu');
     expect(toggleBtn).toHaveAttribute('aria-controls', sidebarId);
-
-    await user.hover(toggleBtn);
-
-    const tooltip = await screen.findByRole('tooltip');
-    expect(tooltip).toBeInTheDocument();
-    expect(tooltip).toHaveTextContent('Collapse Sidebar');
   });
 
-  it('renders an icon-only button with tooltip "Expand Sidebar" if isDefaultOpen is false', async function() {
-    const view = quickRender({ isDefaultOpen: false }),
-        sidebarId = view.container.firstElementChild?.getAttribute('id'),
+  it('sets the Collapse Menu button\'s initial aria-expanded attribute to the isDefaultOpen prop', async function() {
+    const closedView = quickRender(),
+        openView = quickRender({ isDefaultOpen: true });
+
+    await runTimers();
+
+    const closedToggleBtn = closedView.getByRole('button', { name: /Collapse Menu/i }),
+        openToggleBtn = openView.getByRole('button', { name: /Collapse Menu/i });
+
+    expect(closedToggleBtn).toHaveAttribute('aria-expanded', 'false');
+    expect(openToggleBtn).toHaveAttribute('aria-expanded', 'true');
+  });
+
+  it('toggles the aria-expanded attribute of the Collapse Menu button when clicked', async function() {
+    const view = quickRender(),
         user = userEvent.setup();
 
     await runTimers();
 
-    const toggleBtn = view.getByRole('button', { name: /expand sidebar/i });
-
-    expect(toggleBtn).toHaveAccessibleName('Expand Sidebar');
+    const toggleBtn = view.getByRole('button', { name: /Collapse Menu/i });
     expect(toggleBtn).toHaveAttribute('aria-expanded', 'false');
-    expect(toggleBtn).toHaveAttribute('aria-controls', sidebarId);
 
-    await user.hover(toggleBtn);
-
-    const tooltip = await screen.findByRole('tooltip');
-    expect(tooltip).toBeInTheDocument();
-    expect(tooltip).toHaveTextContent('Expand Sidebar');
-  });
-
-  it('clicking on icon-only toggle button "Collapse Sidebar" collapses sidebar ' +
-  'when isDefaultOpen is true', async function() {
-    const view = quickRender({ isDefaultOpen: true }),
-        user = userEvent.setup();
-
-    await runTimers();
-
-    const toggleBtn = view.getByRole('button', { name: /collapse sidebar/i });
-
+    await user.click(toggleBtn);
     expect(toggleBtn).toHaveAttribute('aria-expanded', 'true');
+
     await user.click(toggleBtn);
     expect(toggleBtn).toHaveAttribute('aria-expanded', 'false');
   });
 
-  it('clicking on icon-only toggle button "Expand Sidebar" expands sidebar ' +
-  'when isDefaultOpen is false', async function() {
-    const view = quickRender({ isDefaultOpen: false }),
-        user = userEvent.setup();
-
-    await runTimers();
-
-    const toggleBtn = view.getByRole('button', { name: /expand sidebar/i });
-
-    expect(toggleBtn).toHaveAttribute('aria-expanded', 'false');
-    await user.click(toggleBtn);
-    expect(toggleBtn).toHaveAttribute('aria-expanded', 'true');
-  });
-
-  it('renders passed in children', function() {
+  it('renders passed in children within a navigation element named "global sidebar"', function() {
     const children = (
       <React.Fragment key="1">
         <div className="child-1" data-testid="div1">
@@ -195,9 +70,11 @@ describe('NxStatefulGlobalSidebar', function() {
         <div className="child-2" data-testid="div2"></div>
       </React.Fragment>
     );
-    const view = quickRender({ children });
-    expect(view.getByTestId('div1')).toBeInTheDocument();
-    expect(view.getByTestId('p1')).toBeInTheDocument();
-    expect(view.getByTestId('div2')).toBeInTheDocument();
+    const view = quickRender({ children }),
+        nav = view.getByRole('navigation', { name: 'global sidebar' });
+
+    expect(within(nav).getByTestId('div1')).toBeInTheDocument();
+    expect(within(nav).getByTestId('p1')).toBeInTheDocument();
+    expect(within(nav).getByTestId('div2')).toBeInTheDocument();
   });
 });
