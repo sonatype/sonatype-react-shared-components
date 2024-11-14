384dd8e5c21283b4c30ebfa823bf0641c8bd9d51
obarra-dev <obarra@sonatype.com>
2024-11-14 13:32:23 -0300

    fix linter issue

--- gallery/src/components/NxTransferListHalf/NxTransferListHalfPage.tsx
+++ gallery/src/components/NxTransferListHalf/NxTransferListHalfPage.tsx
@@ -282,8 +282,9 @@ const NxTransferListPage = () =>
                         id="nx-transfer-list-half-long-list-example"
                         codeExamples={nxTransferListHalfLongListExample}
                         liveExample={NxTransferListHalfLongListExample}>
-      Demonstrates an <NxCode>NxTransferListHalf</NxCode> with a long list. Previous versions of this component suffered from performance
-      issues when the list is very long. To address this, it has been updated to use <NxCode>NxScrollRender</NxCode> to render
+      Demonstrates an <NxCode>NxTransferListHalf</NxCode> with a long list. Previous versions of
+      this component suffered from performance issues when the list is very long. To address this,
+      it has been updated to use <NxCode>NxScrollRender</NxCode> to render
       efficiently. Note that even using a long list, rendering and filtering are not affected.
     </GalleryExampleTile>
   </>;
