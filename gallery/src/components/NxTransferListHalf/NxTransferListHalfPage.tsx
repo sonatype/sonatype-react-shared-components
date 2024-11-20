ee6e84fe3fb6c2f9189375e7e58f9635949a6b99
obarra-dev <obarra@sonatype.com>
2024-11-20 17:03:34 -0300

    simpler

--- gallery/src/components/NxTransferListHalf/NxTransferListHalfPage.tsx
+++ gallery/src/components/NxTransferListHalf/NxTransferListHalfPage.tsx
@@ -4,21 +4,49 @@
  * the terms of the Eclipse Public License 2.0 which accompanies this
  * distribution and is available at https://www.eclipse.org/legal/epl-2.0/.
  */
-import React from 'react';
+import React, { useState} from 'react';
 import { NxCode } from '@sonatype/react-shared-components';
 
 import { GalleryExampleTile } from '../../gallery-components/GalleryTiles';
 
 import NxTransferListHalfExample from './NxTransferListHalfExample';
 
+import { NxScrollRender } from '@sonatype/react-shared-components';
+
+
 import '../NxTooltip/NxTooltipExample.scss';
 
 const nxTransferListHalfExample = require('./NxTransferListHalfExample?raw');
 
 
+const ListComponent = () => {
+  const [items, setItems] = useState<string[]>([]);
+
+  const addItem = () => {
+    const newItem = `Item ${items.length + 1}`;
+    setItems([...items, newItem]);
+  };
+
+  return (
+    <div>
+      <h1>Item List</h1>
+      <button onClick={addItem}>Add Item</button>
+      
+      <NxScrollRender>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      </NxScrollRender>
+      
+    </div>
+  );
+};
+
 const NxTransferListPage = () =>
   <>
-
+  <ListComponent></ListComponent>
     <GalleryExampleTile title="Minimal Example"
                         id="nx-transfer-list-half-example"
                         codeExamples={nxTransferListHalfExample}
