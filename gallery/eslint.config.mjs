dd8b57960dee19a9c7080cbd3a6345656ae1de35
Ross Pokorny <rpokorny@sonatype.com>
2025-02-20 14:29:47 -0500

    Remove unused vars and imports from eslint config - RSC-1790

--- gallery/eslint.config.mjs
+++ gallery/eslint.config.mjs
@@ -2,16 +2,10 @@
  * Copyright (c) 2020-present Sonatype, Inc. All rights reserved.
  * "Sonatype" is a trademark of Sonatype, Inc.
  */
-import eslintJs from '@eslint/js';
-import typescriptEslint from "typescript-eslint";
-import reactEslint from 'eslint-plugin-react';
-import stylistic from '@stylistic/eslint-plugin';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
 import globals from 'globals';
 
 import { rscConfig, testConfig } from '../lib/eslint.config.mjs';
 
-const files = ['**/*.{j,t}s{x,}'];
 const testFiles = ['visualtests/*.js', 'setupTests.js'];
 
 const galleryTestConfig = testConfig(testFiles);
